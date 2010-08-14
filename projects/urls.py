0c826a133590fa9d8139e24215c1a08eb1fe0b1a
Charles Leifer <coleifer@gmail.com>
2010-08-14 00:48:55 -0500

    Adding some rudimentary templates and the outlines of some generic views

--- projects/urls.py
+++ projects/urls.py
@@ -1,5 +1,16 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('projects.views',
-    # TODO: urls here
+    url(r'^$',
+        'project_index',
+        name='projects_list'
+    ),
+    url(r'^(?P<username>\w+)/$',
+        'project_index',
+        name='projects_user_list'
+    ),
+    url(r'^(?P<username>\w+)/(?P<project_slug>[-\w]+)/$',
+        'project_detail',
+        name='projects_detail'
+    ),
 )
