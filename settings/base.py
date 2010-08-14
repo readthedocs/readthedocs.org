6838f8cca0cc097f8588a685c3b7f4e35838a86c
Bobby Grace <johnrobertgracejr@gmail.com>
2010-08-14 14:32:17 -0500

    adds pagination

--- settings/base.py
+++ settings/base.py
@@ -47,12 +47,21 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'pagination.middleware.PaginationMiddleware',
 )
 
 ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
     '%s/templates/' % (SITE_ROOT),
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.core.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.request"
 )
 
 INSTALLED_APPS = (
