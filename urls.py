ea838208f56b6f3b2ae85c2b54b3c978daaccd13
Eric Holscher <eric@ericholscher.com>
2010-08-14 06:01:07 +0000

    Fix github bits.

--- urls.py
+++ urls.py
@@ -7,5 +7,5 @@ urlpatterns = patterns('',
     url(r'^accounts/', include('registration.backends.default.urls')),
     url(r'^projects/', include('projects.urls')),
     (r'^admin/', include(admin.site.urls)),
-    (r'^github', 'core.views.github'),
+    (r'^github', 'core.views.github_build'),
 )
