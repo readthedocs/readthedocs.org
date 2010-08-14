573b41ab6e289c3e7537408b1380f8ecff1e5fc9
Charles Leifer <coleifer@gmail.com>
2010-08-14 11:57:58 -0500

    Adding rudimentary revisioning for files

--- projects/models.py
+++ projects/models.py
@@ -7,6 +7,7 @@ from django.template.loader import render_to_string
 from django.utils.functional import memoize
 
 from projects.constants import DEFAULT_THEME_CHOICES, THEME_DEFAULT
+from projects.utils import diff, dmp
 
 from taggit.managers import TaggableManager
 
@@ -56,14 +57,16 @@ class Project(models.Model):
               matches.append(os.path.join(root, filename))
         print "finding %s" % file
         return matches
-    find = memoize(find, {}, 2)
 
+    find = memoize(find, {}, 2)
 
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.name)
         super(Project, self).save(*args, **kwargs)
-        if not self.conf:
+        try:
+            conf = self.conf
+        except Conf.DoesNotExist:
             Conf.objects.create(project=self)
 
     @property
@@ -129,3 +132,80 @@ class File(models.Model):
                     child.save()
                     update_children(child.children.all())
             update_children(self.children.all())
+    
+    def create_revision(self, old_content, comment):
+        FileRevision.objects.create(
+            file=self,
+            comment=comment,
+            diff=diff(self.content, old_content)
+        )
+    
+    @property
+    def current_revision(self):
+        return self.revisions.filter(is_reverted=False)[0]
+    
+    def get_html_diff(self, rev_from, rev_to):
+        rev_from = self.revisions.get(revision_number=rev_from)
+        rev_to = self.revisions.get(revision_number=rev_to)
+        
+        diffs = dmp.diff_main(rev_from.diff, rev_to.diff)
+        return dmp.diff_prettyHtml(diffs)
+ 
+    def revert_to(self, revision_number):
+        revision = self.revisions.get(revision_number=revision_number)
+        revision.apply()
+
+
+class FileRevision(models.Model):
+    file = models.ForeignKey(File, related_name='revisions')
+    comment = models.TextField(blank=True)
+    diff = models.TextField(blank=True)
+
+    revision_number = models.IntegerField()
+    is_reverted = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ('-revision_number',)
+
+    def __unicode__(self):
+        return '%s #%s' % (self.file.heading, self.revision_number)
+
+    def get_file_content(self):
+        """
+        Apply the series of diffs after this revision in reverse order,
+        bringing the content back to the state it was in this revision
+        """
+        after = self.file.revisions.filter(revision__gt=self.revision_number)
+        content = self.file.content
+        
+        for revision in after:
+            patch = dmp.patch_fromText(revision.diff)
+            content = dmp.patch_apply(patch, content)[0]
+        
+        return content
+    
+    def apply(self):
+        original_content = self.file.content
+        
+        # store the old content on the file
+        self.file.content = self.get_file_content()
+        self.file.save()
+        
+        # mark reverted changesets
+        reverted_qs = self.file.revisions.filter(revision__gt=self.revision)
+        reverted_qs.update(is_reverted=True)
+        
+        # create a new revision
+        FileRevision.objects.create(
+            file=self.file,
+            comment='Reverted to #%s' % self.revision,
+            diff=diff(self.file.content, original_content)
+        )
+ 
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            try:
+                self.revision_number = self.file.current_revision.revision_number + 1
+            except IndexError:
+                self.revision_number = 1
+        super(FileRevision, self).save(*args, **kwargs)
--- projects/utils.py
+++ projects/utils.py
@@ -1,7 +1,10 @@
+import commands
 import os
 import fnmatch
+
 from django.conf import settings
-import commands
+
+from projects.libs.diff_match_patch import diff_match_patch
 
 def find_file(file):
     matches = []
@@ -19,3 +22,10 @@ def run(command):
     ret = p.returncode
     return (ret, out, err)
     """
+
+dmp = diff_match_patch()
+
+def diff(txt1, txt2):
+    """Create a 'diff' from txt1 to txt2."""
+    patch = dmp.patch_make(txt1, txt2)
+    return dmp.patch_toText(patch)
