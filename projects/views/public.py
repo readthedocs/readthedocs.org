a015940b2baa14026cc14d4a5baa09b90e225e77
Charles Leifer <coleifer@gmail.com>
2010-08-14 03:43:55 -0500

    Adding basic taggit views

--- projects/views/public.py
+++ projects/views/public.py
@@ -4,8 +4,9 @@ from django.views.generic.list_detail import object_list, object_detail
 
 from projects.models import Project
 
+from taggit.models import Tag, TaggedItem
 
-def project_index(request, username=None):
+def project_index(request, username=None, tag=None):
     queryset = Project.objects.all()
     if username:
         user = get_object_or_404(User, username=username)
@@ -13,10 +14,16 @@ def project_index(request, username=None):
     else:
         user = None
 
+    if tag:
+        tag = get_object_or_404(Tag, slug=tag)
+        queryset = queryset.filter(tags__in=[tag.slug])
+    else:
+        tag = None
+
     return object_list(
         request,
         queryset=queryset,
-        extra_context={'user': user},
+        extra_context={'user': user, 'tag': tag},
         paginate_by=20,
         page=int(request.GET.get('page', 1)),
         template_object_name='project',
@@ -33,4 +40,15 @@ def project_detail(request, username, project_slug):
         slug=project_slug,
         extra_context={'user': user},
         template_object_name='project',
+    )
+
+def tag_index(request):
+    tag_qs = Project.tags.most_common()
+    return object_list(
+        request,
+        queryset=tag_qs,
+        paginate_by=20,
+        page=int(request.GET.get('page', 1)),
+        template_object_name='tag',
+        template_name='projects/tag_list.html',
     )
