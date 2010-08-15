e7c139d0e1cda9ee38f018ebf50f30a446b9dd5f
Eric Holscher <eric@ericholscher.com>
2010-08-15 18:37:57 +0000

    Flash out the build views.

--- builds/views.py
+++ builds/views.py
@@ -6,26 +6,34 @@ from django.shortcuts import get_object_or_404
 from django.views.generic.list_detail import object_list, object_detail
 
 from builds.models import Build
+from projects.models import Project
 
 from taggit.models import Tag, TaggedItem
 
-def build_list(request, username=None, tag=None):
+def build_list(request, username=None, project_slug=None, tag=None):
     queryset = Build.objects.all()
     if username:
         user = get_object_or_404(User, username=username)
-        queryset = queryset.filter(user=user)
+        queryset = queryset.filter(project__user=user)
     else:
         user = None
     if tag:
         tag = get_object_or_404(Tag, slug=tag)
-        queryset = queryset.filter(tags__in=[tag.slug])
+        queryset = queryset.filter(project__tags__in=[tag.slug])
     else:
         tag = None
+
+    if project_slug and username:
+        project = Project.objects.get(user=user, slug=project_slug)
+        queryset = queryset.filter(project=project)
+    else:
+        project = None
+
 
     return object_list(
         request,
         queryset=queryset,
-        extra_context={'person': user, 'tag': tag},
+        extra_context={'person': user, 'project': project, 'tag': tag},
         template_object_name='build',
     )
 
