4cbfe1f08e8891abbf9ee721504cb46da796bee2
Charles Leifer <coleifer@gmail.com>
2010-08-15 02:16:48 -0500

    helly nested snizzzap

--- projects/templatetags/projects_tags.py
+++ projects/templatetags/projects_tags.py
@@ -17,3 +17,7 @@ def annotated_tree(project, max_depth=99):
                 walk_tree(obj.children.order_by('ordering'), depth+1)
     walk_tree(project.files.filter(parent__isnull=True).order_by('ordering'))
     return annotated
+
+@register.filter
+def headline(headline_string):
+    return '%s\n%s' % (headline_string, '=' * len(headline_string))
