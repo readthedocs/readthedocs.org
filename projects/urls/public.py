de3c2b1cf26e58b290c11b03738e182a825f2c68
Eric Holscher <eric@ericholscher.com>
2010-08-14 22:16:07 -0500

    Go back to having a detail page for apps before the docs.

--- projects/urls/public.py
+++ projects/urls/public.py
@@ -17,6 +17,10 @@ urlpatterns = patterns('projects.views.public',
         'project_index',
         name='project_tag_detail',
     ),
+    url(r'^projects/(?P<username>\w+)/(?P<project_slug>[-\w]+)/$',
+        'project_detail',
+        name='projects_detail'
+    ),
     url(r'^(?P<username>\w+)/$',
         'project_index',
         name='projects_user_list'
