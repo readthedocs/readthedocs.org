df669c7d8c29ac68b96c049a0dba586da5f4f77b
Charles Leifer <coleifer@gmail.com>
2010-08-15 20:35:21 -0500

    Adding flatpages

--- settings/base.py
+++ settings/base.py
@@ -49,6 +49,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'pagination.middleware.PaginationMiddleware',
     'core.middleware.NginxSSIMiddleware',
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
 )
 
 ROOT_URLCONF = 'urls'
