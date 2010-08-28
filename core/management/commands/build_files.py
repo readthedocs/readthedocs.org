6c72fdca3eeaaf4ae97698285db0884fe5ddf422
Eric Holscher <eric@ericholscher.com>
2010-08-28 17:55:24 -0500

    Added an easy way to build the importedfile index

---
+++ core/management/commands/build_files.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand
+from projects import tasks
+from projects.models import Project
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **kwargs):
+        if not len(args):
+            for p in Project.objects.all():
+                print "Indexing %s" % p
+                tasks.fileify(p.slug)
+        else:
+            for slug in args:
+                p = Project.objects.get(slug=slug)
+                print "Indexing %s" % p
+                tasks.fileify(p.slug)
+
+
