059b500fa5d611ab088fe320eefa29ea36a722a1
Eric Holscher <eric@ericholscher.com>
2010-08-29 23:43:22 -0500

    Reenable search with solr.

--- settings/base.py
+++ settings/base.py
@@ -89,7 +89,7 @@ INSTALLED_APPS = (
     'taggit',
     'django_extensions',
     'basic.flagging',
-    #'haystack',
+    'haystack',
 
     # our apps
     'projects',
@@ -106,5 +106,6 @@ DEFAULT_FROM_EMAIL = "no-reply@readthedocs.org"
 SESSION_COOKIE_DOMAIN = '.readthedocs.org'
 
 HAYSTACK_SITECONF = 'core.search_sites'
-HAYSTACK_SEARCH_ENGINE = 'whoosh'
-HAYSTACK_WHOOSH_PATH = os.path.join(SITE_ROOT, 'whooshindex.db')
+HAYSTACK_SEARCH_ENGINE = 'solr'
+#HAYSTACK_WHOOSH_PATH = os.path.join(SITE_ROOT, 'whooshindex.db')
+HAYSTACK_SOLR_URL = 'http://loki:8983/solr'
