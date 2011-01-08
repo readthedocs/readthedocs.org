ae906b0782797a1f1b4375cee05be9ffd630f875
Jonas Obrist <jonas.obrist@divio.ch>
2011-01-08 21:46:57 +0100

    Added bzr backend (no tag support)

---
+++ vcs_support/backends/bzr.py
@@ -0,0 +1,32 @@
+from projects.exceptions import ProjectImportError
+from vcs_support.backends.base import BaseVCS
+
+
+class Backend(BaseVCS):
+    supports_tags = False
+    
+    def update(self):
+        retcode = self._run_command('bzr', 'status')[0]
+        if retcode == 0:
+            self._up()
+        else:
+            self._checkout()
+            
+    def _up(self):
+        retcode = self._run_command('bzr', 'revert')[0]
+        if retcode != 0:
+            raise ProjectImportError(
+                "Failed to get code from '%s' (bzr revert): %s" % (self.repo_url, retcode)
+            )
+        retcode = self._run_command('bzr', 'up')[0]
+        if retcode != 0:
+            raise ProjectImportError(
+                "Failed to get code from '%s' (bzr up): %s" % (self.repo_url, retcode)
+            )
+        
+    def _checkout(self):
+        retcode = self._run_command('bzr', 'checkout', self.repo_url, '.')[0]
+        if retcode != 0:
+            raise ProjectImportError(
+                "Failed to get code from '%s' (bzr checkout): %s" % (self.repo_url, retcode)
+            )
