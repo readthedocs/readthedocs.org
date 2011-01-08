9178dfd5c1ac9cbe95d1474b45b009d66586bee5
Jonas Obrist <jonas.obrist@divio.ch>
2011-01-08 22:37:51 +0100

    Added tag support for bzr

--- vcs_support/backends/bzr.py
+++ vcs_support/backends/bzr.py
@@ -1,6 +1,5 @@
 from projects.exceptions import ProjectImportError
-from vcs_support.base import BaseVCS
-
+from vcs_support.base import BaseVCS, VCSTag
 
 class Backend(BaseVCS):
     supports_tags = False
@@ -30,3 +29,33 @@ class Backend(BaseVCS):
             raise ProjectImportError(
                 "Failed to get code from '%s' (bzr checkout): %s" % (self.repo_url, retcode)
             )
+
+    def get_tags(self):
+        retcode, stdout = self._run_command('bzr', 'tags')[:2]
+        # error (or no tags found)
+        if retcode != 0:
+            return []
+        return self._parse_tags(stdout)
+    
+    def _parse_tags(self, data):
+        """
+        Parses output of show-ref --tags, eg:
+        
+            0.1.0                171
+            0.1.1                173
+            0.1.2                174
+            0.2.0-pre-alpha      177
+        """
+        # parse the lines into a list of tuples (commit-hash, tag ref name)
+        raw_tags = [line.rsplit(' ', 1) for line in data.strip('\n').split('\n')]
+        vcs_tags = []
+        for name, commit in raw_tags:
+            clean_name = name.strip(' ')
+            vcs_tags.append(VCSTag(self, commit, clean_name))
+        return vcs_tags
+    
+    def checkout(self, identifier=None):
+        if not identifier:
+            self._up()
+        else:
+            self._run_command('hg', 'update', '-r', identifier)
