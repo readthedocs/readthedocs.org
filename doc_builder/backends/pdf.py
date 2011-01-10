4ffd42f132769b23ee93c8a5c3416877c772d47e
Eric Holscher <eric@ericholscher.com>
2011-01-09 17:56:18 -0800

    Move instead of symlink because we blow that dir away.

--- doc_builder/backends/pdf.py
+++ doc_builder/backends/pdf.py
@@ -23,16 +23,16 @@ class Builder(BaseBuilder):
             pdf_match = pdf_re.search(pdf_results[1])
             if pdf_match:
                 from_path = os.path.join(os.getcwd(),
-                                         "%s.pdf" % project.slug)
+                                         "%s.pdf" % pdf_match.group(1).strip())
                 to_path = os.path.join(settings.MEDIA_ROOT,
                                        'pdf',
                                        project.slug,
                                        'latest')
                 if not os.path.exists(to_path):
                     os.makedirs(to_path)
-                to_file = os.path.join(to_path, pdf_match.group(1).strip())
+                to_file = os.path.join(to_path, '%s.pdf' % project.slug)
                 if os.path.exists(to_file):
-                    run('ln -sf %s %s' % (from_path, to_file))
+                    run('mv -f %s %s' % (from_path, to_file))
                 else:
                     print "File doesn't exist, not symlinking."
             else:
