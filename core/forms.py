882fc867ab115f2b84f2f185bcebf3eb4a1d2fc8
Eric Holscher <eric@ericholscher.com>
2011-01-13 16:39:11 -0800

    Fix profile creation. (Need tests badly).

--- core/forms.py
+++ core/forms.py
@@ -5,16 +5,16 @@ from models import UserProfile
 class UserProfileForm(ModelForm):
     first_name = CharField(label='First name', required=False)
     last_name = CharField(label='Last name', required=False)
-    
+
     class Meta:
         model = UserProfile
         # Don't allow users edit someone else's user page,
         # or to whitelist themselves
         exclude = ('user', 'whitelisted',)
-        
+
     def __init__(self, *args, **kwargs):
         super(UserProfileForm, self).__init__(*args, **kwargs)
-        if self.instance:
+        if self.is_bound:
             self.fields['first_name'].initial = self.instance.user.first_name
             self.fields['last_name'].initial = self.instance.user.last_name
 
