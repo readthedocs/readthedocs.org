d3b526c5079dc61d3bb8a80363c9448de07da331
Eric Holscher <eric@ericholscher.com>
2011-01-22 13:29:00 -0800

    Make it easy to do a full deploy with fab

--- fabfile.py
+++ fabfile.py
@@ -7,20 +7,21 @@ env.code_dir = '/home/docs/sites/readthedocs.org/checkouts/readthedocs.org'
 env.virtualenv = '/home/docs/sites/readthedocs.org'
 env.rundir = '/home/docs/sites/readthedocs.org/run'
 
-def update_requirements():
-    "Update requirements in the virtualenv."
-    run("%s/bin/pip install -r %s/deploy_requirements.txt" % (env.virtualenv, env.code_dir))
-
 def push():
     "Push new code, but don't restart/reload."
     local('git push origin master')
     with cd(env.code_dir):
         run('git pull origin master')
 
-def pull():
-    "Pull new code"
-    with cd(env.code_dir):
-        run('git pull origin master')
+def update_requirements():
+    "Update requirements in the virtualenv."
+    run("%s/bin/pip install -r %s/deploy_requirements.txt" % (env.virtualenv, env.code_dir))
+
+def migrate(project=None):
+    if project:
+        run('django-admin.py migrate %s' % project)
+    else:
+        run('django-admin.py migrate')
 
 def restart():
     "Restart (or just start) the server"
@@ -32,8 +33,14 @@ def celery():
     env.user = "root"
     run("restart readthedocs-celery")
 
-def migrate(project=None):
-    if project:
-        run('django-admin.py migrate %s' % project)
-    else:
-        run('django-admin.py migrate')
+def pull():
+    "Pull new code"
+    with cd(env.code_dir):
+        run('git pull origin master')
+
+def full_deploy():
+    push()
+    update_requirements()
+    migrate()
+    restart()
+    celery()
