e7c5190bfcca0ca816eba558a72fca9f52704197
Issac Kelly <issac.kelly@gmail.com>
2011-01-24 00:41:52 -0500

    Added API for creating projects.  Also added supporting tests.

--- rtd_tests/tests/test_api.py
+++ rtd_tests/tests/test_api.py
@@ -32,7 +32,7 @@ class BasicTests(TestCase):
 
     def test_invalid_make_project(self):
         """
-        Test that the non-superuser can't make a user
+        Test that the authentication is turned on.
         """
         post_data = {"user": "/api/v1/user/2/",
                      "name": "awesome-project-2",
@@ -41,6 +41,37 @@ class BasicTests(TestCase):
         resp = self.client.post('/api/v1/project/',
                                 data=json.dumps(post_data),
                                 content_type='application/json',
-                                HTTP_AUTHORIZATION='Basic %s' % base64.b64encode('test:test')
+                                HTTP_AUTHORIZATION='Basic %s' % base64.b64encode('tester:notapass')
                                 )
         self.assertEqual(resp.status_code, 401)
+
+    def test_make_project_dishonest_user(self):
+        """
+        Test that you can't create a project for another user
+        """
+        # represents dishonest data input, authentication happens for user 2
+        post_data = {"user": "/api/v1/user/1/",
+                     "name": "awesome-project-2",
+                     "repo": "https://github.com/ericholscher/django-bob.git"
+                     }
+        resp = self.client.post('/api/v1/project/',
+                                data=json.dumps(post_data),
+                                content_type='application/json',
+                                HTTP_AUTHORIZATION='Basic %s' % base64.b64encode('tester:test')
+                                )
+        self.assertEqual(resp.status_code, 201)
+        resp = self.client.get(resp['location'].split("testserver")[1],
+                               data={'format':'json'},
+                               HTTP_AUTHORIZATION='Basic %s' % base64.b64encode('tester:test')
+                               )
+        self.assertEqual("/api/v1/user/2/", json.loads(resp.content)["user"])
+
+    def test_ensure_get_unauth(self):
+        """
+        Test that GET requests work without authenticating.
+        """
+
+        resp = self.client.get("/api/v1/project/",
+                         data={"format": "json"}
+                         )
+        self.assertEqual(resp.status_code, 200)
