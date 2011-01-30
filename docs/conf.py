c78fc8a5eb0e566b2571f8613c0cd6e3e036ffe0
Eric Holscher <eric@ericholscher.com>
2011-01-29 20:12:04 -0800

    Remove our custom theme stuff, since it will be on the site now.

--- docs/conf.py
+++ docs/conf.py
@@ -96,7 +96,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinxdoc'
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -219,5 +219,3 @@ man_pages = [
     ('index', 'readthedocs', u'Read The Docs Documentation',
      [u'Eric Holscher, Charlie Leifer, Bobby Grace'], 1)
 ]
-
-html_style = 'rtd.css'
