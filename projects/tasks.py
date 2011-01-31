b00eb697e17ed98f26eeba8141ff1e99479e09c2
Eric Holscher <eric@ericholscher.com>
2011-01-30 17:38:09 -0800

    Curse you subprocess.

--- projects/tasks.py
+++ projects/tasks.py
@@ -218,8 +218,8 @@ def copy_to_app_servers(full_build_path, target):
     #You should be checking for this above.
     servers = settings.MULTIPLE_APP_SERVERS
     for server in servers:
-        ret = run("rsync -e 'ssh -T' -av --delete %s/ %s@%s:%s" % (full_build_path, getpass.getuser(), server, target))
-        if ret[0] != 0:
+        ret = os.system("rsync -e 'ssh -T' -av --delete %s/ %s@%s:%s" % (full_build_path, getpass.getuser(), server, target))
+        if ret != 0:
             print "COPY ERROR: out: %s err: %s" % (ret[1], ret[2])
 
 
