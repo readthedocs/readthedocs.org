9b53331e96421ce68357fec1c3454b4804ea9f9a
Eric Holscher <eric@ericholscher.com>
2011-01-30 17:27:26 -0800

    Rsync. Duh.

--- projects/tasks.py
+++ projects/tasks.py
@@ -218,7 +218,7 @@ def copy_to_app_servers(full_build_path, target):
     #You should be checking for this above.
     servers = settings.MULTIPLE_APP_SERVERS
     for server in servers:
-        ret = run('rsync -e "ssh -T" -r %s/ %s@%s:%s' % (full_build_path, getpass.getuser(), server, target))
+        ret = run('rsync -e "ssh -T" -av --delete %s/ %s@%s:%s' % (full_build_path, getpass.getuser(), server, target))
         if ret[0] != 0:
             print "COPY ERROR: out: %s err: %s" % (ret[1], ret[2])
 
