e8cb8b47c31a59e96590925b00bdb40095670638
Eric Holscher <eric@ericholscher.com>
2011-02-06 01:50:41 -0800

    Fix svn backend.

--- vcs_support/backends/svn.py
+++ vcs_support/backends/svn.py
@@ -4,15 +4,15 @@ from vcs_support.base import BaseVCS, VCSVersion
 
 class Backend(BaseVCS):
     supports_tags = False
-    
-    def __init__(self, repo_url, working_dir):
-        super(Backend, self).__init__(repo_url, working_dir)
+
+    def __init__(self, project):
+        super(Backend, self).__init__(project)
         if self.repo_url[-1] != '/':
             self.repo_url += '/'
         if self.repo_url.endswith('/trunk/'):
             self.supports_tags = True
             self.base_url = self.repo_url[:-7]
-            
+
     def update(self):
         super(Backend, self).update()
         # For some reason `svn status` gives me retcode 0 in non-svn directories
@@ -22,7 +22,7 @@ class Backend(BaseVCS):
             self._up()
         else:
             self._co()
-    
+
     def _up(self):
         retcode = self._run_command('svn', 'revert', '--recursive', '.')[0]
         if retcode != 0:
@@ -41,18 +41,18 @@ class Backend(BaseVCS):
             raise ProjectImportError(
                 "Failed to get code from '%s' (svn checkout): %s" % (self.repo_url, retcode)
             )
-            
+
     def get_tags(self):
         retcode, stdout = self._run_command('svn', 'list', '%s/tags/' % self.base_url)[:2]
         # error (or no tags found)
         if retcode != 0:
             return []
         return self._parse_tags(stdout)
-    
+
     def _parse_tags(self, data):
         """
         Parses output of svn list, eg:
-        
+
             release-1.1/
             release-1.2/
             release-1.3/
@@ -66,7 +66,7 @@ class Backend(BaseVCS):
         for name in raw_tags:
             vcs_tags.append(VCSVersion(self, '/tags/%s/' % name, name))
         return vcs_tags
-    
+
     def checkout(self, identifier=None):
         super(Backend, self).checkout()
         if not identifier:
