53eb28500d737aa4dfb49de74ae3265e553399f2
Eric Holscher <eric@ericholscher.com>
2011-02-15 22:27:39 -0800

    Added a setup.py since it's a package.

---
+++ setup.py
@@ -0,0 +1,24 @@
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name='readthedocs',
+    version="1.0",
+    description='A documentation hosting website',
+    #long_description=readme,
+    author='Eric Holscher, Charles Leifer, Bobby Grace',
+    author_email='eric@ericholscher.com',
+    url='http://readthedocs.org',
+    packages=find_packages(),
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    #test_suite='readthedocs.tests.rtd_tests.runtests'
+)
