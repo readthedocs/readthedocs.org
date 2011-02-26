a5690ac13d113c67e3ca74e74b24da56f75ccf66
Eric Holscher <eric@ericholscher.com>
2011-02-26 11:42:57 -0800

    Added a basic test for importing a project.

--- readthedocs/rtd_tests/tests/view_tests.py
+++ readthedocs/rtd_tests/tests/view_tests.py
@@ -3,9 +3,12 @@ from django.test import TestCase
 class Testmaker(TestCase):
     fixtures = ["eric"]
 
+    def setUp(self):
+        self.client.login(username='eric', password='test')
+
+
     def test_local_built_test(self):
         "A basic smoke test of creating a RTD Built projects"
-        self.assertTrue(self.client.login(username='eric', password='test'))
         r = self.client.get('/', {})
         self.assertEqual(r.status_code, 200)
         r = self.client.get('/dashboard/', {})
@@ -21,4 +24,27 @@ class Testmaker(TestCase):
         self.assertEqual(r.status_code, 302)
         self.assertEqual(r._headers['location'][1], 'http://testserver/projects/new-proj/')
         r = self.client.get('/dashboard/new-proj/edit/', {})
+        self.assertEqual(r.status_code, 200)
+
+    def test_imported_docs(self):
+        r = self.client.get('/dashboard/', {})
+        self.assertEqual(r.status_code, 200)
+        r = self.client.get('/dashboard/import/', {})
+        self.assertEqual(r.status_code, 200)
+        r = self.client.post('/dashboard/import/', {'repo_type': 'git', 'name': 'Django Kong', 'tags': 'big, fucking, monkey', 'default_branch': '', 'project_url': 'http://django-kong.rtfd.org', 'repo': 'https://github.com/ericholscher/django-kong', 'csrfmiddlewaretoken': '34af7c8a5ba84b84564403a280d9a9be', 'description': 'OOHHH AH AH AH KONG SMASH', })
+        self.assertEqual(r.status_code, 302)
+        r = self.client.get('/dashboard/django-kong/', {'docs_not_built': 'True', })
+        self.assertEqual(r.status_code, 200)
+        r = self.client.get('/docs/django-kong/latest/', {})
+        self.assertEqual(r.status_code, 302)
+        r = self.client.get('/docs/django-kong/en/latest/index.html', {})
+        self.assertEqual(r.status_code, 200)
+        r = self.client.get('/dashboard/django-kong/versions/', {})
+        self.assertEqual(r.status_code, 200)
+        r = self.client.post('/dashboard/django-kong/versions/', {'csrfmiddlewaretoken': '34af7c8a5ba84b84564403a280d9a9be', 'version-0.9': 'on', })
+        r = self.client.get('/dashboard/django-kong/', {})
+        self.assertEqual(r.status_code, 200)
+        r = self.client.get('/docs/django-kong/0.9/', {})
+        self.assertEqual(r.status_code, 302)
+        r = self.client.get('/docs/django-kong/en/0.9/index.html', {})
         self.assertEqual(r.status_code, 200)
