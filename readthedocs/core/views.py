fed1390004e50990fae76cf23a1d22b4d48787e8
Eric Holscher <eric@ericholscher.com>
2011-03-04 07:51:34 +0000

    Use the serve view to get content type and everything.

--- readthedocs/core/views.py
+++ readthedocs/core/views.py
@@ -166,15 +166,14 @@ def serve_docs(request, project_slug, lang_slug, version_slug, filename):
         if not created:
             pageview.count = F('count') + 1
             pageview.save()
-    response = HttpResponse()
-    response[' X-Accel-Redirect'] = os.path.join('/omgmedia',
-                                                 proj.user,
+    response = serve(request, filename, basepath)
+    response['X-Accel-Redirect'] = os.path.join('/user_builds',
+                                                 proj.user.username,
                                                  proj.slug,
                                                  'rtd-builds',
                                                  version_slug,
                                                  filename)
     return response
-    #return serve(request, filename, basepath)
 
 def render_header(request):
     """
