004462ef87ed4514ce5807a194bf653955887d7f
Eric Holscher <eric@ericholscher.com>
2011-03-10 10:17:24 -0800

    Fix our docs to actually build. Sucks that we now require virtualenv to build them. Bad documentation citizen!

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@ import sys, os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../readthedocs'))
 
 # Set up the Django environment so Sphinx can autodocument its modules
 import settings.sqlite
