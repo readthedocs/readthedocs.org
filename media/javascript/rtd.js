1e92819edaad9c5077faba661d5d4bdb52d1a1a4
Eric Holscher <eric@ericholscher.com>
2011-03-15 18:11:00 -0400

    Added a basic "repo type guesser" in javascript to the import page.

--- media/javascript/rtd.js
+++ media/javascript/rtd.js
@@ -1,6 +1,7 @@
  $(document).ready(function()
  {
     ShowActionOnOver();
+    guessRepo();
  });
 
  function ShowActionOnOver()
@@ -16,3 +17,33 @@
         }
     );
  }
+
+function guessRepo() {
+  $("#id_repo").blur(
+      function() {
+         val = this.value
+         repo = $("#id_repo_type")[0]
+         if (val.indexOf("git") >= 0) {
+            repo.value = "git"
+         }
+         else if (val.indexOf("bitbucket") >= 0) {
+            repo.value = "hg"
+         }
+         else if (val.indexOf("hg") >= 0) {
+            repo.value = "hg"
+         }
+         else if (val.indexOf("bzr") >= 0) {
+            repo.value = "bzr"
+         }
+         else if (val.indexOf("launchpad") >= 0) {
+            repo.value = "bzr"
+         }
+         else if (val.indexOf("trunk") >= 0) {
+            repo.value = "svn"
+         }
+         else if (val.indexOf("svn") >= 0) {
+            repo.value = "svn"
+         }
+      }
+  )
+}
