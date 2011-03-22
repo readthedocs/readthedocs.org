b47be256f8461943332eee7d1917406336ec3ff8
Eric Holscher <eric@ericholscher.com>
2011-03-21 23:36:37 -0700

    Run unzip files on the app server. Since that's where the file is.

--- readthedocs/projects/views/private.py
+++ readthedocs/projects/views/private.py
@@ -357,7 +357,7 @@ def upload_html(request, project_slug):
 
             #Extract file into the correct place.
             html_path = proj.rtd_build_path(version.slug)
-            unzip_files.delay(dest_file, html_path)
+            unzip_files(dest_file, html_path)
             return HttpResponseRedirect(proj.get_absolute_url())
     else:
         form = FormClass(request=request)
