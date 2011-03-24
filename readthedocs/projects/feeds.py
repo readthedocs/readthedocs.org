fb647b532f09ccb9f89cb241b925cbc2b391b6f7
Eric Holscher <eric@ericholscher.com>
2011-03-23 21:17:49 -0700

    Added basic RSS feed for latest updated projects.

---
+++ readthedocs/projects/feeds.py
@@ -0,0 +1,18 @@
+from django.contrib.syndication.views import Feed
+from django.db.models import Max
+
+from projects.models import Project
+
+class LatestProjectsFeed(Feed):
+    title = "Recently updated documentation"
+    link = "http://readthedocs.org"
+    description = "Recently updated documentation on Read the Docs"
+
+    def items(self):
+        return Project.objects.filter(builds__isnull=False).annotate(max_date=Max('builds__date')).order_by('-max_date')[:10]
+
+    def item_title(self, item):
+        return item.name
+
+    def item_description(self, item):
+        return item.description
