e3b75096888fcade853dc2990603827665a88a6f
Eric Holscher <eric@ericholscher.com>
2011-04-03 00:14:20 -0700

    Flail.

--- readthedocs/core/subdomain_urls.py
+++ readthedocs/core/subdomain_urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import url, patterns
+from django.conf.urls.defaults import url, patterns, include
 
 urlpatterns = patterns('',
     url(r'^(?P<lang_slug>\w{2})/(?P<version_slug>[\w.-]+)/(?P<filename>.+)$',
