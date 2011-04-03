5bf878871ce9064c95e98b4b1737d49b77ffd3ca
Eric Holscher <eric@ericholscher.com>
2011-04-02 19:13:26 -0700

    Turn cached sessions back on.

--- readthedocs/settings/postgres.py
+++ readthedocs/settings/postgres.py
@@ -18,7 +18,7 @@ CELERY_ALWAYS_EAGER = False
 MEDIA_URL = 'http://media.readthedocs.org/'
 ADMIN_MEDIA_PREFIX = MEDIA_URL + 'admin/'
 CACHE_BACKEND = 'memcached://localhost:11211/'
-#SESSION_ENGINE = "django.contrib.sessions.backends.cached_db"
+SESSION_ENGINE = "django.contrib.sessions.backends.cached_db"
 
 HAYSTACK_SEARCH_ENGINE = 'solr'
 HAYSTACK_SOLR_URL = 'http://odin:8983/solr'
