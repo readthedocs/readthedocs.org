6fef8e7db39ece81f0d9fbba862093b1a622fc52
Eric Holscher <eric@ericholscher.com>
2011-04-16 10:41:12 -0700

    Logic is hard.

--- readthedocs/doc_builder/backends/html.py
+++ readthedocs/doc_builder/backends/html.py
@@ -106,7 +106,7 @@ class Builder(BaseBuilder):
 
     def clean(self, version):
         try:
-            if version.project.whitelisted and not version.project.is_imported:
+            if version.project.whitelisted and version.project.is_imported:
                 print "Project whitelisted"
                 self._whitelisted(version)
             else:
