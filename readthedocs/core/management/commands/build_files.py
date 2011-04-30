36b1efc830bd05e2ec5107039832a0c791376a95
Eric Holscher <eric@ericholscher.com>
2011-04-30 17:22:38 +0000

    Fix indexing files.
    Conflicts:
    
    	readthedocs/core/management/commands/build_files.py

--- readthedocs/core/management/commands/build_files.py
+++ readthedocs/core/management/commands/build_files.py
@@ -1,14 +1,18 @@
 from django.core.management.base import BaseCommand
 from projects import tasks
 from projects.models import Project
+from builds.models import Version
 
 class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         if not len(args):
-            for p in Project.objects.all():
-                print "Indexing %s" % p
-                tasks.fileify(p)
+            for v in Version.objects.filter(slug='latest'):
+                print "Indexing %s" % v
+                try:
+		    tasks.fileify(v)
+		except:
+		    pass
         else:
             for slug in args:
                 p = Project.objects.get(slug=slug)
