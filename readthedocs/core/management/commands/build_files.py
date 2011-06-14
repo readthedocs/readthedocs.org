38d475f49e716aa4264476aff8c183f5ebfac59b
Eric Holscher <eric@ericholscher.com>
2011-06-13 19:25:31 -0700

    Crush files when we reindex.

--- readthedocs/core/management/commands/build_files.py
+++ readthedocs/core/management/commands/build_files.py
@@ -1,11 +1,11 @@
 from django.core.management.base import BaseCommand
 from projects import tasks
-from projects.models import Project
+from projects.models import Project, ImportedFile
 from builds.models import Version
 
 class Command(BaseCommand):
-
     def handle(self, *args, **kwargs):
+        ImportedFile.objects.delete()
         if not len(args):
             for v in Version.objects.filter(slug='latest'):
                 print "Indexing %s" % v
