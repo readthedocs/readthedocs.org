39e37834a239e8c3d09525be205b3a498a7fa5e6
Eric Holscher <eric@ericholscher.com>
2011-06-15 22:32:11 -0700

    Fix manpage name so it works on the frontend.

--- docs/conf.py
+++ docs/conf.py
@@ -221,6 +221,6 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'readthedocs', u'Read The Docs Documentation',
+    ('index', 'read-the-docs', u'Read The Docs Documentation',
      [u'Eric Holscher, Charlie Leifer, Bobby Grace'], 1)
 ]
