7bbf83948c49383c8fe177110f35fef7ffb1fa70
Dan Colish <dcolish@gmail.com>
2011-06-15 21:17:26 -0700

    correct man page builder, add links to project dashboard

--- readthedocs/doc_builder/backends/sphinx_man.py
+++ readthedocs/doc_builder/backends/sphinx_man.py
@@ -1,6 +1,10 @@
 import os
+
+from django.conf import settings
+
 from doc_builder.backends.sphinx import Builder as ManpageBuilder
-from projects.utils import safe_write, run
+from projects.tasks import copy_to_app_servers
+from projects.utils import run
 
 
 class Builder(ManpageBuilder):
@@ -9,9 +13,31 @@ class Builder(ManpageBuilder):
         project = version.project
         os.chdir(version.project.conf_dir(version.slug))
         if project.use_virtualenv and project.whitelisted:
-            build_command = '%s -b man . _build/man' % project.venv_bin(
+            build_command = '%s -b man  -d _build/doctrees . _build/man' % project.venv_bin(
                 version=version.slug, bin='sphinx-build')
         else:
             build_command = "sphinx-build -b man . _build/man"
         build_results = run(build_command)
-        return build_results
+        if build_results[0] == 0:
+            os.chdir('_build/man')
+            to_path = os.path.join(settings.MEDIA_ROOT,
+                                   'man',
+                                   project.slug,
+                                   version.slug)
+            if not os.path.exists(to_path):
+                os.makedirs(to_path)
+            from_file = os.path.join(os.getcwd(),
+                                     "%s" % ('%s.1' % project.slug))
+            to_file = os.path.join(to_path, '%s.1' % project.slug)
+            if os.path.exists(from_file):
+                if getattr(settings, "MULTIPLE_APP_SERVERS", None):
+                    copy_to_app_servers('/'.join(
+                            from_file.split('/')[0:-1]),
+                                        '/'.join(to_file.split('/')[0:-1]))
+                else:
+                    run('mv -f %s %s' % (from_file, to_file))
+            else:
+                print "File doesn't exist, not symlinking."
+            return True
+        else:
+            return False
