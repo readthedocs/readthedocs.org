c3b9c97bea146ac92bc0c364e3108da04a7b8eea
Eric Holscher <eric@ericholscher.com>
2011-06-19 11:01:58 -0700

    Added default localhost redis settings.

--- readthedocs/settings/sqlite.py
+++ readthedocs/settings/sqlite.py
@@ -9,6 +9,13 @@ DATABASES = {
                     'NAME': os.path.join(SITE_ROOT, 'dev.db'),
                 }
 }
+REDIS = {
+    'host': 'localhost',
+    'port': 6379,
+    'db': 0,
+}
+
+
 
 SESSION_COOKIE_DOMAIN = None
 HAYSTACK_SOLR_URL = 'http://localhost:8983/solr'
