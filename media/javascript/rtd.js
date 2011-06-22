780b0a78cd20c53b9d6d67391799780ca6627813
Eric Holscher <eric@ericholscher.com>
2011-06-21 20:42:33 -0700

    Don't break urls going to the new version.

--- media/javascript/rtd.js
+++ media/javascript/rtd.js
@@ -13,7 +13,7 @@ warning = '<div class="admonition note"> <p class="first admonition-title">Note<
      url: "/api/v1/version/" + doc_slug + "/highest/" + doc_version + "/",
      success: function(data, textStatus, request) {
       if (!data.is_highest) {
-         $("div.body").prepend(warning + "<a href='http://readthedocs.org" + data.url + "'>" + data.version + "</a> is the newest version. </p></div>")
+         $("div.body").prepend(warning + "<a href='http://readthedocs.org" + data.url + window.location.pathname + "'>" + data.version + "</a> is the newest version. </p></div>")
       }
      },
      dataType: 'json'
