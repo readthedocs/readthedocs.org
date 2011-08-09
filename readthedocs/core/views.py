54e4e619e0e785102cd2e8aa795497a3ba75cc6d
Eric Holscher <eric@ericholscher.com>
2011-08-08 19:20:52 -0700

    Hide the pageview stuff, because it isn't updating anymore.

--- readthedocs/core/views.py
+++ readthedocs/core/views.py
@@ -27,13 +27,14 @@ def homepage(request):
     latest_projects = cache.get('latest_projects', None)
     if not latest_projects:
         latest_projects = Project.objects.filter(builds__isnull=False).annotate(max_date=Max('builds__date')).order_by('-max_date')[:10]
-        cache.set('latest_projects', list(latest_projects), 30)
+        cache.set('latest_projects', list(latest_projects), 60)
     featured = Project.objects.filter(featured=True)
-    updated = PageView.objects.all()[:10]
+    #updated = PageView.objects.all()[:10]
     return render_to_response('homepage.html',
                               {'project_list': latest_projects,
                                'featured_list': featured,
-                               'updated_list': updated},
+                               #'updated_list': updated
+                               },
                 context_instance=RequestContext(request))
 
 def random_page(request, project=None):
