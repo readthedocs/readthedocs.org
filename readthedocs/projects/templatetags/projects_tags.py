5b2f807f37a205d30f0ca0ad680b2f53446c3d6c
Eric Holscher <eric@ericholscher.com>
2011-09-18 01:18:23 -0700

    Fix import to be app-relative.

--- readthedocs/projects/templatetags/projects_tags.py
+++ readthedocs/projects/templatetags/projects_tags.py
@@ -56,6 +56,6 @@ def sort_version_aware(versions):
     Takes a list of versions objects and sort them caring about version schemes
     """
     from distutils2.version import NormalizedVersion
-    from readthedocs.projects.utils import mkversion
+    from projects.utils import mkversion
     fallback = NormalizedVersion('99999999.0', error_on_huge_major_num=False)
     return sorted(versions, key=lambda v:(mkversion(v) or fallback), reverse=True)
