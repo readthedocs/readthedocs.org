088243e6212a9a65bb8434b28e3a799946f12816
Eric Holscher <eric@ericholscher.com>
2011-09-21 13:00:09 -0700

    Make user in userprofile admin a raw_id field.

--- readthedocs/core/admin.py
+++ readthedocs/core/admin.py
@@ -9,5 +9,6 @@ class UserProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'whitelisted', 'homepage')
     search_fields = ('user__username', 'homepage')
     list_editable = ('whitelisted',)
+    raw_id_fields = ('user',)
 
 admin.site.register(UserProfile, UserProfileAdmin)
