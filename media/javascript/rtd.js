494b90b8be1011f9852a91ed117f6d638c54576e
Eric Holscher <eric@ericholscher.com>
2012-01-07 18:55:45 -0800

    Less derpy string concat.

--- media/javascript/rtd.js
+++ media/javascript/rtd.js
@@ -16,7 +16,7 @@ warning = '<div class="admonition note"> <p class="first admonition-title">Note<
      success: function(data, textStatus, request) {
       if (!data.is_highest) {
         current_url = window.location.pathname.replace(doc_version, data.slug)
-         $("div.body").prepend(warning + '"<a href='+ current_url  + "'>" + data.version + "</a> is the newest version. </p></div>")
+         $("div.body").prepend(warning + "<a href='" + current_url  + "'>" + data.version + "</a> is the newest version. </p></div>")
       }
      },
      dataType: 'jsonp'
