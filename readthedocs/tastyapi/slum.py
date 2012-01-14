254ecaf0f5c6942ffccc82c37e9d338345d98a9f
Eric Holscher <eric@ericholscher.com>
2012-01-14 15:37:12 -0800

    Support auth for slumber.

--- readthedocs/tastyapi/slum.py
+++ readthedocs/tastyapi/slum.py
@@ -1,4 +1,11 @@
 import slumber
 import json
 
-api = slumber.API(base_url='http://readthedocs.org/api/v1/')
+from django.conf import settings
+USER = getattr(settings, 'SLUMBER_USERNAME', None)
+PASS = getattr(settings, 'SLUMBER_PASSWORD', None)
+
+if USER and PASS:
+    api = slumber.API(base_url='http://readthedocs.org/api/v1/', auth=(USER, PASS))
+else:
+    api = slumber.API(base_url='http://readthedocs.org/api/v1/')
