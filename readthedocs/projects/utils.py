18fea563a8e2ef431b8cc2f275543340198616ad
Eric Holscher <eric@ericholscher.com>
2012-02-11 16:50:22 -0800

    Remove DB slugify logic for now.

--- readthedocs/projects/utils.py
+++ readthedocs/projects/utils.py
@@ -92,11 +92,13 @@ def slugify_uniquely(model, initial, field, max_length, **filters):
     slug = _custom_slugify(initial)[:max_length]
     current = slug
     index = 0
+    """
     base_qs = model.objects.filter(**filters)
     while base_qs.filter(**{field: current}).exists():
         suffix = '-%s' % index
         current = '%s%s'  % (slug[:-len(suffix)], suffix)
         index += 1
+    """
     return current
 
 def mkversion(version_obj):
