3b1c77af392b1400baf5515664d861cd4808e0e5
Eric Holscher <eric@ericholscher.com>
2012-02-23 21:29:31 -0800

    Fix the unknown ref again.

--- readthedocs/doc_builder/backends/sphinx_pdf.py
+++ readthedocs/doc_builder/backends/sphinx_pdf.py
@@ -19,6 +19,8 @@ class Builder(BaseBuilder):
     def build(self):
         project = self.version.project
         os.chdir(project.conf_dir(self.version.slug))
+        #Default to this so we can return it always.
+        pdf_results = (1, '', '')
         if project.use_virtualenv:
             latex_results = run('%s -b latex -d _build/doctrees   . _build/latex' %
                                 project.venv_bin(version=self.version.slug, bin='sphinx-build'))
@@ -46,8 +48,7 @@ class Builder(BaseBuilder):
                         if not os.path.exists(to_path):
                             os.makedirs(to_path)
                         run('mv -f %s %s' % (from_file, to_file))
-        if not pdf_results:
-            pdf_results = (1, '', '')
+        else:
             print "PDF Building failed. Moving on."
         return (latex_results, pdf_results)
 
