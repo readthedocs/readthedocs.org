67a5fc48e3eddee56922ee9c369b286322b13898
Eric Holscher <eric@ericholscher.com>
2012-07-29 18:18:48 -0700

    Output more helpful debug info.

--- readthedocs/projects/search_indexes.py
+++ readthedocs/projects/search_indexes.py
@@ -84,7 +84,7 @@ class ImportedFileIndex(indexes.SearchIndex, indexes.Indexable):
         except IOError as e:
             log.info('Unable to index file: %s, error :%s' % (file_path, e))
             return
-        log.debug('Indexing %s' % obj.slug)
+        log.debug('Indexing %s' % obj.path)
         try:
             to_index = strip_tags(PyQuery(content)("div.document").html()).replace(u'¶', '')
         except ValueError:
