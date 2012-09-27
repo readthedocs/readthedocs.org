ecf817e802590d0ef193fba703bcc5e74b2d284b
Jonas Obrist <ojiidotch@gmail.com>
2012-09-27 18:16:59 +0200

    I might have forgotten a merge conflict

--- readthedocs/core/management/commands/update_repos.py
+++ readthedocs/core/management/commands/update_repos.py
@@ -48,11 +48,7 @@ class Command(BaseCommand):
         if len(args):
             for slug in args:
                 if version and version != "all":
-<<<<<<< HEAD
                     log.info("Updating version %s for %s" % (version, slug))
-=======
-                    print _("Updating version") + " %s " + _("for") + " %s)" % (version, slug)
->>>>>>> 8249b5c288c3eba2e845684ba818f61e71a95a12
                     for version in Version.objects.filter(project__slug=slug,
                                                           slug=version):
                         tasks.update_docs(version.project_id,
@@ -60,11 +56,7 @@ class Command(BaseCommand):
                                           record=False,
                                           version_pk=version.pk)
                 elif version == "all":
-<<<<<<< HEAD
                     log.info("Updating all versions for %s" % slug)
-=======
-                    print _("Updating all versions for %s") % slug
->>>>>>> 8249b5c288c3eba2e845684ba818f61e71a95a12
                     for version in Version.objects.filter(project__slug=slug,
                                                           active=True,
                                                           uploaded=False):
@@ -74,19 +66,11 @@ class Command(BaseCommand):
                                           version_pk=version.pk)
                 else:
                     p = Project.objects.get(slug=slug)
-<<<<<<< HEAD
                     log.info("Building %s" % p)
                     tasks.update_docs(pk=p.pk, pdf=make_pdf, force=force)
         else:
             if version == "all":
                 log.info("Updating all versions")
-=======
-                    print _("Building %s") % p
-                    tasks.update_docs(pk=p.pk, pdf=make_pdf, force=force)
-        else:
-            if version == "all":
-                print _("Updating all versions")
->>>>>>> 8249b5c288c3eba2e845684ba818f61e71a95a12
                 for version in Version.objects.filter(active=True,
                                                       uploaded=False):
                     tasks.update_docs(pk=version.project_id,
@@ -95,11 +79,7 @@ class Command(BaseCommand):
                                       force=force,
                                       version_pk=version.pk)
             else:
-<<<<<<< HEAD
                 log.info("Updating all docs")
-=======
-                print _("Updating all docs")
->>>>>>> 8249b5c288c3eba2e845684ba818f61e71a95a12
                 tasks.update_docs_pull(pdf=make_pdf,
                                        record=record,
                                        force=force)
