77464ff538f732ebfdacabe754ae4c2801246a14
Eric Holscher <eric@ericholscher.com>
2012-10-13 13:15:36 -0700

    Fix not having any pdf bits.

--- readthedocs/doc_builder/backends/sphinx_pdf.py
+++ readthedocs/doc_builder/backends/sphinx_pdf.py
@@ -38,27 +38,32 @@ class Builder(BaseBuilder):
                 # Otherwise treat all .tex files as sources
             tex_files = glob('*.tex')
 
-            # Run LaTeX -> PDF conversions
-            pdflatex_cmds = ['pdflatex -interaction=nonstopmode %s' % tex_file
-                             for tex_file in tex_files]
-            pdf_results = run(*pdflatex_cmds)
+            if tex_files:
+                
+                # Run LaTeX -> PDF conversions
+                pdflatex_cmds = ['pdflatex -interaction=nonstopmode %s' % tex_file
+                                 for tex_file in tex_files]
+                pdf_results = run(*pdflatex_cmds)
 
-            if pdf_results[0] == 0:
-                for tex_file in tex_files:
-                    to_path = os.path.join(settings.MEDIA_ROOT,
-                           'pdf',
-                           project.slug,
-                           self.version.slug)
-                    to_file = os.path.join(to_path, '%s.pdf' % project.slug)
-                    # pdflatex names its output predictably: foo.tex -> foo.pdf
-                    pdf_filename = os.path.splitext(tex_file)[0] + '.pdf'
-                    from_file = os.path.join(os.getcwd(), pdf_filename)
-                    if getattr(settings, "MULTIPLE_APP_SERVERS", None):
-                        copy_file_to_app_servers(from_file, to_file)
-                    else:
-                        if not os.path.exists(to_path):
-                            os.makedirs(to_path)
-                        run('mv -f %s %s' % (from_file, to_file))
+                if pdf_results[0] == 0:
+                    for tex_file in tex_files:
+                        to_path = os.path.join(settings.MEDIA_ROOT,
+                               'pdf',
+                               project.slug,
+                               self.version.slug)
+                        to_file = os.path.join(to_path, '%s.pdf' % project.slug)
+                        # pdflatex names its output predictably: foo.tex -> foo.pdf
+                        pdf_filename = os.path.splitext(tex_file)[0] + '.pdf'
+                        from_file = os.path.join(os.getcwd(), pdf_filename)
+                        if getattr(settings, "MULTIPLE_APP_SERVERS", None):
+                            copy_file_to_app_servers(from_file, to_file)
+                        else:
+                            if not os.path.exists(to_path):
+                                os.makedirs(to_path)
+                            run('mv -f %s %s' % (from_file, to_file))
+            else:
+                pdf_results = (0, "No tex files found", "No tex files found")
+
 
         if latex_results[0] != 0 or pdf_results[0] != 0:
             log.warning("PDF Building failed. Moving on.")
