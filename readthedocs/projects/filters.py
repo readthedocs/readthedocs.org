ba36c0edafbc5874fd2c4f52125ffbef64aa6604
Eric Holscher <eric@ericholscher.com>
2012-10-14 18:22:07 -0700

    Revert "Revert "Add proper i18n.""
    This reverts commit 9561411d95fc33e29b33a1a936f3bb9a8682c082.

--- readthedocs/projects/filters.py
+++ readthedocs/projects/filters.py
@@ -1,20 +1,22 @@
+from django.utils.translation import ugettext_lazy as _
+
 import django_filters
 
 from projects import constants
 from projects.models import Project
 
 ANY_REPO = (
-    ('', 'Any'),
+    ('', _('Any')),
 )
 
 REPO_CHOICES = ANY_REPO + constants.REPO_CHOICES
 
 class ProjectFilter(django_filters.FilterSet):
-    name = django_filters.CharFilter(label="Name", name='name', lookup_type='icontains')
-    pub_date = django_filters.DateRangeFilter(label="Created Date", name="pub_date")
-    repo = django_filters.CharFilter(label="Repository URL", name='repo', lookup_type='icontains')
+    name = django_filters.CharFilter(label=_("Name"), name='name', lookup_type='icontains')
+    pub_date = django_filters.DateRangeFilter(label=_("Created Date"), name="pub_date")
+    repo = django_filters.CharFilter(label=_("Repository URL"), name='repo', lookup_type='icontains')
     repo_type = django_filters.ChoiceFilter(
-        label="Repository",
+        label=_("Repository"),
         name='repo',
         lookup_type='icontains',
         choices=REPO_CHOICES,
