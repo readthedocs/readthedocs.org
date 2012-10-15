9561411d95fc33e29b33a1a936f3bb9a8682c082
Eric Holscher <eric@ericholscher.com>
2012-10-14 17:54:26 -0700

    Revert "Add proper i18n."
    This reverts commit c16dc05cf3d01ccfaa2ec2555fa24825f5b1b973.

--- readthedocs/builds/filters.py
+++ readthedocs/builds/filters.py
@@ -1,5 +1,3 @@
-from django.utils.translation import ugettext_lazy as _
-
 import django_filters
 
 from builds import constants
@@ -7,7 +5,7 @@ from builds.models import Build, Version
 
 
 ANY_REPO = (
-    ('', _('Any')),
+    ('', 'Any'),
 )
 
 BUILD_TYPES = ANY_REPO + constants.BUILD_TYPES
@@ -15,15 +13,15 @@ BUILD_TYPES = ANY_REPO + constants.BUILD_TYPES
 
 class VersionFilter(django_filters.FilterSet):
     project = django_filters.CharFilter(name='project__name', lookup_type="icontains")
-    slug= django_filters.CharFilter(label=_("Slug"), name='slug', lookup_type='icontains')
+    slug= django_filters.CharFilter(label="Slug", name='slug', lookup_type='icontains')
 
     class Meta:
         model = Version
         fields = ['project', 'slug']
 
 class BuildFilter(django_filters.FilterSet):
-    date = django_filters.DateRangeFilter(label=_("Build Date"), name="date")
-    type = django_filters.ChoiceFilter(label=_("Build Type"), choices=BUILD_TYPES)
+    date = django_filters.DateRangeFilter(label="Build Date", name="date")
+    type = django_filters.ChoiceFilter(label="Build Type", choices=BUILD_TYPES)
 
     class Meta:
         model = Build
