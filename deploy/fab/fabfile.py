309cc6c92cd5f8b65ed6c369d9b4a7d3ba7da768
Eric Holscher <eric@ericholscher.com>
2012-10-20 19:53:33 -0700

    Update fabfile

--- deploy/fab/fabfile.py
+++ deploy/fab/fabfile.py
@@ -4,29 +4,16 @@ from fabric.api import *
 import fabtools
 from fabtools import require
 
-env.code_dir = '/home/docs/checkouts/readthedocs.org'
-env.virtualenv = '/home/docs/'
-
 cwd = os.getcwd()
+all_users = ['docs', 'eric', 'syncer']
+checkout_users = ['docs', 'syncer']
+required_dirs =['checkouts', 'etc', 'run', 'log']
 
 def all():
-    users()
     install_packages()
+    users()
     checkout()
     setup_env()
-
-def users():
-    env.user = 'root'
-    users = ['docs', 'eric']
-    for user in users:
-        if not fabtools.user.exists(user):
-            sudo('adduser --gecos "" -q --disabled-password %s' % user)
-
-    if not fabtools.files.is_file('/home/docs/.ssh/authorized_keys'):
-        sudo('mkdir -p /home/%s/.ssh' % user)
-        put('keys/*.pub', '/home/%s/.ssh/authorized_keys' % user, mode=700)
-        sudo('chown -R %s:%s /home/%s/' % (user, user, user))
-        sudo('chmod -R 700 /home/%s/.ssh' % (user))
 
 def install_packages():
     env.user = 'root'
@@ -38,25 +25,51 @@ def install_packages():
     # Prod Packages
     sudo('apt-get install -y redis-server memcached')
 
+def users():
+    env.user = 'root'
+    for user in all_users:
+        home = '/home/%s' % user
+        if not fabtools.user.exists(user):
+            sudo('adduser --gecos "" -q --disabled-password %s' % user)
 
-def checkout():
-    env.user = 'docs'
-    run('mkdir -p /home/docs/checkouts/')
-    run('mkdir -p /home/docs/etc/')
-    run('mkdir -p /home/docs/run/')
-    run('mkdir -p /home/docs/log/')
-    if not fabtools.files.is_dir(env.code_dir):
-        with cd('/home/docs/checkouts/'):
-            run('git clone git://github.com/rtfd/readthedocs.org.git')
-    if not fabtools.files.is_file('/home/docs/bin/python'):
-        run('virtualenv %s' % env.virtualenv)
-    run('/home/docs/bin/pip install -U -r /home/docs/checkouts/readthedocs.org/deploy_requirements.txt')
+        if not fabtools.files.is_file('%s/.ssh/authorized_keys' % home):
+            sudo('mkdir -p %s/.ssh' % home)
+            put('keys/*.pub', '%s/.ssh/authorized_keys' % home, mode=700)
+            sudo('chown -R %s:%s %s' % (user, user, home))
+            sudo('chmod -R 700 %s' % home)
 
-def setup_env():
-    env.user = 'docs'
-    put('files/bash_profile', '/home/docs/.bash_profile')
-    put('files/supervisord.conf', '/home/docs/etc/supervisord.conf')
-    run('/home/docs/bin/pip install -U supervisor ipython gunicorn')
-    with cd('/home/docs/checkouts/readthedocs.org/readthedocs'):
+def checkout(user):
+    if user:
+        users = [user]
+    else:
+        users = checkout_users
+    for user in users:
+        env.user = user
+        home = '/home/%s' % user
+        for dir in required_dirs:
+            run('mkdir -p %s/%s' % (home, dir))
+        if not fabtools.files.is_dir('%s/checkouts/readthedocs.org' % home):
+            with cd('%s/checkouts/' % home):
+                run('git clone git://github.com/rtfd/readthedocs.org.git')
+        if not fabtools.files.is_file('%s/bin/python' % home):
+            run('virtualenv %s' % home)
+        run('%s/bin/pip install -U -r %s/checkouts/readthedocs.org/deploy_requirements.txt' % (home, home))
+
+def setup_env(user):
+    if user:
+        users = [user]
+    else:
+        users = checkout_users
+    for user in users:
+        env.user = user
+        home = '/home/%s' % user
+        put('files/bash_profile', '%s/.bash_profile' % home)
+        put('files/%s_supervisord.conf' % user, '%s/etc/supervisord.conf' % home)
+        run('%s/bin/pip install -U supervisor ipython gunicorn' % home)
+
+def setup_db():
+    env.user = "docs"
+    home = '/home/%s' % user
+    with cd('%s/checkouts/readthedocs.org/readthedocs' % home):
         run('./manage.py syncdb --noinput')
         run('./manage.py migrate')
