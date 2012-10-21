451d3cc31bdc2fbbc1a860ece6960edbad092070
Eric Holscher <eric@ericholscher.com>
2012-10-20 18:43:21 -0700

    Add bits to sqlite settings for celery.

--- readthedocs/settings/sqlite.py
+++ readthedocs/settings/sqlite.py
@@ -13,6 +13,9 @@ REDIS = {
     'port': 6379,
     'db': 0,
 }
+BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+#CELERY_ALWAYS_EAGER = False
 
 
 SESSION_COOKIE_DOMAIN = None
