b436b6383fd5cbaff6d87a5aaffb67a0109dbbdb
Eric Holscher <eric@ericholscher.com>
2012-11-19 17:14:24 -0800

    Actually add the context processor >:x

---
+++ readthedocs/core/context_processors.py
@@ -0,0 +1,8 @@
+from django.conf import settings
+
+def readthedocs_processor(request):
+    exports = {
+        'PRODUCTION_DOMAIN': getattr(settings, 'PRODUCTION_DOMAIN', None),
+        'USE_SUBDOMAINS': getattr(settings, 'USE_SUBDOMAINS', None),
+    }
+    return exports
