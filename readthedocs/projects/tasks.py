3f2a14dc6ef42b3df5df010557447b34f3e4a4f0
Eric Holscher <eric@ericholscher.com>
2013-01-03 08:43:36 -0800

    Remove possible race condition in doc deleting code.
    We have more disk space on the servers now, so this isn't as important
    as when we were in the "cloud"

--- readthedocs/projects/tasks.py
+++ readthedocs/projects/tasks.py
@@ -633,7 +633,9 @@ def clear_artifacts(version_pk):
     """ Remove artifacts from the build server. """
     version_data = api.version(version_pk).get()
     version = make_api_version(version_data)
-    run('rm -rf %s' % version.project.full_epub_path(version.slug))
-    run('rm -rf %s' % version.project.full_man_path(version.slug))
-    run('rm -rf %s' % version.project.full_build_path(version.slug))
-    run('rm -rf %s' % version.project.full_latex_path(version.slug))
+    # Stop doing this for now as it causes 403s if people build things back to back some times
+    # because of a race condition
+    #run('rm -rf %s' % version.project.full_epub_path(version.slug))
+    #run('rm -rf %s' % version.project.full_man_path(version.slug))
+    #run('rm -rf %s' % version.project.full_build_path(version.slug))
+    #run('rm -rf %s' % version.project.full_latex_path(version.slug))
