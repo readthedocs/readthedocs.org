9383196ced2c5e4d6a820eb347e7cd69b80fadd6
Eric Holscher <eric@ericholscher.com>
2013-07-23 15:52:01 -0700

    Made web support admin nicer

--- readthedocs/websupport/admin.py
+++ readthedocs/websupport/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import SphinxComment, SphinxNode
 
+class SphinxNodeAdmin(admin.ModelAdmin):
+    search_fields = ('id', 'document')
+    list_filter = ('project__name',)
 
-admin.site.register(SphinxNode)
-admin.site.register(SphinxComment)
+class SphinxCommentAdmin(admin.ModelAdmin):
+    search_fields = ('text',)
+
+admin.site.register(SphinxNode, SphinxNodeAdmin)
+admin.site.register(SphinxComment, SphinxCommentAdmin)
