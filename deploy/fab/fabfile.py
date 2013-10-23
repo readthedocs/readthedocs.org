22e27d2fb8a08ac432b9f8a00b327417118c545c
Eric Holscher <eric@ericholscher.com>
2013-10-22 23:13:49 -0700

    Install enchant C library. Fixes #474.

--- deploy/fab/fabfile.py
+++ deploy/fab/fabfile.py
@@ -73,7 +73,7 @@ def install_packages(type):
             ' curl sqlite libxml2-dev libxslt-dev vim g++ python-numpy '
             ' python-scipy build-essential texlive-full libevent-dev '
             ' libmysqlclient-dev python-m2crypto libffi-dev '
-            ' graphviz-dev '
+            ' graphviz-dev libenchant1c2a '
         )
         sudo('pip install -U mercurial')
     if type == 'db':
