20d6004451908d42d74bcee81f3978f5e57ce1a1
Tom Offermann <tom@offermann.us>
2013-10-26 15:56:48 -0700

    Add SubdomainUrlTests.

--- readthedocs/rtd_tests/tests/test_urls.py
+++ readthedocs/rtd_tests/tests/test_urls.py
@@ -2,6 +2,50 @@ from django.core.urlresolvers import reverse
 from django.core.urlresolvers import NoReverseMatch
 from django.test import TestCase
 
+import core.views
+
+class SubdomainUrlTests(TestCase):
+
+    def test_sub_index(self):
+        url = reverse(core.views.subdomain_handler,
+            urlconf='core.subdomain_urls')
+        self.assertEqual(url, '/')
+
+    def test_sub_lang_version(self):
+        url = reverse('docs_detail', urlconf='core.subdomain_urls',
+            kwargs={'lang_slug': 'en', 'version_slug': 'latest'})
+        self.assertEqual(url, '/en/latest/')
+
+    def test_sub_lang_version_filename(self):
+        url = reverse('docs_detail', urlconf='core.subdomain_urls',
+            args=['en', 'latest', 'index.html'])
+        self.assertEqual(url, '/en/latest/index.html')
+
+    def test_sub_project_full_path(self):
+        url = reverse('subproject_docs_detail',
+            urlconf='core.subdomain_urls',
+            kwargs={'project_slug':'pyramid', 'lang_slug': 'en',
+                    'version_slug':'latest', 'filename': 'index.html'})
+        self.assertEqual(url, '/projects/pyramid/en/latest/index.html')
+
+    def test_sub_project_slug_only(self):
+        url = reverse('subproject_docs_detail',
+            urlconf='core.subdomain_urls',
+            kwargs={'project_slug': 'pyramid'})
+        self.assertEqual(url, '/projects/pyramid')
+
+    def test_sub_page(self):
+        url = reverse('docs_detail',
+            urlconf='core.subdomain_urls',
+            kwargs={'filename': 'install.html'})
+        self.assertEqual(url, '/page/install.html')
+
+    def test_sub_version(self):
+        url = reverse('version_subdomain_handler',
+            urlconf='core.subdomain_urls',
+            kwargs={'version_slug': '1.4.1'})
+        self.assertEqual(url, '/1.4.1/')
+
 
 class WipeUrlTests(TestCase):
 
