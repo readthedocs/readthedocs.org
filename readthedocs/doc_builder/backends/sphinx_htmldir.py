46fef5bffffea1ed2343882a318a6a77dd142649
Eric Holscher <eric@ericholscher.com>
2013-10-25 18:49:14 -0700

    Use our own htmldir builder too

--- readthedocs/doc_builder/backends/sphinx_htmldir.py
+++ readthedocs/doc_builder/backends/sphinx_htmldir.py
@@ -18,10 +18,10 @@ class Builder(HtmlBuilder):
         project = self.version.project
         os.chdir(self.version.project.conf_dir(self.version.slug))
         if project.use_virtualenv:
-            build_command = '%s -b dirhtml . _build/html' % project.venv_bin(
+            build_command = '%s -b readthedocsdirhtml . _build/html' % project.venv_bin(
                 version=self.version.slug, bin='sphinx-build')
         else:
-            build_command = "sphinx-build -b dirhtml . _build/html"
+            build_command = "sphinx-build -b readthedocsdirhtml . _build/html"
         build_results = run(build_command)
         if 'no targets are out of date.' in build_results[1]:
             self._changed = False
