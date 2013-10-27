6aa61f7c2682e1ac3872b6196f213e38e929cc27
Eric Holscher <eric@ericholscher.com>
2013-10-27 01:06:33 -0700

    Refactor restapi URL bits

--- readthedocs/restapi/urls.py
+++ readthedocs/restapi/urls.py
@@ -1,4 +1,7 @@
+from django.conf.urls.defaults import url, patterns
+
 from rest_framework import routers
+
 from .views import ProjectViewSet, NotificationViewSet, VersionViewSet
 
 router = routers.DefaultRouter()
@@ -6,5 +9,11 @@ router.register(r'version', VersionViewSet)
 router.register(r'project', ProjectViewSet)
 router.register(r'notification', NotificationViewSet)
 
-urlpatterns = router.urls
+urlpatterns = patterns(
+	'',
+    url(r'footer_html/', 'restapi.views.footer_html', name='footer_html'),
+    url(r'quick_search/', 'restapi.views.quick_search', name='quick_search'),
+    url(r'index_search/', 'restapi.views.index_search', name='index_search'),
+    url(r'search/', 'restapi.views.search', name='search'),
+)
 
