ebc2cb89f4d4889d29fa5a57cf1decaf7cf23c0b
Eric Holscher <eric@ericholscher.com>
2013-10-30 10:18:59 -0700

    Bump theme and included js/css

--- media/javascript/readthedocs-doc-embed.js
+++ media/javascript/readthedocs-doc-embed.js
@@ -47,6 +47,9 @@ $(document).ready(function () {
     $(document).on('click', "[data-toggle='rst-current-version']", function() {
       $("[data-toggle='rst-versions']").toggleClass("shift-up");
     });  
+    // Make tables responsive
+    $("table.docutils:not(.field-list").wrap("<div class='wy-table-responsive'></div>");
+
     
     // Add Grok the Docs Client
     $.ajax({
--- readthedocs/templates/sphinx/sphinx_rtd_theme/static/theme.js
+++ readthedocs/templates/sphinx/sphinx_rtd_theme/static/theme.js
@@ -1,17 +1,16 @@
-/* Comment this because we include this information in our main JS file.
-$(document).ready(function() {
+$( document ).ready(function() {
   // Shift nav in mobile when clicking the menu.
-  $(document).on('click', "[data-toggle='wy-nav-top']", function() {
+  $("[data-toggle='wy-nav-top']").click(function() {
     $("[data-toggle='wy-nav-shift']").toggleClass("shift");
     $("[data-toggle='rst-versions']").toggleClass("shift");
   });
   // Close menu when you click a link.
-  $(document).on('click', ".wy-menu-vertical .current ul li a", function() {
+  $(".wy-menu-vertical .current ul li a").click(function() {
     $("[data-toggle='wy-nav-shift']").removeClass("shift");
     $("[data-toggle='rst-versions']").toggleClass("shift");
   });
-  $(document).on('click', "[data-toggle='rst-current-version']", function() {
+  $("[data-toggle='rst-current-version']").click(function() {
     $("[data-toggle='rst-versions']").toggleClass("shift-up");
-  });  
+  });
+  $("table.docutils:not(.field-list").wrap("<div class='wy-table-responsive'></div>");
 });
-*/
