bd97ebc39fcb4053c17d5a24a1f807bdd428c4b3
Eric Holscher <eric@ericholscher.com>
2013-10-29 18:34:44 -0700

    Include origin/%s in version filtering of branch name

--- readthedocs/projects/models.py
+++ readthedocs/projects/models.py
@@ -609,16 +609,19 @@ class Project(models.Model):
 
     def version_from_branch_name(self, branch):
         try:
-            return (self.versions.filter(identifier=branch) |
-                    self.versions.filter(identifier=('remotes/origin/%s'
-                                                     % branch)))[0]
+            return (
+                self.versions.filter(identifier=branch) |
+                self.versions.filter(identifier=('remotes/origin/%s' % branch))
+                self.versions.filter(identifier=('origin/%s' % branch))
+            )[0]
         except IndexError:
             return None
 
     def versions_from_branch_name(self, branch):
         return (
             self.versions.filter(identifier=branch) |
-            self.versions.filter(identifier='remotes/origin/%s' % branch)
+            self.versions.filter(identifier='remotes/origin/%s' % branch) |
+            self.versions.filter(identifier='origin/%s' % branch)
         )
     def get_default_version(self):
         """
