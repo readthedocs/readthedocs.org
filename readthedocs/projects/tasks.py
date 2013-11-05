c7905962c8fe27e9b07dde70a0170ac5454fe0e1
Eric Holscher <eric@ericholscher.com>
2013-11-05 11:05:06 -0800

    Blow away build dirs explicitly if they exist

--- readthedocs/projects/tasks.py
+++ readthedocs/projects/tasks.py
@@ -376,6 +376,10 @@ def update_imported_docs(version_pk, api=None):
 
         # Do Virtualenv bits:
         if project.use_virtualenv:
+            build_dir = os.path.join(project.venv_path(version=version_slug), 'build')
+            if os.path.exists(build_dir):
+                log.info(LOG_TEMPLATE.format(project=project.slug, version=version.slug, msg='Removing existing build dir'))
+                shutil.remove_dir(build_dir)
             if project.use_system_packages:
                 site_packages = '--system-site-packages'
             else:
