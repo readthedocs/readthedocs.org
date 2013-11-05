e0a1aa7468f9afb8055b5d7dd782d736421d882b
Eric Holscher <eric@ericholscher.com>
2013-11-04 20:54:59 -0800

    Fix tests

--- readthedocs/rtd_tests/tests/test_repo_parsing.py
+++ readthedocs/rtd_tests/tests/test_repo_parsing.py
@@ -18,21 +18,21 @@ class TestRepoParsing(TestCase):
 
     def test_github(self):
         self.pip.repo = 'https://github.com/user/repo'
-        self.assertEqual(self.version.get_github_url('file'), 'https://github.com/user/repo/blob/master/docs/file.rst')
+        self.assertEqual(self.version.get_github_url(docroot='/docs/', filename='file'), 'https://github.com/user/repo/blob/master/docs/file.rst')
 
         self.pip.repo = 'https://github.com/user/repo/'
-        self.assertEqual(self.version.get_github_url('file'), 'https://github.com/user/repo/blob/master/docs/file.rst')
+        self.assertEqual(self.version.get_github_url(docroot='/docs/', filename='file'), 'https://github.com/user/repo/blob/master/docs/file.rst')
 
         self.pip.repo = 'https://github.com/user/repo.git'
-        self.assertEqual(self.version.get_github_url('file'), 'https://github.com/user/repo/blob/master/docs/file.rst')
+        self.assertEqual(self.version.get_github_url(docroot='/docs/', filename='file'), 'https://github.com/user/repo/blob/master/docs/file.rst')
 
     def test_bitbucket(self):
         self.pip.repo = 'https://bitbucket.org/user/repo'
-        self.assertEqual(self.version.get_bitbucket_url('file'), 'https://bitbucket.org/user/repo/src/master/docs/file.rst')
+        self.assertEqual(self.version.get_bitbucket_url(docroot='/foo/bar/', filename='file'), 'https://bitbucket.org/user/repo/src/master/foo/bar/file.rst')
 
         self.pip.repo = 'https://bitbucket.org/user/repo/'
-        self.assertEqual(self.version.get_bitbucket_url('file'), 'https://bitbucket.org/user/repo/src/master/docs/file.rst')
+        self.assertEqual(self.version.get_bitbucket_url(docroot='/foo/bar/', filename='file'), 'https://bitbucket.org/user/repo/src/master/foo/bar/file.rst')
 
         self.pip.repo = 'https://bitbucket.org/user/repo.git'
-        self.assertEqual(self.version.get_bitbucket_url('file'), 'https://bitbucket.org/user/repo/src/master/docs/file.rst')
+        self.assertEqual(self.version.get_bitbucket_url(docroot='/foo/bar/', filename='file'), 'https://bitbucket.org/user/repo/src/master/foo/bar/file.rst')
 
