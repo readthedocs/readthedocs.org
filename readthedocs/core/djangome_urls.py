2c3c5a18c59e7265f411de678ecddba95557cb09
Eric Holscher <eric@ericholscher.com>
2013-12-08 17:39:19 -0500

    Commit full 404/500 pages.

--- readthedocs/core/djangome_urls.py
+++ readthedocs/core/djangome_urls.py
@@ -1,11 +1,8 @@
 from django.conf.urls.defaults import patterns, url
 
-ALL_VERSIONS_RE = '(?P<version>.+)'
-
 urlpatterns = patterns(
     '',  # base view, flake8 complains if it is on the previous line.
     url('^$',
         'djangome.views.redirect_home',
         {'version': 'latest'}),
-    # Disable django.me integration for now.
 )
