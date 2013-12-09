6f5f1811ccf82d4875c16ffd33c6331514c5b7f3
Daniel Oaks <danneh@danneh.net>
2013-12-10 06:57:13 +1000

    Added bitbucket.org urls, since they're the canonical ones anyway

--- readthedocs/builds/utils.py
+++ readthedocs/builds/utils.py
@@ -10,6 +10,9 @@ BB_REGEXS = [
     re.compile('bitbucket.com/(.+)/(.+)/'),
     re.compile('bitbucket.com/(.+)/(.+)'),
     re.compile('bitbucket.com:(.+)/(.+)\.git'),
+    re.compile('bitbucket.org/(.+)/(.+)/'),
+    re.compile('bitbucket.org/(.+)/(.+)'),
+    re.compile('bitbucket.org:(.+)/(.+)\.git'),
 ]
 
 def get_github_username_repo(version):
