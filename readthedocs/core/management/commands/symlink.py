5d7c98840d00f855846fc8a3d7d9090a8bf29e01
Eric Holscher <eric@ericholscher.com>
2014-01-04 16:02:46 -0500

    Modify symlink command to allow generating all symlinks

--- readthedocs/core/management/commands/symlink.py
+++ readthedocs/core/management/commands/symlink.py
@@ -1,19 +1,31 @@
 import logging
 
 from django.core.management.base import BaseCommand
+from django.conf import settings
 
 from projects import tasks
 from tastyapi import apiv2 as api
 
+import redis
+
 log = logging.getLogger(__name__)
+
+def symlink(slug):
+    version_data = api.version().get(project=slug, slug='latest')['results'][0]
+    v = tasks.make_api_version(version_data)
+    log.info("Symlinking %s" % v)
+    tasks.symlink_subprojects(v)
+    tasks.symlink_cnames(v)
+    tasks.symlink_translations(v)
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         if len(args):
             for slug in args:
-                version_data = api.version().get(project=slug, slug='latest')['results'][0]
-                v = tasks.make_api_version(version_data)
-                log.info("Symlinking %s" % v)
-                tasks.symlink_subprojects(v)
-                tasks.symlink_cnames(v)
-                tasks.symlink_translations(v)
+                symlink(slug)
+        else:
+            redis_conn = redis.Redis(**settings.REDIS)
+            slugs = redis_conn.keys('rtd_slug:v1:*')
+            for slug in slugs:
+                log.info("Got slug from redis: %s" % slug)
+                symlink(slug)
