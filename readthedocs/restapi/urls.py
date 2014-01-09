9643a7d27ab5365c3c5e6fbca80412dc9028ac0b
Eric Holscher <eric@ericholscher.com>
2014-01-08 23:37:24 -0500

    Rejigger out restapi views into separate modules.

--- readthedocs/restapi/urls.py
+++ readthedocs/restapi/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import url, patterns, include
 
 from rest_framework import routers
 
-from .views import ProjectViewSet, NotificationViewSet, VersionViewSet
+from .views.model_views import ProjectViewSet, NotificationViewSet, VersionViewSet
 
 router = routers.DefaultRouter()
 router.register(r'version', VersionViewSet)
@@ -12,10 +12,10 @@ router.register(r'notification', NotificationViewSet)
 urlpatterns = patterns(
     '',
 	url(r'^', include(router.urls)),
-    url(r'footer_html/', 'restapi.views.footer_html', name='footer_html'),
-    url(r'quick_search/', 'restapi.views.quick_search', name='quick_search'),
-    url(r'index_search/', 'restapi.views.index_search', name='index_search'),
-    url(r'search/$', 'restapi.views.search', name='search'),
-    url(r'search/project/$', 'restapi.views.project_search', name='project_search'),
-    url(r'search/section/$', 'restapi.views.section_search', name='section_search'),
+    url(r'footer_html/', 'restapi.views.footer_views.footer_html', name='footer_html'),
+    url(r'quick_search/', 'restapi.views.search_views.quick_search', name='quick_search'),
+    url(r'index_search/', 'restapi.views.search_views.index_search', name='index_search'),
+    url(r'search/$', 'restapi.views.search_views.search', name='search'),
+    url(r'search/project/$', 'restapi.views.search_views.project_search', name='project_search'),
+    url(r'search/section/$', 'restapi.views.search_views.section_search', name='section_search'),
 )
