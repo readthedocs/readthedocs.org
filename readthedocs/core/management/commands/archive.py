558e887e57687ffab937bc1e77df7b16720a3ff3
Eric Holscher <eric@ericholscher.com>
2014-01-18 22:33:54 +0700

    Properly use the MEDIA_URL

--- readthedocs/core/management/commands/archive.py
+++ readthedocs/core/management/commands/archive.py
@@ -27,6 +27,7 @@ class Command(BaseCommand):
 
 		context = Context({
 			'doc_index': doc_index,
+			'MEDIA_URL': settings.MEDIA_URL,
 		})
 		html = template_loader.get_template('archive/index.html').render(context)
 		print html
