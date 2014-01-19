8f6fc271b871151dbbe3285151557bbc0eae9762
Eric Holscher <eric@ericholscher.com>
2014-01-19 07:51:18 +0700

    Update theme JS

--- media/javascript/readthedocs-doc-embed.js
+++ media/javascript/readthedocs-doc-embed.js
@@ -71,7 +71,38 @@ $(document).ready(function () {
     });  
     // Make tables responsive
     $("table.docutils:not(.field-list)").wrap("<div class='wy-table-responsive'></div>");
-    
+
+    window.SphinxRtdTheme = (function (jquery) {
+        var stickyNav = (function () {
+            var navBar,
+                win,
+                stickyNavCssClass = 'stickynav',
+                applyStickNav = function () {
+                    if (navBar.height() <= win.height()) {
+                        navBar.addClass(stickyNavCssClass);
+                    } else {
+                        navBar.removeClass(stickyNavCssClass);
+                    }
+                },
+                enable = function () {
+                    applyStickNav();
+                    win.on('resize', applyStickNav);
+                },
+                init = function () {
+                    navBar = jquery('nav.wy-nav-side:first');
+                    win    = jquery(window);
+                };
+            jquery(init);
+            return {
+                enable : enable
+            };
+        }());
+        return {
+            StickyNav : stickyNav
+        };
+    }($));
+
+
     /// Add Grok the Docs Client
 
     $.ajax({
