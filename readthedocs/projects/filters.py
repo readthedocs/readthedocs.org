e6ca4d1decd272a778deeec2649dda46789ef7fd
Eric Holscher <eric@ericholscher.com>
2014-02-08 17:04:38 +0800

    Handle sorting of slug based on sanity. This should really be easier.

--- readthedocs/projects/filters.py
+++ readthedocs/projects/filters.py
@@ -11,12 +11,26 @@ ANY_REPO = (
 
 REPO_CHOICES = ANY_REPO + constants.REPO_CHOICES
 
+def sort_slug(queryset, query):
+  queryset = queryset.filter(slug__icontains=query)
+  ret = []
+  ret.extend([q.pk for q in queryset if q.slug == query])
+  ret.extend([q.pk for q in queryset if q.slug.startswith(query) and q.pk not in ret])
+  ret.extend([q.pk for q in queryset if q.slug.endswith(query) and q.pk not in ret])
+  ret.extend([q.pk for q in queryset if q.pk not in ret])
+
+  # Create a QS preserving ordering
+  # http://blog.mathieu-leplatre.info/django-create-a-queryset-from-a-list-preserving-order.html
+  clauses = ' '.join(['WHEN id=%s THEN %s' % (pk, i) for i, pk in enumerate(ret)])
+  ordering = 'CASE %s END' % clauses
+  ret_queryset = Project.objects.filter(pk__in=ret).extra(
+             select={'ordering': ordering}, order_by=('ordering',))
+  return ret_queryset
 
 class ProjectFilter(django_filters.FilterSet):
     name = django_filters.CharFilter(label=_("Name"), name='name',
                                      lookup_type='icontains')
-    slug = django_filters.CharFilter(label=_("Slug"), name='slug',
-                                     lookup_type='icontains')
+    slug = django_filters.CharFilter(label=_("Slug"), name='slug', action=sort_slug)
     pub_date = django_filters.DateRangeFilter(label=_("Created Date"),
                                               name="pub_date")
     repo = django_filters.CharFilter(label=_("Repository URL"), name='repo',
