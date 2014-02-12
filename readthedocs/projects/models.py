6a309a336e05911ea1bb580c0ad4ed9814dd0cee
Eric Holscher <eric@ericholscher.com>
2014-02-12 18:28:04 +0800

    Scarier warning for single version

--- readthedocs/projects/models.py
+++ readthedocs/projects/models.py
@@ -129,7 +129,7 @@ class Project(models.Model):
                               default='.rst')
     single_version = models.BooleanField(
         _('Single version'), default=False,
-        help_text=_('A single version site has no translations and only your "latest" version, served at the root of the domain.'))
+        help_text=_('A single version site has no translations and only your "latest" version, served at the root of the domain. Use this with caution, only turn it on if you will <b>never</b> have multiple versions of your docs.'))
     default_version = models.CharField(
         _('Default version'), max_length=255, default='latest',
         help_text=_('The version of your project that / redirects to'))
