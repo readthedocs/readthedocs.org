300218ef18a87871322523077b9606cff600b80a
Eric Holscher <eric@ericholscher.com>
2014-02-15 20:03:49 +0800

    Move zip html logic into single local media builder.

--- readthedocs/doc_builder/backends/sphinx.py
+++ readthedocs/doc_builder/backends/sphinx.py
@@ -187,32 +187,9 @@ class Builder(BaseBuilder):
             build_command = ("sphinx-build %s -b readthedocs -D language=%s . _build/html"
                              % (force_str, project.language))
         build_results = run(build_command, shell=True)
-        #self._zip_html()
         if 'no targets are out of date.' in build_results[1]:
             self._changed = False
         return build_results
-
-    @restoring_chdir
-    def _zip_html(self, **kwargs):
-        from_path = self.version.project.full_build_path(self.version.slug)
-        to_path = self.version.project.checkout_path(self.version.slug)
-        to_file = os.path.join(to_path, '%s.zip' % self.version.project.slug)
-
-        log.info("Creating zip file from %s" % from_path)
-        # Create a <slug>.zip file containing all files in file_path
-        os.chdir(from_path)
-        archive = zipfile.ZipFile(to_file, 'w')
-        for root, subfolders, files in os.walk('.'):
-            for file in files:
-                to_write = os.path.join(root, file)
-                archive.write(
-                    filename=to_write,
-                    arcname=os.path.join("%s-%s" % (self.version.project.slug,
-                                                    self.version.slug),
-                                         to_write)
-                )
-        archive.close()
-        return to_file
 
     def move(self, **kwargs):
         project = self.version.project
--- readthedocs/doc_builder/backends/sphinx_singlehtml.py
+++ readthedocs/doc_builder/backends/sphinx_singlehtml.py
@@ -15,10 +15,10 @@ class Builder(HtmlBuilder):
         project = self.version.project
         os.chdir(self.version.project.conf_dir(self.version.slug))
         if project.use_virtualenv:
-            build_command = '%s -b readthedocssinglehtml -D language=%s . _build/html' % (project.venv_bin(
+            build_command = '%s -b readthedocssinglehtml -D language=%s . _build/singlehtml' % (project.venv_bin(
                 version=self.version.slug, bin='sphinx-build'), project.language)
         else:
-            build_command = "sphinx-build -D language=%s -b readthedocssinglehtml . _build/html" % project.language
+            build_command = "sphinx-build -D language=%s -b readthedocssinglehtml . _build/singlehtml" % project.language
         build_results = run(build_command)
         #self._zip_html()
         if 'no targets are out of date.' in build_results[1]:
@@ -32,12 +32,34 @@ class LocalMediaBuilder(HtmlBuilder):
         project = self.version.project
         os.chdir(self.version.project.conf_dir(self.version.slug))
         if project.use_virtualenv:
-            build_command = '%s -b singlehtml -D language=%s . _build/html' % (project.venv_bin(
+            build_command = '%s -b readthedocssinglehtmllocalmedia -D language=%s . _build/singlehtml' % (project.venv_bin(
                 version=self.version.slug, bin='sphinx-build'), project.language)
         else:
-            build_command = "sphinx-build -D language=%s -b singlehtml . _build/html" % project.language
+            build_command = "sphinx-build -D language=%s -b readthedocssinglehtmllocalmedia . _build/singlehtml" % project.language
         build_results = run(build_command)
         self._zip_html()
         if 'no targets are out of date.' in build_results[1]:
             self._changed = False
         return build_results
+
+    @restoring_chdir
+    def _zip_html(self, **kwargs):
+        from_path = self.version.project.full_singlehtml_path(self.version.slug)
+        to_path = self.version.project.checkout_path(self.version.slug)
+        to_file = os.path.join(to_path, '%s.zip' % self.version.project.slug)
+
+        log.info("Creating zip file from %s" % from_path)
+        # Create a <slug>.zip file containing all files in file_path
+        os.chdir(from_path)
+        archive = zipfile.ZipFile(to_file, 'w')
+        for root, subfolders, files in os.walk('.'):
+            for file in files:
+                to_write = os.path.join(root, file)
+                archive.write(
+                    filename=to_write,
+                    arcname=os.path.join("%s-%s" % (self.version.project.slug,
+                                                    self.version.slug),
+                                         to_write)
+                )
+        archive.close()
+        return to_file
