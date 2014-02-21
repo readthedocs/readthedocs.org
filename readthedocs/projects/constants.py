6512322f3af0550e1299ac6a66104689f86ec3bb
Eric Holscher <eric@ericholscher.com>
2014-02-21 11:15:04 +0800

    Add in all Sphinx supported languages. Refs #371

--- readthedocs/projects/constants.py
+++ readthedocs/projects/constants.py
@@ -114,27 +114,26 @@ LANGUAGES = (
     ("fr", "French"),
     ("hr", "Croatian"),
     ("hu", "Hungarian"),
+    ("id", "Indonesian"),
     ("it", "Italian"),
     ("ja", "Japanese"),
     ("ko", "Korean"),
     ("lt", "Lithuanian"),
     ("lv", "Latvian"),
+    ("mk", "Macedonian"),
     ("ne", "Nepali"),
     ("nl", "Dutch"),
     ("pl", "Polish"),
     ("ru", "Russian"),
+    ("si", "Sinhala"),
     ("sk", "Slovak"),
     ("sl", "Slovenian"),
     ("sv", "Swedish"),
     ("tr", "Turkish"),
-
-    # Comment out non-2 letter language names for now.
-    # Update this once we're done with the initial cutover.
-    #("nb_NO", "Norwegian Bokmal"),
-    #("pt_BR", "Brazilian Portuguese"),
-    #("uk_UA", "Ukrainian"),
-
     # Try these to test our non-2 letter language support
+    ("nb_NO", "Norwegian Bokmal"),
+    ("pt_BR", "Brazilian Portuguese"),
+    ("uk_UA", "Ukrainian"),
     ("zh_CN", "Simplified Chinese"),
     ("zh_TW", "Traditional Chinese"),
 )
