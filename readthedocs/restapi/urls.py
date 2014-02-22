d05d301bd0b4e19efbef1671c856bff162e1f488
Wraithan (Chris McDonald) <xwraithanx@gmail.com>
2014-02-21 17:03:47 -0800

    add build stats endpoint

--- readthedocs/restapi/urls.py
+++ readthedocs/restapi/urls.py
@@ -11,11 +11,12 @@ router.register(r'notification', NotificationViewSet)
 
 urlpatterns = patterns(
     '',
-	url(r'^', include(router.urls)),
+    url(r'^', include(router.urls)),
     url(r'footer_html/', 'restapi.views.footer_views.footer_html', name='footer_html'),
     url(r'quick_search/', 'restapi.views.search_views.quick_search', name='quick_search'),
     url(r'index_search/', 'restapi.views.search_views.index_search', name='index_search'),
     url(r'search/$', 'restapi.views.search_views.search', name='search'),
     url(r'search/project/$', 'restapi.views.search_views.project_search', name='project_search'),
     url(r'search/section/$', 'restapi.views.search_views.section_search', name='section_search'),
+    url(r'stats/builds/$', 'restapi.views.stats_views.build_stats', name='build_stats'),
 )
