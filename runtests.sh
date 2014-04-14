c1a2dd2fbf3d6132ae815b149f4cd3bf8ab1e807
Paul Collins <paul.collins.iii@gmail.com>
2014-04-14 14:59:26 -0400

    Make run tests find all the tests

--- runtests.sh
+++ runtests.sh
@@ -1,5 +1,5 @@
 cd readthedocs
-DJANGO_SETTINGS_MODULE=settings.test ./manage.py test rtd_tests --logging-clear-handlers
+DJANGO_SETTINGS_MODULE=settings.test ./manage.py test --logging-clear-handlers ./
 exit=$?
 cd -
 exit $exit
