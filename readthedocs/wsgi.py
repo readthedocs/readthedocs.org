a813d79ccd63c9ff40afaf3fda4e2c8c0a37ee25
Eric Holscher <eric@ericholscher.com>
2014-07-31 15:24:08 -0700

    Add wsgi file

---
+++ readthedocs/wsgi.py
@@ -0,0 +1,13 @@
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+
+# This application object is used by any WSGI server configured to use this
+# file. This includes Django's development server, if the WSGI_APPLICATION
+# setting points here.
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+
+# Apply WSGI middleware here.
+# from helloworld.wsgi import HelloWorldApplication
+# application = HelloWorldApplication(application)
