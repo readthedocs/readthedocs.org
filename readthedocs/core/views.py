4c680ec7847f7aa45c88a909100ed694707b68f3
Eric Holscher <eric@ericholscher.com>
2014-07-31 16:08:54 -0700

    Show latest builds a bit more sanely

--- readthedocs/core/views.py
+++ readthedocs/core/views.py
@@ -37,10 +37,13 @@ class NoProjectException(Exception):
     pass
 
 def homepage(request):
-    latest = (Project.objects.public(request.user)
-              .order_by('-modified_date')[:10])
+    #latest = Project.objects.public(request.user).order_by('-modified_date')[:10]
+    latest_builds = Build.objects.order_by('-date')[:100]
+    latest = set()
+    for build in latest_builds:
+        if build.project not in latest and len(latest) < 10:
+            latest.add(build.project)
     featured = Project.objects.filter(featured=True)
-
     return render_to_response('homepage.html',
                               {'project_list': latest,
                                'featured_list': featured},
