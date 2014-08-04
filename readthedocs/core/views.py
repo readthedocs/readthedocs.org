68a1dfddb01e233e42474f76ea43ec72e377b6a8
Eric Holscher <eric@ericholscher.com>
2014-08-04 16:46:23 -0700

    Remove pdb and fix non-existent variable.

--- readthedocs/core/views.py
+++ readthedocs/core/views.py
@@ -477,14 +477,15 @@ def server_error_404(request, template_name='404.html'):
         project_slug = request.slug
     elif full_path.startswith('/docs/'):
         project_slug = full_path.split('/')[2]
-    else:
-        project_slug = None
 
-    if project_slug:
-        project = get_object_or_404(Project, slug=project_slug)
+    try:
+        project = Project.objects.get(slug=project_slug)
+    except Project.DoesNotExist:
+        project = None
+
+    if project:
         for redirect in project.redirects.all():
             if redirect.redirect_type == 'prefix':
-                import ipdb; ipdb.set_trace()
                 if full_path.startswith(redirect.from_url):
                     log.debug('Redirecting %s' % redirect)
                     cut_path = re.sub('^%s' % redirect.from_url, '', full_path)
