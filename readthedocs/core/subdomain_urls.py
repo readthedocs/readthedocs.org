b57b7049ae42be710aa7a610d45e72823cafb6fe
Eric Holscher <eric@ericholscher.com>
2014-08-05 15:20:36 -0700

    Include custom 404/500 in subdomains.

--- readthedocs/core/subdomain_urls.py
+++ readthedocs/core/subdomain_urls.py
@@ -3,6 +3,8 @@ from django.conf.urls import url, patterns
 from projects.constants import LANGUAGES_REGEX
 from urls import urlpatterns as main_patterns
 
+handler500 = 'core.views.server_error'
+handler404 = 'core.views.server_error_404'
 
 urlpatterns = patterns(
     '',  # base view, flake8 complains if it is on the previous line.
