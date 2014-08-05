7cd80ea25a7ff6b5b9dd5149b471fe020cad306e
Eric Holscher <eric@ericholscher.com>
2014-08-05 15:21:02 -0700

    Add PYTHON_MEDIA setting so we can test media Python response.

--- readthedocs/core/views.py
+++ readthedocs/core/views.py
@@ -453,7 +453,7 @@ def serve_docs(request, lang_slug, version_slug, filename, project_slug=None):
 
     # Serve file
     log.info('Serving %s for %s' % (filename, proj))
-    if not settings.DEBUG:
+    if not settings.DEBUG and not getattr(settings, 'PYTHON_MEDIA', False):
         fullpath = os.path.join(basepath, filename)
         mimetype, encoding = mimetypes.guess_type(fullpath)
         mimetype = mimetype or 'application/octet-stream'
