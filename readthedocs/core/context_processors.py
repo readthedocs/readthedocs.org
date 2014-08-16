eae14402570f2c6b9fae17043c000d715e624dac
Eric Holscher <eric@ericholscher.com>
2014-08-16 13:10:15 -0700

    Add global analytics code as setting

--- readthedocs/core/context_processors.py
+++ readthedocs/core/context_processors.py
@@ -1,9 +1,9 @@
 from django.conf import settings
 
-
 def readthedocs_processor(request):
     exports = {
         'PRODUCTION_DOMAIN': getattr(settings, 'PRODUCTION_DOMAIN', None),
         'USE_SUBDOMAINS': getattr(settings, 'USE_SUBDOMAINS', None),
+        'GLOBAL_ANALYTICS_CODE': getattr(settings, 'GLOBAL_ANALYTICS_CODE', 'UA-17997319-1'),
     }
     return exports
