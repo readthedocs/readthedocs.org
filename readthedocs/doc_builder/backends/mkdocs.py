b057bca832a700807fcc76d84ba8e73dff6a12a0
Eric Holscher <eric@ericholscher.com>
2014-08-18 12:02:46 -0700

    Add analytics to mkdocs builder, and switch back to canonical.

--- readthedocs/doc_builder/backends/mkdocs.py
+++ readthedocs/doc_builder/backends/mkdocs.py
@@ -4,13 +4,16 @@ import json
 import yaml
 
 from django.conf import settings
+from django.template import Template, Context, loader as template_loader
 
 from doc_builder.base import BaseBuilder, restoring_chdir
 from projects.utils import run
 
 log = logging.getLogger(__name__)
 
+
 class Builder(BaseBuilder):
+
     """
     Mkdocs builder
     """
@@ -18,7 +21,8 @@ class Builder(BaseBuilder):
 
     def __init__(self, *args, **kwargs):
         super(Builder, self).__init__(*args, **kwargs)
-        self.old_artifact_path = os.path.join(self.version.project.checkout_path(self.version.slug), 'site')
+        self.old_artifact_path = os.path.join(
+            self.version.project.checkout_path(self.version.slug), 'site')
 
     @restoring_chdir
     def build(self, **kwargs):
@@ -26,6 +30,7 @@ class Builder(BaseBuilder):
         os.chdir(project.checkout_path(self.version.slug))
         user_config = yaml.safe_load(open('mkdocs.yml', 'r'))
         docs_dir = user_config.get('docs_dir', 'docs')
+        
         READTHEDOCS_DATA = {
             'project': project.slug,
             'version': self.version.slug,
@@ -36,16 +41,27 @@ class Builder(BaseBuilder):
             'source_suffix': ".md",
             'api_host': getattr(settings, 'SLUMBER_API_HOST', 'https://readthedocs.org'),
         }
+        rtd_json = json.dumps(READTHEDOCS_DATA, indent=4)
+        rtd_ctx = Context({
+            'rtd_json': rtd_json,
+            'analytics_code': project.analytics_code,
+        })
+        rtd_string = template_loader.get_template(
+            'doc_builder/data.js.tmpl'
+        ).render(rtd_ctx)
+
         data_file = open(os.path.join(docs_dir, 'data.js'), 'w+')
-        data_file.write("var READTHEDOCS_DATA = ")
-        json.dump(READTHEDOCS_DATA, data_file)
+        data_file.write(rtd_string)
         data_file.close()
 
-        MEDIA_URL = getattr(settings, 'MEDIA_URL', 'https://media.readthedocs.org')
+        MEDIA_URL = getattr(
+            settings, 'MEDIA_URL', 'https://media.readthedocs.org')
         if 'extra_javascript' in user_config:
-            user_config['extra_javascript'].append('%sjavascript/jquery/jquery-2.0.3.min.js' % MEDIA_URL)
+            user_config['extra_javascript'].append(
+                '%sjavascript/jquery/jquery-2.0.3.min.js' % MEDIA_URL)
             user_config['extra_javascript'].append('data.js')
-            user_config['extra_javascript'].append('%sjavascript/readthedocs-doc-embed.js' % MEDIA_URL)
+            user_config['extra_javascript'].append(
+                '%sjavascript/readthedocs-doc-embed.js' % MEDIA_URL)
         else:
             user_config['extra_javascript'] = [
                 '%sjavascript/jquery/jquery-2.0.3.min.js' % MEDIA_URL,
@@ -53,8 +69,10 @@ class Builder(BaseBuilder):
                 '%sjavascript/readthedocs-doc-embed.js' % MEDIA_URL,
             ]
         if 'extra_css' in user_config:
-            user_config['extra_css'].append('https://media.readthedocs.org/css/badge_only.css')
-            user_config['extra_css'].append('https://media.readthedocs.org/css/readthedocs-doc-embed.css')
+            user_config['extra_css'].append(
+                'https://media.readthedocs.org/css/badge_only.css')
+            user_config['extra_css'].append(
+                'https://media.readthedocs.org/css/readthedocs-doc-embed.css')
         else:
             user_config['extra_css'] = [
                 'https://media.readthedocs.org/css/badge_only.css',
@@ -65,7 +83,7 @@ class Builder(BaseBuilder):
             build_command = "%s build --theme=readthedocs" % (
                 project.venv_bin(version=self.version.slug,
                                  bin='mkdocs')
-                )
+            )
         else:
             build_command = "mkdocs build --theme=readthedocs"
         results = run(build_command, shell=True)
