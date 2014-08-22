6b1ec9b088287a7f37e32e5672528cc75c4ca11b
Eric Holscher <eric@ericholscher.com>
2014-08-22 13:50:34 -0700

    Clean up help strings for redirects

--- readthedocs/redirects/models.py
+++ readthedocs/redirects/models.py
@@ -22,10 +22,10 @@ TYPE_CHOICES = (
 )
 
 from_url_helptext = _('Absolute path, excluding the domain.'
-                      'Example: <b>/docs/</b> .'
+                      'Example: <b>/docs/</b>  or <b>/install.html</b>.'
                       )
 to_url_helptext = _('Absolute or relative url. Examples: '
-                    '\'http://www.example.com/$path\', \'/en/latest/$path\'. '
+                    '<b>/tutorial/install.html.</b>'
                     )
 redirect_type_helptext = _('The type of redirect you wish to use.')
 
