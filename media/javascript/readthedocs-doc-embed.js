14cdfae09a3336b329ea5c09bfd9007f2daaa363
Eric Holscher <eric@ericholscher.com>
2014-08-25 14:58:48 -0700

    Add out-of-date message back to javascript.

--- media/javascript/readthedocs-doc-embed.js
+++ media/javascript/readthedocs-doc-embed.js
@@ -117,6 +117,36 @@ $(document).ready(function () {
     });
 
 
+    /// Out of date message
+
+      var versionURL = [API_HOST + "/api/v1/version/", READTHEDOCS_DATA['project'],
+                        "/highest/", READTHEDOCS_DATA['version'], "/?callback=?"].join("");
+
+      $.getJSON(versionURL, onData);
+
+      function onData (data) {
+        if (data.is_highest) {
+          return;
+        }
+
+        var currentURL = window.location.pathname.replace(READTHEDOCS_DATA['version'], data.slug),
+            warning = $('<div class="admonition warning"> <p class="first \
+                         admonition-title">Note</p> <p class="last"> \
+                         You are not using the most up to date version \
+                         of the library. <a href="#"></a> is the newest version.</p>\
+                         </div>');
+
+        warning
+          .find('a')
+          .attr('href', currentURL)
+          .text(data.version);
+
+        body = $("div.body")
+        if (!body.length) {
+          body = $("div.document")
+        }
+        body.prepend(warning);
+      }
 
     /// Search
     /// Here be dragons, this is beta quality code. Beware.
