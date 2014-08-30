c1d421d65478592932f326a2b3b0bff18592e0c2
Anthony Johnson <aj@ohess.org>
2014-08-30 01:48:49 -0700

    Fix test suite script

--- runtests.sh
+++ runtests.sh
@@ -1,8 +1,15 @@
-cd readthedocs
-rm -rf rtd_tests/builds/
-export PYTHONPATH=`pwd`:$PYTHONPATH
-export DJANGO_SETTINGS_MODULE=settings.test
-coverage run -m py.test $*
-exit=$?
-cd -
-exit $exit
+#!/bin/sh
+
+(
+    python_path=`pwd`
+    if [ -z "${PYTHON_PATH}" ]; then
+        python_path="${python_path}:${PYTHONPATH}"
+    fi
+    export PYTHONPATH=$python_path
+    export DJANGO_SETTINGS_MODULE=readthedocsinc.settings.test
+
+    cd readthedocs
+    rm -rf rtd_tests/builds/
+
+    coverage run -m py.test $*
+)
