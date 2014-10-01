a9e6aa9a74af4a2d50c2d444bb613df41f579540
Joe Alcorn <joealcorn123@gmail.com>
2014-10-01 11:37:10 +0100

    Add unstyled BUILD_QUEUE_DEPTH warning

--- readthedocs/core/context_processors.py
+++ readthedocs/core/context_processors.py
@@ -1,7 +1,11 @@
 from django.conf import settings
+from redis import Redis
+
+redis = Redis(**settings.REDIS)
 
 def readthedocs_processor(request):
     exports = {
+        'BUILD_QUEUE_DEPTH': redis.llen('celery'),
         'PRODUCTION_DOMAIN': getattr(settings, 'PRODUCTION_DOMAIN', None),
         'USE_SUBDOMAINS': getattr(settings, 'USE_SUBDOMAINS', None),
         'GLOBAL_ANALYTICS_CODE': getattr(settings, 'GLOBAL_ANALYTICS_CODE', 'UA-17997319-1'),
