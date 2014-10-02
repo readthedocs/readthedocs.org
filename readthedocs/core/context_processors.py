77736a1665d88458c65058d7d62bf636944023f7
Joe Alcorn <joealcorn123@gmail.com>
2014-10-02 23:04:12 +0100

    Catch a redis ConnectionError when checking queue length

--- readthedocs/core/context_processors.py
+++ readthedocs/core/context_processors.py
@@ -1,11 +1,16 @@
 from django.conf import settings
-from redis import Redis
+from redis import Redis, ConnectionError
 
 redis = Redis(**settings.REDIS)
 
 def readthedocs_processor(request):
+    try:
+        queue_length = redis.llen('celery')
+    except ConnectionError:
+        queue_length = None
+
     exports = {
-        'BUILD_QUEUE_DEPTH': redis.llen('celery'),
+        'BUILD_QUEUE_DEPTH': queue_length,
         'PRODUCTION_DOMAIN': getattr(settings, 'PRODUCTION_DOMAIN', None),
         'USE_SUBDOMAINS': getattr(settings, 'USE_SUBDOMAINS', None),
         'GLOBAL_ANALYTICS_CODE': getattr(settings, 'GLOBAL_ANALYTICS_CODE', 'UA-17997319-1'),
