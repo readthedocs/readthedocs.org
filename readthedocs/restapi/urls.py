bcc18b2589d64020febea9d2f18d1b3d58cae441
Eric Holscher <eric@ericholscher.com>
2014-10-16 08:29:57 -0700

    Rename api search

--- readthedocs/restapi/urls.py
+++ readthedocs/restapi/urls.py
@@ -16,7 +16,7 @@ urlpatterns = patterns(
     url(r'footer_html/', 'restapi.views.footer_views.footer_html', name='footer_html'),
     url(r'quick_search/', 'restapi.views.search_views.quick_search', name='quick_search'),
     url(r'index_search/', 'restapi.views.search_views.index_search', name='index_search'),
-    url(r'search/$', 'restapi.views.search_views.search', name='search'),
-    url(r'search/project/$', 'restapi.views.search_views.project_search', name='project_search'),
-    url(r'search/section/$', 'restapi.views.search_views.section_search', name='section_search'),
+    url(r'search/$', 'restapi.views.search_views.search', name='api_search'),
+    url(r'search/project/$', 'restapi.views.search_views.project_search', name='api_project_search'),
+    url(r'search/section/$', 'restapi.views.search_views.section_search', name='api_section_search'),
 )
