e9dd4cef41611c339913e539a247c20a93ce6d01
Eric Holscher <eric@ericholscher.com>
2014-10-16 08:25:35 -0700

    404 on missing version for footer

--- readthedocs/restapi/views/footer_views.py
+++ readthedocs/restapi/views/footer_views.py
@@ -23,7 +23,7 @@ def footer_html(request):
     new_theme = (theme == "sphinx_rtd_theme")
     using_theme = (theme == "default")
     project = get_object_or_404(Project, slug=project_slug)
-    version = project.versions.get(slug=version_slug)
+    version = get_object_or_404(project.versions.all(), slug=version_slug)
     main_project = project.main_language_project or project
 
     if page_slug and page_slug != "index":
