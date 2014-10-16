cd599444433fd32f989fa4f61a3b19f773b12f0e
Eric Holscher <eric@ericholscher.com>
2014-10-16 08:05:37 -0700

    Allow email in profile urls

--- readthedocs/profiles/urls/public.py
+++ readthedocs/profiles/urls/public.py
@@ -4,7 +4,7 @@ from profiles import views
 
 
 urlpatterns = patterns('',
-                       url(r'^(?P<username>[\w.-]+)/$',
+                       url(r'^(?P<username>[\w@.-]+)/$',
                            views.profile_detail,
                            {'template_name': 'profiles/public/profile_detail.html'},
                            name='profiles_profile_detail'),
