b8da66bc2fdc50f5027b844b53f3094e4225ab92
Eric Holscher <eric@ericholscher.com>
2014-10-15 17:41:13 -0700

    Specify versions to use local packages w/ a pypi fallback.

--- readthedocs/projects/tasks.py
+++ readthedocs/projects/tasks.py
@@ -405,8 +405,8 @@ def setup_environment(version):
     wheeldir = os.path.join(settings.SITE_ROOT, 'deploy', 'wheels')
     ret_dict['doc_builder'] = run(
         (
-            '{cmd} install --no-index --use-wheel --find-links={wheeldir} -U {ignore_option} '
-            'sphinx virtualenv setuptools docutils readthedocs-sphinx-ext mkdocs mock pillow'
+            '{cmd} install --use-wheel --find-links={wheeldir} -U {ignore_option} '
+            'sphinx==1.2.2 virtualenv==1.10.1 setuptools==1.1 docutils==0.11 readthedocs-sphinx-ext==0.4.3 mkdocs==0.9 mock==1.0.1 pillow==2.6.1'
         ).format(
             cmd=project.venv_bin(version=version.slug, bin='pip'),
             ignore_option=ignore_option,
