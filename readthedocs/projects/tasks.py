f543b35aaee9b9955d485911e6506ec5768dcee8
Eric Holscher <eric@ericholscher.com>
2014-10-30 12:43:50 -0700

    Add mkdocs 0.10

--- readthedocs/projects/tasks.py
+++ readthedocs/projects/tasks.py
@@ -312,7 +312,7 @@ def setup_environment(version):
     ret_dict['doc_builder'] = run(
         (
             '{cmd} install --use-wheel --find-links={wheeldir} -U {ignore_option} '
-            'sphinx==1.2.2 virtualenv==1.10.1 setuptools==1.1 docutils==0.11 readthedocs-sphinx-ext==0.4.3 mkdocs==0.9 mock==1.0.1 pillow==2.6.1'
+            'sphinx==1.2.2 virtualenv==1.10.1 setuptools==1.1 docutils==0.11 readthedocs-sphinx-ext==0.4.3 mkdocs==0.10 mock==1.0.1 pillow==2.6.1'
         ).format(
             cmd=project.venv_bin(version=version.slug, bin='pip'),
             ignore_option=ignore_option,
