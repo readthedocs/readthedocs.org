37d3bf8d4a9c6fe7cdbe00ae1373eec511ac5b02
Eric Holscher <eric@ericholscher.com>
2014-11-19 13:50:04 -0800

    Update to using mkdocs 0.11. Fixes #1032

--- readthedocs/projects/tasks.py
+++ readthedocs/projects/tasks.py
@@ -282,7 +282,7 @@ def setup_environment(version):
     ret_dict['doc_builder'] = run(
         (
             '{cmd} install --use-wheel --find-links={wheeldir} -U {ignore_option} '
-            'sphinx==1.2.2 virtualenv==1.10.1 setuptools==1.1 docutils==0.11 readthedocs-sphinx-ext==0.4.4 mkdocs==0.10 mock==1.0.1 pillow==2.6.1'
+            'sphinx==1.2.2 virtualenv==1.10.1 setuptools==1.1 docutils==0.11 readthedocs-sphinx-ext==0.4.4 mkdocs==0.11 mock==1.0.1 pillow==2.6.1'
         ).format(
             cmd=project.venv_bin(version=version.slug, bin='pip'),
             ignore_option=ignore_option,
