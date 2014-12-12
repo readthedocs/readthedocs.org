b39db786b73cc00676d35cd14b42c70d63b21ba3
Eric Holscher <eric@ericholscher.com>
2014-12-12 14:20:42 -0800

    Fix version sorting to make latest and stable first.

--- readthedocs/projects/templatetags/projects_tags.py
+++ readthedocs/projects/templatetags/projects_tags.py
@@ -1,6 +1,20 @@
 from django import template
 
+from distutils2.version import NormalizedVersion
+from projects.utils import mkversion
+
 register = template.Library()
+
+
+def make_version(version):
+    ver = mkversion(version)
+    if not ver:
+        if version.slug == 'latest':
+            return NormalizedVersion('99999.0', error_on_huge_major_num=False)
+        elif version.slug == 'stable':
+            return NormalizedVersion('9999.0', error_on_huge_major_num=False)
+        else:
+            return NormalizedVersion('999.0', error_on_huge_major_num=False)
 
 
 @register.filter
@@ -8,12 +22,10 @@ def sort_version_aware(versions):
     """
     Takes a list of versions objects and sort them caring about version schemes
     """
-    from distutils2.version import NormalizedVersion
-    from projects.utils import mkversion
-    fallback = NormalizedVersion('99999999.0', error_on_huge_major_num=False)
-    return sorted(versions,
-                  key=lambda v: (mkversion(v) or fallback),
-                  reverse=True)
+    sorted_verisons = sorted(versions,
+                             key=make_version,
+                             reverse=True)
+    return sorted_verisons
 
 
 @register.filter
