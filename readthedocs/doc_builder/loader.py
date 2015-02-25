07aca8e96d5e93edb684d0c4684ef8f837e8fc58
Eric Holscher <eric@ericholscher.com>
2015-02-25 11:46:55 -0800

    Use comment builder for dirhtml too

--- readthedocs/doc_builder/loader.py
+++ readthedocs/doc_builder/loader.py
@@ -8,7 +8,7 @@ sphinx = import_module(getattr(settings, 'SPHINX_BACKEND', 'doc_builder.backends
 loading = {
     # Possible HTML Builders
     'sphinx': sphinx.HtmlBuilderComments,
-    'sphinx_htmldir': sphinx.HtmlDirBuilder,
+    'sphinx_htmldir': sphinx.HtmlDirBuilderComments,
     'sphinx_singlehtml': sphinx.SingleHtmlBuilder,
     # Other Sphinx Builders
     'sphinx_pdf': sphinx.PdfBuilder,
