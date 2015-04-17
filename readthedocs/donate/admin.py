0dcd5d1b5de5aeeeb653ea6cb6fdc04a931518b6
Eric Holscher <eric@ericholscher.com>
2015-04-16 17:39:50 -0700

    SPeed up donate admin

--- readthedocs/donate/admin.py
+++ readthedocs/donate/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import Supporter
 
-admin.site.register(Supporter)
+class SupporterAdmin(admin.ModelAdmin):
+    model = Supporter
+    raw_id_fields = ('user',)
+    list_display = ('name', 'email', 'dollars', 'public')
+    list_filter = ('name', 'email', 'dollars', 'public')
+
+admin.site.register(Supporter, SupporterAdmin)
+
