b2cefcc3f24e7e3c496f507459e4bff4d07e98cf
Gregor Müllegger <gregor@muellegger.de>
2015-06-15 09:41:24 +0200

    Adding tests for checking if pdf/epub flag is respected in builds.

--- readthedocs/rtd_tests/tests/test_builds.py
+++ readthedocs/rtd_tests/tests/test_builds.py
@@ -70,6 +70,7 @@ class BuildTests(TestCase):
 
         conf_path = os.path.join(project.checkout_path(version.slug), project.conf_py_file)
 
+        # Mock open to simulate existing conf.py file
         with mock.patch('codecs.open', mock.mock_open(), create=True):
             with fake_paths(conf_path):
                 built_docs = build_docs(version,
@@ -107,3 +108,99 @@ class BuildTests(TestCase):
         builder_class = get_builder_class(project.documentation_type)
         builder = builder_class(version)
         self.assertEqual(builder.sphinx_builder, 'readthedocs')
+
+    @mock.patch('slumber.Resource')
+    @mock.patch('os.chdir')
+    @mock.patch('subprocess.Popen')
+    @mock.patch('doc_builder.backends.sphinx.HtmlBuilder.build')
+    @mock.patch('doc_builder.backends.sphinx.PdfBuilder.build')
+    @mock.patch('doc_builder.backends.sphinx.EpubBuilder.build')
+    def test_build_respects_pdf_flag(self,
+                                     EpubBuilder_build,
+                                     PdfBuilder_build,
+                                     HtmlBuilder_build,
+                                     mock_Popen,
+                                     mock_chdir,
+                                     mock_apiv2_downloads):
+
+        # subprocess mock logic
+
+        mock_process = mock.Mock()
+        process_return_dict = {'communicate.return_value': ('SOMEGITHASH', '')}
+        mock_process.configure_mock(**process_return_dict)
+        mock_Popen.return_value = mock_process
+        mock_Popen.side_effect = build_subprocess_side_effect
+
+        project = ProjectFactory(
+            sphinx_enable_pdf_build=True,
+            sphinx_enable_epub_build=False)
+        version = project.versions.all()[0]
+
+        conf_path = os.path.join(project.checkout_path(version.slug), project.conf_py_file)
+
+        # Mock open to simulate existing conf.py file
+        with mock.patch('codecs.open', mock.mock_open(), create=True):
+            with fake_paths(conf_path):
+                built_docs = build_docs(version,
+                                        False,
+                                        False,
+                                        False,
+                                        False,
+                                        False,
+                                        False,
+                                        False,
+                                        )
+
+        # The HTML and the PDF format were built.
+        self.assertEqual(HtmlBuilder_build.call_count, 1)
+        self.assertEqual(PdfBuilder_build.call_count, 1)
+        # Epub however was disabled and therefore not built.
+        self.assertEqual(EpubBuilder_build.call_count, 0)
+
+    @mock.patch('slumber.Resource')
+    @mock.patch('os.chdir')
+    @mock.patch('subprocess.Popen')
+    @mock.patch('doc_builder.backends.sphinx.HtmlBuilder.build')
+    @mock.patch('doc_builder.backends.sphinx.PdfBuilder.build')
+    @mock.patch('doc_builder.backends.sphinx.EpubBuilder.build')
+    def test_build_respects_epub_flag(self,
+                                      EpubBuilder_build,
+                                      PdfBuilder_build,
+                                      HtmlBuilder_build,
+                                      mock_Popen,
+                                      mock_chdir,
+                                      mock_apiv2_downloads):
+
+        # subprocess mock logic
+
+        mock_process = mock.Mock()
+        process_return_dict = {'communicate.return_value': ('SOMEGITHASH', '')}
+        mock_process.configure_mock(**process_return_dict)
+        mock_Popen.return_value = mock_process
+        mock_Popen.side_effect = build_subprocess_side_effect
+
+        project = ProjectFactory(
+            sphinx_enable_pdf_build=False,
+            sphinx_enable_epub_build=True)
+        version = project.versions.all()[0]
+
+        conf_path = os.path.join(project.checkout_path(version.slug), project.conf_py_file)
+
+        # Mock open to simulate existing conf.py file
+        with mock.patch('codecs.open', mock.mock_open(), create=True):
+            with fake_paths(conf_path):
+                built_docs = build_docs(version,
+                                        False,
+                                        False,
+                                        False,
+                                        False,
+                                        False,
+                                        False,
+                                        False,
+                                        )
+
+        # The HTML and the Epub format were built.
+        self.assertEqual(HtmlBuilder_build.call_count, 1)
+        self.assertEqual(EpubBuilder_build.call_count, 1)
+        # PDF however was disabled and therefore not built.
+        self.assertEqual(PdfBuilder_build.call_count, 0)
