8ebec2efc308f9d4aefe9b0627d19b66a4027baa
Gregor Müllegger <gregor@muellegger.de>
2015-07-17 14:31:51 +0200

    Make github/bitbucket repo syncing async in the frontend
    We are using a ajax request to the API to trigger the update and then monitor
    the progress. When the sync job has finished, we update the page.
    
    Also removing the synchronous sync code from the urls and view logic.

--- media/javascript/rtd-import.js
+++ media/javascript/rtd-import.js
@@ -31,6 +31,72 @@
 
       repo.val(type);
     });
+
+    $('[data-sync-repositories]').each(function () {
+      var $button = $(this);
+      var target = $(this).attr('data-target');
+
+      $button.on('click', function () {
+        var url = $button.attr('data-sync-repositories');
+        $.ajax({
+          method: 'POST',
+          url: url,
+          success: function (data) {
+            $button.attr('disabled', true);
+            watchProgress(data.url);
+          },
+          error: function () {
+            onError();
+          }
+        });
+        $('.sync-repositories').addClass('hide');
+        $('.sync-repositories-progress').removeClass('hide');
+      });
+
+      function watchProgress(url) {
+        setTimeout(function () {
+          $.ajax({
+            method: 'GET',
+            url: url,
+            success: function (data) {
+              if (data.finished) {
+                if (data.success) {
+                  onSuccess();
+                } else {
+                  onError();
+                }
+              } else {
+                watchProgress(url);
+              }
+            },
+            error: function () {
+              watchProgress(url);
+            }
+          });
+        }, 2000);
+      }
+
+      function onSuccess(url) {
+        $.ajax({
+          method: 'GET',
+          url: window.location.href,
+          success: function (data) {
+            var $newContent = $(data).find(target);
+            $('body').find(target).replaceWith($newContent);
+            $('.sync-repositories').addClass('hide');
+            $('.sync-repositories-progress').addClass('hide');
+            $('.sync-repositories-success').removeClass('hide');
+          },
+          error: onError
+        });
+      }
+
+      function onError() {
+        $('.sync-repositories').addClass('hide');
+        $('.sync-repositories-progress').addClass('hide');
+        $('.sync-repositories-error').removeClass('hide');
+      }
+    });
   });
 
 })();
