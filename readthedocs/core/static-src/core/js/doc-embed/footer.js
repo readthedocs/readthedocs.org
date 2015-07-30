88a4ddbe64567630998a594d3295e5c4de3d1d9d
Gregor Müllegger <gregor@muellegger.de>
2015-07-30 12:10:07 +0200

    Refactoring embed footer js

--- readthedocs/core/static-src/core/js/doc-embed/footer.js
+++ readthedocs/core/static-src/core/js/doc-embed/footer.js
@@ -3,7 +3,6 @@ var versionCompare = require('./version-compare');
 
 
 function init(build) {
-
     var get_data = {
         project: rtd['project'],
         version: rtd['version'],
@@ -11,7 +10,6 @@ function init(build) {
         theme: rtd['theme'],
         format: "jsonp",
     };
-
 
     // Crappy heuristic, but people change the theme name on us.
     // So we have to do some duck typing.
@@ -27,80 +25,72 @@ function init(build) {
         get_data['subproject'] = true;
     }
 
-    // Theme popout code
+    // Get footer HTML from API and inject it into the page.
     $.ajax({
         url: rtd.api_host + "/api/v2/footer_html/",
         crossDomain: true,
         xhrFields: {
-        withCredentials: true,
+            withCredentials: true,
         },
         dataType: "jsonp",
         data: get_data,
         success: function (data) {
             versionCompare.init(data.version_compare);
-
-            // If the theme looks like ours, update the existing badge
-            // otherwise throw a a full one into the page.
-            if (build.is_rtd_theme()) {
-                $("div.rst-other-versions").html(data['html']);
-            } else {
-                $("body").append(data['html']);
-            }
-
-            if (!data['version_active']) {
-                $('.rst-current-version').addClass('rst-out-of-date');
-            } else if (!data['version_supported']) {
-                //$('.rst-current-version').addClass('rst-active-old-version')
-            }
-
-            // Show promo selectively
-            if (data.promo && build.show_promo()) {
-                var promo = new sponsorship.Promo(
-                    data.promo_data.id,
-                    data.promo_data.text,
-                    data.promo_data.link,
-                    data.promo_data.image
-                )
-                if (promo) {
-                    promo.display();
-                }
-            }
-
-            // using jQuery
-            function getCookie(name) {
-                var cookieValue = null;
-                if (document.cookie && document.cookie !== '') {
-                    var cookies = document.cookie.split(';');
-                    for (var i = 0; i < cookies.length; i++) {
-                        var cookie = jQuery.trim(cookies[i]);
-                        // Does this cookie string begin with the name we want?
-                        if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                            cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                            break;
-                        }
-                    }
-                }
-                return cookieValue;
-            }
-
-                function csrfSafeMethod(method) {
-                    // these HTTP methods do not require CSRF protection
-                    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
-                }
-                $.ajaxSetup({
-                    beforeSend: function(xhr, settings) {
-                        if (!csrfSafeMethod(settings.type)) {
-                            xhr.setRequestHeader("X-CSRFToken", $('a.bookmark[token]').attr('token'));
-                        }
-                    }
-                });
+            injectFooter(data);
+            setupBookmarkCSRFToken();
         },
         error: function () {
-            console.log('Error loading Read the Docs footer');
+            console.error('Error loading Read the Docs footer');
         }
     });
-
 }
+
+
+function injectFooter(data) {
+    // If the theme looks like ours, update the existing badge
+    // otherwise throw a a full one into the page.
+    if (build.is_rtd_theme()) {
+        $("div.rst-other-versions").html(data['html']);
+    } else {
+        $("body").append(data['html']);
+    }
+
+    if (!data['version_active']) {
+        $('.rst-current-version').addClass('rst-out-of-date');
+    } else if (!data['version_supported']) {
+        //$('.rst-current-version').addClass('rst-active-old-version')
+    }
+
+    // Show promo selectively
+    if (data.promo && build.show_promo()) {
+        var promo = new sponsorship.Promo(
+            data.promo_data.id,
+            data.promo_data.text,
+            data.promo_data.link,
+            data.promo_data.image
+        )
+        if (promo) {
+            promo.display();
+        }
+    }
+}
+
+
+function setupBookmarkCSRFToken() {
+    function csrfSafeMethod(method) {
+        // these HTTP methods do not require CSRF protection
+        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+    }
+
+    $.ajaxSetup({
+        beforeSend: function(xhr, settings) {
+            if (!csrfSafeMethod(settings.type)) {
+                xhr.setRequestHeader("X-CSRFToken", $('a.bookmark[token]').attr('token'));
+            }
+        }
+    });
+}
+
 
 module.exports = {
     init: init
