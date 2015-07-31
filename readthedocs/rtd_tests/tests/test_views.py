readthedocs/readthedocs.org

commit c5bed3d43c1d0d47221bdb0515fc9876a5de145d

Adjusting url kwargs in urls.py to be consistent. Using project_id_or_slug instead of pk for generic_build and using project_slug instead of project for random_page.

readthedocs/core/views.py
@@ -65,12 +65,12 @@ class HomepageView(DonateProgressMixin, TemplateView):
         return context
 
 
-def random_page(request, project=None):
-    imp_file = ImportedFile.objects.order_by('?')
+def random_page(request, project_slug=None):
+    imported_file = ImportedFile.objects.order_by('?')
     if project:
-        return HttpResponseRedirect((imp_file.filter(project__slug=project)[0]
-                                             .get_absolute_url()))
-    return HttpResponseRedirect(imp_file[0].get_absolute_url())
+        imported_file = imported_file.filter(project__slug=project_slug)
+    url = imported_file.first().get_absolute_url()
+    return HttpResponseRedirect(url)
 
 
 def queue_depth(request):
@@ -275,17 +275,19 @@ def bitbucket_build(request):
 
 
 @csrf_exempt
-def generic_build(request, pk=None):
+def generic_build(request, project_id_or_slug=None):
     try:
-        project = Project.objects.get(pk=pk)
+        project = Project.objects.get(pk=project_id_or_slug)
     # Allow slugs too
     except (Project.DoesNotExist, ValueError):
         try:
-            project = Project.objects.get(slug=pk)
+            project = Project.objects.get(slug=project_id_or_slug)
         except (Project.DoesNotExist, ValueError):
             pc_log.error(
-                "(Incoming Generic Build) Repo not found:  %s" % pk)
-            return HttpResponseNotFound('Repo not found: %s' % pk)
+                "(Incoming Generic Build) Repo not found:  %s" % (
+                    project_id_or_slug))
+            return HttpResponseNotFound(
+                'Repo not found: %s' % project_id_or_slug)
     if request.method == 'POST':
         slug = request.POST.get('version_slug', None)
         if slug:

readthedocs/urls.py
@@ -82,10 +82,10 @@ urlpatterns = patterns(
     url(r'^dashboard/', include('projects.urls.private')),
     url(r'^github', 'core.views.github_build', name='github_build'),
     url(r'^bitbucket', 'core.views.bitbucket_build', name='bitbucket_build'),
-    url(r'^build/(?P<pk>[-\w]+)',
+    url(r'^build/(?P<project_id_or_slug>[-\w]+)',
         'core.views.generic_build',
         name='generic_build'),
-    url(r'^random/(?P<project>[\w-]+)',
+    url(r'^random/(?P<project_slug>[\w-]+)',
         'core.views.random_page',
         name='random_page'),
     url(r'^random/$', 'core.views.random_page', name='random_page'),

commit 6ebfef41b554796d2fe97899faf25799fb890ced

Fix undeclared variable in core.views.random_page
I broke it in c5bed3d4 :( Now I'm fixing it and adding tests.
Was revealed by pylint.

readthedocs/core/views.py
@@ -67,9 +67,12 @@ class HomepageView(DonateProgressMixin, TemplateView):
 
 def random_page(request, project_slug=None):
     imported_file = ImportedFile.objects.order_by('?')
-    if project:
+    if project_slug:
         imported_file = imported_file.filter(project__slug=project_slug)
-    url = imported_file.first().get_absolute_url()
+    imported_file = imported_file.first()
+    if imported_file is None:
+        raise Http404
+    url = imported_file.get_absolute_url()
     return HttpResponseRedirect(url)
 
 

readthedocs/rtd_tests/tests/test_views.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 from django.utils.six.moves.urllib.parse import urlsplit
 
 from readthedocs.builds.constants import LATEST
+from readthedocs.projects.models import ImportedFile
 from readthedocs.projects.models import Project
 from readthedocs.projects.forms import UpdateProjectForm
 
@@ -168,3 +169,37 @@ class PrivateViewsAreProtectedTests(TestCase):
     def test_project_redirects_delete(self):
         response = self.client.get('/dashboard/pip/redirects/delete/')
         self.assertRedirectToLogin(response)
+
+
+class RandomPageTests(TestCase):
+    fixtures = ['eric', 'test_data']
+
+    def setUp(self):
+        self.pip = Project.objects.get(slug='pip')
+        self.pip_version = self.pip.versions.all()[0]
+        ImportedFile.objects.create(
+            project=self.pip,
+            version=self.pip_version,
+            name='File',
+            slug='file',
+            path='file.html',
+            md5='abcdef',
+            commit='1234567890abcdef')
+
+    def test_random_page_view_redirects(self):
+        response = self.client.get('/random/')
+        self.assertEqual(response.status_code, 302)
+
+    def test_takes_project_slug(self):
+        response = self.client.get('/random/pip/')
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue('/pip/' in response['Location'])
+
+    def test_404_for_unknown_project(self):
+        response = self.client.get('/random/not-existent/')
+        self.assertEqual(response.status_code, 404)
+
+    def test_404_for_with_no_imported_files(self):
+        ImportedFile.objects.all().delete()
+        response = self.client.get('/random/pip/')
+        self.assertEqual(response.status_code, 404)