3b7756135a00c6d92e743bea272e1277a805dbdf
Eric Holscher <eric@ericholscher.com>
2015-07-31 12:13:28 -0700

    Add views/forms for adding & editing Gold projects

--- readthedocs/gold/views.py
+++ readthedocs/gold/views.py
@@ -4,14 +4,15 @@ from django.core.urlresolvers import reverse
 from django.conf import settings
 from django.db import IntegrityError
 from django.http import HttpResponseRedirect
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
 
 import stripe
 
-from .forms import CardForm
+from .forms import CardForm, GoldProjectForm
 from .models import GoldUser
+from readthedocs.projects.models import Project
 
 stripe.api_key = settings.STRIPE_SECRET
 
@@ -140,3 +141,47 @@ def thanks(request):
         },
         context_instance=RequestContext(request)
     )
+
+
+@login_required
+def projects(request):
+    try:
+        gold_user = GoldUser.objects.get(user=request.user)
+        gold_projects = gold_user.projects.all()
+    except:
+        raise Exception('Not Found')
+
+    if request.method == 'POST':
+        form = GoldProjectForm(request.POST)
+        if form.is_valid():
+            if gold_projects.count() < gold_user.num_supported_projects:
+                to_add = Project.objects.get(slug=form.cleaned_data['project'])
+                gold_user.projects.add(to_add)
+                return HttpResponseRedirect(reverse('gold_projects'))
+            else:
+                form.add_error(None, 'You already have the max number of supported projects.')
+    else:
+        form = GoldProjectForm()
+
+    return render_to_response(
+        'gold/projects.html',
+        {
+            'form': form,
+            'gold_user': gold_user,
+            'publishable': settings.STRIPE_PUBLISHABLE,
+            'user': request.user,
+            'projects': gold_projects
+        },
+        context_instance=RequestContext(request)
+    )
+
+
+@login_required
+def projects_remove(request, project_slug):
+    try:
+        gold_user = GoldUser.objects.get(user=request.user)
+    except:
+        raise Exception('Not Found')
+    project = get_object_or_404(Project.objects.all(), slug=project_slug)
+    gold_user.projects.remove(project)
+    return HttpResponseRedirect(reverse('gold_projects'))
