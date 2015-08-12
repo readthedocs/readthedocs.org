9595beac70fe94d1137cc41d6a7270da33817b11
Eric Holscher <eric@ericholscher.com>
2015-08-12 15:11:31 -0700

    Clean up spacing

--- readthedocs/oauth/utils.py
+++ readthedocs/oauth/utils.py
@@ -118,33 +118,35 @@ def import_github(user, sync):
 
 
 def add_github_webhook(session, project):
-        owner, repo = build_utils.get_github_username_repo(url=project.repo)
-        data = json.dumps({
-            'name': 'readthedocs',
-            'active': True,
-            'config': {'url': 'https://{domain}/github'.format(domain=settings.PRODUCTION_DOMAIN)}
-        })
-        resp = session.post(
-            'https://api.github.com/repos/{owner}/{repo}/hooks'.format(owner=owner, repo=repo),
-            data=data,
-            headers={'content-type': 'application/json'}
-        )
-        log.info("Creating GitHub webhook response code: {code}".format(code=resp.status_code))
-        return resp
+    owner, repo = build_utils.get_github_username_repo(url=project.repo)
+    data = json.dumps({
+        'name': 'readthedocs',
+        'active': True,
+        'config': {'url': 'https://{domain}/github'.format(domain=settings.PRODUCTION_DOMAIN)}
+    })
+    resp = session.post(
+        'https://api.github.com/repos/{owner}/{repo}/hooks'.format(owner=owner, repo=repo),
+        data=data,
+        headers={'content-type': 'application/json'}
+    )
+    log.info("Creating GitHub webhook response code: {code}".format(code=resp.status_code))
+    return resp
 
 
 def add_bitbucket_webhook(session, project):
-        owner, repo = build_utils.get_bitbucket_username_repo(url=project.repo)
-        data = {
-            'type': 'POST',
-            'url': 'https://{domain}/bitbucket'.format(domain=settings.PRODUCTION_DOMAIN),
-        }
-        resp = session.post(
-            'https://api.bitbucket.org/1.0/repositories/{owner}/{repo}/services'.format(owner=owner, repo=repo),
-            data=data,
-        )
-        log.info("Creating BitBucket webhook response code: {code}".format(code=resp.status_code))
-        return resp
+    owner, repo = build_utils.get_bitbucket_username_repo(url=project.repo)
+    data = {
+        'type': 'POST',
+        'url': 'https://{domain}/bitbucket'.format(domain=settings.PRODUCTION_DOMAIN),
+    }
+    resp = session.post(
+        'https://api.bitbucket.org/1.0/repositories/{owner}/{repo}/services'.format(
+            owner=owner, repo=repo
+        ),
+        data=data,
+    )
+    log.info("Creating BitBucket webhook response code: {code}".format(code=resp.status_code))
+    return resp
 
 ###
 # Bitbucket
