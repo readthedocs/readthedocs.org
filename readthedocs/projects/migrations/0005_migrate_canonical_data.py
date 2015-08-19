6b6eaa286b90c9b5dbaaaa78f0ce6fcff7050aab
Eric Holscher <eric@ericholscher.com>
2015-08-19 12:14:19 -0700

    Add data migration

---
+++ readthedocs/projects/migrations/0005_migrate_canonical_data.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+
+
+def migrate_canonical(apps, schema_editor):
+    Project = apps.get_model("projects", "Project")
+    for project in Project.objects.all():
+        if project.canonical_url:
+            domain = project.domains.create(
+                url=project.canonical_url,
+                canonical=True,
+                active=True,
+            )
+            print "Added {url} to {project}".format(url=domain.url, project=project.name)
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('projects', '0004_add_cname_modeling'),
+    ]
+
+    operations = [
+        migrations.RunPython(migrate_canonical)
+    ]
