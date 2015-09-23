bf49fe20e6eb291e669506620318d76bf6fe86f5
Anthony Johnson <aj@ohess.org>
2015-09-23 16:23:58 -0700

    Fix import fields on oauth models

--- readthedocs/oauth/managers.py
+++ readthedocs/oauth/managers.py
@@ -1,8 +1,11 @@
 import logging
 import json
+import re
 
 from django.conf import settings
 from django.db import models
+
+from .constants import OAUTH_SOURCE_GITHUB, OAUTH_SOURCE_BITBUCKET
 
 
 logger = logging.getLogger(__name__)
@@ -12,6 +15,8 @@ DEFAULT_PRIVACY_LEVEL = getattr(settings, 'DEFAULT_PRIVACY_LEVEL', 'public')
 
 
 class OAuthRepositoryManager(models.Manager):
+
+    """Model managers for remote repositories"""
 
     def create_from_github_api(self, api_json, user, organization=None,
                         privacy=DEFAULT_PRIVACY_LEVEL):
@@ -35,6 +40,11 @@ class OAuthRepositoryManager(models.Manager):
             project.clone_url = api_json['git_url']
             project.ssh_url = api_json['ssh_url']
             project.html_url = api_json['html_url']
+            project.private = api_json['private']
+            project.admin = api_json.get('permissions', {}).get('admin', False)
+            project.vcs = 'git'
+            project.source = OAUTH_SOURCE_GITHUB
+            project.avatar_url = api_json.get('owner', {}).get('avatar_url')
             project.json = json.dumps(api_json)
             project.save()
             return project
@@ -62,6 +72,12 @@ class OAuthRepositoryManager(models.Manager):
             project.ssh_url = api_json['links']['clone'][1]['href']
             project.html_url = api_json['links']['html']['href']
             project.vcs = api_json['scm']
+            project.private = api_json['is_private']
+            project.source = OAUTH_SOURCE_BITBUCKET
+
+            avatar_url = api_json['links']['avatar']['href'] or ''
+            project.avatar_url = re.sub(r'\/16\/$', r'/32/', avatar_url)
+
             project.json = json.dumps(api_json)
             project.save()
             return project
@@ -77,6 +93,7 @@ class OAuthOrganizationManager(models.Manager):
         organization.html_url = api_json.get('html_url')
         organization.name = api_json.get('name')
         organization.email = api_json.get('email')
+        organization.avatar_url = api_json.get('avatar_url')
         organization.json = json.dumps(api_json)
         organization.users.add(user)
         organization.save()
