40c2691f75f558df1ba4f56323a2261a9b9251cf
Eric Holscher <eric@ericholscher.com>
2015-09-23 17:45:37 -0700

    Use existing clear_artifacts for version deactivating.

--- readthedocs/builds/forms.py
+++ readthedocs/builds/forms.py
@@ -1,8 +1,14 @@
+import logging
+
 from django import forms
 
 from readthedocs.builds.models import VersionAlias, Version
-from readthedocs.projects.models import Project
 from readthedocs.core.utils import trigger_build
+from readthedocs.projects.models import Project
+from readthedocs.projects.tasks import clear_artifacts
+
+
+log = logging.getLogger(__name__)
 
 
 class AliasForm(forms.ModelForm):
@@ -33,3 +39,11 @@ class VersionForm(forms.ModelForm):
         obj = super(VersionForm, self).save(*args, **kwargs)
         if obj.active and not obj.built and not obj.uploaded:
             trigger_build(project=obj.project, version=obj)
+
+    def clean(self):
+        cleaned_data = super(VersionForm, self).clean()
+        if self.instance.pk is not None:  # new instance only
+            if self.instance.active is True and cleaned_data['active'] is False:
+                log.info('Removing files for version %s' % self.instance.slug)
+                clear_artifacts.delay(version_pk=[self.instance.pk])
+        return cleaned_data
