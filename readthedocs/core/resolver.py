94748de747b61309a668108d990f653387a18ddf
Eric Holscher <eric@ericholscher.com>
2015-09-25 10:45:48 -0700

    Add resolver concept that will build URL’s

---
+++ readthedocs/core/resolver.py
@@ -0,0 +1,107 @@
+"""
+Read the Docs URL Resolver.
+
+Url Types:
+
+- Subproject
+- Subdomain
+- CNAME
+- Single Version
+- Normal
+
+All possible URL's::
+
+    Subdomain or CNAME:
+
+    /<lang>/<version>/<filename> # Default
+    /<filename> # Single Version
+    /projects/<subproject_slug>/<lang>/<version>/ # Subproject
+
+    Normal Serving:
+
+    /docs/<project_slug>/<lang>/<version>/<filename> # Default
+    /docs/<project_slug>/<filename> # Single Version
+    /docs/<project_slug>/projects/<subproject_slug>/<lang>/<version>/ # Subproject
+"""
+
+from django.conf import settings
+
+
+def base_resolve(project_slug, filename, version_slug=None, language=None,
+                 single_version=None, subproject_slug=None,  subdomain=None, cname=None):
+    """ Resolve a with nothing smart, just filling in the blanks."""
+    filename = filename.lstrip('/')
+
+    if subdomain or cname:
+        url = '/'
+    else:
+        url = '/docs/{project_slug}/'
+
+    if subproject_slug:
+        url += 'projects/{subproject_slug}/'
+
+    if single_version:
+        url += '{filename}'
+    else:
+        url += '{language}/{version_slug}/{filename}'
+
+    return url.format(
+        project_slug=project_slug, filename=filename,
+        version_slug=version_slug, language=language,
+        single_version=single_version, subproject_slug=subproject_slug,
+    )
+
+
+def resolve(project, filename='', version_slug=None, language=None,
+            single_version=None, subdomain=None, cname=None):
+    """ Resolve a URL with a subset of fields defined."""
+    subdomain = getattr(settings, 'USE_SUBDOMAIN', False)
+    relation = project.superprojects.first()
+    cname = cname or project.domains.first()
+
+    version_slug = version_slug or project.get_default_version()
+    language = language or project.language
+
+    if relation:
+        project_slug = relation.parent.slug
+        subproject_slug = relation.child.slug
+    else:
+        project_slug = project.slug
+        subproject_slug = None
+
+    if project.single_version or single_version:
+        single_version = True
+    else:
+        single_version = False
+
+    return base_resolve(project_slug=project_slug, filename=filename,
+                        version_slug=version_slug, language=language,
+                        single_version=single_version, subproject_slug=subproject_slug,
+                        subdomain=subdomain, cname=cname)
+
+
+def smart_resolve(project, filename=''):
+    """ Resolve a URL with all fields automatically filled in from the project."""
+    subdomain = getattr(settings, 'USE_SUBDOMAIN', False)
+    relation = project.superprojects.first()
+    cname = project.domains.first()
+
+    version_slug = project.get_default_version()
+    language = project.language
+
+    if relation:
+        project_slug = relation.parent.slug
+        subproject_slug = relation.child.slug
+    else:
+        project_slug = project.slug
+        subproject_slug = None
+
+    if project.single_version:
+        single_version = True
+    else:
+        single_version = False
+
+    return base_resolve(project_slug=project_slug, filename=filename,
+                        version_slug=version_slug, language=language,
+                        single_version=single_version, subproject_slug=subproject_slug,
+                        subdomain=subdomain, cname=cname)
---
+++ readthedocs/rtd_tests/tests/test_resolver.py
@@ -0,0 +1,140 @@
+from django.test import TestCase
+from django.test.utils import override_settings
+
+from readthedocs.projects.models import Project, Domain
+from readthedocs.rtd_tests.utils import create_user
+from readthedocs.core.resolver import base_resolve, resolve, smart_resolve
+
+from django_dynamic_fixture import get
+
+
+class SmartResolverTests(TestCase):
+
+    def setUp(self):
+        self.owner = create_user(username='owner', password='test')
+        self.tester = create_user(username='tester', password='test')
+        self.pip = get(Project, slug='pip', users=[self.owner])
+        self.subproject = get(Project, slug='sub', language='ja', users=[self.owner])
+        self.pip.add_subproject(self.subproject)
+        self.pip.translations.add(self.subproject)
+
+    def test_smart_resolver_filename(self):
+        with override_settings(USE_SUBDOMAIN=False):
+            url = smart_resolve(project=self.pip, filename='/foo/bar/blah.html')
+            self.assertEqual(url, '/docs/pip/en/latest/foo/bar/blah.html')
+        with override_settings(USE_SUBDOMAIN=True):
+            url = smart_resolve(project=self.pip, filename='/foo/bar/blah.html')
+            self.assertEqual(url, '/en/latest/foo/bar/blah.html')
+
+        with override_settings(USE_SUBDOMAIN=False):
+            url = smart_resolve(project=self.pip, filename='foo/bar/blah.html')
+            self.assertEqual(url, '/docs/pip/en/latest/foo/bar/blah.html')
+        with override_settings(USE_SUBDOMAIN=True):
+            url = smart_resolve(project=self.pip, filename='foo/bar/blah.html')
+            self.assertEqual(url, '/en/latest/foo/bar/blah.html')
+
+    def test_smart_resolver_subdomain(self):
+        with override_settings(USE_SUBDOMAIN=False):
+            url = smart_resolve(project=self.pip, filename='index.html')
+            self.assertEqual(url, '/docs/pip/en/latest/index.html')
+        with override_settings(USE_SUBDOMAIN=True):
+            url = smart_resolve(project=self.pip, filename='index.html')
+            self.assertEqual(url, '/en/latest/index.html')
+
+    def test_smart_resolver_domain_object(self):
+        self.domain = get(Domain, url='http://docs.foobar.com', project=self.pip)
+        with override_settings(USE_SUBDOMAIN=False):
+            url = smart_resolve(project=self.pip, filename='index.html')
+            self.assertEqual(url, '/en/latest/index.html')
+        with override_settings(USE_SUBDOMAIN=True):
+            url = smart_resolve(project=self.pip, filename='index.html')
+            self.assertEqual(url, '/en/latest/index.html')
+
+    def test_smart_resolver_subproject_subdomain(self):
+        with override_settings(USE_SUBDOMAIN=False):
+            url = smart_resolve(project=self.subproject, filename='index.html')
+            self.assertEqual(url, '/docs/pip/projects/sub/ja/latest/index.html')
+        with override_settings(USE_SUBDOMAIN=True):
+            url = smart_resolve(project=self.subproject, filename='index.html')
+            self.assertEqual(url, '/projects/sub/ja/latest/index.html')
+
+    def test_smart_resolver_subproject_single_version(self):
+        self.subproject.single_version = True
+        with override_settings(USE_SUBDOMAIN=False):
+            url = smart_resolve(project=self.subproject, filename='index.html')
+            self.assertEqual(url, '/docs/pip/projects/sub/index.html')
+        with override_settings(USE_SUBDOMAIN=True):
+            url = smart_resolve(project=self.subproject, filename='index.html')
+            self.assertEqual(url, '/projects/sub/index.html')
+
+    def test_smart_resolver_subproject_both_single_version(self):
+        self.pip.single_version = True
+        self.subproject.single_version = True
+        with override_settings(USE_SUBDOMAIN=False):
+            url = smart_resolve(project=self.subproject, filename='index.html')
+            self.assertEqual(url, '/docs/pip/projects/sub/index.html')
+        with override_settings(USE_SUBDOMAIN=True):
+            url = smart_resolve(project=self.subproject, filename='index.html')
+            self.assertEqual(url, '/projects/sub/index.html')
+
+
+class ResolverTests(TestCase):
+
+    def setUp(self):
+        self.owner = create_user(username='owner', password='test')
+        self.tester = create_user(username='tester', password='test')
+        self.pip = get(Project, slug='pip', users=[self.owner])
+        self.subproject = get(Project, slug='sub', language='ja', users=[self.owner])
+        self.pip.add_subproject(self.subproject)
+        self.pip.translations.add(self.subproject)
+
+    def test_resolver_force_single_version(self):
+        self.pip.single_version = False
+        with override_settings(USE_SUBDOMAIN=False):
+            url = resolve(project=self.pip, filename='index.html', single_version=True)
+            self.assertEqual(url, '/docs/pip/index.html')
+        with override_settings(USE_SUBDOMAIN=True):
+            url = resolve(project=self.pip, filename='index.html', single_version=True)
+            self.assertEqual(url, '/index.html')
+
+    def test_resolver_force_domain(self):
+        with override_settings(USE_SUBDOMAIN=False):
+            url = resolve(project=self.pip, filename='index.html', cname=True)
+            self.assertEqual(url, '/en/latest/index.html')
+        with override_settings(USE_SUBDOMAIN=True):
+            url = resolve(project=self.pip, filename='index.html', cname=True)
+            self.assertEqual(url, '/en/latest/index.html')
+
+    def test_resolver_force_domain_single_version(self):
+        self.pip.single_version = False
+        with override_settings(USE_SUBDOMAIN=False):
+            url = resolve(project=self.pip, filename='index.html', single_version=True, cname=True)
+            self.assertEqual(url, '/index.html')
+        with override_settings(USE_SUBDOMAIN=True):
+            url = resolve(project=self.pip, filename='index.html', single_version=True, cname=True)
+            self.assertEqual(url, '/index.html')
+
+    def test_resolver_force_language(self):
+        with override_settings(USE_SUBDOMAIN=False):
+            url = resolve(project=self.pip, filename='index.html', language='cz')
+            self.assertEqual(url, '/docs/pip/cz/latest/index.html')
+        with override_settings(USE_SUBDOMAIN=True):
+            url = resolve(project=self.pip, filename='index.html', language='cz')
+            self.assertEqual(url, '/cz/latest/index.html')
+
+    def test_resolver_force_version(self):
+        with override_settings(USE_SUBDOMAIN=False):
+            url = resolve(project=self.pip, filename='index.html', version_slug='foo')
+            self.assertEqual(url, '/docs/pip/en/foo/index.html')
+        with override_settings(USE_SUBDOMAIN=True):
+            url = resolve(project=self.pip, filename='index.html', version_slug='foo')
+            self.assertEqual(url, '/en/foo/index.html')
+
+    def test_resolver_force_language_version(self):
+        with override_settings(USE_SUBDOMAIN=False):
+            url = resolve(project=self.pip, filename='index.html', language='cz', version_slug='foo')
+            self.assertEqual(url, '/docs/pip/cz/foo/index.html')
+        with override_settings(USE_SUBDOMAIN=True):
+            url = resolve(project=self.pip, filename='index.html', language='cz', version_slug='foo')
+            self.assertEqual(url, '/cz/foo/index.html')
+
