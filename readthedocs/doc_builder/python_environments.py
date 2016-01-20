03eb41378e473d28903f652470a304f07bab71c6
Eric Holscher <eric@ericholscher.com>
2016-01-20 10:23:56 -0800

    Remove docutils pinning, and don't install mock

--- readthedocs/doc_builder/python_environments.py
+++ readthedocs/doc_builder/python_environments.py
@@ -189,8 +189,7 @@ class Conda(PythonEnvironment):
         requirements = [
             'sphinx==1.3.1',
             'Pygments==2.0.2',
-            'docutils==0.11',
-            'mock==1.0.1',
+            'docutils',
             'pillow==3.0.0',
             'sphinx_rtd_theme==0.1.7',
             'alabaster>=0.7,<0.8,!=0.7.5',
