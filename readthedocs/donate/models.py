f588f7698a44f262721e0437f5196a62af37bfa9
Eric Holscher <eric@ericholscher.com>
2016-04-01 14:49:18 -0700

    Comment about 0

--- readthedocs/donate/models.py
+++ readthedocs/donate/models.py
@@ -123,5 +123,5 @@ class SupporterImpressions(models.Model):
     def shown(self):
         """Return the percentage of times this ad was shown when offered."""
         if self.views == 0:
-            return 0
+            return 0  # Don't divide by 0
         return float(self.views) / float(self.offers) * 100.0
