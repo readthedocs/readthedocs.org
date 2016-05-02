29c957616567ca093eac971b9f6989701ad8b7d6
Eric Holscher <eric@ericholscher.com>
2016-05-02 10:44:15 -0700

    Serve media in subdomains during DEBUG

--- readthedocs/core/urls/subdomain.py
+++ readthedocs/core/urls/subdomain.py
@@ -1,11 +1,15 @@
+from operator import add
+
 from django.conf.urls import url, patterns
+from django.conf import settings
+from django.conf.urls.static import static
 
 from readthedocs.urls import urlpatterns as main_patterns
 
 handler500 = 'readthedocs.core.views.server_error'
 handler404 = 'readthedocs.core.views.server_error_404'
 
-urlpatterns = patterns(
+subdomain_urls = patterns(
     '',  # base view, flake8 complains if it is on the previous line.
     url(r'^page/(?P<filename>.*)$',
         'readthedocs.core.views.serve.redirect_page_with_filename',
@@ -15,4 +19,11 @@ urlpatterns = patterns(
     url(r'', 'readthedocs.core.views.serve.serve_symlink_docs', name='serve_symlink_docs'),
 )
 
-urlpatterns += main_patterns
+groups = [subdomain_urls]
+
+if getattr(settings, 'DEBUG', False):
+    groups.insert(0, static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
+
+groups.append(main_patterns)
+
+urlpatterns = reduce(add, groups)
