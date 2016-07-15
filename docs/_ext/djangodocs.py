9c8ead9b6099c7b8c9a561c0909a4ce9f0158ffc
Berker Peksag <berker.peksag@gmail.com>
2016-07-15 13:48:10 +0300

    Delete djangodocs extension
    It was added in d8fea873ca6f1a5197a02fed092e2434a139e6b7
    but:
    
    * only used in one place in the whole documentation:
      https://docs.readthedocs.io/en/latest/custom_installs/customization.html
    * and it doesn't work
    
    It would be nice to remove it to make conf.py simpler.

--- docs/_ext/djangodocs.py
+++
@@ -1,6 +0,0 @@
-def setup(app):
-    app.add_crossref_type(
-        directivename="setting",
-        rolename="setting",
-        indextemplate="pair: %s; setting",
-    )
--- docs/conf.py
+++ docs/conf.py
@@ -5,21 +5,10 @@ import sys
 
 from recommonmark.parser import CommonMarkParser
 
-sys.path.insert(0, os.path.abspath('..'))
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "readthedocs.settings.dev")
-
-from django.conf import settings
-
-import django
-django.setup()
-
-
-sys.path.append(os.path.abspath('_ext'))
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
     'sphinxcontrib.httpdomain',
-    'djangodocs',
 ]
 templates_path = ['_templates']
 
