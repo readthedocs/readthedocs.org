c716a9add203570f8c5d798ecd91c6a71a38b5fb
Ronak Khunt <ronak.khunt@nobroker.in>
2016-07-26 23:16:13 +0530

    Indentation issue fixed.

--- readthedocs/core/context_processors.py
+++ readthedocs/core/context_processors.py
@@ -4,7 +4,7 @@ from django.conf import settings
 def readthedocs_processor(request):
 
     exports = {
-    	'PUBLIC_DOMAIN': getattr(settings, 'PUBLIC_DOMAIN', None),
+        'PUBLIC_DOMAIN': getattr(settings, 'PUBLIC_DOMAIN', None),
         'PRODUCTION_DOMAIN': getattr(settings, 'PRODUCTION_DOMAIN', None),
         'USE_SUBDOMAINS': getattr(settings, 'USE_SUBDOMAINS', None),
         'GLOBAL_ANALYTICS_CODE': getattr(settings, 'GLOBAL_ANALYTICS_CODE', 'UA-17997319-1'),
