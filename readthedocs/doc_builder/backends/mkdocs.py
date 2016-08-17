3cbd5db581804424248e0e520159af2994d9aeaa
qqbuby <qqbuby@gmail.com>
2016-08-17 11:04:20 +0800

    Fix mkdocs.py

--- readthedocs/doc_builder/backends/mkdocs.py
+++ readthedocs/doc_builder/backends/mkdocs.py
@@ -59,7 +59,7 @@ class BaseMkdocs(BaseBuilder):
         # Mkdocs needs a full domain here because it tries to link to local media files
         if not media_url.startswith('http'):
             domain = getattr(settings, 'PRODUCTION_DOMAIN')
-            media_url = 'http://{}/{}'.format(domain, media_url)
+            media_url = 'http://{}{}'.format(domain, media_url) # media_url has already a prefix slash
 
         if 'extra_javascript' in user_config:
             user_config['extra_javascript'].append('readthedocs-data.js')
