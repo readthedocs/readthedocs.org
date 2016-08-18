ee2cad7a7f0566be8b6d86f7de9e045c3477decc
Eric Holscher <eric@ericholscher.com>
2016-08-18 13:43:57 -0700

    Properly map project to domain.

--- readthedocs/core/signals.py
+++ readthedocs/core/signals.py
@@ -2,7 +2,7 @@ from urlparse import urlparse
 
 from corsheaders import signals
 
-from readthedocs.projects.models import Domain
+from readthedocs.projects.models import Project, Domain
 
 
 WHITELIST_URLS = ['/api/v2/footer_html', '/api/v2/search']
@@ -10,12 +10,25 @@ WHITELIST_URLS = ['/api/v2/footer_html', '/api/v2/search']
 
 def handler(sender, request, **kwargs):
     host = urlparse(request.META['HTTP_ORIGIN']).netloc.split(':')[0]
-    domains = Domain.objects.filter(domain__icontains=host)
-    if domains.exist():
-        for url in WHITELIST_URLS:
-            if request.path_info.startswith(url):
-                return True
-    return False
+    valid_url = False
+    for url in WHITELIST_URLS:
+        if request.path_info.startswith(url):
+            valid_url = True
 
+    if valid_url:
+        project_slug = request.GET.get('project', None)
+        try:
+            project = Project.objects.get(slug=project_slug)
+        except Project.DoesNotExist:
+            return False
+
+        domain = Domain.objects.filter(
+            domain__icontains=host,
+            project=project
+        )
+        if domain.exists():
+            return True
+
+    return False
 
 signals.check_request_enabled.connect(handler)
