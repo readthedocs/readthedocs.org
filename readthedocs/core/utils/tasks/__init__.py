5f8d59646875d4e4aa75ec22a2ddc666c1802a23
Kamyar Rasta <kamyar.rasta@gmail.com>
2016-08-24 23:51:05 +0200

    Revert previous commit by adding missing imports

--- readthedocs/core/utils/tasks/__init__.py
+++ readthedocs/core/utils/tasks/__init__.py
@@ -1,4 +1,6 @@
 from .permission_checks import user_id_matches
+from .public import PublicTask
+from .public import TaskNoPermission
 from .public import permission_check
 from .public import get_public_task_data
 from .retrieve import TaskNotFound
