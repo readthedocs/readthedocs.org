5c637c803e576cc88202ab95998ac5305b63b2b7
krsion <ondrej.krsicka@gmail.com>
2017-05-10 21:59:43 +0200

    Select of amount is now defined in django form, not hardcoded in template. Amounts are defined in constants.py. Actual field is defined in SupporterForm in forms.py.

--- readthedocs/donate/constants.py
+++ readthedocs/donate/constants.py
@@ -32,3 +32,21 @@ THEMES = (
     (ALABASTER_THEME, 'Alabaster Theme'),
     (READTHEDOCS_THEME, 'Read the Docs Sphinx Theme'),
 )
+
+DOLLAR_AMOUNTS = [
+    ('custom', "Custom amount"),
+    (5, "$5"),
+    (10, "$10"),
+    (25, "$25"),
+    (50, "1 Hour ($50)"),
+    (100, "2 Hours ($100)"),
+    (200, "4 Hours ($200)"),
+    (400, "1 Day ($400)"),
+    (800, "2 Days ($800)"),
+    (1200, "3 Days ($1200)"),
+    (1600, "4 Days ($1600)"),
+    (2000, "5 Days ($2000)"),
+    (4000, "2 Weeks ($4000)"),
+    (6000, "3 Weeks ($6000)"),
+    (8000, "4 Weeks ($8000)"),
+]
