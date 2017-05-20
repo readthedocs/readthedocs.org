7f7a7c83bc32d8ecaf5a5a5ee0cfad8daeddc18d
Hank Gay <hank@rescuetime.com>
2017-05-20 11:57:06 -0700

    Use six for unittest assertion that was renamed in Py3.

--- readthedocs/rtd_tests/tests/test_project_views.py
+++ readthedocs/rtd_tests/tests/test_project_views.py
@@ -21,6 +21,8 @@ from readthedocs.projects.models import Project, Domain
 from readthedocs.projects.views.private import ImportWizardView
 from readthedocs.projects.views.mixins import ProjectRelationMixin
 
+import six
+
 
 @patch('readthedocs.projects.views.private.trigger_build', lambda x, basic: None)
 class TestProfileMiddleware(RequestFactoryTestMixin, TestCase):
@@ -166,10 +168,9 @@ class TestAdvancedForm(TestBasicsForm):
         data = self.step_data['basics']
         del data['advanced']
         del self.step_data['extra']['tags']
-        self.assertItemsEqual(
-            [tag.name for tag in proj.tags.all()],
-            [u'bar', u'baz', u'foo']
-        )
+        six.assertCountEqual(self,
+                             [tag.name for tag in proj.tags.all()],
+                             [u'bar', u'baz', u'foo'])
         data.update(self.step_data['extra'])
         for (key, val) in data.items():
             self.assertEqual(getattr(proj, key), val)
