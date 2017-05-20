4c9ede4fcc96154d921f715fee4a791900db0a03
Hank Gay <hank@rescuetime.com>
2017-05-19 18:43:20 -0700

    Use 'reduce' from six instead of assuming it's a global builtin.

--- readthedocs/core/urls/subdomain.py
+++ readthedocs/core/urls/subdomain.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, division, print_function
 
 from operator import add
+from six.moves import reduce
 
 from django.conf.urls import url, patterns
 from django.conf import settings
