b3a9337997484ea761a7d1c23e5d93dbe8f46826
shannon <smcoll@gmail.com>
2017-05-22 12:33:18 -0700

    linting for the api module

--- readthedocs/api/utils.py
+++ readthedocs/api/utils.py
@@ -1,3 +1,4 @@
+"""Utility classes for api module"""
 import logging
 
 from django.core.paginator import Paginator, InvalidPage
@@ -40,7 +41,7 @@ class SearchMixin(object):
             search_highlight = True
     """
 
-    def get_search(self, request, **kwargs):
+    def get_search(self, request):
         self.method_check(request, allowed=['get'])
         self.is_authenticated(request)
         self.throttle_check(request)
@@ -70,14 +71,15 @@ class SearchMixin(object):
         })
         return url_template + '?' + query_string
 
-    def _search(self, request, model, facets=None, page_size=20,
+    def _search(self, request, model, facets=None, page_size=20,  # pylint: disable=too-many-locals
                 highlight=True):
         """
-        `facets`
+        Return a paginated list of objects for a request.
 
+        `model`
+            Limit the search to a particular model
+        `facets`
             A list of facets to include with the results
-        `models`
-            Limit the search to one or more models
         """
         form = FacetedSearchForm(request.GET, facets=facets or [],
                                  models=(model,), load_all=True)
@@ -145,6 +147,9 @@ class SearchMixin(object):
 
 
 class PostAuthentication(BasicAuthentication):
+
+    """Require HTTP Basic authentication for any method other than GET."""
+
     def is_authenticated(self, request, **kwargs):
         val = super(PostAuthentication, self).is_authenticated(request,
                                                                **kwargs)
@@ -154,7 +159,7 @@ class PostAuthentication(BasicAuthentication):
 
 
 class EnhancedModelResource(ModelResource):
-    def obj_get_list(self, request=None, *args, **kwargs):
+    def obj_get_list(self, request=None, **kwargs):
         """
         A ORM-specific implementation of ``obj_get_list``.
 
