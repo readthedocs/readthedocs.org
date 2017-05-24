add3f4949632880a6f1ab979f5a1b301909004b6
Anthony Johnson <aj@ohess.org>
2017-05-24 16:45:25 -0700

    Lint fix

--- readthedocs/settings/dev.py
+++ readthedocs/settings/dev.py
@@ -52,7 +52,7 @@ class CommunityDevSettings(CommunityBaseSettings):
     )
 
     @property
-    def LOGGING(self):
+    def LOGGING(self):  # noqa - avoid pep8 N802
         logging = super(CommunityDevSettings, self).LOGGING
         logging['formatters']['default']['format'] = '[%(asctime)s] ' + self.LOG_FORMAT
         return logging
