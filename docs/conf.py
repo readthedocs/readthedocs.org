a6ba3573efb0881fe46d696494cffe3fe270b2ee
Anthony Johnson <aj@ohess.org>
2017-05-24 22:50:16 -0700

    Replace logo with wordmark

--- docs/conf.py
+++ docs/conf.py
@@ -68,7 +68,7 @@ gettext_compact = False
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-html_logo = 'img/logo.png'
+html_logo = 'img/logo.svg'
 html_theme_options = {
     'logo_only': True,
     'display_version': False,
