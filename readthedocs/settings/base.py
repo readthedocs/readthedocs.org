4a9059fadf3515063867a4e50a19cf1f089522f2
Eric Holscher <eric@ericholscher.com>
2017-05-25 15:10:39 -0700

    Remove last ref from settings

--- readthedocs/settings/base.py
+++ readthedocs/settings/base.py
@@ -325,7 +325,6 @@ class CommunityBaseSettings(Settings):
         'field_name_limit': 50,
     }
     REST_FRAMEWORK = {
-        # 'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
         'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
         'PAGE_SIZE': 10,
     }
