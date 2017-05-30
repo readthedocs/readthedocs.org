633877b42828ba3e8b501865bdfa4dbf0b1ad2f2
Hank Gay <hank@rescuetime.com>
2017-05-30 18:44:44 -0400

    Apply Eric's Unicode version-parsing fix.

--- readthedocs/projects/version_handling.py
+++ readthedocs/projects/version_handling.py
@@ -11,7 +11,6 @@ from packaging.version import InvalidVersion
 from readthedocs.builds.constants import LATEST_VERBOSE_NAME
 from readthedocs.builds.constants import STABLE_VERBOSE_NAME
 import six
-from six.moves import range
 
 
 def get_major(version):
@@ -114,7 +113,8 @@ def version_windows(versions, major=1, minor=1, point=1):
 def parse_version_failsafe(version_string):
     try:
         return Version(
-            unicodedata.normalize('NFKD', six.text_type(version_string)).encode('ascii', 'ignore')
+            six.text_type(
+                unicodedata.normalize('NFKD', six.text_type(version_string)))
         )
     except (UnicodeError, InvalidVersion):
         return None
