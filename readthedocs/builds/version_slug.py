67b13cae75c8ea8648be2d481aec33efe5b781e5
Jose Robles <jjvrobles@gmail.com>
2017-08-17 13:15:54 -0700

    Replaced usage of deprecated function get_fields_with_model with new local function, which does the same thing.

--- readthedocs/builds/version_slug.py
+++ readthedocs/builds/version_slug.py
@@ -28,6 +28,15 @@ from django.utils.encoding import force_text
 from builtins import range
 
 
+def get_fields_with_model(cls):
+    return [
+        (f, f.model if f.model != cls else None)
+        for f in cls._meta.get_fields()
+        if not f.is_relation or f.one_to_one or
+        (f.many_to_one and f.related_model)
+    ]
+
+
 # Regex breakdown:
 #   [a-z0-9] -- start with alphanumeric value
 #   [-._a-z0-9] -- allow dash, dot, underscore, digit, lowercase ascii
@@ -59,7 +68,7 @@ class VersionSlugField(models.CharField):
 
     def get_queryset(self, model_cls, slug_field):
         # pylint: disable=protected-access
-        for field, model in model_cls._meta.get_fields_with_model():
+        for field, model in get_fields_with_model(model_cls):
             if model and field == slug_field:
                 return model._default_manager.all()
         return model_cls._default_manager.all()
