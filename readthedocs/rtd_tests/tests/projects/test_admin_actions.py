74dbdcd6530236f4dcd539cd221174cab3ef8dff
Anthony <aj@ohess.org>
2017-10-04 20:22:32 -0600

    Add some basic spam removal features to admin (#3131)
    * Add some basic spam removal features to admin
    
    This adds actions for:
    
    * Mark project owner as banned
    * Remove project and files on web server
    
    Closes #2762
    
    * Remove unused attempt at using utils
    
    * Add docs on admin removal

---
+++ readthedocs/rtd_tests/tests/projects/test_admin_actions.py
@@ -0,0 +1,80 @@
+import mock
+import django_dynamic_fixture as fixture
+from django.contrib.admin.helpers import ACTION_CHECKBOX_NAME
+from django.contrib.auth.models import User
+from django.core import urlresolvers
+from django.test import TestCase
+
+from readthedocs.core.models import UserProfile
+from readthedocs.projects.models import Project
+
+
+class ProjectAdminActionsTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.owner = fixture.get(User)
+        cls.profile = fixture.get(UserProfile, user=cls.owner, banned=False)
+        cls.admin = fixture.get(User, is_staff=True, is_superuser=True)
+        cls.project = fixture.get(
+            Project,
+            main_language_project=None,
+            users=[cls.owner],
+        )
+
+    def setUp(self):
+        self.client.force_login(self.admin)
+
+    def test_project_ban_owner(self):
+        self.assertFalse(self.owner.profile.banned)
+        action_data = {
+            ACTION_CHECKBOX_NAME: [self.project.pk],
+            'action': 'ban_owner',
+            'index': 0,
+        }
+        resp = self.client.post(
+            urlresolvers.reverse('admin:projects_project_changelist'),
+            action_data
+        )
+        self.assertTrue(self.project.users.filter(profile__banned=True).exists())
+        self.assertFalse(self.project.users.filter(profile__banned=False).exists())
+
+    def test_project_ban_multiple_owners(self):
+        owner_b = fixture.get(User)
+        profile_b = fixture.get(UserProfile, user=owner_b, banned=False)
+        self.project.users.add(owner_b)
+        self.assertFalse(self.owner.profile.banned)
+        self.assertFalse(owner_b.profile.banned)
+        action_data = {
+            ACTION_CHECKBOX_NAME: [self.project.pk],
+            'action': 'ban_owner',
+            'index': 0,
+        }
+        resp = self.client.post(
+            urlresolvers.reverse('admin:projects_project_changelist'),
+            action_data
+        )
+        self.assertFalse(self.project.users.filter(profile__banned=True).exists())
+        self.assertEqual(self.project.users.filter(profile__banned=False).count(), 2)
+
+    @mock.patch('readthedocs.projects.admin.remove_dir')
+    def test_project_delete(self, remove_dir):
+        """Test project and artifacts are removed"""
+        action_data = {
+            ACTION_CHECKBOX_NAME: [self.project.pk],
+            'action': 'delete_selected',
+            'index': 0,
+            'post': 'yes',
+        }
+        resp = self.client.post(
+            urlresolvers.reverse('admin:projects_project_changelist'),
+            action_data
+        )
+        self.assertFalse(Project.objects.filter(pk=self.project.pk).exists())
+        remove_dir.apply_async.assert_has_calls([
+            mock.call(
+                kwargs={},
+                queue='celery',
+                args=[self.project.doc_path]
+            ),
+        ])
