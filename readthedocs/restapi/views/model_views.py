3e9171e22920abce986286cf652c15b7428f37ef
Eric Holscher <eric@ericholscher.com>
2017-10-05 12:31:09 -0700

    Small docstring

--- readthedocs/restapi/views/model_views.py
+++ readthedocs/restapi/views/model_views.py
@@ -187,6 +187,9 @@ class BuildViewSetBase(viewsets.ModelViewSet):
 
 
 class BuildViewSet(SettingsOverrideObject):
+
+    """A pluggable class to allow for build cold storage."""
+
     _default_class = BuildViewSetBase
 
 
