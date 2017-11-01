fe9be7bf3d55edb68264c3b1132565aaaf8be9e5
Eric Holscher <eric@ericholscher.com>
2017-11-01 15:23:22 -0700

    Smarter project deletion logic

--- deploy/delete_stale_projects.py
+++ deploy/delete_stale_projects.py
@@ -3,17 +3,21 @@ import os
 
 from readthedocs.projects.models import Project
 
-slugs = [p.slug for p in Project.objects.all()]
-build_projects = os.listdir('/home/docs/checkouts/readthedocs.org/user_builds/')
+all_slugs = set([p.slug for p in Project.objects.all().iterator()])
+json_projects = set(os.listdir('/home/docs/checkouts/readthedocs.org/media/json/'))
+build_projects = set(os.listdir('/home/docs/checkouts/readthedocs.org/user_builds/'))
+combined_ondisk_projects = json_projects + build_projects
 
 final = []
-for slug in build_projects:
-    if slug not in slugs and slug.replace('_', '-') not in slugs:
+for count, slug in enumerate(combined_ondisk_projects):
+    if slug not in all_slugs and slug.replace('_', '-') not in all_slugs:
         final.append(slug)
+    if count % 100 == 0:
+        print(count)
 
 print "To delete: %s" % len(final)
 
-for to_del in final:
+for count, to_del in enumerate(final):
     root = '/home/docs/checkouts/readthedocs.org'
     print "Deleting " + to_del
     shutil.rmtree('{root}/user_builds/{slug}'.format(root=root, slug=to_del), ignore_errors=True)
@@ -22,3 +26,5 @@ for to_del in final:
     shutil.rmtree('{root}/media/json/{slug}'.format(root=root, slug=to_del), ignore_errors=True)
     shutil.rmtree('{root}/media/man/{slug}'.format(root=root, slug=to_del), ignore_errors=True)
     shutil.rmtree('{root}/media/htmlzip/{slug}'.format(root=root, slug=to_del), ignore_errors=True)
+    if count % 100 == 0:
+        print(count)
