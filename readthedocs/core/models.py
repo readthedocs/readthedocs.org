c4d0319f285cbd929258eaf04bbe23f9aecb73ba
Manuel Kaufmann <humitos@gmail.com>
2017-11-14 22:59:46 -0500

    readthedocs.core.models auto styling applied

--- readthedocs/core/models.py
+++ readthedocs/core/models.py
@@ -1,44 +1,46 @@
-"""Models for the core app."""
+"""
+Models for the core app.
+"""
+from __future__ import absolute_import, division, print_function, unicode_literals
 
-from __future__ import absolute_import
 import logging
 
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
-from django.utils.translation import ugettext_lazy as _, ugettext
+from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ugettext
+
 from annoying.fields import AutoOneToOneField
 
-STANDARD_EMAIL = "anonymous@readthedocs.org"
+STANDARD_EMAIL = 'anonymous@readthedocs.org'
 
 log = logging.getLogger(__name__)
 
 
 @python_2_unicode_compatible
-class UserProfile (models.Model):
+class UserProfile(models.Model):
+    """
+    Additional information about a User.
+    """
 
-    """Additional information about a User."""
-
-    user = AutoOneToOneField('auth.User', verbose_name=_('User'),
-                             related_name='profile')
+    user = AutoOneToOneField('auth.User', verbose_name=_('User'), related_name='profile')
     whitelisted = models.BooleanField(_('Whitelisted'), default=False)
     banned = models.BooleanField(_('Banned'), default=False)
     homepage = models.CharField(_('Homepage'), max_length=100, blank=True)
-    allow_ads = models.BooleanField(_('See paid advertising'),
-                                    help_text=_('If unchecked, you will still see community ads.'),
-                                    default=True,
-                                    )
-    allow_email = models.BooleanField(_('Allow email'),
-                                      help_text=_('Show your email on VCS '
-                                                  'contributions.'),
-                                      default=True)
+    allow_ads = models.BooleanField(
+        _('See paid advertising'),
+        help_text=_('If unchecked, you will still see community ads.'),
+        default=True,
+    )
+    allow_email = models.BooleanField(
+        _('Allow email'), help_text=_('Show your email on VCS '
+                                      'contributions.'), default=True)
 
     def __str__(self):
-        return (ugettext("%(username)s's profile")
-                % {'username': self.user.username})
+        return (ugettext("%(username)s's profile") % {'username': self.user.username})
 
     def get_absolute_url(self):
-        return ('profiles_profile_detail', (),
-                {'username': self.user.username})
+        return ('profiles_profile_detail', (), {'username': self.user.username})
 
     get_absolute_url = models.permalink(get_absolute_url)
 
