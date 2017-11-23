9e328a7af7764ee80474788b09ae2fc402d08222
Manuel Kaufmann <humitos@gmail.com>
2017-11-23 14:48:44 -0500

    Fix old test to make sure a public branch is listed

--- readthedocs/rtd_tests/tests/test_privacy.py
+++ readthedocs/rtd_tests/tests/test_privacy.py
@@ -150,13 +150,14 @@ class PrivacyTests(TestCase):
 
         self.client.login(username='eric', password='test')
         Version.objects.create(project=kong, identifier='test id',
-                               verbose_name='test verbose', slug='test-slug', active=True)
+                               verbose_name='test verbose', slug='test-slug',
+                               active=True, built=True)
         self.assertEqual(Version.objects.count(), 2)
         self.assertEqual(Version.objects.all()[0].privacy_level, 'public')
         r = self.client.get('/projects/django-kong/')
         self.assertContains(r, 'test-slug')
 
-        # Make sure it doesn't show up as tester
+        # Make sure it does show up as tester
         self.client.login(username='tester', password='test')
         r = self.client.get('/projects/django-kong/')
         self.assertContains(r, 'test-slug')
