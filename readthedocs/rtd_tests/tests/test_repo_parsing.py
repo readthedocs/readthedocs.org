a7b8a2e1afeff31801d02765e9b90d49a99d30c9
Santos Gallegos <santos_g@outlook.com>
2018-01-11 18:54:24 -0500

    Add tests for ssh clone url

--- readthedocs/rtd_tests/tests/test_repo_parsing.py
+++ readthedocs/rtd_tests/tests/test_repo_parsing.py
@@ -37,6 +37,13 @@ class TestRepoParsing(TestCase):
         self.pip.repo = 'https://github.com/user/repo.git.git'
         self.assertEqual(self.version.get_github_url(docroot='/docs/', filename='file'), 'https://github.com/user/repo.git/blob/master/docs/file.rst')
 
+    def test_github_ssh(self):
+        self.pip.repo = 'git@github.com:user/repo.git'
+        self.assertEqual(self.version.get_github_url(docroot='/docs/', filename='file'), 'https://github.com/user/repo/blob/master/docs/file.rst')
+
+        self.pip.repo = 'git@github.com:user/repo.github.io.git'
+        self.assertEqual(self.version.get_github_url(docroot='/docs/', filename='file'), 'https://github.com/user/repo.github.io/blob/master/docs/file.rst')
+
     def test_gitlab(self):
         self.pip.repo = 'https://gitlab.com/user/repo'
         self.assertEqual(self.version.get_gitlab_url(docroot='/foo/bar/', filename='file'), 'https://gitlab.com/user/repo/blob/master/foo/bar/file.rst')
@@ -59,6 +66,13 @@ class TestRepoParsing(TestCase):
         self.pip.repo = 'https://gitlab.com/user/repo.git.git'
         self.assertEqual(self.version.get_gitlab_url(docroot='/foo/bar/', filename='file'), 'https://gitlab.com/user/repo.git/blob/master/foo/bar/file.rst')
 
+    def test_gitlab_ssh(self):
+        self.pip.repo = 'git@gitlab.com:user/repo.git'
+        self.assertEqual(self.version.get_gitlab_url(docroot='/foo/bar/', filename='file'), 'https://gitlab.com/user/repo/blob/master/foo/bar/file.rst')
+
+        self.pip.repo = 'git@gitlab.com:user/repo.gitlab.io.git'
+        self.assertEqual(self.version.get_gitlab_url(docroot='/foo/bar/', filename='file'), 'https://gitlab.com/user/repo.gitlab.io/blob/master/foo/bar/file.rst')
+
     def test_bitbucket(self):
         self.pip.repo = 'https://bitbucket.org/user/repo'
         self.assertEqual(self.version.get_bitbucket_url(docroot='/foo/bar/', filename='file'), 'https://bitbucket.org/user/repo/src/master/foo/bar/file.rst')
@@ -80,3 +94,17 @@ class TestRepoParsing(TestCase):
 
         self.pip.repo = 'https://bitbucket.org/user/repo.git.git'
         self.assertEqual(self.version.get_bitbucket_url(docroot='/foo/bar/', filename='file'), 'https://bitbucket.org/user/repo.git.git/src/master/foo/bar/file.rst')
+
+    def test_bitbucket_https(self):
+        self.pip.repo = 'https://user@bitbucket.org/user/repo.git'
+        self.assertEqual(self.version.get_bitbucket_url(docroot='/foo/bar/', filename='file'), 'https://bitbucket.org/user/repo/src/master/foo/bar/file.rst')
+
+        self.pip.repo = 'https://user@bitbucket.org/user/repo.gitbucket.io.git'
+        self.assertEqual(self.version.get_bitbucket_url(docroot='/foo/bar/', filename='file'), 'https://bitbucket.org/user/repo.gitbucket.io/src/master/foo/bar/file.rst')
+
+    def test_bitbucket_ssh(self):
+        self.pip.repo = 'git@bitbucket.org:user/repo.git'
+        self.assertEqual(self.version.get_bitbucket_url(docroot='/foo/bar/', filename='file'), 'https://bitbucket.org/user/repo/src/master/foo/bar/file.rst')
+
+        self.pip.repo = 'git@bitbucket.org:user/repo.gitbucket.io.git'
+        self.assertEqual(self.version.get_bitbucket_url(docroot='/foo/bar/', filename='file'), 'https://bitbucket.org/user/repo.gitbucket.io/src/master/foo/bar/file.rst')
