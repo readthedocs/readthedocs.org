e4cb814b91079dae380fca3f8c5148fa6fdd6deb
David Fischer <david@readthedocs.org>
2018-03-03 23:55:21 -0800

    Refactor and simplify doc embed CSS

--- readthedocs/core/static-src/core/js/doc-embed/sponsorship.js
+++ readthedocs/core/static-src/core/js/doc-embed/sponsorship.js
@@ -79,19 +79,23 @@ function get_placements () {
  *  Creates a sidebar div where an ad could go
  */
 function create_sidebar_placement () {
-    var element_id = 'rtd-ethical-sidebar',
+    var element_id = 'rtd-' + (Math.random() + 1).toString(36).substring(4),
         display_type = constants.PROMO_TYPES.LEFTNAV,
-        selector = null;
+        selector = null,
+        class_name;     // Used for theme specific CSS customizations
 
     if (rtd.is_rtd_theme()) {
         selector = 'nav.wy-nav-side > div.wy-side-scroll';
+        class_name = 'ethical-rtd';
     } else if (rtd.get_theme_name() == constants.THEME_ALABASTER ||
                rtd.get_theme_name() == constants.THEME_CELERY) {
         selector = 'div.sphinxsidebar > div.sphinxsidebarwrapper';
+        class_name = 'ethical-alabaster';
     }
 
     if (selector) {
-        $('<div />').attr('id', element_id).appendTo(selector);
+        $('<div />').attr('id', element_id)
+            .addClass(class_name).appendTo(selector);
         return {'div_id': element_id, 'display_type': display_type};
     }
 
@@ -103,19 +107,23 @@ function create_sidebar_placement () {
  *  Returns the ID of the div or none if no footer ad is possible
  */
 function create_footer_placement () {
-    var element_id = 'rtd-ethical-footer',
+    var element_id = 'rtd-' + (Math.random() + 1).toString(36).substring(4),
         display_type = constants.PROMO_TYPES.FOOTER,
-        selector = null;
+        selector = null,
+        class_name;
 
     if (rtd.is_rtd_theme()) {
         selector = $('<div />').insertAfter('footer hr');
+        class_name = 'ethical-rtd';
     } else if (rtd.get_theme_name() == constants.THEME_ALABASTER ||
                rtd.get_theme_name() == constants.THEME_CELERY) {
-        selector = 'div.bodywrapper';
+        selector = 'div.bodywrapper .body';
+        class_name = 'ethical-alabaster';
     }
 
     if (selector) {
-        $('<div />').attr('id', element_id).appendTo(selector);
+        $('<div />').attr('id', element_id)
+            .addClass(class_name).appendTo(selector);
         return {'div_id': element_id, 'display_type': display_type};
     }
 
@@ -151,10 +159,25 @@ Promo.prototype.display = function () {
     $('#' + this.div_id).html(this.html);
     $('#' + this.div_id).find('a[href*="/sustainability/click/"]')
         .on('click', this.click_handler);
+
+    this.post_promo_display();
 };
 
 Promo.prototype.disable = function () {
     $('#' + this.div_id).hide();
+};
+
+/*
+ * Perform any additional tweaks after the ad is successfully injected
+ */
+Promo.prototype.post_promo_display = function () {
+    if (this.display_type === constants.PROMO_TYPES.FOOTER) {
+        $('<hr />').insertAfter('#' + this.div_id);
+
+        // Alabaster only
+        $('<hr />').insertBefore('#' + this.div_id + '.ethical-alabaster .ethical-footer');
+    }
+
 };
 
 module.exports = {
