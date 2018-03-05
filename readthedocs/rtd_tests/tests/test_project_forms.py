333e96a51f067ec65e929b50d809582f9087e351
Santos Gallegos <santos_g@outlook.com>
2018-03-05 16:36:03 -0500

    Refactor tests

--- readthedocs/rtd_tests/tests/test_project_forms.py
+++ readthedocs/rtd_tests/tests/test_project_forms.py
@@ -31,81 +31,89 @@ class TestProjectForms(TestCase):
 
 class TestTranslationForm(TestCase):
 
+    def setUp(self):
+        self.user_a = get(User)
+        self.project_a_es = self.get_project(lang='es', users=[self.user_a])
+        self.project_b_en = self.get_project(lang='en', users=[self.user_a])
+        self.project_c_br = self.get_project(lang='br', users=[self.user_a])
+        self.project_d_ar = self.get_project(lang='ar', users=[self.user_a])
+        self.project_e_en = self.get_project(lang='en', users=[self.user_a])
+
+        self.user_b = get(User)
+        self.project_f_ar = self.get_project(lang='ar', users=[self.user_b])
+        self.project_g_ga = self.get_project(lang='ga', users=[self.user_b])
+
+        self.project_s_fr = self.get_project(
+            lang='fr',
+            users=[self.user_b, self.user_a]
+        )
+
     def get_project(self, lang, users, **kwargs):
         return get(
             Project, language=lang, users=users,
             main_language_project=None, **kwargs
         )
 
-    def test_form_translation_list_only_owner_projects(self):
-        user = get(User)
-        project_a = self.get_project(lang='es', users=[user])
-        project_b = self.get_project(lang='en', users=[user])
-        project_c = self.get_project(lang='br', users=[user])
-
-        user_b = get(User)
-        project_d = self.get_project(lang='ar', users=[user_b])
-        project_e = self.get_project(lang='ga', users=[user_b])
-
-        # shared project
-        project_s = self.get_project(lang='fr', users=[user_b, user])
-
+    def test_list_only_owner_projects(self):
         form = TranslationForm(
-            {'project': project_b.slug},
-            parent=project_a,
-            user=user,
+            {'project': self.project_b_en.slug},
+            parent=self.project_a_es,
+            user=self.user_a,
         )
         self.assertTrue(form.is_valid())
+        expected_projects = [
+            self.project_b_en,
+            self.project_c_br,
+            self.project_d_ar,
+            self.project_e_en,
+            self.project_s_fr,
+        ]
         self.assertEqual(
             {proj_slug for proj_slug, _ in form.fields['project'].choices},
-            {project_b.slug, project_c.slug, project_s.slug}
+            {project.slug for project in expected_projects}
         )
 
         form = TranslationForm(
-            {'project': project_e.slug},
-            parent=project_d,
-            user=user_b,
+            {'project': self.project_g_ga.slug},
+            parent=self.project_f_ar,
+            user=self.user_b,
         )
         self.assertTrue(form.is_valid())
+        expected_projects = [
+            self.project_g_ga,
+            self.project_s_fr,
+        ]
         self.assertEqual(
             {proj_slug for proj_slug, _ in form.fields['project'].choices},
-            {project_e.slug, project_s.slug}
+            {project.slug for project in expected_projects}
         )
 
-    def test_form_translation_excludes_existing_translations(self):
-        user = get(User)
-        project_a = self.get_project(lang='es', users=[user])
-        project_b = self.get_project(lang='en', users=[user])
-        project_c = self.get_project(lang='br', users=[user])
-        project_d = self.get_project(lang='ar', users=[user])
-        project_e = self.get_project(lang='aa', users=[user])
-
-        project_a.translations.add(project_b)
-        project_a.translations.add(project_c)
-        project_a.save()
+    def test_excludes_existing_translations(self):
+        self.project_a_es.translations.add(self.project_b_en)
+        self.project_a_es.translations.add(self.project_c_br)
+        self.project_a_es.save()
 
         form = TranslationForm(
-            {'project': project_d.slug},
-            parent=project_a,
-            user=user,
+            {'project': self.project_d_ar.slug},
+            parent=self.project_a_es,
+            user=self.user_a,
         )
         self.assertTrue(form.is_valid())
+        expected_projects = [
+            self.project_d_ar,
+            self.project_e_en,
+            self.project_s_fr,
+        ]
         self.assertEqual(
             {proj_slug for proj_slug, _ in form.fields['project'].choices},
-            {project_d.slug, project_e.slug}
+            {project.slug for project in expected_projects}
         )
 
-    def test_form_translation_user_cant_add_other_user_project(self):
-        user = get(User)
-        project_a = self.get_project(lang='es', users=[user])
-
-        user_b = get(User)
-        project_b = self.get_project(lang='en', users=[user_b])
-
+    def test_user_cant_add_other_user_project(self):
         form = TranslationForm(
-            {'project': project_b.slug},
-            parent=project_a,
-            user=user,
+            {'project': self.project_f_ar.slug},
+            parent=self.project_b_en,
+            user=self.user_a,
         )
         self.assertFalse(form.is_valid())
         self.assertIn(
@@ -113,39 +121,30 @@ class TestTranslationForm(TestCase):
             ''.join(form.errors['project'])
         )
         self.assertNotIn(
-            project_b.slug,
+            self.project_f_ar,
             [proj_slug for proj_slug, _ in form.fields['project'].choices]
         )
 
-    def test_form_translation_user_cant_add_project_with_same_lang(self):
-        user = get(User)
-        project_a = self.get_project(lang='es', users=[user])
-        project_b = self.get_project(lang='es', users=[user])
-
+    def test_user_cant_add_project_with_same_lang(self):
         form = TranslationForm(
-            {'project': project_b.slug},
-            parent=project_a,
-            user=user,
+            {'project': self.project_b_en.slug},
+            parent=self.project_e_en,
+            user=self.user_a,
         )
         self.assertFalse(form.is_valid())
         self.assertIn(
-            'Both projects can not have the same language (Spanish).',
+            'Both projects can not have the same language (English).',
             ''.join(form.errors['project'])
         )
 
-    def test_form_translation_user_cant_add_project_with_same_lang_of_other_translation(self):
-        user = get(User)
-        project_a = self.get_project(lang='es', users=[user])
-        project_b = self.get_project(lang='en', users=[user])
-        project_c = self.get_project(lang='en', users=[user])
-
-        project_a.translations.add(project_b)
-        project_a.save()
+    def test_user_cant_add_project_with_same_lang_of_other_translation(self):
+        self.project_a_es.translations.add(self.project_b_en)
+        self.project_a_es.save()
 
         form = TranslationForm(
-            {'project': project_c.slug},
-            parent=project_a,
-            user=user,
+            {'project': self.project_e_en.slug},
+            parent=self.project_a_es,
+            user=self.user_a,
         )
         self.assertFalse(form.is_valid())
         self.assertIn(
@@ -153,19 +152,14 @@ class TestTranslationForm(TestCase):
             ''.join(form.errors['project'])
         )
 
-    def test_form_translation_no_nesting_translation(self):
-        user = get(User)
-        project_a = self.get_project(lang='es', users=[user])
-        project_b = self.get_project(lang='en', users=[user])
-        project_c = self.get_project(lang='br', users=[user])
-
-        project_a.translations.add(project_b)
-        project_a.save()
+    def test_no_nesting_translation(self):
+        self.project_a_es.translations.add(self.project_b_en)
+        self.project_a_es.save()
 
         form = TranslationForm(
-            {'project': project_b.slug},
-            parent=project_c,
-            user=user,
+            {'project': self.project_b_en.slug},
+            parent=self.project_c_br,
+            user=self.user_a,
         )
         self.assertFalse(form.is_valid())
         self.assertIn(
@@ -173,19 +167,14 @@ class TestTranslationForm(TestCase):
             ''.join(form.errors['project'])
         )
 
-    def test_form_translation_no_nesting_translation_case_2(self):
-        user = get(User)
-        project_a = self.get_project(lang='es', users=[user])
-        project_b = self.get_project(lang='en', users=[user])
-        project_c = self.get_project(lang='br', users=[user])
-
-        project_a.translations.add(project_b)
-        project_a.save()
+    def test_no_nesting_translation_case_2(self):
+        self.project_a_es.translations.add(self.project_b_en)
+        self.project_a_es.save()
 
         form = TranslationForm(
-            {'project': project_a.slug},
-            parent=project_c,
-            user=user,
+            {'project': self.project_a_es.slug},
+            parent=self.project_c_br,
+            user=self.user_a,
         )
         self.assertFalse(form.is_valid())
         self.assertIn(
@@ -194,18 +183,13 @@ class TestTranslationForm(TestCase):
         )
 
     def test_not_already_translation(self):
-        user = get(User)
-        project_a = self.get_project(lang='es', users=[user])
-        project_b = self.get_project(lang='en', users=[user])
-        project_c = self.get_project(lang='br', users=[user])
-
-        project_a.translations.add(project_b)
-        project_a.save()
+        self.project_a_es.translations.add(self.project_b_en)
+        self.project_a_es.save()
 
         form = TranslationForm(
-            {'project': project_c.slug},
-            parent=project_b,
-            user=user,
+            {'project': self.project_c_br.slug},
+            parent=self.project_b_en,
+            user=self.user_a,
         )
         self.assertFalse(form.is_valid())
         self.assertIn(
