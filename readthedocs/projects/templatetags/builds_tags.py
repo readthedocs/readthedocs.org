f1465d77ac9ef0b01aebdf950d9452ef6ae76aa1
Santos Gallegos <santos_g@outlook.com>
2018-04-17 15:13:54 -0500

    Run pre-commit

--- readthedocs/projects/templatetags/builds_tags.py
+++ readthedocs/projects/templatetags/builds_tags.py
@@ -1,6 +1,10 @@
-"""Build template tags and filters"""
+# -*- coding: utf-8 -*-
+"""Build template tags and filters."""
+
+from __future__ import division, print_function, unicode_literals
 
 from django import template
+
 from readthedocs.projects.constants import PRIVATE
 
 register = template.Library()
@@ -11,6 +15,4 @@ def get_docs_url(build):
     version = build.version
     private = version.privacy_level == PRIVATE
     return version.project.get_docs_url(
-        version_slug=version.slug,
-        private=private
-    )
+        version_slug=version.slug, private=private)
