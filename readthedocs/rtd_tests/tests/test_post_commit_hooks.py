d55a15eac618fb14bb71feddd0e3b0f257e07a08
Manuel Kaufmann <humitos@gmail.com>
2018-05-15 15:13:26 -0500

    Generalize tests naming

--- readthedocs/rtd_tests/tests/test_post_commit_hooks.py
+++ readthedocs/rtd_tests/tests/test_post_commit_hooks.py
@@ -176,7 +176,7 @@ class GitLabWebHookTest(BasePostCommitTest):
         self.assertEqual(r.status_code, 403)
 
 
-class GitHubPostCommitTest(BasePostCommitTest):
+class GitHubWebHookTest(BasePostCommitTest):
     fixtures = ["eric"]
 
     def setUp(self):
@@ -391,7 +391,7 @@ class CorePostCommitTest(BasePostCommitTest):
         self.assertEqual(r.status_code, 403)
 
 
-class BitBucketHookTests(BasePostCommitTest):
+class BitBucketWebHookTest(BasePostCommitTest):
 
     def setUp(self):
         self._setup()
