b2e5dbc1fa16f2f55d18ef7b182c8648e9e88fc4
Santos Gallegos <santos_g@outlook.com>
2018-05-29 19:09:57 -0500

    Add sphinx and mkdocs to the spec

--- readthedocs/rtd_tests/tests/test_yml_schema.py
+++ readthedocs/rtd_tests/tests/test_yml_schema.py
@@ -8,6 +8,7 @@ from readthedocs_build.testing import utils
 def create_yaml(tmpdir, content):
     fs = {
         'environment.yml': '',
+        'mkdocs.yml': '',
         'rtd.yml': content,
         'docs': {
             'conf.py': '',
@@ -301,6 +302,15 @@ def test_sphinx(tmpdir):
     content = '''
 version: "2"
 sphinx:
+  configuration: docs/conf.py
+    '''
+    assertValidConfig(tmpdir, content)
+
+
+def test_sphinx_default_value(tmpdir):
+    content = '''
+version: "2"
+sphinx:
   file: docs/conf.py  # Default value for configuration key
     '''
     assertValidConfig(tmpdir, content)
@@ -317,6 +327,84 @@ sphinx:
         tmpdir,
         content,
         ['is not a path']
+    )
+
+
+def test_sphinx_fail_on_warning(tmpdir):
+    content = '''
+version: "2"
+sphinx:
+  fail_on_warning: true
+    '''
+    assertValidConfig(tmpdir, content)
+
+
+@pytest.mark.parametrize('value', ['not true', "''", '[]'])
+def test_sphinx_fail_on_warning_invalid(tmpdir, value):
+    content = '''
+version: "2"
+sphinx:
+  fail_on_warning: {value}
+    '''
+    assertInvalidConfig(
+        tmpdir,
+        content.format(value=value),
+        ['is not a bool']
+    )
+
+
+def test_mkdocs(tmpdir):
+    content = '''
+version: "2"
+mkdocs:
+  configuration: mkdocs.yml
+    '''
+    assertValidConfig(tmpdir, content)
+
+
+def test_mkdocs_default_value(tmpdir):
+    content = '''
+version: "2"
+mkdocs:
+  file: mkdocs.yml  # Default value for configuration key
+    '''
+    assertValidConfig(tmpdir, content)
+
+
+@pytest.mark.parametrize('value', ['2', 'environment.py'])
+def test_mkdocs_invalid(tmpdir, value):
+    content = '''
+version: "2"
+mkdocs:
+  configuration: {value}
+    '''
+    assertInvalidConfig(
+        tmpdir,
+        content,
+        ['is not a path']
+    )
+
+
+def test_mkdocs_fail_on_warning(tmpdir):
+    content = '''
+version: "2"
+mkdocs:
+  fail_on_warning: true
+    '''
+    assertValidConfig(tmpdir, content)
+
+
+@pytest.mark.parametrize('value', ['not true', "''", '[]'])
+def test_mkdocs_fail_on_warning_invalid(tmpdir, value):
+    content = '''
+version: "2"
+mkdocs:
+  fail_on_warning: {value}
+    '''
+    assertInvalidConfig(
+        tmpdir,
+        content.format(value=value),
+        ['is not a bool']
     )
 
 
