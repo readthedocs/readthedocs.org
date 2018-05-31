dd8fb6e36d0a1b8d711effae782991a0200e14e9
Eric Holscher <eric@ericholscher.com>
2018-05-31 10:28:39 -0400

    Remove old API docs

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,6 @@ copyright = '2010-{}, Read the Docs, Inc & contributors'.format(
 )
 version = '1.0'
 release = '1.0'
-exclude_patterns = ['_build']
 default_role = 'obj'
 pygments_style = 'sphinx'
 intersphinx_mapping = {
@@ -61,6 +60,7 @@ man_pages = [
 ]
 
 exclude_patterns = [
+    '_build',
     # 'api' # needed for ``make gettext`` to not die.
 ]
 
