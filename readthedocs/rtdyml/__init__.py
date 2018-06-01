18a6485c8994850fa28ea54de395eee6e07b3049
Santos Gallegos <santos_g@outlook.com>
2018-06-01 10:16:38 -0500

    Isort

--- readthedocs/rtdyml/__init__.py
+++ readthedocs/rtdyml/__init__.py
@@ -1,9 +1,10 @@
 """Validator for the RTD configuration file."""
 
+from __future__ import division, print_function, unicode_literals
+
 from os import path
 
 import six
-
 import yamale
 from yamale.validators import DefaultValidators, Validator
 
