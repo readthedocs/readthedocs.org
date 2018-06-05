2e9e916e16f92e1dbf0e22dca85e58039c24c924
Safwan Rahman <safwan.rahman15@gmail.com>
2018-06-05 22:20:32 +0600

    move from readthedocs-ext to readthedocs.org

---
+++ readthedocs/search/search/tests/test_search.py
@@ -0,0 +1,26 @@
+import mock
+
+import django_dynamic_fixture as fixture
+from django.test import TestCase, RequestFactory
+
+from readthedocs.projects.models import Project
+from readthedocsext.search.docsearch import DocSearch
+
+
+@mock.patch('readthedocsext.search.docsearch.execute_version_search')
+class SearchTests(TestCase):
+
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.project = fixture.get(Project, slug='test')
+
+    def test_subversions(self, execute_version_search):
+        execute_version_search.return_value = {'fake': True}
+        request = self.factory.get(
+            '/api/v2/docsearch',
+            {'project': 'test', 'version': 'latest', 'q': 'query'}
+        )
+        view = DocSearch.as_view()
+        resp = view(request).render()
+        self.assertEqual(resp.status_code, 200)
+        self.assertTrue('fake' in resp.content)
