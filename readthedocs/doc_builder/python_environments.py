c53bf05e9a54b29daf2190d3d1fbb7099c2f2804
Florian Kuckelkorn <development@florian-kuckelkorn.de>
2018-06-07 23:25:54 +0200

    - Fixed breaking change in pip 10.0.0b1 (2018-03-31) - "--use-wheel" was removed fro pip's cli

--- readthedocs/doc_builder/python_environments.py
+++ readthedocs/doc_builder/python_environments.py
@@ -253,7 +253,7 @@ class Virtualenv(PythonEnvironment):
             'python',
             self.venv_bin(filename='pip'),
             'install',
-            '--use-wheel',
+            '--only-binary=:all:',
             '--upgrade',
             '--cache-dir',
             self.project.pip_cache_path,
