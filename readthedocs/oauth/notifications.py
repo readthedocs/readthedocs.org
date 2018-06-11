db4a084caef4b55ccbbef9f342ce36b3d34ae1c3
Manuel Kaufmann <humitos@gmail.com>
2018-06-11 16:38:57 -0300

    Make string translatable

--- readthedocs/oauth/notifications.py
+++ readthedocs/oauth/notifications.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import division, print_function, unicode_literals
 
+from django.utils.translation import ugettext_lazy as _
 from readthedocs.notifications import SiteNotification
 
 
@@ -11,9 +12,9 @@ class AttachWebhookNotification(SiteNotification):
     NO_ACCOUNTS = 'no_accounts'
 
     context_object_name = 'provider'
-    success_message = 'Webhook activated successfully.'
+    success_message = _('Webhook activated successfully.')
     failure_message = {
-        NO_CONNECTED_SERVICES: 'Webhook activation failed. There are no connected services for this project.',
-        NO_PERMISSIONS: 'Webhook activation failed. Make sure you have permissions to set it.',
-        NO_ACCOUNTS: 'No accounts available to set webhook on. Please connect your {provider.name} account.',
+        NO_CONNECTED_SERVICES: _('Webhook activation failed. There are no connected services for this project.'),
+        NO_PERMISSIONS: _('Webhook activation failed. Make sure you have permissions to set it.'),
+        NO_ACCOUNTS: _('No accounts available to set webhook on. Please connect your {provider.name} account.'),
     }
