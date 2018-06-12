39237cf55fbecfff57827e6934357911b71f8031
Manuel Kaufmann <humitos@gmail.com>
2018-06-12 11:47:50 -0300

    Adapt AttachWebhookNotification to the new features

--- readthedocs/oauth/notifications.py
+++ readthedocs/oauth/notifications.py
@@ -1,20 +1,32 @@
 # -*- coding: utf-8 -*-
 from __future__ import division, print_function, unicode_literals
 
+from django.core.urlresolvers import reverse
 from django.utils.translation import ugettext_lazy as _
 from readthedocs.notifications import SiteNotification
 
 
 class AttachWebhookNotification(SiteNotification):
 
-    NO_CONNECTED_SERVICES = 'no_connected_services'
+    # Fail reasons
     NO_PERMISSIONS = 'no_permissions'
     NO_ACCOUNTS = 'no_accounts'
 
     context_object_name = 'provider'
-    success_message = _('Webhook activated successfully.')
+    success_message = _('Webhook successfully added.')
     failure_message = {
-        NO_CONNECTED_SERVICES: _('Webhook activation failed. There are no connected services for ''this project.'),  # noqa
-        NO_PERMISSIONS: _('Webhook activation failed. Make sure you have permissions to set it.'),  # noqa
-        NO_ACCOUNTS: _('No accounts available to set webhook on. Please connect your {provider.name} account.'),  # noqa
+        NO_PERMISSIONS: _('Could not add webhook for {{ project.name }}. Make sure <a href="{{ url_docs_webhook }}">you have the correct {{ provider.name }} permissions</a>.'),  # noqa
+        NO_ACCOUNTS: _('Could not add webhook for {{ project.name }}. Please <a href="{{ url_connect_account }}">connect your {{ provider.name }} account</a>.'),  # noqa
     }
+
+    def get_context_data(self):
+        context = super(AttachWebhookNotification, self).get_context_data()
+        project = self.extra_context.get('project')
+        context.update({
+            'url_connect_account': reverse(
+                'projects_integrations',
+                args=[project.slug],
+            ),
+            'url_docs_webhook': 'http://docs.readthedocs.io/en/latest/webhooks.html',  # noqa
+        })
+        return context
