6352a617da1f9381c0e079e99aaeb0ebb800e53f
Santos Gallegos <santos_g@outlook.com>
2018-06-13 14:47:49 -0500

    Move utils module

---
+++ readthedocs/config/tests/test_utils.py
@@ -0,0 +1,26 @@
+from .utils import apply_fs
+
+
+def test_apply_fs_with_empty_contents(tmpdir):
+    # Doesn't do anything if second paramter is empty.
+    apply_fs(tmpdir, {})
+    assert tmpdir.listdir() == []
+
+
+def test_apply_fs_create_empty_file(tmpdir):
+    # Create empty file.
+    apply_fs(tmpdir, {'file': ''})
+    assert len(tmpdir.listdir()) == 1
+    assert tmpdir.join('file').read() == ''
+
+
+def test_apply_fs_create_file_with_content(tmpdir):
+    # Create file with content.
+    apply_fs(tmpdir, {'file': 'content'})
+    assert tmpdir.join('file').read() == 'content'
+
+
+def test_apply_fs_create_subdirectory(tmpdir):
+    # Create file with content.
+    apply_fs(tmpdir, {'subdir': {'file': 'content'}})
+    assert tmpdir.join('subdir', 'file').read() == 'content'
---
+++ readthedocs/config/tests/utils.py
@@ -0,0 +1,13 @@
+def apply_fs(tmpdir, contents):
+    """
+    Create the directory structure specified in ``contents``. It's a dict of
+    filenames as keys and the file contents as values. If the value is another
+    dict, it's a subdirectory.
+    """
+    for filename, content in contents.items():
+        if hasattr(content, 'items'):
+            apply_fs(tmpdir.mkdir(filename), content)
+        else:
+            file = tmpdir.join(filename)
+            file.write(content)
+    return tmpdir
