a4bfff0807d2c864ec1fd84b0c19439652712957
Santos Gallegos <santos_g@outlook.com>
2018-06-13 15:14:53 -0500

    Move implementation from find.py

--- readthedocs/config/find.py
+++ readthedocs/config/find.py
@@ -1,6 +1,16 @@
+import os
+
+
 def find_all(path, filenames):
-    pass
+    path = os.path.abspath(path)
+    for root, dirs, files in os.walk(path, topdown=True):
+        dirs.sort()
+        for filename in filenames:
+            if filename in files:
+                yield os.path.abspath(os.path.join(root, filename))
 
 
 def find_one(path, filenames):
-    pass
+    for _path in find_all(path, filenames):
+        return _path
+    return ''
