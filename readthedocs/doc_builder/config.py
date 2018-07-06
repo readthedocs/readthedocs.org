8d8197ad85177ed34e5d297eb44471f1aa4b6dd7
Santos Gallegos <santos_g@outlook.com>
2018-07-05 22:48:04 -0500

    Goodbye ConfigWrapper

--- readthedocs/doc_builder/config.py
+++ readthedocs/doc_builder/config.py
@@ -4,87 +4,10 @@
 from __future__ import (
     absolute_import, division, print_function, unicode_literals)
 
-from builtins import object
-
 from readthedocs.config import BuildConfig, ConfigError, InvalidConfig
 from readthedocs.config import load as load_config
 
 from .constants import DOCKER_IMAGE, DOCKER_IMAGE_SETTINGS
-
-
-class ConfigWrapper(object):
-
-    """
-    A config object that wraps the Project & YAML based configs.
-
-    Gives precedence to YAML, falling back to project if it isn't defined.
-
-    We only currently implement a subset of the existing YAML config.
-    This should be the canonical source for our usage of the YAML files,
-    never accessing the config object directly.
-    """
-
-    def __init__(self, version, yaml_config):
-        self._version = version
-        self._project = version.project
-        self._yaml_config = yaml_config
-
-    @property
-    def pip_install(self):
-        return self._yaml_config.pip_install
-
-    @property
-    def install_project(self):
-        return self._yaml_config.install_project
-
-    @property
-    def extra_requirements(self):
-        return self._yaml_config.extra_requirements
-
-    @property
-    def python_interpreter(self):
-        return self._yaml_config.python_interpreter
-
-    @property
-    def python_version(self):
-        return self._yaml_config.python_version
-
-    @property
-    def python_full_version(self):
-        return self._yaml_config.python_full_version
-
-    @property
-    def use_system_site_packages(self):
-        return self._yaml_config.use_system_site_packages
-
-    @property
-    def use_conda(self):
-        return self._yaml_config.use_conda
-
-    @property
-    def conda_file(self):
-        return self._yaml_config.conda_file
-
-    @property
-    def requirements_file(self):
-        return self._yaml_config.requirements_file
-
-    @property
-    def formats(self):
-        return self._yaml_config.formats
-
-    @property
-    def build_image(self):
-        return self._yaml_config.build_image
-
-    # Not implemented until we figure out how to keep in sync with the webs.
-    # Probably needs to be version-specific as well, not project.
-    # @property
-    # def documentation_type(self):
-    #     if 'type' in self._yaml_config:
-    #         return self._yaml_config['type']
-    #     else:
-    #         return self._project.documentation_type
 
 
 def load_yaml_config(version):
@@ -149,7 +72,7 @@ def load_yaml_config(version):
             source_position=0,
         )
         config.validate()
-    return ConfigWrapper(version=version, yaml_config=config)
+    return config
 
 
 def get_default_formats(project):
