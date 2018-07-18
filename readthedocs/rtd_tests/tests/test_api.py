efc3fb02ea29e30cdc2820e46c5e8c546265cc78
David Fischer <david@readthedocs.org>
2018-07-17 17:19:48 -0700

    Updates based on feedback

--- readthedocs/rtd_tests/tests/test_api.py
+++ readthedocs/rtd_tests/tests/test_api.py
@@ -574,13 +574,15 @@ class APITests(TestCase):
         api_project = APIProject(**project_data)
         self.assertEqual(api_project.slug, 'test-project')
         self.assertEquals(api_project.features, [])
-        self.assertEqual(api_project.ad_free, False)
+        self.assertFalse(api_project.ad_free)
+        self.assertTrue(api_project.show_advertising)
 
         project_data['features'] = ['test-feature']
         project_data['show_advertising'] = False
         api_project = APIProject(**project_data)
         self.assertEquals(api_project.features, ['test-feature'])
-        self.assertEqual(api_project.ad_free, True)
+        self.assertTrue(api_project.ad_free)
+        self.assertFalse(api_project.show_advertising)
 
 
 class APIImportTests(TestCase):
