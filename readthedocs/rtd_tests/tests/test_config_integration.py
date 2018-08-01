73731d817d88afef694e8007520450fad7f12203
Santos Gallegos <santos_g@outlook.com>
2018-08-01 12:22:22 -0500

    Skip new tests

--- readthedocs/rtd_tests/tests/test_config_integration.py
+++ readthedocs/rtd_tests/tests/test_config_integration.py
@@ -233,6 +233,7 @@ class LoadConfigTests(TestCase):
         self.assertEqual(config.python.requirements, '__init__.py')
 
 
+@pytest.mark.skip('No implemented yet')
 @pytest.mark.django_db
 @mock.patch('readthedocs.projects.models.Project.checkout_path')
 class TestLoadConfigV2(object):
