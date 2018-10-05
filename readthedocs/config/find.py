95733b739f01ce545004483e018664a24e946c89
sriks123 <srksriks123@gmail.com>
2018-10-06 00:22:20 +0530

    Remove functionality of iterating all directories and fixed tests

--- readthedocs/config/find.py
+++ readthedocs/config/find.py
@@ -6,16 +6,11 @@ import os
 import re
 
 
-def find_all(path, filename_regex):
-    """Find all files in ``path`` that match ``filename_regex`` regex."""
-    path = os.path.abspath(path)
-    for filename in os.listdir(path):
-        if re.match(filename_regex, filename):
-            yield os.path.abspath(os.path.join(path, filename))
-
-
 def find_one(path, filename_regex):
     """Find the first file in ``path`` that match ``filename_regex`` regex."""
-    for _path in find_all(path, filename_regex):
-        return _path
+    _path = os.path.abspath(path)
+    for filename in os.listdir(_path):
+        if re.match(filename_regex, filename):
+            return os.path.abspath(os.path.join(_path, filename))
+
     return ''
--- readthedocs/config/tests/test_find.py
+++ readthedocs/config/tests/test_find.py
@@ -1,18 +1,19 @@
 from __future__ import division, print_function, unicode_literals
 
 import os
-
 import pytest
 import six
 
-from readthedocs.config.find import find_all, find_one
+import pdb
+
+from readthedocs.config.find import find_one
 
 from .utils import apply_fs
 
 
 def test_find_no_files(tmpdir):
     with tmpdir.as_cwd():
-        paths = list(find_all(os.getcwd(), r'readthedocs.yml'))
+        paths = str(find_one(os.getcwd(), r'readthedocs.yml'))
     assert len(paths) == 0
 
 
@@ -20,58 +21,8 @@ def test_find_at_root(tmpdir):
     apply_fs(tmpdir, {'readthedocs.yml': '', 'otherfile.txt': ''})
 
     base = str(tmpdir)
-    paths = list(find_all(base, r'readthedocs\.yml'))
-    assert paths == [
-        os.path.abspath(os.path.join(base, 'readthedocs.yml'))
-    ]
-
-
-def test_find_nested(tmpdir):
-    apply_fs(tmpdir, {
-        'first': {
-            'readthedocs.yml': '',
-        },
-        'second': {
-            'confuser.txt': 'content',
-        },
-        'third': {
-            'readthedocs.yml': 'content',
-            'Makefile': '',
-        },
-    })
-    apply_fs(tmpdir, {'first/readthedocs.yml': ''})
-
-    base = str(tmpdir)
-    paths = set(find_all(base, r'readthedocs\.yml'))
-    assert paths == {
-        str(tmpdir.join('first', 'readthedocs.yml')),
-        str(tmpdir.join('third', 'readthedocs.yml')),
-    }
-
-
-def test_find_multiple_files(tmpdir):
-    apply_fs(tmpdir, {
-        'first': {
-            'readthedocs.yml': '',
-            '.readthedocs.yml': 'content',
-        },
-        'second': {
-            'confuser.txt': 'content',
-        },
-        'third': {
-            'readthedocs.yml': 'content',
-            'Makefile': '',
-        },
-    })
-    apply_fs(tmpdir, {'first/readthedocs.yml': ''})
-
-    base = str(tmpdir)
-    paths = set(find_all(base, r'\.?readthedocs\.yml'))
-    assert paths == {
-        str(tmpdir.join('first', 'readthedocs.yml')),
-        str(tmpdir.join('first', '.readthedocs.yml')),
-        str(tmpdir.join('third', 'readthedocs.yml')),
-    }
+    paths = find_one(base, r'readthedocs\.yml')
+    assert paths == str(os.path.abspath(os.path.join(base, 'readthedocs.yml')))
 
 
 @pytest.mark.skipif(not six.PY2, reason='Only for python2')
