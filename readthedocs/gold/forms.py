886a513190597091cb89306674149f8aa65982e9
dojutsu-user <vaibhgupt199@gmail.com>
2018-10-09 02:49:36 +0530

    Add import for ugettext_lazy

--- readthedocs/gold/forms.py
+++ readthedocs/gold/forms.py
@@ -5,6 +5,8 @@ from __future__ import absolute_import
 from builtins import object
 from django import forms
 
+from django.utils.translation import ugettext_lazy as _
+
 from readthedocs.payments.forms import StripeModelForm, StripeResourceMixin
 from readthedocs.projects.models import Project
 
