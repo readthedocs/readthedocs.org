571bf6cb561f0b3e53ec3fe1375c842049af3eb5
dojutsu-user <vaibhgupt199@gmail.com>
2018-11-07 21:35:09 +0530

    Clean constants.py

--- readthedocs/projects/constants.py
+++ readthedocs/projects/constants.py
@@ -13,35 +13,12 @@ import re
 
 from django.utils.translation import ugettext_lazy as _
 
-THEME_DEFAULT = 'default'
-THEME_SPHINX = 'sphinxdoc'
-THEME_SCROLLS = 'scrolls'
-THEME_AGOGO = 'agogo'
-THEME_TRADITIONAL = 'traditional'
-THEME_NATURE = 'nature'
-THEME_HAIKU = 'haiku'
-
 DOCUMENTATION_CHOICES = (
     ('auto', _('Automatically Choose')),
     ('sphinx', _('Sphinx Html')),
     ('mkdocs', _('Mkdocs (Markdown)')),
     ('sphinx_htmldir', _('Sphinx HtmlDir')),
     ('sphinx_singlehtml', _('Sphinx Single Page HTML')),
-)
-
-DEFAULT_THEME_CHOICES = (
-    # Translators: This is a name of a Sphinx theme.
-    (THEME_DEFAULT, _('Default')),
-    # Translators: This is a name of a Sphinx theme.
-    (THEME_SPHINX, _('Sphinx Docs')),
-    # (THEME_SCROLLS, 'Scrolls'),
-    # (THEME_AGOGO, 'Agogo'),
-    # Translators: This is a name of a Sphinx theme.
-    (THEME_TRADITIONAL, _('Traditional')),
-    # Translators: This is a name of a Sphinx theme.
-    (THEME_NATURE, _('Nature')),
-    # Translators: This is a name of a Sphinx theme.
-    (THEME_HAIKU, _('Haiku')),
 )
 
 SAMPLE_FILES = (
