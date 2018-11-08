f35d147868c88ff3d508e499db50fe5ea6fc9554
dojutsu-user <vaibhgupt199@gmail.com>
2018-11-08 14:53:06 +0530

    Disable django.security.DisallowedHost from logging

--- readthedocs/settings/base.py
+++ readthedocs/settings/base.py
@@ -390,6 +390,9 @@ class CommunityBaseSettings(Settings):
                 'filename': os.path.join(LOGS_ROOT, 'debug.log'),
                 'formatter': 'default',
             },
+            'null': {   
+                'class': 'logging.NullHandler',
+            },
         },
         'loggers': {
             '': {  # root logger
@@ -401,6 +404,10 @@ class CommunityBaseSettings(Settings):
                 'handlers': ['debug', 'console'],
                 'level': 'DEBUG',
                 # Don't double log at the root logger for these.
+                'propagate': False,
+            },
+            'django.security.DisallowedHost': {
+                'handlers': ['null'],
                 'propagate': False,
             },
         },
