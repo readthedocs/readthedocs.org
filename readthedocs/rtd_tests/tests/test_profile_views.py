b890b1f06775691a4cea868921eea0c12e29596e
dojutsu-user <vaibhgupt199@gmail.com>
2018-11-18 16:57:54 +0530

    Add tests

--- readthedocs/rtd_tests/tests/test_profile_views.py
+++ readthedocs/rtd_tests/tests/test_profile_views.py
@@ -35,6 +35,33 @@ class ProfileViewsTest(TestCase):
         self.assertEqual(self.user.last_name, 'Docs')
         self.assertEqual(self.user.profile.homepage, 'readthedocs.org')
 
+    def test_edit_profile_with_invalid_values(self):
+        resp = self.client.get(
+            reverse('profiles_profile_edit'),
+        )
+        self.assertTrue(resp.status_code, 200)
+
+        long_first_name = 'this-is-first_name-whose-length-is-greater-than-30'
+        long_last_name = 'this-is-last_name-whose-length-is-greater-than-30'
+        long_homepage = 'this-is-the-the-link-to-the-homepage-of-the-user-which-is-pretty-long-and-that-is-why-wrong-value.org'
+
+        self.assertTrue(len(long_first_name) > 30)
+        self.assertTrue(len(long_last_name) > 30)
+        self.assertTrue(len(long_homepage) > 100)
+
+        resp = self.client.post(
+            reverse('profiles_profile_edit'),
+            data={
+                'first_name': long_first_name,
+                'last_name': long_last_name,
+                'homepage': long_homepage,
+            }
+        )
+        
+        self.assertFormError(resp, form='form', field='first_name', errors='Ensure this value has at most 30 characters (it has {}).'.format(len(long_first_name)))
+        self.assertFormError(resp, form='form', field='last_name', errors='Ensure this value has at most 30 characters (it has {}).'.format(len(long_last_name)))
+        self.assertFormError(resp, form='form', field='homepage', errors='Ensure this value has at most 100 characters (it has {}).'.format(len(long_homepage)))
+
     def test_delete_account(self):
         resp = self.client.get(
             reverse('delete_account')
