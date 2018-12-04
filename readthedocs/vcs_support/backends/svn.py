375a646884bfd398361e05574844b3062af02b9f
dojutsu-user <vaibhgupt199@gmail.com>
2018-12-04 14:02:59 +0530

    Remove trailing slashes on svn checkout

--- readthedocs/vcs_support/backends/svn.py
+++ readthedocs/vcs_support/backends/svn.py
@@ -58,7 +58,7 @@ class Backend(BaseVCS):
     def co(self, identifier=None):
         self.make_clean_working_dir()
         if identifier:
-            url = self.base_url + identifier
+            url = self.base_url.rstrip('/') + identifier
         else:
             url = self.repo_url
         retcode, out, err = self.run('svn', 'checkout', url, '.')
