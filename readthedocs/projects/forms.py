a7b36569264cc1e8d816259f1d1c2ff853b49090
dojutsu-user <vaibhgupt199@gmail.com>
2018-12-12 13:45:39 +0530

    add default value

--- readthedocs/projects/forms.py
+++ readthedocs/projects/forms.py
@@ -183,13 +183,12 @@ class ProjectExtraForm(ProjectForm):
     )
 
     def clean_tags(self):
-        tags = self.cleaned_data.get('tags')
-        if tags:
-            for tag in tags:
-                if len(tag) > 100:
-                    raise forms.ValidationError(
-                        _('Length of each tag must be less than or equal to 100 characters.')
-                    )
+        tags = self.cleaned_data.get('tags', [])
+        for tag in tags:
+            if len(tag) > 100:
+                raise forms.ValidationError(
+                    _('Length of each tag must be less than or equal to 100 characters.')
+                )
         return tags
 
 
