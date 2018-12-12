b4969e03475b920344c62b3c499f970a3050507f
dojutsu-user <vaibhgupt199@gmail.com>
2018-12-12 23:01:28 +0530

    add option in version admin

--- readthedocs/builds/admin.py
+++ readthedocs/builds/admin.py
@@ -1,7 +1,7 @@
 """Django admin interface for `~builds.models.Build` and related models."""
 
 from __future__ import absolute_import
-from django.contrib import admin
+from django.contrib import admin, messages
 from readthedocs.builds.models import Build, Version, BuildCommandResult
 from guardian.admin import GuardedModelAdmin
 
@@ -29,6 +29,17 @@ class VersionAdmin(GuardedModelAdmin):
     list_display = ('slug', 'type', 'project', 'privacy_level', 'active', 'built')
     list_filter = ('type', 'privacy_level', 'active', 'built')
     raw_id_fields = ('project',)
+    actions = ['reindex']
+
+    def reindex(self, request, queryset):
+        for version in queryset:
+            try:
+                # Reindex version code
+                self.message_user(request, 'Reindexing triggered for {}'.format(version))
+            except Exception as e:
+                self.message_user(request, 'Reindexing fail for {}'.format(version), level=messages.ERROR)
+
+    reindex.short_description = 'Reindex selected versions'
 
 
 admin.site.register(Build, BuildAdmin)
