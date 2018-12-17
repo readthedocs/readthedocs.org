93673ed3fb403e952f858fb378f19a56192d68eb
Manuel Kaufmann <humitos@gmail.com>
2018-12-17 17:05:06 +0100

    Lint

--- readthedocs/restapi/authentication.py
+++ readthedocs/restapi/authentication.py
@@ -3,12 +3,13 @@ from rest_framework.authentication import SessionAuthentication
 
 
 class CsrfExemptSessionAuthentication(SessionAuthentication):
+
     """
     Session authentication class exempt of CSRF.
 
     DRF by default when using a ``SessionAuthentication`` it enforces CSRF.
 
-    See: https://github.com/encode/django-rest-framework/blob/3.9.0/rest_framework/authentication.py#L134-L144
+    See: https://github.com/encode/django-rest-framework/blob/3.9.0/rest_framework/authentication.py#L134-L144  # noqa
     """
 
     def enforce_csrf(self, request):
