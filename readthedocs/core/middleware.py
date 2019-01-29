6a549293150acd9182ae355ddc4187f65e78715e
Santos Gallegos <santos_g@outlook.com>
2019-01-29 10:26:16 -0500

    Isort

--- readthedocs/core/middleware.py
+++ readthedocs/core/middleware.py
@@ -1,12 +1,3 @@
-# -*- coding: utf-8 -*-
-
-from __future__ import (
-    absolute_import,
-    division,
-    print_function,
-    unicode_literals,
-)
-
 import logging
 
 from django.conf import settings
--- readthedocs/rtd_tests/tests/test_middleware.py
+++ readthedocs/rtd_tests/tests/test_middleware.py
@@ -1,12 +1,3 @@
-# -*- coding: utf-8 -*-
-
-from __future__ import (
-    absolute_import,
-    division,
-    print_function,
-    unicode_literals,
-)
-
 from corsheaders.middleware import CorsMiddleware
 from django.conf import settings
 from django.http import Http404
