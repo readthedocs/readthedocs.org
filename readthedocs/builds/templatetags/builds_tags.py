c20509ebf2c720a96f8f7f26c17f70af204351c1
dojutsu-user <vaibhgupt199@gmail.com>
2019-03-02 01:13:30 +0530

    use template tags

---
+++ readthedocs/builds/templatetags/builds_tags.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from django import template
+
+
+register = template.Library()
+
+
+@register.simple_tag
+def is_latest_built_success(version):
+    """
+    Checks the build status of the passed version.
+
+    Returns true if the latest build for the ``version``
+    is passed else returns false.
+    """
+    res = version.builds.all().order_by('-date').first().success
+    return res
