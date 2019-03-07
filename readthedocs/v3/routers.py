bbeb9b780908cf1322722669f1c68259345fe261
Manuel Kaufmann <humitos@gmail.com>
2019-03-07 19:41:58 +0100

    Add documentation to the root view of BrowsableAPI

--- readthedocs/v3/routers.py
+++ readthedocs/v3/routers.py
@@ -1,6 +1,21 @@
-from rest_framework.routers import DefaultRouter
+from rest_framework.routers import DefaultRouter, APIRootView
 from rest_framework_extensions.routers import NestedRouterMixin
 
 
+class DocsAPIRootView(APIRootView):
+
+    # Overridden only to add documentation for BrowsableAPIRenderer.
+
+    """
+    Read the Docs APIv3 root endpoint.
+
+    Full documentation at [https://docs.readthedocs.io/en/latest/api/v3.html](https://docs.readthedocs.io/en/latest/api/v3.html).
+    """
+
+    def get_view_name(self):
+        return 'Read the Docs APIv3'
+
+
 class DefaultRouterWithNesting(NestedRouterMixin, DefaultRouter):
-    pass
+    APIRootView = DocsAPIRootView
+    root_view_name = 'api-v3-root'
