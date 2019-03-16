4b7fc50097655caa55a4cede4436cfa5bff18a8a
saadmk11 <saad.mk11@hotmail.com>
2019-03-16 16:11:48 +0600

    Profile page performance issue Fix.

--- readthedocs/core/templatetags/privacy_tags.py
+++ readthedocs/core/templatetags/privacy_tags.py
@@ -3,7 +3,9 @@
 """Template tags to query projects by privacy."""
 
 from django import template
+from django.db.models import Exists, OuterRef, Subquery
 
+from readthedocs.builds.models import Build
 from readthedocs.core.permissions import AdminPermission
 from readthedocs.projects.models import Project
 
@@ -18,9 +20,13 @@ def is_admin(user, project):
 
 @register.simple_tag(takes_context=True)
 def get_public_projects(context, user):
+    builds = Build.objects.filter(project=OuterRef('pk'))
+    date_sub_query = Subquery(builds.values('date')[:1])
     projects = Project.objects.for_user_and_viewer(
         user=user,
         viewer=context['request'].user,
-    )
+    ).prefetch_related('users').annotate(
+        latest_build_date=date_sub_query,
+        good_build=Exists(builds.filter(success=True)))
     context['public_projects'] = projects
     return ''
