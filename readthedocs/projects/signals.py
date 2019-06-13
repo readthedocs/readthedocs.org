8cd2ad102129b9b44926fe157ac6aaecc04302c6
Santos Gallegos <santos_g@outlook.com>
2019-06-13 13:16:56 -0500

    Bring back files_changed signal

--- readthedocs/projects/signals.py
+++ readthedocs/projects/signals.py
@@ -11,5 +11,8 @@ after_build = django.dispatch.Signal(providing_args=['version'])
 
 project_import = django.dispatch.Signal(providing_args=['project'])
 
+# Used to purge files from the CDN
+files_changed = django.dispatch.Signal(providing_args=['project', 'files'])
+
 # Used to force verify a domain (eg. for SSL cert issuance)
 domain_verify = django.dispatch.Signal(providing_args=['domain'])
