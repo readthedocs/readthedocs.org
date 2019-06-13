f2ddf9444b6cbfb54e8f7609dc321ad17ea8451c
Manuel Kaufmann <humitos@gmail.com>
2019-06-13 14:35:46 +0200

    Remove no-needed URL
    This was moved to Corporate project, where it belongs.

--- readthedocs/core/urls/subdomain.py
+++ readthedocs/core/urls/subdomain.py
@@ -47,17 +47,6 @@ subdomain_urls = [
         name='docs_detail',
     ),
 
-    # Special case for single-version subprojects
-    # docs.customdomain.org/projects/subproject/_static/readthedocs-data.js
-    # docs.customdomain.org/projects/subproject/chapter/section/page.html
-    url(
-        (
-            r'^projects/(?P<subproject_slug>{project_slug})/'
-            r'(?P<filename>{filename_slug})$'.format(**pattern_opts)
-        ),
-        serve_docs,
-        name='docs_detail_singleversion_subproject',
-    ),
 ]
 
 groups = [subdomain_urls]
