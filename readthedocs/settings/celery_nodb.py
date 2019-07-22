d855f29be7aa9a64c99ee43b38ea3e6277f3c195
Eric Holscher <eric@ericholscher.com>
2019-07-22 16:35:39 -0700

    Add a `celery_nodb` settings file for dev
    This allows us to have a specific settings file to run Celery against,
    which will test not having access to the DB like prod.

---
+++ readthedocs/settings/celery_nodb.py
@@ -0,0 +1,26 @@
+"""Local setting for celery without DB access"""
+from __future__ import absolute_import
+
+import os
+
+from .dev import CommunityDevSettings
+
+
+class CeleryDevSettings(CommunityDevSettings):
+    @property
+    def DATABASES(self):  # noqa
+        return {
+            'default': {}
+        }
+
+    DONT_HIT_DB = True
+
+
+CeleryDevSettings.load_settings(__name__)
+
+if not os.environ.get('DJANGO_SETTINGS_SKIP_LOCAL', False):
+    try:
+        # pylint: disable=unused-wildcard-import
+        from .local_settings import *  # noqa
+    except ImportError:
+        pass
