8b9eafdd29192c1c57a6eee7e0113145126c15ce
dojutsu-user <vaibhgupt199@gmail.com>
2019-08-01 19:30:35 +0530

    update admin.py

--- readthedocs/search/admin.py
+++ readthedocs/search/admin.py
@@ -7,7 +7,7 @@ from .models import SearchQuery
 
 class SearchQueryAdmin(admin.ModelAdmin):
     list_filter = ('created',)
-    list_display = ('__str__', 'count', 'created', 'modified')
+    list_display = ('__str__', 'created')
     search_fields = ('project__slug', 'version__slug', 'query')
     readonly_fields = ('created', 'modified')
     list_select_related = ('project', 'version', 'version__project')
