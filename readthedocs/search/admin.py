3497800f1612e64bf615afc507122ab29e3bd967
Eric Holscher <eric@ericholscher.com>
2019-08-07 12:07:36 -0700

    Fix raw_id_fields on SearchQueryAdmin

--- readthedocs/search/admin.py
+++ readthedocs/search/admin.py
@@ -6,6 +6,7 @@ from .models import SearchQuery
 
 
 class SearchQueryAdmin(admin.ModelAdmin):
+    raw_id_fields = ('project', 'version')
     list_filter = ('created',)
     list_display = ('__str__', 'created')
     search_fields = ('project__slug', 'version__slug', 'query')
