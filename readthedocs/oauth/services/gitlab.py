ca9c9917205fd96ca3a438d29858176105beb017
saadmk11 <saad.mk112@gmail.com>
2019-08-16 20:40:10 +0600

    GitLab Build Status Reporting added

--- readthedocs/oauth/services/gitlab.py
+++ readthedocs/oauth/services/gitlab.py
@@ -9,6 +9,11 @@ from django.conf import settings
 from django.urls import reverse
 from requests.exceptions import RequestException
 
+from readthedocs.builds.constants import (
+    BUILD_STATUS_SUCCESS,
+    RTD_BUILD_STATUS_API_NAME,
+    SELECT_BUILD_STATUS,
+)
 from readthedocs.builds.utils import get_gitlab_username_repo
 from readthedocs.integrations.models import Integration
 from readthedocs.projects.models import Project
@@ -387,3 +392,90 @@ class GitLabService(Service):
                 debug_data = resp.content
             log.debug('GitLab webhook update failure response: %s', debug_data)
             return (False, resp)
+
+    def send_build_status(self, build, commit, state):
+        """
+        Create GitLab commit status for project.
+
+        :param build: Build to set up commit status for
+        :type build: Build
+        :param state: build state failure, pending, or success.
+        :type state: str
+        :param commit: commit sha of the pull request
+        :type commit: str
+        :returns: boolean based on commit status creation was successful or not.
+        :rtype: Bool
+        """
+        session = self.get_session()
+        project = build.project
+
+        repo_id = self._get_repo_id(project)
+
+        if repo_id is None:
+            return (False, None)
+
+        # select the correct state and description.
+        gitlab_build_state = SELECT_BUILD_STATUS[state]['gitlab']
+        description = SELECT_BUILD_STATUS[state]['description']
+
+        target_url = build.get_full_url()
+
+        if state == BUILD_STATUS_SUCCESS:
+            target_url = build.version.get_absolute_url()
+
+        data = {
+            'state': gitlab_build_state,
+            'target_url': target_url,
+            'description': description,
+            'context': RTD_BUILD_STATUS_API_NAME
+        }
+        url = self.adapter.provider_base_url
+
+        resp = None
+
+        try:
+            resp = session.post(
+                f'{url}/api/v4/projects/{repo_id}/statuses/{commit}',
+                data=json.dumps(data),
+                headers={'content-type': 'application/json'},
+            )
+
+            if resp.status_code == 201:
+                log.info(
+                    "GitLab commit status created for project: %s, commit status: %s",
+                    project,
+                    gitlab_build_state,
+                )
+                return True
+
+            if resp.status_code in [401, 403, 404]:
+                log.info(
+                    'GitLab project does not exist or user does not have '
+                    'permissions: project=%s',
+                    project,
+                )
+                return False
+
+            return False
+
+        # Catch exceptions with request or deserializing JSON
+        except (RequestException, ValueError):
+            log.exception(
+                'GitLab commit status creation failed for project: %s',
+                project,
+            )
+            # Response data should always be JSON, still try to log if not
+            # though
+            if resp is not None:
+                try:
+                    debug_data = resp.json()
+                except ValueError:
+                    debug_data = resp.content
+            else:
+                debug_data = resp
+
+            log.debug(
+                'GitLab commit status creation failure response: %s',
+                debug_data,
+            )
+            return False
