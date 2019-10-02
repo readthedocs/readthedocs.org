05efe3a618b1eeeff616116a36e0c61fc80e9866
Santos Gallegos <santos_g@outlook.com>
2019-10-02 13:42:52 -0500

    Translate help text

--- readthedocs/builds/forms.py
+++ readthedocs/builds/forms.py
@@ -21,7 +21,7 @@ class VersionForm(HideProtectedLevelMixin, forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['slug'].help_text = 'Warning: changing the slug will break existing URLs'
+        self.fields['slug'].help_text = _('Warning: changing the slug will break existing URLs')
 
         if self.instance.pk and self.instance.machine:
             self.fields['slug'].disabled = True
