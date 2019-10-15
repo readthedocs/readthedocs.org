876f3e53be76f9bbb8683f801d2cb82d256776fe
Manuel Kaufmann <humitos@gmail.com>
2019-10-15 20:29:21 +0200

    docker-compose setting files

--- docker/entrypoints/web.sh
+++ docker/entrypoints/web.sh
@@ -1,8 +1,11 @@
 #! /bin/sh
 
+# pip install -r requirements/deploy.txt
+# pip install -e ../readthedocs-ext/
+
 python manage.py migrate
 cat ../../docker/scripts/createsuperuser.py | python manage.py shell
 python manage.py collectstatic --no-input
 python manage.py loaddata test_data
 
-gunicorn readthedocs.wsgi:application -w 3 -b :8000
+python manage.py runserver 0.0.0.0:8000
