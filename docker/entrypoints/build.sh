4b77abd208af73e887fa5fea5537cdf7bc5f6f91
Manuel Kaufmann <humitos@gmail.com>
2019-10-18 23:27:24 +0200

    More work
    - use volumes
    - use azure-cli to create the containers at startup
    - fix requirements
    - better headers for El Proxito

--- docker/entrypoints/build.sh
+++ docker/entrypoints/build.sh
@@ -1,6 +1,3 @@
 #! /bin/sh
 
-# pip install -r requirements/deploy.txt
-# pip install -e ../readthedocs-ext/
-
 python3 -m celery worker -A readthedocs.worker -Ofair -c 2 -Q builder,celery,default,build01 -l DEBUG
--- docker/entrypoints/web.sh
+++ docker/entrypoints/web.sh
@@ -1,11 +1,9 @@
 #! /bin/sh
 
-# pip install -r requirements/deploy.txt
-# pip install -e ../readthedocs-ext/
 
-python manage.py migrate
-cat ../../docker/scripts/createsuperuser.py | python manage.py shell
-python manage.py collectstatic --no-input
-python manage.py loaddata test_data
+python3 manage.py migrate
+cat ../../docker/scripts/createsuperuser.py | python3 manage.py shell
+python3 manage.py collectstatic --no-input
+python3 manage.py loaddata test_data
 
-python manage.py runserver 0.0.0.0:8000
+python3 manage.py runserver 0.0.0.0:8000
