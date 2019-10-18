d5ae2e14b8882fbff9cb578109f1259fd7ad2de7
Manuel Kaufmann <humitos@gmail.com>
2019-10-18 23:27:24 +0200

    More work
    - use volumes
    - use azure-cli to create the containers at startup
    - fix requirements
    - better headers for El Proxito

--- docker/entrypoints/celery.sh
+++ docker/entrypoints/celery.sh
@@ -1,6 +1,3 @@
 #! /bin/sh
 
-# pip install -r requirements/deploy.txt
-# pip install -e ../readthedocs-ext/
-
 python3 -m celery worker -A readthedocs.worker -Ofair -c 2 -Q web,web01,reindex -l DEBUG
