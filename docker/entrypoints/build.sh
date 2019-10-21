f07349066cea3f0b3608dcb4767648c21d0965fe
Manuel Kaufmann <humitos@gmail.com>
2019-10-21 15:49:24 +0200

    Use watchdog to auto restart celery processes

--- docker/entrypoints/build.sh
+++ docker/entrypoints/build.sh
@@ -1,3 +1,12 @@
 #! /bin/sh
 
-python3 -m celery worker -A readthedocs.worker -Ofair -c 2 -Q builder,celery,default,build01 -l DEBUG
+watchmedo auto-restart \
+  --patterns="*.py" \
+  --ignore-patterns="*.#*.py;./user_builds/*;./public_*;./private_*;*.pyo;*.pyc;*flycheck*.py;./media/*;./.tox/*" \
+  --ignore-directories \
+  --recursive \
+  --signal=SIGTERM \
+  --kill-after=5 \
+  --interval=5 \
+  -- \
+  python3 -m celery worker -A readthedocs.worker -Ofair -c 2 -Q builder,celery,default,build01 -l DEBUG
