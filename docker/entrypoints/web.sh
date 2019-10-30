bf8e3f5abda4691478f83b52e82aabb1cbcad202
Manuel Kaufmann <humitos@gmail.com>
2019-10-30 13:58:07 +0100

    Run collectstatic in background to speed up initialization

--- docker/entrypoints/web.sh
+++ docker/entrypoints/web.sh
@@ -4,7 +4,10 @@
 
 python3 manage.py migrate
 cat ../../docker/scripts/createsuperuser.py | python3 manage.py shell
-python3 manage.py collectstatic --no-input
+
+# collect static in background
+python3 manage.py collectstatic --no-input &
+
 python3 manage.py loaddata test_data
 
 python3 manage.py runserver 0.0.0.0:8000
