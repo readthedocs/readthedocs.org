ba0ccfb9afad7ac010c4392d3ca3576a54811ded
Santos Gallegos <santos_g@outlook.com>
2019-11-11 15:52:34 -0500

    Move js to a file

---
+++ readthedocs/projects/static-src/projects/js/automation-rules.js
@@ -0,0 +1,27 @@
+var $ = require('jquery');
+
+function set_help_text(value) {
+  var help_texts = {
+    'all-versions': 'All versions will be matched.',
+    'semver-versions': 'Versions incremented based on semantic versioning rules will be matched.',
+    '': ''
+  };
+  $('#id_predefined_match_arg').siblings('.helptext').text(help_texts[value]);
+}
+
+$(function () {
+  var value = $('#id_predefined_match_arg').val();
+  if (value !== '') {
+    $('#id_match_arg').parent().hide();
+  }
+  set_help_text(value);
+
+  $('#id_predefined_match_arg').bind('change', function(ev) {
+    if (this.value === '') {
+      $('#id_match_arg').parent().show();
+    } else {
+      $('#id_match_arg').parent().hide();
+    }
+    set_help_text(this.value);
+  });
+});
