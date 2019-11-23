ddeffc09ce1eab426fe46129bead712059f93f45
Manuel Kaufmann <humitos@gmail.com>
2019-11-23 22:00:42 +0100

    Remove DEBUG=False that's not needed anymore
    Now we are serving 404s via El Proxito and forcing DEBUG=False is not
    needed anymore.

--- docker/settings/web.py
+++ docker/settings/web.py
@@ -1,9 +1,8 @@
 from .docker_compose import DockerBaseSettings
 
-class WebDevSettings(DockerBaseSettings):
 
-    # Needed to serve 404 pages properly
-    # NOTE: it may introduce some strange behavior
-    DEBUG = False
+class WebDevSettings(DockerBaseSettings):
+    pass
+
 
 WebDevSettings.load_settings(__name__)
