4e21e68335cf3c38c90c47f170681ef0d82c527d
Manuel Kaufmann <humitos@gmail.com>
2019-11-25 16:42:11 +0100

    Update tests to match the current behavior

--- readthedocs/rtd_tests/tests/test_project_views.py
+++ readthedocs/rtd_tests/tests/test_project_views.py
@@ -406,7 +406,9 @@ class TestPublicViews(MockBuildTestCase):
     def test_project_download_media(self):
         url = reverse('project_download_media', args=[self.pip.slug, 'pdf', LATEST])
         response = self.client.get(url)
-        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response._headers['x-accel-redirect'][1], '/proxito/media/pdf/pip/latest/pip.pdf')
+        self.assertEqual(response._headers['content-disposition'][1], 'filename=pip-latest.pdf')
 
     def test_project_detail_view_only_shows_internal_versons(self):
         url = reverse('projects_detail', args=[self.pip.slug])
