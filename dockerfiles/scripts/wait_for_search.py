5b6cb01bd280c13b96837ab10b411dce7af3c120
Manuel Kaufmann <humitos@gmail.com>
2019-12-04 22:34:51 +0100

    Wait for search before running celery process

---
+++ dockerfiles/scripts/wait_for_search.py
@@ -0,0 +1,19 @@
+import os
+import time
+import requests
+
+wait_sleep = 3
+max_loops = 15
+
+loops = 1
+while loops < max_loops:
+    try:
+        print('Waiting for search service...')
+        response = requests.get('http://search:9200/_cluster/health')
+        if response.status_code == 200:
+            print('Search is ready!')
+            break
+        loops += 1
+    except:
+        pass
+    time.sleep(wait_sleep)
