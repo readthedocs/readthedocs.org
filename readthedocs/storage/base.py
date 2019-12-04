ea5291937b7cf6d0bdfe9f92c4717fc19a9281a1
Manuel Kaufmann <humitos@gmail.com>
2019-12-04 11:43:10 +0100

    Bring Azure storage backend classes to this repository
    This commit allow us to build and run all the Read the Docs stack
    without depending on readthedocs-ext repository allowing anyone to
    launch "docker-compose up"

---
+++ readthedocs/storage/base.py
@@ -0,0 +1,23 @@
+from urllib.parse import urlsplit, urlunsplit
+
+class OverrideHostnameMixin:
+
+    """
+    Override the hostname when outputting URLs.
+
+    This is useful for use with a CDN or when proxying outside of Blob Storage
+
+    See: https://github.com/jschneier/django-storages/pull/658
+    """
+
+    override_hostname = None    # Just the hostname without scheme (eg. 'assets.readthedocs.org')
+
+    def url(self, *args, **kwargs):
+        url = super().url(*args, **kwargs)
+
+        if self.override_hostname:
+            parts = list(urlsplit(url))
+            parts[1] = self.override_hostname
+            url = urlunsplit(parts)
+
+        return url
