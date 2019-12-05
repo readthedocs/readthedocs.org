9a9eee9fb1006dd68ce11580ba3458f6ace50da1
Manuel Kaufmann <humitos@gmail.com>
2019-12-05 17:53:55 +0100

    Remove unneeded comment

--- dockerfiles/entrypoints/web.sh
+++ dockerfiles/entrypoints/web.sh
@@ -7,10 +7,7 @@ then
     echo "Performing initial tasks..."
     python3 manage.py migrate
     cat ../../docker/createsuperuser.py | python3 manage.py shell
-
-    # collect static in background
     python3 manage.py collectstatic --no-input
-
     python3 manage.py loaddata test_data
 fi
 
