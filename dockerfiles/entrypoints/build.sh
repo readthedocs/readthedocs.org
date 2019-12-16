1458c48732676815cbe0d3bedf4d283f812b5f5d
Eric Holscher <eric@ericholscher.com>
2019-12-16 12:26:43 +0100

    Add DOCKER_NORELOAD to compose settings
    This stops containers from auto-reloading.

--- dockerfiles/entrypoints/build.sh
+++ dockerfiles/entrypoints/build.sh
@@ -1,8 +1,15 @@
-#! /bin/sh
+#! /bin/bash
 
 ../../docker/common.sh
 
-watchmedo auto-restart \
+CMD='python3 -m celery worker -A readthedocs.worker -Ofair -c 2 -Q builder,celery,default,build01 -l DEBUG'
+
+if [ -n "${DOCKER_NO_RELOAD}" ]; then
+  echo "Running Docker with no reload"
+  $CMD
+else
+  echo "Running Docker with reload"
+  watchmedo auto-restart \
   --patterns="*.py" \
   --ignore-patterns="*.#*.py;./user_builds/*;./public_*;./private_*;*.pyo;*.pyc;*flycheck*.py;./media/*;./.tox/*" \
   --ignore-directories \
@@ -11,4 +18,6 @@ watchmedo auto-restart \
   --kill-after=5 \
   --interval=5 \
   -- \
-  python3 -m celery worker -A readthedocs.worker -Ofair -c 2 -Q builder,celery,default,build01 -l DEBUG
+  $CMD
+fi
+
--- dockerfiles/entrypoints/web.sh
+++ dockerfiles/entrypoints/web.sh
@@ -11,4 +11,13 @@ then
     python3 manage.py loaddata test_data
 fi
 
-python3 manage.py runserver 0.0.0.0:8000
+if [ -n "${DOCKER_NO_RELOAD}" ]; 
+then
+    RELOAD='--noreload'
+    echo "Running Docker with no reload"
+else
+    RELOAD=''
+    echo "Running Docker with reload"
+fi
+
+python3 manage.py runserver 0.0.0.0:8000 $RELOAD
