43186ad5b3c1550c58dc5ec4f27b6623eb645c8e
Manuel Kaufmann <humitos@gmail.com>
2019-12-23 17:07:18 +0100

    Split PublicDetailPrivateListing permission class
    Move out the logic that authorize a user hitting /projects/ and create
    a different class for this (it's used in corporate as well).
    
    Leave the PublicDetailPrivateListing class only with the needed logic
    for this goal: detail access is public and listing access is private.

--- readthedocs/api/v3/permissions.py
+++ readthedocs/api/v3/permissions.py
@@ -1,5 +1,18 @@
 from rest_framework.permissions import IsAuthenticated, BasePermission
 
+from readthedocs.core.utils.extend import SettingsOverrideObject
+
+
+class UserProjectsListing(BasePermission):
+    def has_permission(self, request, view):
+        if view.basename == 'projects' and any([
+                view.action == 'list',
+                view.action == 'create',  # used to create Form in BrowsableAPIRenderer
+                view.action is None,  # needed for BrowsableAPIRenderer
+        ]):
+            # hitting ``/projects/``, allowing
+            return True
+
 
 class PublicDetailPrivateListing(IsAuthenticated):
 
@@ -12,29 +25,19 @@ class PublicDetailPrivateListing(IsAuthenticated):
     """
 
     def has_permission(self, request, view):
-        is_authenticated = super().has_permission(request, view)
-        if is_authenticated:
-            if view.basename == 'projects' and any([
-                    view.action == 'list',
-                    view.action == 'create',  # used to create Form in BrowsableAPIRenderer
-                    view.action is None,  # needed for BrowsableAPIRenderer
-            ]):
-                # hitting ``/projects/``, allowing
-                return True
+        # NOTE: ``superproject`` is an action name, defined by the class
+        # method under ``ProjectViewSet``. We should apply the same
+        # permissions restrictions than for a detail action (since it only
+        # returns one superproject if exists). ``list`` and ``retrieve`` are
+        # DRF standard action names (same as ``update`` or ``partial_update``).
+        if view.detail and view.action in ('list', 'retrieve', 'superproject'):
+            # detail view is only allowed on list/retrieve actions (not
+            # ``update`` or ``partial_update``).
+            return True
 
-            # NOTE: ``superproject`` is an action name, defined by the class
-            # method under ``ProjectViewSet``. We should apply the same
-            # permissions restrictions than for a detail action (since it only
-            # returns one superproject if exists). ``list`` and ``retrieve`` are
-            # DRF standard action names (same as ``update`` or ``partial_update``).
-            if view.detail and view.action in ('list', 'retrieve', 'superproject'):
-                # detail view is only allowed on list/retrieve actions (not
-                # ``update`` or ``partial_update``).
-                return True
-
-            project = view._get_parent_project()
-            if view.has_admin_permission(request.user, project):
-                return True
+        project = view._get_parent_project()
+        if view.has_admin_permission(request.user, project):
+            return True
 
         return False
 
