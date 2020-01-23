88cbc3887207b02bcc31e87c78d7fa978650a378
Eric Holscher <25510+ericholscher@users.noreply.github.com>
2020-01-22 16:50:05 -0800

    Use our standard auth mixin for proxito downloads (#6572)
    * Use our standard auth mixin for proxito downloads
    
    This is the same logic as in the Proxito views,
    it just extends it to the downloads.
    
    * re-add 404 logic
    
    * Add a bit of janky hacks
    
    * Remove functions to remove hacky isinstance
    
    * Fix lint

--- readthedocs/proxito/views/serve.py
+++ readthedocs/proxito/views/serve.py
@@ -146,9 +146,6 @@ class ServeDocsBase(ServeRedirectMixin, ServeDocsMixin, View):
             path=final_url,
         )
 
-    def allowed_user(self, *args, **kwargs):
-        return True
-
 
 class ServeDocs(SettingsOverrideObject):
     _default_class = ServeDocsBase
