f1a65e8e8f9091b5b812b76db395d1a4685dc05d
Manuel Kaufmann <humitos@gmail.com>
2020-01-23 16:34:35 +0100

    Comment

--- readthedocs/proxito/tests/mixins.py
+++ readthedocs/proxito/tests/mixins.py
@@ -1,11 +1,18 @@
-from readthedocs.rtd_tests.storage import BuildMediaFileSystemStorageTest
+from readthedocs.proxito.tests.storage import BuildMediaStorageTest
 
 
 class MockStorageMixin:
 
+    """
+    Mixin to controls ``BuildMediaStorageTests.exists`` method.
+
+    This mixin provides a helper to update which files does exist in the storage
+    backend and reset them to an empty list on tear down.
+    """
+
     def tearDown(self):
         super().tearDown()
-        BuildMediaFileSystemStorageTest._existing_files = []
+        BuildMediaStorageTest._existing_files = []
 
     def _storage_exists(self, files):
-        BuildMediaFileSystemStorageTest._existing_files = files
+        BuildMediaStorageTest._existing_files = files
