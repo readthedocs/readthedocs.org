27ded3ffc85e2f2450f6c50cd45a4f946df39ce2
David Fischer <david@readthedocs.org>
2020-02-13 10:39:00 -0800

    Remove check on the number of fields

--- readthedocs/settings/docker_compose.py
+++ readthedocs/settings/docker_compose.py
@@ -124,4 +124,6 @@ class DockerBaseSettings(CommunityDevSettings):
     AZURE_CUSTOM_DOMAIN = 'storage:10000'
     AZURE_SSL = False
 
-    DATA_UPLOAD_MAX_NUMBER_FIELDS = 1000000
+    # Remove the checks on the number of fields being submitted
+    # This limit is mostly hit on large forms in the Django admin
+    DATA_UPLOAD_MAX_NUMBER_FIELDS = None
