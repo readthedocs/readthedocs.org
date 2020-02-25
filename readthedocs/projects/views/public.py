ede9fb0d7ff1ed3a4868ba3884a0ee96b67da915
Eric Holscher <eric@ericholscher.com>
2020-02-25 07:28:26 -0800

    Ignore silly lint issue

--- readthedocs/projects/views/public.py
+++ readthedocs/projects/views/public.py
@@ -60,7 +60,7 @@ class ProjectTagIndex(ListView):
 
         return queryset
 
-    def get_context_data(self, **kwargs):
+    def get_context_data(self, **kwargs):  # pylint: disable=arguments-differ
         context = super().get_context_data(**kwargs)
         context['tag'] = self.tag
         return context
