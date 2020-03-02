6890b0bb7f86cebe5a516ac753c5d286f31a1c8a
Eric Holscher <eric@ericholscher.com>
2020-03-02 15:41:28 -0800

    Fix lint

--- readthedocs/projects/tasks.py
+++ readthedocs/projects/tasks.py
@@ -159,13 +159,19 @@ class SyncRepositoryMixin:
         """
         version_post_data = {'repo': version_repo.repo_url}
 
-        if version_repo.supports_tags and not self.project.has_feature(Feature.SKIP_SYNC_TAGS):
+        if all(
+            version_repo.supports_tags,
+            not self.project.has_feature(Feature.SKIP_SYNC_TAGS)
+        ):
             version_post_data['tags'] = [{
                 'identifier': v.identifier,
                 'verbose_name': v.verbose_name,
             } for v in version_repo.tags]
 
-        if version_repo.supports_branches and not self.project.has_feature(Feature.SKIP_SYNC_BRANCHES):
+        if all(
+            version_repo.supports_branches,
+            not self.project.has_feature(Feature.SKIP_SYNC_BRANCHES)
+        ):
             version_post_data['branches'] = [{
                 'identifier': v.identifier,
                 'verbose_name': v.verbose_name,
