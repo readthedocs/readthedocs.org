fb0bb548cebf1860e5851e10d6c7a4af0ee84538
Manuel Kaufmann <humitos@gmail.com>
2020-03-04 15:01:08 -0800

    Define SUPPORT_EMAIL setting and use it where its needed

--- readthedocs/settings/base.py
+++ readthedocs/settings/base.py
@@ -59,7 +59,7 @@ class CommunityBaseSettings(Settings):
     # Email
     DEFAULT_FROM_EMAIL = 'no-reply@readthedocs.org'
     SERVER_EMAIL = DEFAULT_FROM_EMAIL
-    SUPPORT_EMAIL = None
+    SUPPORT_EMAIL = 'support@readthedocs.org'
 
     # Sessions
     SESSION_COOKIE_DOMAIN = 'readthedocs.org'
