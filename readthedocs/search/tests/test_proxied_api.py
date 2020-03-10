518c3dd257075b3b8e2d8a8b06694ff8fc56ae72
Santos Gallegos <santos_g@outlook.com>
2020-03-09 19:59:44 -0500

    Fix tests

--- readthedocs/search/tests/test_proxied_api.py
+++ readthedocs/search/tests/test_proxied_api.py
@@ -9,5 +9,9 @@ class TestProxiedSearchAPI(BaseTestDocumentSearch):
 
     host = 'pip.readthedocs.io'
 
+    @pytest.fixture(autouse=True)
+    def setup_settings(self, settings):
+        settings.PUBLIC_DOMAIN = 'readthedocs.io'
+
     def get_search(self, api_client, search_params):
         return api_client.get(self.url, search_params, HTTP_HOST=self.host)
