8c9c473a5153d3ea7f7ba83f1948dcd623ae33c4
Manuel Kaufmann <humitos@gmail.com>
2020-03-10 19:15:53 -0700

    Update settings

--- readthedocs/settings/docker_compose.py
+++ readthedocs/settings/docker_compose.py
@@ -117,6 +117,7 @@ class DockerBaseSettings(CommunityDevSettings):
     RTD_BUILD_MEDIA_STORAGE = 'readthedocs.storage.azure_storage.AzureBuildMediaStorage'
     AZURE_STATIC_STORAGE_HOSTNAME = PRODUCTION_DOMAIN
 
+    # Storage backend for build cached environments
     RTD_BUILD_ENVIRONMENT_STORAGE = 'readthedocs.storage.azure_storage.AzureBuildEnvironmentStorage'
 
     # Storage for static files (those collected with `collectstatic`)
