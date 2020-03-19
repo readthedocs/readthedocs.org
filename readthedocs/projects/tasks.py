798fc0fdf6e57815c2b6e8e8600069a9c3b6edb9
Manuel Kaufmann <humitos@gmail.com>
2020-03-18 20:08:06 -0700

    Do not reset the build start time when running build env
    We are creating one Setup Environment and another Build Environment.
    Once each of them is created they define the `start_time` which is UTC
    now. Then they use the `start_time` to calculate the `length` of the
    build each time the build object is updated via the API.
    
    Since the Build Environment calls again UTC now, we override the old
    `start_time` and we loose track of the timing for the Setup
    Environment (close repository, among others).
    
    This commit saves the original `start_time` and passes it to the Build
    Environment so it continues calculating the difference for the
    `length` considering the Setup Environment times as well.

--- readthedocs/projects/tasks.py
+++ readthedocs/projects/tasks.py
@@ -473,6 +473,7 @@ class UpdateDocsTaskStep(SyncRepositoryMixin, CachedEnvironmentMixin):
         if config is not None:
             self.config = config
         self.task = task
+        self.build_start_time = None
         # TODO: remove this
         self.setup_env = None
 
@@ -577,6 +578,7 @@ class UpdateDocsTaskStep(SyncRepositoryMixin, CachedEnvironmentMixin):
             update_on_success=False,
             environment=self.get_rtd_env_vars(),
         )
+        self.build_start_time = environment.start_time
 
         # TODO: Remove.
         # There is code that still depends of this attribute
@@ -667,6 +669,9 @@ class UpdateDocsTaskStep(SyncRepositoryMixin, CachedEnvironmentMixin):
             build=self.build,
             record=record,
             environment=env_vars,
+
+            # Pass ``start_time`` here to not reset the timer
+            start_time=self.build_start_time,
         )
 
         # Environment used for building code, usually with Docker
