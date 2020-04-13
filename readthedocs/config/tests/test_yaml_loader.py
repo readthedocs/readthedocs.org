9e25fa3ae007b1a5076592a97032c7fa182fb131
Manuel Kaufmann <humitos@gmail.com>
2020-04-13 22:18:53 +0200

    Add test for yaml_load_safely

---
+++ readthedocs/config/tests/test_yaml_loader.py
@@ -0,0 +1,18 @@
+import textwrap
+from readthedocs.config.utils import yaml_load_safely
+
+
+def test_yaml_load_safely():
+    expected = {
+        'int': 3,
+        'float': 3.0,
+        'unknown': None,
+    }
+
+    content = textwrap.dedent('''
+    int: 3
+    float: !!float 3
+    unknown: !!python/name:unknown_funtion
+    ''')
+    data = yaml_load_safely(content)
+    assert data == expected
