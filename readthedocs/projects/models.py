f239442a1b18b461f6b631b7a9a74b3d7bb52095
Eric Holscher <eric@ericholscher.com>
2020-05-18 11:46:01 -0700

    Add a project-level configuration for PR builds
    This allows users with the feature flag to enable/disable this feature.

--- readthedocs/projects/models.py
+++ readthedocs/projects/models.py
@@ -202,6 +202,12 @@ class Project(models.Model):
         ),
     )
 
+    external_builds_enabled = models.BooleanField(
+        _('Build pull requests for this project'),
+        default=False,
+        help_text=_('More information in <a href="https://docs.readthedocs.io/en/latest/guides/autobuild-docs-for-pull-requests.html">our docs</a>')  # noqa
+    )
+
     # Project features
     cdn_enabled = models.BooleanField(_('CDN Enabled'), default=False)
     analytics_code = models.CharField(
