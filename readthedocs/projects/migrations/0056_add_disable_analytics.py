48e04895d206eddb943c07020120ed12798b1bf6
David Fischer <david@readthedocs.org>
2020-06-15 22:09:04 -0700

    Fix the project migration conflict

--- readthedocs/projects/migrations/0056_add_disable_analytics.py
+++ readthedocs/projects/migrations/0056_add_disable_analytics.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('projects', '0054_urlconf_blank'),
+        ('projects', '0055_change_help_text_description'),
     ]
 
     operations = [
