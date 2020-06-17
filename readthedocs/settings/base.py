d2677375b53a523b6fa1a1e9040ec4398bf18bc8
Manuel Kaufmann <humitos@gmail.com>
2020-06-17 14:07:56 +0200

    Use total_memory to calculate "time" Docker limit
    4Gb servers:
     - total memory: 3417
     - memory limit: 2700
     - time limit (using memory limit): 700
     - time limit (using total memory): 900 (15 minutes)
    
    8Gb servers:
     - total memory: 7935
     - memory limit: 7200
     - time limit (using memory limit): 1800
     - time limit (using total memory): 2000 (33 minutes)
    
    These numbers are more similar to what we communicate in
    https://docs.readthedocs.io/en/stable/builds.html. We are giving an increase of
    3 extra minutes in Read the Docs for Business instead of 4 minutes less in Read
    the Docs Community.
    
    ```
    >>> round(3417 - 750, -2)
    2700
    >>> round(2700 * 0.25, -2)
    700.0
    >>> round(3417 * 0.25, -2)
    900.0
    >>> round(7935 - 750, -2)
    7200
    >>> round(7200 * 0.25, -2)
    1800.0
    >>> round(7935 * 0.25, -2)
    2000.0
    ```

--- readthedocs/settings/base.py
+++ readthedocs/settings/base.py
@@ -2,6 +2,7 @@
 
 import os
 import subprocess
+import socket
 
 from celery.schedules import crontab
 
@@ -445,7 +446,7 @@ class CommunityBaseSettings(Settings):
                 "free -m | awk '/^Mem:/{print $2}'",
                 shell=True,
             ))
-            return round(total_memory - 750, -2)
+            return total_memory, round(total_memory - 750, -2)
         except ValueError:
             # On systems without a `free` command it will return a string to
             # int and raise a ValueError
@@ -475,15 +476,21 @@ class CommunityBaseSettings(Settings):
 
         # Only run on our servers
         if self.RTD_IS_PRODUCTION:
-            memory_limit = self._get_docker_memory_limit()
+            total_memory, memory_limit = self._get_docker_memory_limit()
             if memory_limit:
                 limits = {
                     'memory': f'{memory_limit}m',
                     'time': max(
                         limits['time'],
-                        round(memory_limit * self.DOCKER_TIME_LIMIT_COEFF, -2),
+                        round(total_memory * self.DOCKER_TIME_LIMIT_COEFF, -2),
                     )
                 }
+        log.info(
+            'Using dynamic docker limits. hostname=%s memory=%s time=%s',
+            socket.gethostname(),
+            limits['memory'],
+            limits['time'],
+        )
         return limits
 
     # All auth
