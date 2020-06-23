odairmario/readthedocs-self-own

Mock where it's used

readthedocs/rtd_tests/tests/test_sync_versions.py
@@ -736,7 +736,7 @@ class TestSyncVersions(TestCase):
             1,
         )
 
-    @mock.patch('readthedocs.api.v2.utils.run_automation_rules')
+    @mock.patch('readthedocs.api.v2.view.model_views.run_automation_rules')
     def test_automation_rules_are_triggered_for_new_versions(self, run_automation_rules):
         Version.objects.create(
             project=self.pip,

readthedocs/readthedocs.org

readthedocs/rtd_tests/tests/test_sync_versions.py
@@ -736,7 +736,7 @@ class TestSyncVersions(TestCase):
             1,
         )
 
-    @mock.patch('readthedocs.api.v2.utils.run_automation_rules')
+    @mock.patch('readthedocs.api.v2.views.model_views.run_automation_rules')
     def test_automation_rules_are_triggered_for_new_versions(self, run_automation_rules):
         Version.objects.create(
             project=self.pip,