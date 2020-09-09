27717cbc2b1fb1bbab1af81c97b42b6f93490e85
Santos Gallegos <stsewd@protonmail.com>
2020-09-09 15:57:09 -0500

    Update migration

--- readthedocs/projects/migrations/0061_add_imported_file_ignore.py
+++ readthedocs/projects/migrations/0061_add_imported_file_ignore.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('projects', '0057_add_page_rank'),
+        ('projects', '0060_make_rank_not_null'),
     ]
 
     operations = [
