32535d50ad4169dabc0efff2bdad74633ad7c0ba
Santos Gallegos <stsewd@protonmail.com>
2020-11-19 09:31:25 -0500

    Update migration

--- readthedocs/projects/migrations/0066_make_imported_file_slug_nullable.py
+++ readthedocs/projects/migrations/0066_make_imported_file_slug_nullable.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('projects', '0060_make_rank_not_null'),
+        ('projects', '0065_add_feature_future_default_true'),
     ]
 
     operations = [
