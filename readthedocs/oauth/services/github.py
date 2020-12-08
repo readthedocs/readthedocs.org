ef99b33b04c57f141930ee2aacf2c02b84edb64c
Maksudul Haque <saad.mk112@gmail.com>
2020-12-08 20:49:49 +0600

    Use remote_id and vcs_provider to get RemoteRepository on sync

--- readthedocs/oauth/services/github.py
+++ readthedocs/oauth/services/github.py
@@ -106,7 +106,8 @@ class GitHubService(Service):
         ]):
 
             repo, _ = RemoteRepository.objects.get_or_create(
-                full_name=fields['full_name']
+                remote_id=fields['id'],
+                vcs_provider=GITHUB
             )
             remote_relation, _ = RemoteRepositoryRelation.objects.get_or_create(
                 remoterepository=repo,
@@ -122,9 +123,8 @@ class GitHubService(Service):
                 return None
 
             repo.organization = organization
-            repo.remote_id = fields['id']
-            repo.vcs_provider = GITHUB
             repo.name = fields['name']
+            repo.full_name = fields['full_name']
             repo.description = fields['description']
             repo.ssh_url = fields['ssh_url']
             repo.html_url = fields['html_url']
