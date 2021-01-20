157f9afcbc9247421c62fed88b8a9fda2effe6e4
Manuel Kaufmann <humitos@gmail.com>
2021-01-20 13:03:57 +0100

    Add more tasks for build:large queue

--- readthedocs/metrics/tasks.py
+++ readthedocs/metrics/tasks.py
@@ -6,7 +6,12 @@ Override the base metric tasks to add specific ones only required on community.
 
 # Disable import error because -ext is not available on pylint
 # pylint: disable=import-error
-from readthedocsext.monitoring.metrics.database import RunningBuildsMetric
+from readthedocsext.monitoring.metrics.database import (
+    AvgBuildTimeMetric,
+    AvgBuildTriggeredAndFirstCommandTimeMetric,
+    ConcurrencyLimitedBuildsMetric,
+    RunningBuildsMetric,
+)
 from readthedocsext.monitoring.metrics.redislen import RedislenMetric
 from readthedocsext.monitoring.metrics.latency import BuildLatencyMetric
 from readthedocsext.monitoring.metrics.tasks import (
@@ -20,12 +25,21 @@ class CommunityMetrics1mTask(Metrics1mTaskBase):
     metrics = Metrics1mTaskBase.metrics + [
         RedislenMetric(queue_name='build-large'),
         RunningBuildsMetric(builder='large'),
+        ConcurrencyLimitedBuildsMetric(builder='large'),
     ]
 
 
 class CommunityMetrics5mTask(Metrics5mTaskBase):
 
     metrics = Metrics5mTaskBase.metrics + [
+        AvgBuildTimeMetric(
+            builder='large',
+            minutes=Metrics5mTaskBase.interval,
+        ),
+        AvgBuildTriggeredAndFirstCommandTimeMetric(
+            builder='large',
+            minutes=Metrics5mTaskBase.interval,
+        ),
         BuildLatencyMetric(
             project='time-test',
             queue_name='build-large',
