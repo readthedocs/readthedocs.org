8def9f22d59acf87517b6588b8e935ffe38bf64d
Santos Gallegos <stsewd@protonmail.com>
2021-02-24 17:14:10 -0500

    Remove requests mock

--- readthedocs/embed/tests/test_api.py
+++ readthedocs/embed/tests/test_api.py
@@ -4,12 +4,12 @@ from contextlib import contextmanager
 from pathlib import Path
 from unittest import mock
 
-import requests_mock
 from django.test import TestCase
 from django.test.utils import override_settings
 from django_dynamic_fixture import get
 
 from readthedocs.embed.views import do_embed
+from readthedocs.projects.constants import MKDOCS
 from readthedocs.projects.models import Project
 
 data_path = Path(__file__).parent.resolve() / 'data'
@@ -50,32 +50,29 @@ class APITest(TestCase):
             json.dumps(json_content)
         )
 
-        requests_mocker = requests_mock.Mocker()
-        with requests_mocker:
-            response = do_embed(
-                project=self.project,
-                version=self.project.versions.first(),
-                doc='index',
-                section='Features',
-                path='index.html',
-            )
-
-        self.assertDictEqual(
-            response.data,
-            {
-                'content': [],
-                'headers': [
-                    {'Welcome to Read The Docs': '#'},
-                ],
-                'url': 'http://project.readthedocs.io/en/latest/index.html',
-                'meta': {
-                    'project': 'project',
-                    'version': 'latest',
-                    'doc': 'index',
-                    'section': 'Features',
-                },
-            }
+        response = do_embed(
+            project=self.project,
+            version=self.project.versions.first(),
+            doc='index',
+            section='Features',
+            path='index.html',
         )
+
+        expected = {
+            'content': [],
+            'headers': [
+                {'Welcome to Read The Docs': '#'},
+            ],
+            'url': 'http://project.readthedocs.io/en/latest/index.html',
+            'meta': {
+                'project': 'project',
+                'version': 'latest',
+                'doc': 'index',
+                'section': 'Features',
+            },
+        }
+
+        self.assertDictEqual(response.data, expected)
 
     @mock.patch('readthedocs.embed.views.build_media_storage')
     def test_embed_mkdocs(self, storage_mock):
@@ -85,38 +82,36 @@ class APITest(TestCase):
             json_file.open().read()
         )
 
-        requests_mocker = requests_mock.Mocker()
-        with requests_mocker:
-            response = do_embed(
-                project=self.project,
-                version=self.project.versions.first(),
-                doc='index',
-                section='Installation',
-                path='index.html',
-            )
+        self.project.versions.update(documentation_type=MKDOCS)
 
-        self.assertDictEqual(
-            response.data,
-            {
-                'content': mock.ANY,  # too long to compare here
-                'headers': [
-                    {'Overview': 'overview'},
-                    {'Installation': 'installation'},
-                    {'Getting Started': 'getting-started'},
-                    {'Adding pages': 'adding-pages'},
-                    {'Theming our documentation': 'theming-our-documentation'},
-                    {'Changing the Favicon Icon': 'changing-the-favicon-icon'},
-                    {'Building the site': 'building-the-site'},
-                    {'Other Commands and Options': 'other-commands-and-options'},
-                    {'Deploying': 'deploying'},
-                    {'Getting help': 'getting-help'},
-                ],
-                'url': 'http://project.readthedocs.io/en/latest/index.html',
-                'meta': {
-                    'project': 'project',
-                    'version': 'latest',
-                    'doc': 'index',
-                    'section': 'Installation',
-                },
-            }
+        response = do_embed(
+            project=self.project,
+            version=self.project.versions.first(),
+            doc='index',
+            section='Installation',
+            path='index.html',
         )
+
+        expected = {
+            'content': mock.ANY,  # too long to compare here
+            'headers': [
+                {'Overview': 'overview'},
+                {'Installation': 'installation'},
+                {'Getting Started': 'getting-started'},
+                {'Adding pages': 'adding-pages'},
+                {'Theming our documentation': 'theming-our-documentation'},
+                {'Changing the Favicon Icon': 'changing-the-favicon-icon'},
+                {'Building the site': 'building-the-site'},
+                {'Other Commands and Options': 'other-commands-and-options'},
+                {'Deploying': 'deploying'},
+                {'Getting help': 'getting-help'},
+            ],
+            'url': 'http://project.readthedocs.io/en/latest/index.html',
+            'meta': {
+                'project': 'project',
+                'version': 'latest',
+                'doc': 'index',
+                'section': 'Installation',
+            },
+        }
+        self.assertDictEqual(response.data, expected)
