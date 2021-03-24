24224f405df941af47f624980389b4d3abb46cc5
Maksudul Haque <saad.mk112@gmail.com>
2021-03-24 16:00:13 +0600

    Remove unused imports and fix lint

--- readthedocs/api/v3/views.py
+++ readthedocs/api/v3/views.py
@@ -1,5 +1,4 @@
 import django_filters.rest_framework as filters
-from django.utils.safestring import mark_safe
 from rest_flex_fields.views import FlexFieldsMixin
 from rest_framework import status
 from rest_framework.authentication import SessionAuthentication, TokenAuthentication
@@ -39,8 +38,6 @@ from .mixins import OrganizationQuerySetMixin, ProjectQuerySetMixin, UpdateMixin
 from .permissions import (
     CommonPermissions,
     IsProjectAdmin,
-    IsOrganizationAdmin,
-    UserOrganizationsListing,
 )
 from .renderers import AlphabeticalSortedJSONRenderer
 from .serializers import (
