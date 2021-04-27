fd3da020a4c52cb1765ffcfcaa3b7435eccde5b9
Santos Gallegos <stsewd@protonmail.com>
2021-04-27 17:09:34 -0500

    Add comment

--- readthedocs/embed/views.py
+++ readthedocs/embed/views.py
@@ -163,7 +163,8 @@ class EmbedAPIBase(CachedResponseMixin, APIView):
                 status=status.HTTP_400_BAD_REQUEST
             )
 
-        # Update doc from path
+        # Generate the docname from path
+        # by removing the ``.html`` extension and trailing ``/``.
         if path:
             doc = re.sub(r'(.+)\.html$', r'\1', path.strip('/'))
 
