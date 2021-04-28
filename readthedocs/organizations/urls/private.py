18fa0835381e6e9dee3e93c860b6883d7ea3ed07
Santos Gallegos <stsewd@protonmail.com>
2021-04-28 12:09:07 -0500

    Move urls

---
+++ readthedocs/organizations/urls/private.py
@@ -0,0 +1,69 @@
+from django.conf.urls import url
+
+from ..views import private as views
+
+urlpatterns = [
+    url(
+        r'^(?P<slug>[\w.-]+)/edit/$',
+        views.EditOrganization.as_view(),
+        name='organization_edit',
+    ),
+    url(
+        r'^(?P<slug>[\w.-]+)/delete/$',
+        views.DeleteOrganization.as_view(),
+        name='organization_delete',
+    ),
+    # Owners
+    url(
+        r'^(?P<slug>[\w.-]+)/owners/(?P<owner>\d+)/delete/$',
+        views.DeleteOrganizationOwner.as_view(),
+        name='organization_owner_delete',
+    ),
+    url(
+        r'^(?P<slug>[\w.-]+)/owners/add/$',
+        views.AddOrganizationOwner.as_view(),
+        name='organization_owner_add',
+    ),
+    url(
+        r'^(?P<slug>[\w.-]+)/owners/$',
+        views.EditOrganizationOwners.as_view(),
+        name='organization_owners',
+    ),
+    url(
+        r'^(?P<slug>[\w.-]+)/authorization/$',
+        views.OrganizationSSO.as_view(),
+        name='organization_sso',
+    ),
+    # Teams
+    url(
+        r'^(?P<slug>[\w.-]+)/teams/add/$',
+        views.AddOrganizationTeam.as_view(),
+        name='organization_team_add',
+    ),
+    url(
+        r'^(?P<slug>[\w.-]+)/teams/(?P<team>[\w.-]+)/edit/$',
+        views.EditOrganizationTeam.as_view(),
+        name='organization_team_edit',
+    ),
+    url(
+        r'^(?P<slug>[\w.-]+)/teams/(?P<team>[\w.-]+)/projects/$',
+        views.UpdateOrganizationTeamProject.as_view(),
+        name='organization_team_project_edit',
+    ),
+    url(
+        r'^(?P<slug>[\w.-]+)/teams/(?P<team>[\w.-]+)/delete/$',
+        views.DeleteOrganizationTeam.as_view(),
+        name='organization_team_delete',
+    ),
+    url(
+        r'^(?P<slug>[\w.-]+)/teams/(?P<team>[\w.-]+)/members/invite/$',
+        views.AddOrganizationTeamMember.as_view(),
+        name='organization_team_member_add',
+    ),
+    url(
+        r'^(?P<slug>[\w.-]+)/teams/(?P<team>[\w.-]+)/members/(?P<member>\d+)/revoke/$',
+        views.DeleteOrganizationTeamMember.as_view(),
+        name='organization_team_member_delete',
+    ),
+]
+
---
+++ readthedocs/organizations/urls/public.py
@@ -0,0 +1,49 @@
+from django.conf.urls import url
+from django.views.generic.base import TemplateView
+
+from readthedocs.organizations.views import public as views
+
+urlpatterns = [
+    url(
+        r'^$',
+        views.ListOrganization.as_view(),
+        name='organization_list',
+    ),
+    url(
+        r'^create/$',
+        views.CreateOrganizationSignup.as_view(),
+        name='organization_create',
+    ),
+    url(
+        r'^verify-email/$',
+        TemplateView.as_view(template_name='organizations/verify_email.html'),
+        name='organization_verify_email',
+    ),
+    url(
+        r'^(?P<slug>[\w.-]+)/$',
+        views.DetailOrganization.as_view(),
+        name='organization_detail',
+    ),
+    # Teams
+    url(
+        r'^(?P<slug>[\w.-]+)/teams/(?P<team>[\w.-]+)/$',
+        views.ListOrganizationTeamMembers.as_view(),
+        name='organization_team_detail',
+    ),
+    url(
+        r'^(?P<slug>[\w.-]+)/teams/$',
+        views.ListOrganizationTeams.as_view(),
+        name='organization_team_list',
+    ),
+    url(
+        r'^invite/(?P<hash>[\w.-]+)/redeem/$',
+        views.UpdateOrganizationTeamMember.as_view(),
+        name='organization_invite_redeem',
+    ),
+    # Members
+    url(
+        r'^(?P<slug>[\w.-]+)/members/$',
+        views.ListOrganizationMembers.as_view(),
+        name='organization_members',
+    ),
+]
