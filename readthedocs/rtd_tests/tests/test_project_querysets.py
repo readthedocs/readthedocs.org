cbdf56cdbaf89da9cc1e9ea41718856948d22c5b
Santos Gallegos <stsewd@protonmail.com>
2021-05-13 20:09:04 -0500

    Remove unneeded test

--- readthedocs/rtd_tests/tests/test_project_querysets.py
+++ readthedocs/rtd_tests/tests/test_project_querysets.py
@@ -79,18 +79,6 @@ class ProjectQuerySetTests(TestCase):
         self.assertEqual(ChildRelatedProjectQuerySet.project_field, 'child')
         self.assertTrue(ChildRelatedProjectQuerySet.use_for_related_fields)
 
-    def test_subproject_queryset_as_manager_gets_correct_class(self):
-        mgr = ChildRelatedProjectQuerySet.as_manager()
-        self.assertEqual(
-            mgr.__class__.__name__,
-            'ManagerFromChildRelatedProjectQuerySetBase',
-        )
-        mgr = ParentRelatedProjectQuerySet.as_manager()
-        self.assertEqual(
-            mgr.__class__.__name__,
-            'ManagerFromParentRelatedProjectQuerySetBase',
-        )
-
     def test_is_active(self):
         project = get(Project, skip=False)
         self.assertTrue(Project.objects.is_active(project))
