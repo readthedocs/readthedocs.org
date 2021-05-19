6239b5f1648ccc41f4bfb0a3ce0304c2d1541fd0
Santos Gallegos <santos_g@outlook.com>
2021-05-19 17:46:40 -0500

    Proxito: redirect to main project from subprojects (#8187)
    Closes https://github.com/readthedocs/readthedocs.org/issues/7881
    We can revert https://github.com/readthedocs/readthedocs.org/pull/7880/
    after releasing this.

--- readthedocs/proxito/tests/test_middleware.py
+++ readthedocs/proxito/tests/test_middleware.py
@@ -73,6 +73,42 @@ class MiddlewareTests(RequestFactoryTestMixin, TestCase):
             self.assertTrue(hasattr(request, 'canonicalize'))
             self.assertEqual(request.canonicalize, 'canonical-cname')
 
+    def test_subproject_redirect(self):
+        """Requests to a subproject should redirect to the domain of the main project."""
+        subproject = get(
+            Project,
+            name='subproject',
+            slug='subproject',
+            users=[self.owner],
+            privacy_level=PUBLIC,
+        )
+        subproject.versions.update(privacy_level=PUBLIC)
+        get(
+            ProjectRelationship,
+            parent=self.pip,
+            child=subproject,
+        )
+
+        for url in (self.url, '/subdir/', '/en/latest/'):
+            request = self.request(url, HTTP_HOST='subproject.dev.readthedocs.io')
+            res = self.run_middleware(request)
+            self.assertIsNone(res)
+            self.assertEqual(getattr(request, 'canonicalize', None), 'subproject-main-domain')
+
+        # Using a custom domain in a subproject isn't supported (or shouldn't be!).
+        cname = 'docs.random.com'
+        domain = get(
+            Domain,
+            project=subproject,
+            domain=cname,
+            canonical=True,
+            https=True,
+        )
+        request = self.request(self.url, HTTP_HOST='subproject.dev.readthedocs.io')
+        res = self.run_middleware(request)
+        self.assertIsNone(res)
+        self.assertEqual(getattr(request, 'canonicalize', None), 'canonical-cname')
+
     # We are not canonicalizing custom domains -> public domain for now
     @pytest.mark.xfail(strict=True)
     def test_canonical_cname_redirect_public_domain(self):
