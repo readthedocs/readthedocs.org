fe3a50757abb71d8aff2e1dd808c1b0aee11a16b
Manuel Kaufmann <humitos@gmail.com>
2021-07-06 17:13:56 +0200

    Use timeout= when requesting external pages

--- readthedocs/embed/v3/views.py
+++ readthedocs/embed/v3/views.py
@@ -64,7 +64,7 @@ class EmbedAPIBase(CachedResponseMixin, APIView):
             log.debug('Cached response. url=%s', url)
             return cached_response
 
-        response = requests.get(url)
+        response = requests.get(url, timeout=1)
         if response.ok:
             cache.set(url, response.text, timeout=60 * 5)
             return response.text
