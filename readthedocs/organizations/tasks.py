c95224f888aaf557c1abbde37f0087deb886e3f6
Manuel Kaufmann <humitos@gmail.com>
2021-08-23 17:00:47 +0200

    Lint

--- readthedocs/organizations/tasks.py
+++ readthedocs/organizations/tasks.py
@@ -1,5 +1,8 @@
+"""Organization's tasks related."""
+
 import logging
 
+from readthedocs.builds.models import Build
 from readthedocs.worker import app
 
 
@@ -8,6 +11,7 @@ log = logging.getLogger(__name__)
 
 @app.task(queue='web')
 def mark_organization_assets_not_cleaned(build_pk):
+    """Mark an organization as `artifacts_cleaned=False`."""
     try:
         build = Build.objects.get(pk=build_pk)
     except Build.DoesNotExist:
