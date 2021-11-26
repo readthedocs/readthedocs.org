d3241ad1f0ffe5283b55258de790901c26f2791f
Manuel Kaufmann <humitos@gmail.com>
2021-11-26 12:52:23 +0100

    Remove providing_args
    ```
    RemovedInDjango40Warning: The providing_args argument is deprecated. As it is purely documentational, it has no replacement. If you rely on this argument as documentation, you can move the text to a code comment or docstring.
    ```

--- readthedocs/projects/signals.py
+++ readthedocs/projects/signals.py
@@ -2,12 +2,12 @@
 
 import django.dispatch
 
-before_vcs = django.dispatch.Signal(providing_args=['version', 'environmemt'])
+before_vcs = django.dispatch.Signal()
 
-before_build = django.dispatch.Signal(providing_args=['version', 'environmemt'])
-after_build = django.dispatch.Signal(providing_args=['version'])
+before_build = django.dispatch.Signal()
+after_build = django.dispatch.Signal()
 
-project_import = django.dispatch.Signal(providing_args=['project'])
+project_import = django.dispatch.Signal()
 
 # Used to purge files from the CDN
-files_changed = django.dispatch.Signal(providing_args=['project', 'version'])
+files_changed = django.dispatch.Signal()
