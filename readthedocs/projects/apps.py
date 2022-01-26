a393ddbb664c25b9e211517d5049e5f2297f4959
Manuel Kaufmann <humitos@gmail.com>
2022-01-26 08:50:51 +0100

    Celery: re-register new task names under old names

--- readthedocs/projects/apps.py
+++ readthedocs/projects/apps.py
@@ -4,12 +4,21 @@
 
 from django.apps import AppConfig
 
+from readthedocs.worker import app, register_renamed_tasks
+
 
 class ProjectsConfig(AppConfig):
 
     name = 'readthedocs.projects'
 
     def ready(self):
-        # import readthedocs.projects.tasks
         import readthedocs.projects.tasks.builds
         import readthedocs.projects.tasks.search
+        import readthedocs.projects.tasks.utils
+
+        # TODO: remove calling to `register_renamed_tasks` after deploy
+        renamed_tasks = {
+            'readthedocs.projects.tasks.update_docs_task': 'readthedocs.projects.tasks.builds.update_docs_task',
+            'readthedocs.projects.tasks.sync_repository_task': 'readthedocs.projects.tasks.builds.sync_repository_task'
+        }
+        register_renamed_tasks(app, renamed_tasks)
