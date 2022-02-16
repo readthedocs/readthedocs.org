71aba8136db2e963955d68e9fe997d9b7e879073
Manuel Kaufmann <humitos@gmail.com>
2022-02-16 12:33:01 -0300

    Celery: remove duplication of task names
    This was used to allow new instances to execute old Celery task names since they
    were moved into different modules and their name changed.

--- readthedocs/projects/apps.py
+++ readthedocs/projects/apps.py
@@ -8,10 +8,6 @@ class ProjectsConfig(AppConfig):
     name = 'readthedocs.projects'
 
     def ready(self):
-        # TODO: remove this import after the deploy together with the
-        # `tasks/__init__.py` file
-        import readthedocs.projects.tasks
-
         import readthedocs.projects.tasks.builds
         import readthedocs.projects.tasks.search
         import readthedocs.projects.tasks.utils
