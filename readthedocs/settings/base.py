0f6b1df3c6198e3063d6f6bcd2443f83ae26505f
Manuel Kaufmann <humitos@gmail.com>
2022-02-22 12:55:41 -0300

    Archive builds: define default in celery beat

--- readthedocs/settings/base.py
+++ readthedocs/settings/base.py
@@ -426,12 +426,14 @@ class CommunityBaseSettings(Settings):
             'schedule': crontab(minute=0, hour=4),
             'options': {'queue': 'web'},
         },
-        'hourly-archive-builds': {
+        'half-hourly-archive-builds': {
             'task': 'readthedocs.builds.tasks.archive_builds',
             'schedule': crontab(minute=30),
             'options': {'queue': 'web'},
             'kwargs': {
                 'days': 1,
+                'limit': 2000,
+                'delete': True,
             },
         },
         'every-day-delete-inactive-external-versions': {
