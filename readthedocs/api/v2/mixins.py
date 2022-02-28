29263fd2bbbd868eb7ad8d12628f8b3eb70c584a
Santos Gallegos <stsewd@protonmail.com>
2022-02-28 13:50:23 -0500

    Use helper

--- readthedocs/api/v2/mixins.py
+++ readthedocs/api/v2/mixins.py
@@ -3,6 +3,19 @@ import structlog
 log = structlog.get_logger(__name__)
 
 
+def get_cache_tag(*args):
+    """
+    Generate a cache tag from the given args.
+
+    The final tag is composed of several parts
+    that form a unique tag (like project and version slug).
+
+    All parts are separated using a character that isn't
+    allowed in slugs to avoid collitions.
+    """
+    return ':'.join(args)
+
+
 class CachedResponseMixin:
 
     """
@@ -37,10 +50,10 @@ class CachedResponseMixin:
             version = self._get_version()
             tags = [
                 project.slug,
-                f'{project.slug}:{version.slug}',
+                get_cache_tag(project.slug, version.slug),
             ]
             if self.project_cache_tag:
-                tags.append(f'{project.slug}:{self.project_cache_tag}')
+                tags.append(get_cache_tag(project.slug, self.project_cache_tag))
             return tags
         except Exception as e:
             log.debug('Error while retrieving project and version for this view.')
