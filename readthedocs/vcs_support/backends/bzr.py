4d3f02dde4bd9e81d9e4c9fa1a3ec73fcaa89392
Manuel Kaufmann <humitos@gmail.com>
2022-04-06 16:10:05 +0200

    Build: bugfix `RepositoryError.CLONE_ERROR` message
    This commit converts the `@property` used to select the correct message
    depending on the platform (.org or .com) by a `@classmethod` to make it simpler
    to realize that we need to call it as a regular method.
    
    In Python 3.9, we will be able to combine these two decorators and we won't need
    parenthesis anymore. For now, we need to add parenthesis to make the call of the
    method.

--- readthedocs/vcs_support/backends/bzr.py
+++ readthedocs/vcs_support/backends/bzr.py
@@ -38,7 +38,7 @@ class Backend(BaseVCS):
         try:
             self.run("bzr", "checkout", self.repo_url, ".")
         except RepositoryError:
-            raise RepositoryError(RepositoryError.CLONE_ERROR)
+            raise RepositoryError(RepositoryError.CLONE_ERROR())
 
     @property
     def tags(self):
