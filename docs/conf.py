6665dc9629a4148d97d1742cdf4bef5572dc8a25
Santos Gallegos <stsewd@protonmail.com>
2022-04-18 14:50:38 -0500

    Fix docs

--- docs/conf.py
+++ docs/conf.py
@@ -83,7 +83,7 @@ intersphinx_mapping = {
     "myst-nb": ("https://myst-nb.readthedocs.io/en/v0.12.3/", None),
     "ipywidgets": ("https://ipywidgets.readthedocs.io/en/7.6.3/", None),
     "jupytext": ("https://jupytext.readthedocs.io/en/stable/", None),
-    "ipyleaflet": ("https://ipyleaflet.readthedocs.io/en/stable/", None),
+    "ipyleaflet": ("https://ipyleaflet.readthedocs.io/en/latest/", None),
     "poliastro": ("https://docs.poliastro.space/en/v0.15.2/", None),
     "qiskit": ("https://qiskit.org/documentation/", None),
     "myst-parser": ("https://myst-parser.readthedocs.io/en/v0.15.1/", None),
