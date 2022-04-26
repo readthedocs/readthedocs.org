ea5d9e34da15be8a9eaad86b536801b68849d488
Santos Gallegos <stsewd@protonmail.com>
2022-04-26 18:36:42 -0500

    Linter

--- readthedocs/telemetry/tasks.py
+++ readthedocs/telemetry/tasks.py
@@ -1,3 +1,5 @@
+"""Tasks related to telemetry."""
+
 from readthedocs.builds.models import Build
 from readthedocs.telemetry.models import BuildData
 from readthedocs.worker import app
@@ -5,6 +7,12 @@ from readthedocs.worker import app
 
 @app.task(queue="web")
 def save_build_data(build_id, data):
+    """
+    Save the build data asynchronously.
+
+    Mainly used from the builders,
+    since they don't have access to the database.
+    """
     build = Build.objects.filter(id=build_id).first()
     if build:
         BuildData.objects.collect(build, data)
