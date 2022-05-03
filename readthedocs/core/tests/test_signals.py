0f35dc3aa7f360c65e38ca0828fa378e3e83e8d2
Santos Gallegos <stsewd@protonmail.com>
2022-05-03 18:57:59 -0500

    User: delete organizations where the user is the last owner
    This also shows a warning with a list of projects/organizations to be
    deleted.

--- readthedocs/core/tests/test_signals.py
+++ readthedocs/core/tests/test_signals.py
@@ -1,50 +1,82 @@
-# -*- coding: utf-8 -*-
-import django_dynamic_fixture
 import pytest
-
 from django.contrib.auth.models import User
+from django_dynamic_fixture import get
 
+from readthedocs.builds.models import Version
+from readthedocs.organizations.models import Organization, Team, TeamInvite, TeamMember
 from readthedocs.projects.models import Project
 
 
 @pytest.mark.django_db
 class TestProjectOrganizationSignal:
 
-    def test_project_get_deleted_upon_user_delete(self):
-        """If the user has Project where he is the only
-        user, upon deleting his account, the Project
-        should also get deleted."""
+    def test_delete_user_deletes_projects(self):
+        """When deleting a user, delete projects where it's the only owner."""
+        user = get(User)
+        another_user = get(User)
+        project_one = get(Project, slug="one", users=[user])
+        project_two = get(Project, slug="two", users=[user])
+        project_three = get(Project, slug="three", users=[another_user])
+        project_four = get(Project, slug="four", users=[user, another_user])
 
-        project = django_dynamic_fixture.get(Project)
-        user1 = django_dynamic_fixture.get(User)
-        project.users.add(user1)
+        project_five = get(
+            Project,
+            slug="five",
+            users=[],
+        )
+        assert Project.objects.all().count() == 5
+        assert Version.objects.all().count() == 5
 
-        project.refresh_from_db()
-        assert project.users.all().count() == 1
+        user.delete()
+        assert {project_three, project_four, project_five} == set(Project.objects.all())
+        assert Version.objects.all().count() == 3
 
-        # Delete the user
-        user1.delete()
-        # The object should not exist
-        project = Project.objects.all().filter(id=project.id)
-        assert not project.exists()
+    def test_delete_user_deletes_organizations(self):
+        """When deleting a user, delete organizations where it's the only owner."""
+        user = get(User)
+        member = get(User)
+        another_user = get(User)
+        project_one = get(Project, slug="one")
+        project_two = get(Project, slug="two")
+        project_three = get(Project, slug="three")
+        org_one = get(Organization, slug="one", owners=[user], projects=[project_one])
+        org_two = get(
+            Organization,
+            slug="two",
+            owners=[user, another_user],
+            projects=[project_two],
+        )
+        org_three = get(
+            Organization, slug="three", owners=[another_user], projects=[project_three]
+        )
 
-    def test_multiple_users_project_not_delete(self):
-        """Check Project which have multiple users do not
-        get deleted when any of the user delete his account."""
+        team_one = get(
+            Team, organization=org_one, members=[member, user], projects=[project_one]
+        )
+        team_two = get(
+            Team,
+            organization=org_three,
+            members=[another_user],
+            projects=[project_three],
+        )
 
-        project = django_dynamic_fixture.get(Project)
-        user1 = django_dynamic_fixture.get(User)
-        user2 = django_dynamic_fixture.get(User)
-        project.users.add(user1, user2)
+        get(TeamInvite, team=team_one, organization=org_one)
 
-        project.refresh_from_db()
-        assert project.users.all().count() > 1
-        # Delete 1 user of the project
-        user1.delete()
 
-        # The project should still exist and it should have 1 user
-        project.refresh_from_db()
-        assert project.id
-        obj_users = project.users.all()
-        assert len(obj_users) == 1
-        assert user2 in obj_users
+        assert Organization.objects.all().count() == 3
+        assert Project.objects.all().count() == 3
+        assert Version.objects.all().count() == 3
+        assert Team.objects.all().count() == 2
+        assert TeamInvite.objects.all().count() == 1
+        assert TeamMember.objects.all().count() == 3
+        assert User.objects.all().count() == 3
+
+        user.delete()
+
+        assert {org_two, org_three} == set(Organization.objects.all())
+        assert {project_two, project_three} == set(Project.objects.all())
+        assert Version.objects.all().count() == 2
+        assert {team_two} == set(Team.objects.all())
+        assert TeamInvite.objects.all().count() == 0
+        assert TeamMember.objects.all().count() == 1
+        assert User.objects.all().count() == 2
