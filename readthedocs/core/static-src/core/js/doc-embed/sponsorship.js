0605f8c02f92e6fe2573c5a2a1bb0efe35e8fe9e
Matthias Geier <Matthias.Geier@gmail.com>
2022-06-06 20:13:00 +0200

    Remove a few more jQuery usages

--- readthedocs/core/static-src/core/js/doc-embed/sponsorship.js
+++ readthedocs/core/static-src/core/js/doc-embed/sponsorship.js
@@ -71,7 +71,7 @@ function create_ad_placement() {
         // Assumes the ad would be ~200px high
         element = $("<div />").appendTo(selector);
         offset = element.offset();
-        if (!offset || (offset.top - $(window).scrollTop() + 200) > $(window).height()) {
+        if (!offset || (offset.top - window.scrollTop + 200) > window.offsetHeight) {
             if (rtd.is_rtd_like_theme()) {
                 selector = $('<div />').insertAfter('footer hr');
                 class_name = 'ethical-rtd';
@@ -199,7 +199,7 @@ function init() {
             } else {
                 // The ad client hasn't loaded yet which could happen due to a variety of issues
                 // Add an event listener for it to load
-                $("#ethicaladsjs").on("load", function () {
+                document.getElementById("ethicaladsjs").addEventListener("load", function () {
                     if (typeof ethicalads !== "undefined") {
                         ethicalads.load();
                     }
