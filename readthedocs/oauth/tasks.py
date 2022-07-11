d26eb936e3f355f35a3d329e7e1518ce4d0be5f8
Manuel Kaufmann <humitos@gmail.com>
2022-07-11 12:45:28 +0200

    OAuth: re-sync `RemoteRepository` in 1 Celery process only
    Weekly resync will use only one Celery process to avoid backing up our queue.

--- readthedocs/oauth/tasks.py
+++ readthedocs/oauth/tasks.py
@@ -104,7 +104,11 @@ def sync_remote_repositories_organizations(organization_slugs=None):
             )
 
 
-@app.task(queue='web')
+@app.task(
+    queue="web",
+    time_limit=60 * 60 * 3,  # 3h
+    soft_time_limit=(60 * 60 * 3) - 5 * 60,  # 2h 55m
+)
 def sync_active_users_remote_repositories():
     """
     Sync ``RemoteRepository`` for active users.
@@ -114,6 +118,9 @@ def sync_active_users_remote_repositories():
     This task is thought to be executed daily. It checks the weekday of the
     last login of the user with today's weekday. If they match, the re-sync is
     triggered. This logic guarantees us the re-sync to be done once a week per user.
+
+    Note this is a long running task syncronizhing all the users in the same Celery process,
+    and it will require a pretty high ``time_limit`` and ``soft_time_limit``.
     """
     today_weekday = timezone.now().isoweekday()
     three_months_ago = timezone.now() - datetime.timedelta(days=90)
@@ -123,9 +130,26 @@ def sync_active_users_remote_repositories():
         weekday=today_weekday,
     )
 
-    log.info("Re-syncing RemoteRepository for active users.", users=users.count())
-    for user in users:
-        sync_remote_repositories.delay(user.pk)
+    users_count = users.count()
+    log.bind(total_users=users_count)
+    log.info("Triggering re-sync of RemoteRepository for active users.")
+
+    for i, user in enumerate(users):
+        log.bind(
+            user_username=user.username,
+            progress=f"{i}/{users_count}",
+        )
+
+        # Log an update every 50 users
+        if i % 50 == 0:
+            log.info("Progress on re-syncing RemoteRepository for active users.")
+
+        try:
+            # NOTE: sync all the users/repositories in the same Celery process.
+            # Do not trigger a new task per user.
+            sync_remote_repositories(user.pk)
+        except Exception:
+            log.exception("There was a problem re-syncing RemoteRepository.")
 
 
 @app.task(queue="web")
