1cdfd09402758f37d9105eee3f4a33a45e0bf74f
David Fischer <david@readthedocs.org>
2022-07-11 13:36:18 -0700

    Stickybox ad fix (#9421)
    * Stickybox ad fix
    
    - Stickybox will ONLY be used on RTD theme
      and only if the ad would be in the footer
    - It will be used 25% of the time
    
    * Commit the built JS

--- readthedocs/core/static-src/core/js/doc-embed/sponsorship.js
+++ readthedocs/core/static-src/core/js/doc-embed/sponsorship.js
@@ -53,13 +53,7 @@ function create_ad_placement() {
         class_name = 'ethical-rtd ethical-dark-theme';
     } else if (rtd.is_rtd_like_theme()) {
         selector = 'nav.wy-nav-side > div.wy-side-scroll';
-        if (Math.random() <= 0.1) {
-          // Use the stickybox placement 10% of the time during rollout
-          style_name = 'stickybox';
-          ad_type = 'image';
-        } else {
-          class_name = 'ethical-rtd ethical-dark-theme';
-        }
+        class_name = 'ethical-rtd ethical-dark-theme';
     } else if (rtd.is_alabaster_like_theme()) {
         selector = 'div.sphinxsidebar > div.sphinxsidebarwrapper';
         class_name = 'ethical-alabaster';
@@ -75,6 +69,13 @@ function create_ad_placement() {
             if (rtd.is_rtd_like_theme()) {
                 selector = $('<div />').insertAfter('footer hr');
                 class_name = 'ethical-rtd';
+
+                // Use the stickybox placement 25% of the time during rollout
+                // But only when the ad would be in the footer
+                if (Math.random() <= 0.25) {
+                    style_name = 'stickybox';
+                    ad_type = 'image';
+                }
             } else if (rtd.is_alabaster_like_theme()) {
                 selector = 'div.bodywrapper .body';
                 class_name = 'ethical-alabaster';
