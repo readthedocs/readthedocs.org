bcde94a3ea476fa82e3c09e4be5a3315cdad1e7b
Matthias Geier <Matthias.Geier@gmail.com>
2022-07-15 20:58:00 +0200

    FIXUP: use scrollY and innerHeight

--- readthedocs/core/static-src/core/js/doc-embed/sponsorship.js
+++ readthedocs/core/static-src/core/js/doc-embed/sponsorship.js
@@ -71,7 +71,7 @@ function create_ad_placement() {
         // Assumes the ad would be ~200px high
         element = $("<div />").appendTo(selector);
         offset = element.offset();
-        if (!offset || (offset.top - window.scrollTop + 200) > window.offsetHeight) {
+        if (!offset || (offset.top - window.scrollY + 200) > window.innerHeight) {
             if (rtd.is_rtd_like_theme()) {
                 selector = $('<div />').insertAfter('footer hr');
                 class_name = 'ethical-rtd';
