af3b86fb86edbb059e0c82a280ab378398f5a8c3
Santos Gallegos <stsewd@protonmail.com>
2022-08-04 13:25:24 -0500

    Don't inject JQuery

--- readthedocs/core/static-src/core/js/readthedocs-doc-embed.js
+++ readthedocs/core/static-src/core/js/readthedocs-doc-embed.js
@@ -7,9 +7,8 @@ var sphinx = require('./doc-embed/sphinx');
 var search = require('./doc-embed/search');
 
 
-// Parts of this script rely on jQuery,
-// since Sphinx no longer includes it,
-// we must inject it if isn't found before executing our script.
+// While much of this script relies on jQuery (which Sphinx relies on),
+// we purposefully do not use it before DOMContentLoaded in case scripts are loaded out of order
 (function () {
     function domReady(fn) {
         // If the DOM is already done parsing
@@ -20,26 +19,10 @@ var search = require('./doc-embed/search');
         }
     }
 
-    function init() {
+    domReady(function () {
         footer.init();
         sphinx.init();
         search.init();
         sponsorship.init();
-    }
-
-    domReady(function () {
-      // Inject jQuery if isn't present already.
-      if (!window.jQuery) {
-          console.log("jQuery not found. Injecting.");
-          var script = document.createElement("script");
-          script.type = "text/javascript";
-          script.src = "https://code.jquery.com/jquery-3.6.0.min.js";
-          script.integrity = "sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==";
-          script.crossOrigin = "anonymous";
-          script.onload = init;
-          document.head.appendChild(script);
-      } else {
-          init();
-      }
     });
 }());
