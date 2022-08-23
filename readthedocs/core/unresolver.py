7da5fd0363570554aeca285e1c5ff63437240ef2
Santos Gallegos <stsewd@proton.me>
2022-08-23 16:59:33 -0500

    Unresolver: port changes from #9540
    https://github.com/readthedocs/readthedocs.org/pull/9540 was
    merged to rel only, I'm porting the changes to main.

--- readthedocs/core/unresolver.py
+++ readthedocs/core/unresolver.py
@@ -249,7 +249,7 @@ class Unresolver:
             # Serve from the PUBLIC_DOMAIN, ensuring it looks like `foo.PUBLIC_DOMAIN`.
             if public_domain == root_domain:
                 project_slug = subdomain
-                log.info("Public domain.", domain=domain)
+                log.debug("Public domain.", domain=domain)
                 return project_slug, None, False
 
             # TODO: This can catch some possibly valid domains (docs.readthedocs.io.com)
@@ -260,7 +260,7 @@ class Unresolver:
         # Serve PR builds on external_domain host.
         if external_domain == root_domain:
             try:
-                log.info("External versions domain.", domain=domain)
+                log.debug("External versions domain.", domain=domain)
                 project_slug, _ = subdomain.rsplit("--", maxsplit=1)
                 return project_slug, None, True
             except ValueError:
@@ -272,7 +272,7 @@ class Unresolver:
             Domain.objects.filter(domain=domain).prefetch_related("project").first()
         )
         if domain_object:
-            log.info("Custom domain.", domain=domain)
+            log.debug("Custom domain.", domain=domain)
             project_slug = domain_object.project.slug
             return project_slug, domain_object, False
 
