c38096ad2802734ae14212dc1045f169449e2bb0
Santos Gallegos <stsewd@proton.me>
2022-08-23 11:12:08 -0500

    Email: render template before sending it to the task
    The txt formats are the same as the html ones,
    but with tags removed and small changes to include
    links along the text.
    
    Closes https://github.com/readthedocs/readthedocs.org/issues/9466

--- readthedocs/core/tasks.py
+++ readthedocs/core/tasks.py
@@ -1,9 +1,6 @@
-# -*- coding: utf-8 -*-
-
 """Basic tasks."""
 
 import structlog
-
 from django.conf import settings
 from django.core.mail import EmailMultiAlternatives
 from django.template import TemplateDoesNotExist
@@ -13,7 +10,6 @@ from messages_extends.models import Message as PersistentMessage
 
 from readthedocs.worker import app
 
-
 log = structlog.get_logger(__name__)
 
 EMAIL_TIME_LIMIT = 30
@@ -21,8 +17,7 @@ EMAIL_TIME_LIMIT = 30
 
 @app.task(queue='web', time_limit=EMAIL_TIME_LIMIT)
 def send_email_task(
-        recipient, subject, template, template_html, context=None,
-        from_email=None, **kwargs
+    recipient, subject, content, content_html=None, from_email=None, **kwargs
 ):
     """
     Send multipart email.
@@ -33,32 +28,24 @@ def send_email_task(
     subject
         Email subject header
 
-    template
+    content
         Plain text template to send
 
-    template_html
-        HTML template to send as new message part
-
-    context
-        A dictionary to pass into the template calls
+    content_html
+        HTML content to send as new message part
 
     kwargs
         Additional options to the EmailMultiAlternatives option.
     """
     msg = EmailMultiAlternatives(
         subject,
-        get_template(template).render(context), from_email or
-        settings.DEFAULT_FROM_EMAIL,
-        [recipient], **kwargs
+        content,
+        from_email or settings.DEFAULT_FROM_EMAIL,
+        [recipient],
+        **kwargs
     )
-    try:
-        msg.attach_alternative(
-            get_template(template_html).render(context),
-            'text/html',
-        )
-    except (TypeError, TemplateDoesNotExist):
-        # TypeError is raised when ``template_html`` is ``None``
-        pass
+    if content_html:
+        msg.attach_alternative(content_html, "text/html")
     msg.send()
     log.info('Sent email to recipient.', recipient=recipient)
 
