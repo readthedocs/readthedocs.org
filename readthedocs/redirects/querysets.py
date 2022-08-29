8e4e5ef46e703560fa0ff2f362a06ec605d8c4b7
Eric Holscher <eric@ericholscher.com>
2022-08-29 11:21:16 -0700

    Pass full_path to exact redirect

--- readthedocs/redirects/querysets.py
+++ readthedocs/redirects/querysets.py
@@ -105,8 +105,10 @@ class RedirectQuerySet(models.QuerySet):
         # can't think in a case where there can be more at this point. I'm
         # leaving the loop just in case for now
         for redirect in queryset.select_related('project'):
+            log.info('Got a redirect', normalized_path=normalized_path)
             new_path = redirect.get_redirect_path(
                 path=normalized_path,
+                full_path=normalized_full_path,
                 language=language,
                 version_slug=version_slug,
             )
