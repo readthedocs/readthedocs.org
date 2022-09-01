4fdaf6b9c16c458692c01a8dbf8f5ddaa1c49459
Santos Gallegos <stsewd@proton.me>
2022-09-01 14:51:02 -0500

    Linter

--- readthedocs/invitations/signals.py
+++ readthedocs/invitations/signals.py
@@ -1,3 +1,5 @@
+"""Invitations related signals."""
+
 import structlog
 from django.db.models.signals import pre_delete
 from django.dispatch import receiver
@@ -9,10 +11,17 @@ from readthedocs.projects.models import Project
 log = structlog.get_logger(__name__)
 
 
+# pylint: disable=unused-argument
 @receiver(pre_delete, sender=Project)
 @receiver(pre_delete, sender=Organization)
 @receiver(pre_delete, sender=Team)
 def delete_related_invitations(sender, instance, **kwargs):
+    """
+    Delete related invitations of an object.
+
+    Generic foreign keys don't have a way to cascade delete,
+    so we need to do it manually.
+    """
     invitations = Invitation.objects.for_object(instance)
     log.info(
         "Deleting related invitations.",
