12b56bd20352eea9a7431f14bba5420c42de6ec0
Santos Gallegos <stsewd@proton.me>
2022-09-21 18:34:46 -0500

    Fix tests

--- readthedocs/search/api/v2/urls.py
+++ readthedocs/search/api/v2/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from readthedocs.search.api.v2 import PageSearchAPIView
+from readthedocs.search.api.v2.views import PageSearchAPIView
 
 urlpatterns = [
     path("", PageSearchAPIView.as_view(), name="search_api"),
