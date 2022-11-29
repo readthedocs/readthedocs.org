23557eeebed9f38e94d6fb9bee5b6f5f830520de
Eric Holscher <eric@ericholscher.com>
2022-11-28 19:29:07 -0800

    Release 9.0.0

--- docs/conf.py
+++ docs/conf.py
@@ -69,7 +69,7 @@ templates_path = ["_templates"]
 
 master_doc = "index"
 copyright = "Read the Docs, Inc & contributors"
-version = "8.9.0"
+version = "9.0.0"
 release = version
 exclude_patterns = ["_build", "shared"]
 default_role = "obj"
