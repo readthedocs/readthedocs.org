34ef3ff1aed86990053f7a3cfb8a78c2a9212c8a
Tanvi Moharir <tanvimoharir@gmail.com>
2022-12-12 18:32:22 +0530

    Changing fixture data to be list of dictionaries

--- readthedocs/projects/management/commands/fixtures_projects.py
+++ readthedocs/projects/management/commands/fixtures_projects.py
@@ -18,31 +18,112 @@ class Command(BaseCommand):
 
     def load_data(self):
         projects = [
-            ("Read The Docs", "https://github.com/rtfd/readthedocs.org"),
-            ("Django Test Utils", "https://github.com/ericholscher/django-test-utils"),
-            ("Fabric", "https://github.com/fabric/fabric"),
-            ("South", "https://github.com/dmishe/django-south"),
-            ("Pip", "https://github.com/pypa/pip"),
-            ("Kong", "https://github.com/ericholscher/django-kong"),
-            ("Djangoembed", "https://github.com/worldcompany/djangoembed"),
-            ("Tastypie", "https://github.com/codysoyland/django-tastypie"),
-            ("Django Uni Form", "https://github.com/pydanny/django-uni-form"),
-            ("Cue", "https://github.com/coleifer/cue.git"),
-            ("Pinax", "https://github.com/pinax/pinax"),
-            ("Haystack", "https://github.com/toastdriven/django-haystack"),
-            ("Taggit", "https://github.com/alex/django-taggit"),
-            ("test_project", "https://github.com/bogususer/non-existing-repo"),
-            ("Sphinx", "https://github.com/sphinx-doc/sphinx"),
-            ("Numpy", "https://github.com/numpy/numpy"),
-            ("Conda", "https://github.com/conda/conda"),
+            {
+                "name": "Read The Docs",
+                "slug": "read-the-docs",
+                "repo": "https://github.com/rtfd/readthedocs.org",
+                "repo_type": "git",
+            },
+            {
+                "name": "Django Test Utils",
+                "slug": "django-test-utils",
+                "repo": "https://github.com/ericholscher/django-test-utils",
+                "repo_type": "git",
+            },
+            {
+                "name": "Fabric",
+                "slug": "fabric",
+                "repo": "https://github.com/fabric/fabric",
+                "repo_type": "git",
+            },
+            {
+                "name": "South",
+                "slug": "south",
+                "repo": "https://github.com/dmishe/django-south",
+                "repo_type": "git",
+            },
+            {
+                "name": "Pip",
+                "slug": "pip",
+                "repo": "https://github.com/pypa/pip",
+                "repo_type": "git",
+            },
+            {
+                "name": "Kong",
+                "slug": "kong",
+                "repo": "https://github.com/ericholscher/django-kong",
+                "repo_type": "git",
+            },
+            {
+                "name": "Djangoembed",
+                "slug": "djangoembed",
+                "repo": "https://github.com/worldcompany/djangoembed",
+                "repo_type": "git",
+            },
+            {
+                "name": "Tastypie",
+                "slug": "tastypie",
+                "repo": "https://github.com/codysoyland/django-tastypie",
+                "repo_type": "git",
+            },
+            {
+                "name": "Django Uni Form",
+                "slug": "django-uni-form",
+                "repo": "https://github.com/pydanny/django-uni-form",
+                "repo_type": "git",
+            },
+            {
+                "name": "Cue",
+                "slug": "cue",
+                "repo": "https://github.com/coleifer/cue.git",
+                "repo_type": "git",
+            },
+            {
+                "name": "Pinax",
+                "slug": "pinax",
+                "repo": "https://github.com/pinax/pinax",
+                "repo_type": "git",
+            },
+            {
+                "name": "Haystack",
+                "slug": "haystack",
+                "repo": "https://github.com/toastdriven/django-haystack",
+                "repo_type": "git",
+            },
+            {
+                "name": "Taggit",
+                "slug": "taggit",
+                "repo": "https://github.com/alex/django-taggit",
+                "repo_type": "git",
+            },
+            {
+                "name": "test_project",
+                "slug": "test_project",
+                "repo": "https://github.com/bogususer/non-existing-repo",
+                "repo_type": "git",
+            },
+            {
+                "name": "Sphinx",
+                "slug": "sphinx",
+                "repo": "https://github.com/sphinx-doc/sphinx",
+                "repo_type": "git",
+            },
+            {
+                "name": "Numpy",
+                "slug": "numpy",
+                "repo": "https://github.com/numpy/numpy",
+                "repo_type": "git",
+            },
+            {
+                "name": "Conda",
+                "slug": "conda",
+                "repo": "https://github.com/conda/conda",
+                "repo_type": "git",
+            },
         ]
-        for name, repo in projects:
-            Project.objects.get_or_create(
-                name=name,
-                slug=name.replace(" ", "-").lower(),
-                repo=repo,
-                repo_type="git",
-            )
+
+        for project in projects:
+            Project.objects.get_or_create(**project)
 
         # Clear Version table since it gets populated with all "latest" slugs
         # Create specific Version objects matching the original fixtures test data
@@ -57,93 +138,117 @@ class Command(BaseCommand):
         )
 
         # Create only specific Version objects for projects readthedocs, pip, Sphinx, Numpy, Conda
-        # Read the doc versions
+        # Specific versions same as fixtures
         rtd = Project.objects.get(slug="read-the-docs")
-        for type, identifier, verbose_name, slug, supported, active, built in [
-            # changed type=external here for version compare tests
-            (
-                "unknown",
-                "2ff3d36340fa4d3d39424e8464864ca37c5f191c",
-                "0.2.1",
-                "0.2.1",
-                True,
-                True,
-                True,
-            ),
-            (
-                "unknown",
-                "354456a7dba2a75888e2fe91f6d921e5fe492bcd",
-                "0.2.2",
-                "0.2.2",
-                True,
-                True,
-                True,
-            ),
-            ("unknown", "master", "latest", "latest", True, True, True),
-            ("unknown", "not_ok", "not_ok", "not_ok", True, False, True),
-            ("unknown", "awesome", "awesome", "awesome", True, True, True),
-        ]:
-            Version.objects.get_or_create(
-                project=rtd,
-                type=type,
-                identifier=identifier,
-                verbose_name=verbose_name,
-                slug=slug,
-                supported=supported,
-                active=active,
-                built=built,
-            )
-
-        # Pip versions
         pip = Project.objects.get(slug="pip")
-        for type, identifier, verbose_name, slug, supported, active, built in [
-            (
-                "tag",
-                "2404a34eba4ee9c48cc8bc4055b99a48354f4950",
-                "0.8",
-                "0.8",
-                False,
-                True,
-                False,
-            ),
-            (
-                "tag",
-                "f55c28e560c92cafb6e6451f8084232b6d717603",
-                "0.8.1",
-                "0.8.1",
-                False,
-                True,
-                False,
-            ),
-        ]:
-            Version.objects.get_or_create(
-                project=pip,
-                type=type,
-                identifier=identifier,
-                verbose_name=verbose_name,
-                slug=slug,
-                supported=supported,
-                active=active,
-                built=built,
-            )
-
-        # Versions for sphinx, numpy and conda
         sphinx = Project.objects.get(slug="sphinx")
         numpy = Project.objects.get(slug="numpy")
         conda = Project.objects.get(slug="conda")
+        versions = [
+            {
+                "project": rtd,
+                "type": "unknown",
+                "identifier": "2ff3d36340fa4d3d39424e8464864ca37c5f191c",
+                "verbose_name": "0.2.1",
+                "slug": "0.2.1",
+                "supported": True,
+                "active": True,
+                "built": True,
+            },
+            {
+                "project": rtd,
+                "type": "unknown",
+                "identifier": "354456a7dba2a75888e2fe91f6d921e5fe492bcd",
+                "verbose_name": "0.2.2",
+                "slug": "0.2.2",
+                "supported": True,
+                "active": True,
+                "built": True,
+            },
+            {
+                "project": rtd,
+                "type": "unknown",
+                "identifier": "master",
+                "verbose_name": "latest",
+                "slug": "latest",
+                "supported": True,
+                "active": True,
+                "built": True,
+            },
+            {
+                "project": rtd,
+                "type": "unknown",
+                "identifier": "not_ok",
+                "verbose_name": "not_ok",
+                "slug": "not_ok",
+                "supported": True,
+                "active": False,
+                "built": True,
+            },
+            {
+                "project": rtd,
+                "type": "unknown",
+                "identifier": "awesome",
+                "verbose_name": "awesome",
+                "slug": "awesome",
+                "supported": True,
+                "active": True,
+                "built": True,
+            },
+            {
+                "project": pip,
+                "type": "tag",
+                "identifier": "2404a34eba4ee9c48cc8bc4055b99a48354f4950",
+                "verbose_name": "0.8",
+                "slug": "0.8",
+                "supported": False,
+                "active": True,
+                "built": False,
+            },
+            {
+                "project": pip,
+                "type": "tag",
+                "identifier": "f55c28e560c92cafb6e6451f8084232b6d717603",
+                "verbose_name": "0.8.1",
+                "slug": "0.8.1",
+                "supported": False,
+                "active": True,
+                "built": False,
+            },
+            {
+                "project": sphinx,
+                "type": "branch",
+                "identifier": "master",
+                "verbose_name": "latest",
+                "slug": "latest",
+                "supported": True,
+                "active": True,
+                "built": False,
+            },
+            {
+                "project": numpy,
+                "type": "branch",
+                "identifier": "master",
+                "verbose_name": "latest",
+                "slug": "latest",
+                "supported": True,
+                "active": True,
+                "built": False,
+            },
+            {
+                "project": conda,
+                "type": "branch",
+                "identifier": "master",
+                "verbose_name": "latest",
+                "slug": "latest",
+                "supported": True,
+                "active": True,
+                "built": False,
+            },
+        ]
 
-        for project, type, identifier, verbose_name, slug, supported, active, built in [
-            (sphinx, "branch", "master", "latest", "latest", True, True, False),
-            (numpy, "branch", "master", "latest", "latest", True, True, False),
-            (conda, "branch", "master", "latest", "latest", True, True, False),
-        ]:
-            Version.objects.get_or_create(
-                project=project,
-                type=type,
-                identifier=identifier,
-                verbose_name=verbose_name,
-                slug=slug,
-            )
+        for version in versions:
+            Version.objects.get_or_create(**version)
 
     def handle(self, *args, **kwargs):
         self.load_data()
--- readthedocs/search/management/commands/search_queries.py
+++ readthedocs/search/management/commands/search_queries.py
@@ -15,45 +15,200 @@ class Command(BaseCommand):
     def load_data(self):
         project = Project.objects.get(slug="pip")
         version = Version.objects.get(project_id=project.id, slug="0.8")
-        for pk, query, created in [
-            (5, "search", "2019-08-02T00:00:00Z"),
-            (6, "sphinx", "2019-08-02T00:20:00Z"),
-            (7, "read the docs", "2019-08-02T01:30:00Z"),
-            (8, "elasticsearch", "2019-08-02T02:00:00Z"),
-            (9, "hello", "2019-08-02T03:00:00Z"),
-            (10, "advertising", "2019-08-02T05:00:00Z"),
-            (11, "github", "2019-08-02T04:00:00Z"),
-            (12, "advertising", "2019-08-01T15:00:00Z"),
-            (13, "advertising", "2019-08-01T15:15:00Z"),
-            (14, "sphinx", "2019-08-01T16:00:00Z"),
-            (15, "read the docs", "2019-07-31T20:00:00Z"),
-            (16, "read the docs", "2019-07-31T20:20:00Z"),
-            (17, "read the docs", "2019-07-31T20:30:00Z"),
-            (18, "elasticsearch", "2019-07-31T21:00:00Z"),
-            (19, "documentation", "2019-07-15T06:00:00Z"),
-            (20, "documentation", "2019-07-16T12:00:00Z"),
-            (21, "documentation", "2019-07-17T18:00:00Z"),
-            (22, "documentation", "2019-07-17T12:00:00Z"),
-            (23, "hello world", "2019-06-10T06:00:00Z"),
-            (24, "hello world", "2019-06-11T18:00:00Z"),
-            (25, "hello world", "2019-06-12T18:00:00Z"),
-            (26, "hello world", "2019-07-02T18:00:00Z"),
-            (27, "hello world", "2019-06-25T18:00:00Z"),
-        ]:
-            SearchQuery.objects.get_or_create(
-                id=pk,
-                project=project,
-                version=version,
-                query=query,
-                created=created,
-                modified=created,
-            )
-            s = SearchQuery.objects.get(id=pk)
+        queries = [
+            {
+                "id": 5,
+                "project": project,
+                "version": version,
+                "query": "search",
+                "created": "2019-08-02T00:00:00Z",
+                "modified": "2019-08-02T00:00:00Z",
+            },
+            {
+                "id": 6,
+                "project": project,
+                "version": version,
+                "query": "sphinx",
+                "created": "2019-08-02T00:20:00Z",
+                "modified": "2019-08-02T00:20:00Z",
+            },
+            {
+                "id": 7,
+                "project": project,
+                "version": version,
+                "query": "read the docs",
+                "created": "2019-08-02T01:30:00Z",
+                "modified": "2019-08-02T01:30:00Z",
+            },
+            {
+                "id": 8,
+                "project": project,
+                "version": version,
+                "query": "elasticsearch",
+                "created": "2019-08-02T02:00:00Z",
+                "modified": "2019-08-02T02:00:00Z",
+            },
+            {
+                "id": 9,
+                "project": project,
+                "version": version,
+                "query": "hello",
+                "created": "2019-08-02T03:00:00Z",
+                "modified": "2019-08-02T03:00:00Z",
+            },
+            {
+                "id": 10,
+                "project": project,
+                "version": version,
+                "query": "advertising",
+                "created": "2019-08-02T05:00:00Z",
+                "modified": "2019-08-02T05:00:00Z",
+            },
+            {
+                "id": 11,
+                "project": project,
+                "version": version,
+                "query": "github",
+                "created": "2019-08-02T04:00:00Z",
+                "modified": "2019-08-02T04:00:00Z",
+            },
+            {
+                "id": 12,
+                "project": project,
+                "version": version,
+                "query": "advertising",
+                "created": "2019-08-01T15:00:00Z",
+                "modified": "2019-08-01T15:00:00Z",
+            },
+            {
+                "id": 13,
+                "project": project,
+                "version": version,
+                "query": "advertising",
+                "created": "2019-08-01T15:15:00Z",
+                "modified": "2019-08-01T15:15:00Z",
+            },
+            {
+                "id": 14,
+                "project": project,
+                "version": version,
+                "query": "sphinx",
+                "created": "2019-08-01T16:00:00Z",
+                "modified": "2019-08-01T16:00:00Z",
+            },
+            {
+                "id": 15,
+                "project": project,
+                "version": version,
+                "query": "read the docs",
+                "created": "2019-07-31T20:00:00Z",
+                "modified": "2019-07-31T20:00:00Z",
+            },
+            {
+                "id": 16,
+                "project": project,
+                "version": version,
+                "query": "read the docs",
+                "created": "2019-07-31T20:20:00Z",
+                "modified": "2019-07-31T20:20:00Z",
+            },
+            {
+                "id": 17,
+                "project": project,
+                "version": version,
+                "query": "read the docs",
+                "created": "2019-07-31T20:30:00Z",
+                "modified": "2019-07-31T20:30:00Z",
+            },
+            {
+                "id": 18,
+                "project": project,
+                "version": version,
+                "query": "elasticsearch",
+                "created": "2019-07-31T21:00:00Z",
+                "modified": "2019-07-31T21:00:00Z",
+            },
+            {
+                "id": 19,
+                "project": project,
+                "version": version,
+                "query": "documentation",
+                "created": "2019-07-15T06:00:00Z",
+                "modified": "2019-07-15T06:00:00Z",
+            },
+            {
+                "id": 20,
+                "project": project,
+                "version": version,
+                "query": "documentation",
+                "created": "2019-07-16T12:00:00Z",
+                "modified": "2019-07-16T12:00:00Z",
+            },
+            {
+                "id": 21,
+                "project": project,
+                "version": version,
+                "query": "documentation",
+                "created": "2019-07-17T18:00:00Z",
+                "modified": "2019-07-17T18:00:00Z",
+            },
+            {
+                "id": 22,
+                "project": project,
+                "version": version,
+                "query": "documentation",
+                "created": "2019-07-17T12:00:00Z",
+                "modified": "2019-07-17T12:00:00Z",
+            },
+            {
+                "id": 23,
+                "project": project,
+                "version": version,
+                "query": "hello world",
+                "created": "2019-06-10T06:00:00Z",
+                "modified": "2019-06-10T06:00:00Z",
+            },
+            {
+                "id": 24,
+                "project": project,
+                "version": version,
+                "query": "hello world",
+                "created": "2019-06-11T18:00:00Z",
+                "modified": "2019-06-11T18:00:00Z",
+            },
+            {
+                "id": 25,
+                "project": project,
+                "version": version,
+                "query": "hello world",
+                "created": "2019-06-12T18:00:00Z",
+                "modified": "2019-06-12T18:00:00Z",
+            },
+            {
+                "id": 26,
+                "project": project,
+                "version": version,
+                "query": "hello world",
+                "created": "2019-07-02T18:00:00Z",
+                "modified": "2019-07-02T18:00:00Z",
+            },
+            {
+                "id": 27,
+                "project": project,
+                "version": version,
+                "query": "hello world",
+                "created": "2019-06-25T18:00:00Z",
+                "modified": "2019-06-25T18:00:00Z",
+            },
+        ]
+        for query in queries:
+            SearchQuery.objects.get_or_create(**query)
+            s = SearchQuery.objects.get(id=query["id"])
             s.created = timezone.make_aware(
-                datetime.datetime.strptime(created, "%Y-%m-%dT%H:%M:%SZ")
+                datetime.datetime.strptime(query["created"], "%Y-%m-%dT%H:%M:%SZ")
             )
             s.modified = timezone.make_aware(
-                datetime.datetime.strptime(created, "%Y-%m-%dT%H:%M:%SZ")
+                datetime.datetime.strptime(query["created"], "%Y-%m-%dT%H:%M:%SZ")
             )
             s.save()
 
