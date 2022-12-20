f8497306c56900c5e8d6b0794e63b6bc4cef4cde
Benjamin Bach <benjamin@overtag.dk>
2022-12-20 19:45:57 +0100

    Changes label from "Child" to "Subproject"

--- readthedocs/projects/forms.py
+++ readthedocs/projects/forms.py
@@ -433,7 +433,8 @@ class ProjectRelationshipForm(forms.ModelForm):
 
     class Meta:
         model = ProjectRelationship
-        fields = '__all__'
+        fields = "__all__"
+        labels = {"child": _("Subproject")}
 
     def __init__(self, *args, **kwargs):
         self.project = kwargs.pop('project')
