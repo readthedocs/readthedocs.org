1654e06d8f9818bf8d5d7fde90ca92cca2ac3eb2
Santos Gallegos <stsewd@proton.me>
2023-01-04 10:56:04 -0500

    Embedded JS: Inject jQuery is isn't present
    For injecting the flyout menu we are relying on a script from our theme,
    that script depends on jQuery, so it needs to be present for the
    flyout menu to work.
    
    https://github.com/readthedocs/readthedocs.org/blob/67c44a1f1b27dcc66bd14520d019459fcf86354d/readthedocs/core/static-src/core/js/doc-embed/sphinx.js#L43-L59
    
    We have migrated most of our code to vanilla JS,
    but this piece isn't simple to migrate.

--- readthedocs/core/static-src/core/js/readthedocs-doc-embed.js
+++ readthedocs/core/static-src/core/js/readthedocs-doc-embed.js
@@ -6,11 +6,36 @@ const sphinx = require('./doc-embed/sphinx');
 const search = require('./doc-embed/search');
 const { domReady } = require('./doc-embed/utils');
 
+/*
+ * Inject JQuery if isn't present already.
+ *
+ * Parts of this script rely on JQuery (mainly the flyout menu injection),
+ * since Sphinx no longer includes it, and other tools may not include it,
+ * we must inject it if isn't found before executing our script.
+*/
+function injectJQuery(init) {
+    if (window.jQuery) {
+        init()
+        return
+    }
+    console.log("JQuery not found. Injecting.");
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = "https://code.jquery.com/jquery-3.6.3.min.js";
+    script.integrity = "sha512-STof4xm1wgkfm7heWqFJVn58Hm3EtS31XFaagaa8VMReCXAkQnJZ+jEy8PCC/iT18dFy95WcExNHFTqLyp72eQ==";
+    script.crossOrigin = "anonymous";
+    script.onload = init;
+    document.head.appendChild(script);
+}
+
+
 (function () {
     domReady(function () {
-        footer.init();
-        sphinx.init();
-        search.init();
-        sponsorship.init();
+        injectJQuery(function() {
+            footer.init();
+            sphinx.init();
+            search.init();
+            sponsorship.init();
+        })
     });
 }());
