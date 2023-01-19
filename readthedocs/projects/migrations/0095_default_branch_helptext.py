4c6f1a721489f7afcb1b1884494f2c65d1025353
Manuel Kaufmann <humitos@gmail.com>
2023-01-19 19:11:54 +0100

    Migrations dependencies

--- readthedocs/projects/migrations/0095_default_branch_helptext.py
+++ readthedocs/projects/migrations/0095_default_branch_helptext.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("projects", "0090_dont_allow_ips_on_domains"),
+        ("projects", "0094_auto_20221221_1045"),
     ]
 
     operations = [
