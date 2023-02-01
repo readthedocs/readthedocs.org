3e766c3962bb10f8985e5ad26c87bc2a2830e006
Manuel Kaufmann <humitos@gmail.com>
2023-02-01 14:03:53 +0100

    Lint

--- readthedocs/doc_builder/python_environments.py
+++ readthedocs/doc_builder/python_environments.py
@@ -318,7 +318,7 @@ class Conda(PythonEnvironment):
     .. _Conda: https://conda.io/docs/
     """
 
-    def venv_bin(self, filename=None):
+    def venv_bin(self, filename=None):  # pylint disable=arguments-differ
         return super().venv_bin(filename=filename, conda=True)
 
     def conda_bin_name(self):
