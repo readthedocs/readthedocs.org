afbed37de6eab2245b6e55ef90ecb442fb129cfa
Manuel Kaufmann <humitos@gmail.com>
2023-02-07 14:04:53 +0100

    Release 9.4.0

--- docs/conf.py
+++ docs/conf.py
@@ -56,7 +56,7 @@ templates_path = ["_templates"]
 
 master_doc = "index"
 copyright = "Read the Docs, Inc & contributors"
-version = "9.3.1"
+version = "9.4.0"
 release = version
 exclude_patterns = ["_build", "shared"]
 default_role = "obj"
