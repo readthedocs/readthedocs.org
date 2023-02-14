e5ac6384b3c4e675afb7de22ecffebdf00525528
Santos Gallegos <stsewd@proton.me>
2023-02-13 20:22:46 -0500

    Dark

--- readthedocs/proxito/tests/test_headers.py
+++ readthedocs/proxito/tests/test_headers.py
@@ -21,23 +21,25 @@ class ProxitoHeaderTests(BaseDocServing):
         self.assertEqual(
             r['Location'], 'https://project.dev.readthedocs.io/en/latest/',
         )
-        self.assertEqual(r['Cache-Tag'], 'project')
-        self.assertEqual(r['X-RTD-Project'], 'project')
-        self.assertEqual(r['X-RTD-Project-Method'], 'public_domain')
-        self.assertEqual(r['X-RTD-Domain'], 'project.dev.readthedocs.io')
-        self.assertIsNone(r.get('X-RTD-Version'))
-        self.assertIsNone(r.get('X-RTD-Path'))
+        self.assertEqual(r["Cache-Tag"], "project")
+        self.assertEqual(r["X-RTD-Project"], "project")
+        self.assertEqual(r["X-RTD-Project-Method"], "public_domain")
+        self.assertEqual(r["X-RTD-Domain"], "project.dev.readthedocs.io")
+        self.assertIsNone(r.get("X-RTD-Version"))
+        self.assertIsNone(r.get("X-RTD-Path"))
 
     def test_serve_headers(self):
         r = self.client.get('/en/latest/', HTTP_HOST='project.dev.readthedocs.io')
         self.assertEqual(r.status_code, 200)
-        self.assertEqual(r['Cache-Tag'], 'project,project:latest')
-        self.assertEqual(r['X-RTD-Domain'], 'project.dev.readthedocs.io')
-        self.assertEqual(r['X-RTD-Project'], 'project')
-        self.assertEqual(r['X-RTD-Project-Method'], 'public_domain')
-        self.assertEqual(r['X-RTD-Version'], 'latest')
-        self.assertEqual(r['X-RTD-version-Method'], 'path')
-        self.assertEqual(r['X-RTD-Path'], '/proxito/media/html/project/latest/index.html')
+        self.assertEqual(r["Cache-Tag"], "project,project:latest")
+        self.assertEqual(r["X-RTD-Domain"], "project.dev.readthedocs.io")
+        self.assertEqual(r["X-RTD-Project"], "project")
+        self.assertEqual(r["X-RTD-Project-Method"], "public_domain")
+        self.assertEqual(r["X-RTD-Version"], "latest")
+        self.assertEqual(r["X-RTD-version-Method"], "path")
+        self.assertEqual(
+            r["X-RTD-Path"], "/proxito/media/html/project/latest/index.html"
+        )
 
     def test_subproject_serve_headers(self):
         r = self.client.get('/projects/subproject/en/latest/', HTTP_HOST='project.dev.readthedocs.io')
@@ -49,7 +51,7 @@ class ProxitoHeaderTests(BaseDocServing):
         # I think it's not accurate saying that it's `subdomain` the method
         # that we use to get the project slug here, since it was in fact the
         # URL's path but we don't have that feature built
-        self.assertEqual(r['X-RTD-Project-Method'], 'public_domain')
+        self.assertEqual(r["X-RTD-Project-Method"], "public_domain")
 
         self.assertEqual(r['X-RTD-Version'], 'latest')
         self.assertEqual(r['X-RTD-version-Method'], 'path')
@@ -58,13 +60,13 @@ class ProxitoHeaderTests(BaseDocServing):
     def test_404_headers(self):
         r = self.client.get('/foo/bar.html', HTTP_HOST='project.dev.readthedocs.io')
         self.assertEqual(r.status_code, 404)
-        self.assertEqual(r['Cache-Tag'], 'project')
-        self.assertEqual(r['X-RTD-Domain'], 'project.dev.readthedocs.io')
-        self.assertEqual(r['X-RTD-Project'], 'project')
-        self.assertEqual(r['X-RTD-Project-Method'], 'public_domain')
-        self.assertEqual(r['X-RTD-version-Method'], 'path')
-        self.assertIsNone(r.get('X-RTD-Version'))
-        self.assertIsNone(r.get('X-RTD-Path'))
+        self.assertEqual(r["Cache-Tag"], "project")
+        self.assertEqual(r["X-RTD-Domain"], "project.dev.readthedocs.io")
+        self.assertEqual(r["X-RTD-Project"], "project")
+        self.assertEqual(r["X-RTD-Project-Method"], "public_domain")
+        self.assertEqual(r["X-RTD-version-Method"], "path")
+        self.assertIsNone(r.get("X-RTD-Version"))
+        self.assertIsNone(r.get("X-RTD-Path"))
 
     def test_custom_domain_headers(self):
         hostname = 'docs.random.com'
@@ -75,13 +77,15 @@ class ProxitoHeaderTests(BaseDocServing):
         )
         r = self.client.get("/en/latest/", HTTP_HOST=hostname)
         self.assertEqual(r.status_code, 200)
-        self.assertEqual(r['Cache-Tag'], 'project,project:latest')
-        self.assertEqual(r['X-RTD-Domain'], self.domain.domain)
-        self.assertEqual(r['X-RTD-Project'], self.project.slug)
-        self.assertEqual(r['X-RTD-Project-Method'], 'custom_domain')
-        self.assertEqual(r['X-RTD-Version'], 'latest')
-        self.assertEqual(r['X-RTD-version-Method'], 'path')
-        self.assertEqual(r['X-RTD-Path'], '/proxito/media/html/project/latest/index.html')
+        self.assertEqual(r["Cache-Tag"], "project,project:latest")
+        self.assertEqual(r["X-RTD-Domain"], self.domain.domain)
+        self.assertEqual(r["X-RTD-Project"], self.project.slug)
+        self.assertEqual(r["X-RTD-Project-Method"], "custom_domain")
+        self.assertEqual(r["X-RTD-Version"], "latest")
+        self.assertEqual(r["X-RTD-version-Method"], "path")
+        self.assertEqual(
+            r["X-RTD-Path"], "/proxito/media/html/project/latest/index.html"
+        )
 
     def test_footer_headers(self):
         version = self.project.versions.get(slug=LATEST)
