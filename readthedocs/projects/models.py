d0d8621889e76c89e24c7227197cf87643f728f2
Benjamin Bach <benjamin@readthedocs.org>
2023-02-24 18:18:16 +0100

    lint a github suggestion

--- readthedocs/projects/models.py
+++ readthedocs/projects/models.py
@@ -360,7 +360,7 @@ class Project(models.Model):
 
     featured = models.BooleanField(_('Featured'), default=False)
 
-    skip = models.BooleanField(_('Skip (disable) building this project'), default=False)
+    skip = models.BooleanField(_("Skip (disable) building this project"), default=False)
 
     delisted = models.BooleanField(
         default=False,
