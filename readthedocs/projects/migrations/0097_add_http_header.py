8ba7a0501865f0cecc926504bbe9b50bb2a2032d
Santos Gallegos <stsewd@proton.me>
2023-03-01 17:38:25 -0500

    Fix migration (#10096)

--- readthedocs/projects/migrations/0097_add_http_header.py
+++ readthedocs/projects/migrations/0097_add_http_header.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("projects", "0095_default_branch_helptext"),
+        ("projects", "0096_add_project_delisted"),
     ]
 
     operations = [
