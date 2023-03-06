6bb753a3ba24a829d6ab9d02bf83eb844d222b11
Manuel Kaufmann <humitos@gmail.com>
2023-03-06 18:51:01 +0100

    Celery: cheat `job_status` view to return `finished` after 5 polls (#10107)
    * Celery: cheat `job_status` view to return `finished` after 5 polls
    
    The frontend hits this endpoint every 1s. As we don't know when the task has
    finished now since we disabled the Celery result backend, we are just cheating
    it and return finished after 5 times the view is called with the same `task_id`.
    
    This fixes the issue we have in the UI when clicking the circled arrows to
    re-sync the remote repositories.
    
    * Update readthedocs/api/v2/views/task_views.py
    
    Co-authored-by: Anthony <aj@ohess.org>
    
    ---------
    
    Co-authored-by: Anthony <aj@ohess.org>

--- readthedocs/core/utils/tasks/__init__.py
+++ readthedocs/core/utils/tasks/__init__.py
@@ -6,6 +6,4 @@ from .permission_checks import (  # noqa for unused import
 )
 from .public import PublicTask  # noqa
 from .public import TaskNoPermission  # noqa
-from .public import get_public_task_data  # noqa
 from .retrieve import TaskNotFound  # noqa
-from .retrieve import get_task_data  # noqa
