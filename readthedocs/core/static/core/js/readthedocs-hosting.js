1e391b820c2b45504419b9affe00f7592dd09a32
Manuel Kaufmann <humitos@gmail.com>
2023-03-08 15:15:25 +0100

    Old version warning

--- readthedocs/core/static/core/js/readthedocs-hosting.js
+++ readthedocs/core/static/core/js/readthedocs-hosting.js
@@ -4,7 +4,7 @@ function injectExternalVersionWarning() {
   <p class="admonition-title">Warning</p>
   <p>
     This page
-    <a class="reference external" href="${ window.location.protocol }//${ window.location.domain }/projects/${ READTHEDOCS_DATA.project }/builds/${ READTHEDOCS_DATA.build.id }/">was created </a>
+    <a class="reference external" href="${ window.location.protocol }//${ window.location.hostname }/projects/${ READTHEDOCS_DATA.project }/builds/${ READTHEDOCS_DATA.build.id }/">was created </a>
     from a pull request
     (<a class="reference external" href="${ READTHEDOCS_DATA.repository_url }/pull/${ READTHEDOCS_DATA.version }">#${ READTHEDOCS_DATA.version }</a>).
   </p>
@@ -24,6 +24,27 @@ function injectDocDiff() {
     document.head.appendChild(docdiff);
 }
 
+function injectOldVersionWarning() {
+    // TODO: compute if the user is reading the latest version or not before showing the warning.
+    let admonition = `
+<div class="admonition warning">
+  <p class="admonition-title">Warning</p>
+  <p>
+    This page documents version
+    <a class="reference" href="${ window.location.protocol }//${ window.location.hostname }/${ READTHEDOCS_DATA.language }/${ READTHEDOCS_DATA.version }/">${ READTHEDOCS_DATA.version }</a>.
+    The latest version is
+    <a class="reference" href="${ window.location.protocol }//${ window.location.hostname }/${ READTHEDOCS_DATA.language }/${ READTHEDOCS_DATA.version }/">${ READTHEDOCS_DATA.version }</a>.
+  </p>
+</div>
+`;
+
+    let main = document.querySelector('[role=main]');
+    let node = document.createElement("div");
+    node.innerHTML = admonition;
+    main.insertBefore(node, main.firstChild);
+}
+
+
 window.addEventListener("load", (event) => {
     if (READTHEDOCS_DATA.build.external_version === true) {
         injectExternalVersionWarning();
@@ -32,4 +53,5 @@ window.addEventListener("load", (event) => {
             injectDocDiff();
         }
     }
+    injectOldVersionWarning();
 });
