7754d5f98278561c04dcdbccad59390030b07980
Manuel Kaufmann <humitos@gmail.com>
2023-03-08 14:06:50 +0100

    Include Read the Docs analytics to integrations

--- readthedocs/core/static/core/js/integrations.js
+++ readthedocs/core/static/core/js/integrations.js
@@ -6,7 +6,12 @@ link.setAttribute("type", "text/css");
 link.setAttribute("href", "/_/static/css/readthedocs-doc-embed.css");
 document.head.appendChild(link);
 
-let script = document.createElement("script");
-script.setAttribute("async", "async");
-script.setAttribute("src", "/_/static/javascript/readthedocs-doc-embed.js");
-document.head.appendChild(script);
+let embed = document.createElement("script");
+embed.setAttribute("async", "async");
+embed.setAttribute("src", "/_/static/javascript/readthedocs-doc-embed.js");
+document.head.appendChild(embed);
+
+let analytics = document.createElement("script");
+analytics.setAttribute("async", "async");
+analytics.setAttribute("src", "/_/static/javascript/readthedocs-analytics.js");
+document.head.appendChild(analytics);
