cc77f5ebc5b4c5763245794cbccedc442703eb35
Benjamin Bach <benjamin@readthedocs.org>
2023-03-20 18:06:00 +0100

    test case for Sphinx autodoc HTML

--- readthedocs/search/tests/test_parsers.py
+++ readthedocs/search/tests/test_parsers.py
@@ -308,7 +308,32 @@ class TestParsers:
 
         parsed_json = page_file.processed_json
         expected_json = json.load(open(data_path / "sphinx/out/httpdomain.json"))
-        print(json.dumps(parsed_json, indent=2))
+        assert parsed_json == expected_json
+
+    @mock.patch.object(BuildMediaFileSystemStorage, "exists")
+    @mock.patch.object(BuildMediaFileSystemStorage, "open")
+    def test_sphinx_autodoc(self, storage_open, storage_exists):
+        # Source:
+        # https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html#directive-automodule
+        json_file = data_path / "sphinx/in/autodoc.json"
+        html_content = data_path / "sphinx/in/autodoc.html"
+
+        json_content = json.load(json_file.open())
+        json_content["body"] = html_content.open().read()
+        storage_open.side_effect = self._mock_open(json.dumps(json_content))
+        storage_exists.return_value = True
+
+        self.version.save()
+
+        page_file = get(
+            HTMLFile,
+            project=self.project,
+            version=self.version,
+            path="autodoc.html",
+        )
+
+        parsed_json = page_file.processed_json
+        expected_json = json.load(open(data_path / "sphinx/out/autodoc.json"))
         assert parsed_json == expected_json
 
     @mock.patch.object(BuildMediaFileSystemStorage, "exists")
