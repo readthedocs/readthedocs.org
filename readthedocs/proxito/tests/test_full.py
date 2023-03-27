8f6c99024c34fb9f63c05cc694c1e1bb2639e1da
Santos Gallegos <stsewd@proton.me>
2023-03-27 18:12:55 -0500

    Proxito: allow serving files under the projects dir (#10180)
    Proxito: allow serving files under the projects dir
    
    This is only available in the new implementation.
    
    Closes https://github.com/readthedocs/readthedocs.org/issues/2292

--- readthedocs/proxito/tests/test_full.py
+++ readthedocs/proxito/tests/test_full.py
@@ -375,6 +375,17 @@ class ProxitoV2TestFullDocServing(TestFullDocServing):
             future_default_true=True,
         )
 
+    def test_single_version_serving_projects_dir(self):
+        self.project.single_version = True
+        self.project.save()
+        url = "/projects/awesome.html"
+        host = "project.dev.readthedocs.io"
+        resp = self.client.get(url, HTTP_HOST=host)
+        self.assertEqual(
+            resp["x-accel-redirect"],
+            "/proxito/media/html/project/latest/projects/awesome.html",
+        )
+
 
 @override_settings(
     PUBLIC_DOMAIN="dev.readthedocs.io",
