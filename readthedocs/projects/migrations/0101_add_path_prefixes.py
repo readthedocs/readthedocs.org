d6124992f7aea09895258e90e3c434ac6e79af10
Santos Gallegos <stsewd@proton.me>
2023-05-03 18:18:20 -0500

    Fix migration

--- readthedocs/projects/migrations/0101_add_path_prefixes.py
+++ readthedocs/projects/migrations/0101_add_path_prefixes.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("projects", "0099_alter_domain_https"),
+        ("projects", "0100_project_readthedocs_yaml_path"),
     ]
 
     operations = [
