e8d988ed9d9f7ef495d424650d5670791be2ad61
Benjamin Bach <benjamin@readthedocs.org>
2023-05-22 20:11:12 +0200

    Do not handle additional kwargs, it's meaningless. Raise 404 and log a warning.

--- readthedocs/organizations/views/decorators.py
+++ readthedocs/organizations/views/decorators.py
@@ -4,6 +4,11 @@ from django.conf import settings
 from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import resolve_url
 from django.utils.http import urlencode
+from structlog import get_logger
+
+from readthedocs.organizations.models import Organization
+
+log = get_logger(__name__)
 
 
 def redirect_if_organization_unspecified(dispatch):
@@ -24,30 +29,38 @@ def redirect_if_organization_unspecified(dispatch):
 
         # Magic '-' slug means that an organization hasn't been set in the URL.
         if slug == "-":
-            organizations = cbv_object.get_queryset()
+
+            organizations = Organization.objects.for_user(user=cbv_object.request.user)
 
             current_url_name = cbv_object.request.resolver_match.url_name
             current_url_kwargs = cbv_object.request.resolver_match.kwargs
             current_url_args = cbv_object.request.resolver_match.args
 
+            # This type of redirect is pretty meaningless for views with arguments
+            # because the kwargs are almost always dependent on the organization slug
+            # (teams, members etc)
+            if current_url_args or not all(
+                k == "slug" for k in current_url_kwargs.keys()
+            ):
+                log.warning(
+                    "Tried to use unspecific 'slug' argument for a url with addition arguments",
+                    kwargs=current_url_kwargs,
+                    args=current_url_args,
+                )
+                raise Http404
+
             querystring = cbv_object.request.META.get("QUERY_STRING", None)
 
-            # The slug is what we are parameterizing when choosing an organization.
-            # Remove it to avoid any confusion.
-            if "slug" in current_url_kwargs:
-                del current_url_kwargs["slug"]
-
-            # User controls exactly 1 organization, so we can redirect here.
+            # User is a member of exactly 1 organization, so we can redirect here.
             if organizations.count() == 1:
-                current_url_kwargs["slug"] = organizations.first().slug
                 destination_url = resolve_url(
-                    current_url_name, *current_url_args, **current_url_kwargs
+                    current_url_name, slug=organizations.first().slug
                 )
                 if querystring:
                     destination_url += "?" + querystring
                 return HttpResponseRedirect(destination_url)
 
-            # TODO: Figure out what to do with the URL path's kwargs?
+            # User is a member of 0 or >1 organizations: redirect to the chooser page
             destination_url = resolve_url(
                 "organization_choose",
                 next_name=current_url_name,
@@ -57,6 +70,7 @@ def redirect_if_organization_unspecified(dispatch):
                 destination_url += "?next_querystring=" + urlencode(querystring)
             return HttpResponseRedirect(destination_url)
 
+        # Continue the call
         return dispatch(cbv_object, *args, **kwargs)
 
     return inner
