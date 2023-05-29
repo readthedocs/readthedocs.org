9a819fae6cb81b9f72c3e3f1d9124f1278059ec5
Manuel Kaufmann <humitos@gmail.com>
2023-05-29 12:39:53 +0200

    Template: add more style to the wizard config suggestion

--- readthedocs/projects/forms.py
+++ readthedocs/projects/forms.py
@@ -196,6 +196,7 @@ class ProjectConfigForm(forms.Form):
     """Simple intermediate step to communicate about the .readthedocs.yaml file."""
 
     confirm = forms.BooleanField(
+        label="",
         help_text="I've already added a '.readthedocs.yaml' file to my project",
         required=True,
     )
