f85bf2cf1401536d6f80a7cb0f6f02dcc80bcbd9
Manuel Kaufmann <humitos@gmail.com>
2023-06-01 09:38:31 +0200

    Consistency on naming

--- readthedocs/projects/tasks/utils.py
+++ readthedocs/projects/tasks/utils.py
@@ -240,9 +240,9 @@ def deprecated_config_file_used_notification():
         projects=n_projects,
     )
 
-    projects = Project.objects.filter(slug__in=projects).order_by("id")
+    queryset = Project.objects.filter(slug__in=projects).order_by("id")
     start_datetime = datetime.datetime.now()
-    for i, project in enumerate(projects.iterator()):
+    for i, project in enumerate(queryset.iterator()):
 
         if i % 500 == 0:
             log.info(
