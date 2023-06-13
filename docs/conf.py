de1c1faf663e6525319a08be6e046460b7a35eea
Manuel Kaufmann <humitos@gmail.com>
2023-06-13 05:01:46 -0700

    Release 9.13.3 (#10428)

--- docs/conf.py
+++ docs/conf.py
@@ -75,7 +75,7 @@ html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "/")
 
 master_doc = "index"
 copyright = "Read the Docs, Inc & contributors"
-version = "9.13.2"
+version = "9.13.3"
 release = version
 exclude_patterns = ["_build", "shared", "_includes"]
 default_role = "obj"
