9fb226e2d1c54d48605b071320403352095b700d
Manuel Kaufmann <humitos@gmail.com>
2023-07-12 17:53:01 +0200

    Addons: rename library (#10531)
    Related https://github.com/readthedocs/addons/issues/4

--- readthedocs/projects/models.py
+++ readthedocs/projects/models.py
@@ -2086,7 +2086,7 @@ class Feature(models.Model):
         (
             HOSTING_INTEGRATIONS,
             _(
-                "Proxito: Inject 'readthedocs-client.js' as <script> HTML tag in responses."
+                "Proxito: Inject 'readthedocs-addons.js' as <script> HTML tag in responses."
             ),
         ),
         (
