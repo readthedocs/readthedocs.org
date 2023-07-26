724ab21e6673828f3b721644e80a50a7fc441fa7
Manuel Kaufmann <humitos@gmail.com>
2023-07-26 13:19:46 +0200

    Test: create an "index.html" to run build successfully

--- readthedocs/projects/tests/test_build_tasks.py
+++ readthedocs/projects/tests/test_build_tasks.py
@@ -294,6 +294,14 @@ class TestBuildTask(BuildEnvironmentBase):
                 )
             ).touch()
 
+        # Create an "index.html" at root to avoid failing the builds
+        pathlib.Path(
+            os.path.join(
+                self.project.artifact_path(version=self.version.slug, type_="html"),
+                "index.html",
+            )
+        ).touch()
+
         self._trigger_update_docs_task()
 
         # Update version state
@@ -437,6 +445,14 @@ class TestBuildTask(BuildEnvironmentBase):
                     f"{self.project.slug}.{f}",
                 )
             ).touch()
+
+        # Create an "index.html" at root to avoid failing the builds
+        pathlib.Path(
+            os.path.join(
+                self.project.artifact_path(version=self.version.slug, type_="html"),
+                "index.html",
+            )
+        ).touch()
 
         self._trigger_update_docs_task()
 
