9d68d925f6b2dc923bda854f6c066ba6b1477970
Eric Holscher <eric@ericholscher.com>
2023-07-27 09:52:24 -0700

    Uninstall sphinx_domains app to it's models aren't registered

--- readthedocs/settings/base.py
+++ readthedocs/settings/base.py
@@ -248,7 +248,6 @@ class CommunityBaseSettings(Settings):
             'readthedocs.notifications',
             'readthedocs.integrations',
             'readthedocs.analytics',
-            'readthedocs.sphinx_domains',
             'readthedocs.search',
             'readthedocs.embed',
             'readthedocs.telemetry',
