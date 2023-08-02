57a1e0b7cf664df422bbcf76e768f33d2b629321
Anthony Johnson <aj@ohess.org>
2023-08-01 17:50:48 -0700

    Make prospector happy

--- readthedocs/core/context_processors.py
+++ readthedocs/core/context_processors.py
@@ -4,7 +4,6 @@ from django.conf import settings
 
 
 def readthedocs_processor(request):
-    # pylint: disable=unused-argument
     exports = {
         "PUBLIC_DOMAIN": settings.PUBLIC_DOMAIN,
         "PRODUCTION_DOMAIN": settings.PRODUCTION_DOMAIN,
