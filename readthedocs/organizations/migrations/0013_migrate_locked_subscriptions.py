f95a0f331e4b3bff26aea440274a5cc3da0cdab5
Santos Gallegos <stsewd@proton.me>
2023-08-21 16:36:56 -0500

    Format

--- readthedocs/organizations/migrations/0013_migrate_locked_subscriptions.py
+++ readthedocs/organizations/migrations/0013_migrate_locked_subscriptions.py
@@ -2,19 +2,21 @@
 
 from django.db import migrations
 
+
 def forwards_func(apps, schema_editor):
     """Migrate locked subscriptions to never_disable organizations."""
     Subscription = apps.get_model("subscriptions", "Subscription")
-    locked_subscriptions = Subscription.objects.filter(locked=True).select_related('organization')
+    locked_subscriptions = Subscription.objects.filter(locked=True).select_related(
+        "organization"
+    )
     for subscription in locked_subscriptions:
         subscription.organization.never_disable = True
         subscription.organization.save()
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('organizations', '0012_add_organization_never_disable'),
+        ("organizations", "0012_add_organization_never_disable"),
     ]
 
     operations = [
