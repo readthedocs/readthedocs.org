d821bc813a7a1a943e0d876ba482fd173eba3e4b
Santos Gallegos <stsewd@proton.me>
2023-09-26 11:19:59 -0500

    Linter

--- readthedocs/search/apps.py
+++ readthedocs/search/apps.py
@@ -2,7 +2,6 @@ from django.apps import AppConfig
 
 
 class SearchConfig(AppConfig):
-
     default_auto_field = "django.db.models.BigAutoField"
     name = 'readthedocs.search'
 
--- readthedocs/search/migrations/0005_alter_searchquery_id.py
+++ readthedocs/search/migrations/0005_alter_searchquery_id.py
@@ -4,7 +4,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("search", "0004_make_total_results_not_null"),
     ]
