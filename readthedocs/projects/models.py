7bc5c763f49f7e4f1bbd34501675052f465cb428
Manuel Kaufmann <humitos@gmail.com>
2023-09-26 18:48:21 +0200

    Build: remove support for MkDocs <= 0.17.3 (#10584)
    We want to keep deprecating old feature flags we don't want to support forever.
    In particular, those that keep our users using pretty old versions of doctools
    and freeze them in a status that's hard to maintain.
    
    This PR removes the `DEFATUL_TO_MKDOCS_0_17_3` feature flag and always install
    the latest `mkdocs` version. Note this feature flag has `default_true=True` for
    those projects created _before 2019-04-03_.
    
    I did a small DB query and found:
    
    ```python
    >>> Project.objects.filter(pub_date__lt=timezone.datetime(2019, 4, 3),
    documentation_type='mkdocs').count()
    7529
    ```
    
    However, note we don't know if these projects are pinning a version of MkDocs,
    so we don't exactly know how many of those are using this feature. However, it
    gives us a maximum number at least.
    
    On the other hand, taking a look at Metabase, using the build data from the past
    6 months I found that we only have 293 projects:
    https://ethicalads.metabaseapp.com/question/218-projects-using-mkdocs-group-by-version
    
    Related https://github.com/readthedocs/readthedocs.org/issues/9779

--- readthedocs/projects/models.py
+++ readthedocs/projects/models.py
@@ -1892,7 +1892,6 @@ class Feature(models.Model):
     USE_NEW_PIP_RESOLVER = 'use_new_pip_resolver'
     DONT_INSTALL_LATEST_PIP = 'dont_install_latest_pip'
     USE_SPHINX_LATEST = 'use_sphinx_latest'
-    DEFAULT_TO_MKDOCS_0_17_3 = 'default_to_mkdocs_0_17_3'
     USE_SPHINX_RTD_EXT_LATEST = 'rtd_sphinx_ext_latest'
     INSTALL_LATEST_CORE_REQUIREMENTS = "install_latest_core_requirements"
 
@@ -1999,10 +1998,6 @@ class Feature(models.Model):
             _("Build: Don't install the latest version of pip"),
         ),
         (USE_SPHINX_LATEST, _("Sphinx: Use latest version of Sphinx")),
-        (
-            DEFAULT_TO_MKDOCS_0_17_3,
-            _("MkDOcs: Install mkdocs 0.17.3 by default"),
-        ),
         (
             USE_SPHINX_RTD_EXT_LATEST,
             _("Sphinx: Use latest version of the Read the Docs Sphinx extension"),
