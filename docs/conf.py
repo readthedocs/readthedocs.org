2e4bb4d534c1c6139fb524d3d785e8d9cdea2f29
Manuel Kaufmann <humitos@gmail.com>
2023-10-10 14:10:40 +0200

    Release 10.7.0

--- docs/conf.py
+++ docs/conf.py
@@ -77,7 +77,7 @@ html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "/")
 
 master_doc = "index"
 copyright = "Read the Docs, Inc & contributors"
-version = "10.6.1"
+version = "10.7.0"
 release = version
 exclude_patterns = ["_build", "shared", "_includes"]
 default_role = "obj"
--- readthedocs/__init__.py
+++ readthedocs/__init__.py
@@ -1,4 +1,4 @@
 """Read the Docs."""
 
 
-__version__ = "10.6.1"
+__version__ = "10.7.0"
