8cf28b3db166182fa9afcdfc4a345ab8571e6fa4
Anthony Johnson <aj@ohess.org>
2024-02-08 19:47:31 -0800

    Refactor project create form to use form validation errors
    Instead of using notifications without a database, use form validation
    errors to raise error states on specific forms, or outright disable
    them.

--- readthedocs/projects/exceptions.py
+++ readthedocs/projects/exceptions.py
@@ -2,7 +2,6 @@
 
 
 from readthedocs.doc_builder.exceptions import BuildAppError, BuildUserError
-from readthedocs.notifications.exceptions import NotificationBaseException
 
 
 class ProjectConfigurationError(BuildUserError):
@@ -33,20 +32,3 @@ class SyncRepositoryLocked(BuildAppError):
     """Error risen when there is another sync_repository_task already running."""
 
     REPOSITORY_LOCKED = "project:repository:locked"
-
-
-class ProjectAutomaticCreationDisallowed(NotificationBaseException):
-
-    """Used for raising messages in the project automatic create form view"""
-
-    NO_CONNECTED_ACCOUNT = "project:create:automatic:no-connected-account"
-    SSO_ENABLED = "project:create:automatic:sso-enabled"
-    INADEQUATE_PERMISSIONS = "project:create:automatic:inadequate-permissions"
-
-
-class ProjectManualCreationDisallowed(NotificationBaseException):
-
-    """Used for raising messages in the project manual create form view"""
-
-    SSO_ENABLED = "project:create:manual:sso-enabled"
-    INADEQUATE_PERMISSIONS = "project:create:manual:inadequate-permissions"
