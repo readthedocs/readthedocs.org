c55598d051576bfa812f8f242879e496818093f4
Manuel Kaufmann <humitos@gmail.com>
2024-02-21 10:34:34 +0100

    Lint

--- readthedocs/rtd_tests/tests/test_privacy.py
+++ readthedocs/rtd_tests/tests/test_privacy.py
@@ -1,6 +1,6 @@
-import structlog
 from unittest import mock
 
+import structlog
 from django.contrib.auth.models import User
 from django.test import TestCase
 from django.test.utils import override_settings
