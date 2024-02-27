e74350b1ba90943f79010ace6270b9b57cfa3d43
Manuel Kaufmann <humitos@gmail.com>
2024-02-27 18:37:27 +0100

    New dashboard: notification to point users there
    We can add the notifications to all the users by running the following code:
    
    ```python
    from readthedocs.core.notifications import MESSAGE_BETA_DASHBOARD_AVAILABLE
    
    for user in User.objects.all():
        Notification.objects.add(
          message_id=MESSAGE_BETA_DASHBOARD_AVAILABLE,
          attached_to=user,
          dismissable=True,
        )
    ```
    
    This will show a one-time dismissable notification to these users.
    In the future (weeks, months) if we can show them another notification like this
    one as a reminder, we can run the same code again.
    
    Related https://github.com/readthedocs/ext-theme/pull/183

--- readthedocs/core/notifications.py
+++ readthedocs/core/notifications.py
@@ -4,10 +4,11 @@ import textwrap
 
 from django.utils.translation import gettext_lazy as _
 
-from readthedocs.notifications.constants import WARNING
+from readthedocs.notifications.constants import INFO, WARNING
 from readthedocs.notifications.messages import Message, registry
 
 MESSAGE_EMAIL_VALIDATION_PENDING = "core:email:validation-pending"
+MESSAGE_BETA_DASHBOARD_AVAILABLE = "core:dashboard:beta-available"
 messages = [
     Message(
         id=MESSAGE_EMAIL_VALIDATION_PENDING,
@@ -21,6 +22,19 @@ messages = [
             ).strip(),
         ),
         type=WARNING,
+    ),
+    Message(
+        id=MESSAGE_BETA_DASHBOARD_AVAILABLE,
+        header=_("New beta dashboard available"),
+        body=_(
+            textwrap.dedent(
+                """
+                We are launching our new <strong>beta dashboard</strong> with a fresh redesign and new powerful features.
+                <a href="https://beta.readthedocs.org/">Give it a try</a> and send us feedback.
+            """
+            ).strip(),
+        ),
+        type=INFO,
     ),
 ]
 
