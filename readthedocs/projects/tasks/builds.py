readthedocs/readthedocs.org

commit e778db59ce79e642cb26aafe483d0d16e2eabb0f

New notification system: implementation (#10922)
* Add some comments to understand the scope of the changes

* Add more comments about the plan

* Delete old code

We are not sending any kind of notification from the Django admin.
We used to use this in the past but we are not using it anymore.

I'm deleting it now to avoid re-implementing it using the new notification
system.

* Add more comments to organize the work of new notification system

* Notification: initial modelling

* Define `max_length=128` that's required for SQLite (tests)

* Initial implementation for build notifications API endpoint

* API endpoint to create build notifications

* Use APIv2 for internal endpoint to call from builders

* Use APIv2 to create notifications from the builder

* Check project slug when attaching to a Build/Project instance

* Add extra TODO comments

* Initial migration from Exceptions to Messages

* Disable `Notification.format_values` for now

I'll come back to this later.

* Define all the notifications

* Messages for symlink exceptions

* Simplify how `NOTIFICATION_MESSAGES` is built

* Add `Notification.format_values` field

This allows to store values required for this notification at render time

* Notification with `format_values` passed when exception is raised

* Use `get_display_icon` to show the proper icon in the serializer

* Proper relation name

* Initial work to replace site notifications

* We are not going to create Notifications via APIv3 for now

* Send `format_values` to exception

* Small changes and comments added

* Initial work to migrate an old SiteNotification to new system

* Create a notification `registry` to register messages by application

* Use a custom `models.Manager` to implement de-duplication

Use `Notification.objects.add()` to create a notification with de-duplication
capabilities. If the notification already exists, it updates all the fields with

readthedocs/projects/tasks/builds.py
@@ -37,8 +37,8 @@ from readthedocs.builds.constants import (
 from readthedocs.builds.models import APIVersion, Build
 from readthedocs.builds.signals import build_complete
 from readthedocs.builds.utils import memcache_lock
-from readthedocs.config import ConfigError
 from readthedocs.config.config import BuildConfigV2
+from readthedocs.config.exceptions import ConfigError
 from readthedocs.doc_builder.director import BuildDirector
 from readthedocs.doc_builder.environments import (
     DockerBuildEnvironment,
@@ -49,10 +49,7 @@ from readthedocs.doc_builder.exceptions import (
     BuildCancelled,
     BuildMaxConcurrencyError,
     BuildUserError,
-    BuildUserSkip,
     MkDocsYAMLParseError,
-    ProjectBuildsSkippedError,
-    YAMLParseError,
 )
 from readthedocs.projects.models import Feature
 from readthedocs.storage import build_media_storage
@@ -292,12 +289,9 @@ class UpdateDocsTask(SyncRepositoryMixin, Task):
     # All exceptions generated by a user miss-configuration should be listed
     # here. Actually, every subclass of ``BuildUserError``.
     throws = (
-        ProjectBuildsSkippedError,
         ConfigError,
-        YAMLParseError,
         BuildCancelled,
         BuildUserError,
-        BuildUserSkip,
         RepositoryError,
         MkDocsYAMLParseError,
         ProjectConfigurationError,
@@ -305,16 +299,14 @@ class UpdateDocsTask(SyncRepositoryMixin, Task):
 
     # Do not send notifications on failure builds for these exceptions.
     exceptions_without_notifications = (
-        BuildCancelled,
-        BuildMaxConcurrencyError,
-        BuildUserSkip,
-        ProjectBuildsSkippedError,
+        BuildCancelled.CANCELLED_BY_USER,
+        BuildUserError.MAX_CONCURRENCY,
+        BuildUserError.SKIPPED_EXIT_CODE_183,
+        BuildAppError.BUILDS_DISABLED,
     )
 
     # Do not send external build status on failure builds for these exceptions.
-    exceptions_without_external_build_status = (
-        BuildMaxConcurrencyError,
-    )
+    exceptions_without_external_build_status = (BuildUserError.MAX_CONCURRENCY,)
 
     acks_late = True
     track_started = True
@@ -339,7 +331,7 @@ class UpdateDocsTask(SyncRepositoryMixin, Task):
                 log.warning('Ignoring cancelling the build at "Uploading" state.')
                 return
 
-            raise BuildCancelled
+            raise BuildCancelled(message_id=BuildCancelled.CANCELLED_BY_USER)
 
         # Do not send the SIGTERM signal to children (pip is automatically killed when
         # receives SIGTERM and make the build to fail one command and stop build)
@@ -373,16 +365,17 @@ class UpdateDocsTask(SyncRepositoryMixin, Task):
             log.info("Concurrency limit reached, retrying task.")
             self.retry(
                 exc=BuildMaxConcurrencyError(
-                    BuildMaxConcurrencyError.message.format(
-                        limit=max_concurrent_builds,
-                    )
+                    BuildMaxConcurrencyError.LIMIT_REACHED,
+                    format_values={
+                        "limit": max_concurrent_builds,
+                    },
                 )
             )
 
     def _check_project_disabled(self):
         if self.data.project.skip:
             log.warning('Project build skipped.')
-            raise ProjectBuildsSkippedError
+            raise BuildAppError(BuildAppError.BUILDS_DISABLED)
 
     def before_start(self, task_id, args, kwargs):
         # Create the object to store all the task-related data
@@ -416,6 +409,10 @@ class UpdateDocsTask(SyncRepositoryMixin, Task):
         # Also note there are builds that are triggered without a commit
         # because they just build the latest commit for that version
         self.data.build_commit = kwargs.get('build_commit')
+
+        self.data.build_director = BuildDirector(
+            data=self.data,
+        )
 
         log.bind(
             # NOTE: ``self.data.build`` is just a regular dict, not an APIBuild :'(
@@ -475,46 +472,37 @@ class UpdateDocsTask(SyncRepositoryMixin, Task):
                 'id': self.data.build_pk,
             }
 
-        # TODO: handle this `ConfigError` as a `BuildUserError` in the
-        # following block
-        if isinstance(exc, ConfigError):
-            self.data.build['error'] = str(
-                YAMLParseError(
-                    YAMLParseError.GENERIC_WITH_PARSE_EXCEPTION.format(
-                        exception=str(exc),
-                    ),
-                ),
-            )
         # Known errors in our application code (e.g. we couldn't connect to
         # Docker API). Report a generic message to the user.
-        elif isinstance(exc, BuildAppError):
-            self.data.build['error'] = BuildAppError.GENERIC_WITH_BUILD_ID.format(
-                build_id=self.data.build['id'],
-            )
+        if isinstance(exc, BuildAppError):
+            message_id = BuildAppError.GENERIC_WITH_BUILD_ID
+
         # Known errors in the user's project (e.g. invalid config file, invalid
         # repository, command failed, etc). Report the error back to the user
-        # using the `message` and `state` attributes from the exception itself.
-        # Otherwise, use a generic message and default state.
+        # by creating a notification attached to the build
+        # Otherwise, use a notification with a generic message.
         elif isinstance(exc, BuildUserError):
-            if hasattr(exc, 'message') and exc.message is not None:
-                self.data.build['error'] = exc.message
+            if hasattr(exc, "message_id") and exc.message_id is not None:
+                message_id = exc.message_id
             else:
-                self.data.build['error'] = BuildUserError.GENERIC
+                message_id = BuildUserError.GENERIC
 
-            if hasattr(exc, "state"):
-                self.data.build["state"] = exc.state
         else:
             # We don't know what happened in the build. Log the exception and
-            # report a generic message to the user.
+            # report a generic notification to the user.
             # Note we are using `log.error(exc_info=...)` instead of `log.exception`
             # because this is not executed inside a try/except block.
             log.error("Build failed with unhandled exception.", exc_info=exc)
-            self.data.build["error"] = BuildAppError.GENERIC_WITH_BUILD_ID.format(
-                build_id=self.data.build["id"],
-            )
+            message_id = BuildAppError.GENERIC_WITH_BUILD_ID
+
+        # Grab the format values from the exception in case it contains
+        format_values = exc.format_values if hasattr(exc, "format_values") else None
+
+        # Attach the notification to the build
+        self.data.build_director.attach_notification(message_id, format_values)
 
         # Send notifications for unhandled errors
-        if not isinstance(exc, self.exceptions_without_notifications):
+        if message_id not in self.exceptions_without_notifications:
             self.send_notifications(
                 self.data.version_pk,
                 self.data.build['id'],
@@ -527,15 +515,16 @@ class UpdateDocsTask(SyncRepositoryMixin, Task):
         # Oh, I think this is to differentiate a task triggered with
         # `Build.commit` than a one triggered just with the `Version` to build
         # the _latest_ commit of it
-        if self.data.build_commit and not isinstance(
-            exc, self.exceptions_without_external_build_status
+        if (
+            self.data.build_commit
+            and message_id not in self.exceptions_without_external_build_status
         ):
             version_type = None
             if self.data.version:
                 version_type = self.data.version.type
 
             status = BUILD_STATUS_FAILURE
-            if isinstance(exc, BuildUserSkip):
+            if message_id == BuildUserError.SKIPPED_EXIT_CODE_183:
                 # The build was skipped by returning the magic exit code,
                 # marked as CANCELLED, but communicated to GitHub as successful.
                 # This is because the PR has to be available for merging when the build
@@ -594,9 +583,10 @@ class UpdateDocsTask(SyncRepositoryMixin, Task):
                     output_format=artifact_type,
                 )
                 raise BuildUserError(
-                    BuildUserError.BUILD_OUTPUT_IS_NOT_A_DIRECTORY.format(
-                        artifact_type=artifact_type
-                    )
+                    BuildUserError.BUILD_OUTPUT_IS_NOT_A_DIRECTORY,
+                    format_values={
+                        "artifact_type": artifact_type,
+                    },
                 )
 
             # Check if there are multiple files on artifact directories.
@@ -694,7 +684,9 @@ class UpdateDocsTask(SyncRepositoryMixin, Task):
                 project_slug=self.data.project.slug,
                 version_slug=self.data.version.slug,
             )
-            self.data.build['error'] = exc.message
+            self.data.director.attach_notification(
+                BuildMaxConcurrencyError.LIMIT_REACHED
+            )
             self.update_build(state=BUILD_STATE_TRIGGERED)
 
     def after_return(self, status, retval, task_id, args, kwargs, einfo):
@@ -762,10 +754,6 @@ class UpdateDocsTask(SyncRepositoryMixin, Task):
             log.exception("Error while updating the build object.", state=state)
 
     def execute(self):
-        self.data.build_director = BuildDirector(
-            data=self.data,
-        )
-
         # Clonning
         self.update_build(state=BUILD_STATE_CLONING)
 

commit b62afbc6c8ffdaa9dc0c589d2e282532b40be7c9

Build: set `CANCELLED` state when the build is cancelled
This bug was introduced in
https://github.com/readthedocs/readthedocs.org/pull/10922/files#diff-4eae6664ca2124d124dbfe04a8000483c7a23d426c3f390721d6e80fe5891064L504
by mistake.

Closes #11170

readthedocs/projects/tasks/builds.py
@@ -24,6 +24,7 @@ from readthedocs.builds.constants import (
     ARTIFACT_TYPES_WITHOUT_MULTIPLE_FILES_SUPPORT,
     BUILD_FINAL_STATES,
     BUILD_STATE_BUILDING,
+    BUILD_STATE_CANCELLED,
     BUILD_STATE_CLONING,
     BUILD_STATE_FINISHED,
     BUILD_STATE_INSTALLING,
@@ -487,6 +488,10 @@ class UpdateDocsTask(SyncRepositoryMixin, Task):
                 message_id = exc.message_id
             else:
                 message_id = BuildUserError.GENERIC
+
+            # Set build state as cancelled if the user cancelled the build
+            if isinstance(exc, BuildCancelled):
+                self.data.build["state"] = BUILD_STATE_CANCELLED
 
         else:
             # We don't know what happened in the build. Log the exception and