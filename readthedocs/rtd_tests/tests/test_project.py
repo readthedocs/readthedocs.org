80152e16af0c5c2feaed0f14d4af1c826abd0e9c
Santos Gallegos <stsewd@proton.me>
2024-03-04 12:28:54 -0500

    Fix call to reverse

--- readthedocs/rtd_tests/tests/test_project.py
+++ readthedocs/rtd_tests/tests/test_project.py
@@ -263,7 +263,7 @@ class TestProjectTranslations(ProjectMixin, TestCase):
         # Create translation of ``main_project``.
         get(Project, main_language_project=main_project)
 
-        url = reverse("project-translations", [main_project.id])
+        url = reverse("project-translations", args=[main_project.id])
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
