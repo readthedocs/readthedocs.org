a72ca029f11b175591459dbfcb093c04d5d469e3
Manuel Kaufmann <humitos@gmail.com>
2024-03-11 18:07:28 +0100

    Listen to Version instead

--- readthedocs/projects/signals.py
+++ readthedocs/projects/signals.py
@@ -3,11 +3,16 @@
 import datetime
 
 import django.dispatch
+import structlog
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+from readthedocs.builds.models import Version
 from readthedocs.projects.constants import MKDOCS
-from readthedocs.projects.models import AddonsConfig, Project
+from readthedocs.projects.models import AddonsConfig
+
+log = structlog.get_logger(__name__)
+
 
 before_vcs = django.dispatch.Signal()
 
@@ -20,7 +25,7 @@ project_import = django.dispatch.Signal()
 files_changed = django.dispatch.Signal()
 
 
-@receiver(post_save, sender=Project)
+@receiver(post_save, sender=Version)
 def enable_addons_on_new_mkdocs_projects(instance, *args, **kwargs):
     """
     Enable Addons on projects created after 2024-04-01.
@@ -34,12 +39,18 @@ def enable_addons_on_new_mkdocs_projects(instance, *args, **kwargs):
 
     Reference https://github.com/readthedocs/addons/issues/72#issuecomment-1926647293
     """
+    project = instance.project
 
     if (
-        instance.pub_date > datetime.date(2024, 4, 1)
+        project.pub_date
+        > datetime.datetime(2023, 4, 1, 0, 0, 0, tzinfo=datetime.timezone.utc)
         and instance.documentation_type == MKDOCS
     ):
-        created, config = AddonsConfig.objects.get_or_create(project=instance)
+        created, config = AddonsConfig.objects.get_or_create(project=project)
         if created:
+            log.info(
+                "Creating AddonsConfig automatically for MkDocs project.",
+                project_slug=project.slug,
+            )
             config.enabled = True
             config.save()
