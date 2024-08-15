readthedocs/readthedocs.org

commit 05c8a32518963959c0839b9025a5067f19e542c8

Ask for confirmation when adding a user to a project/organization/team (#9440)
Closes https://github.com/readthedocs/readthedocs-corporate/issues/454

readthedocs/invitations/backends.py
@@ -1,0 +1,157 @@
+"""Backends for the objects from the invitations."""
+import structlog
+from django.conf import settings
+from django.urls import reverse
+from django.utils import formats, timesince, translation
+
+from readthedocs.core.permissions import AdminPermission
+from readthedocs.core.utils import send_email
+from readthedocs.organizations.models import Organization, Team
+from readthedocs.projects.models import Project
+
+log = structlog.get_logger(__name__)
+
+
+class Backend:
+
+    """Base backend to define the behavior of the object attached the the invitation."""
+
+    klass = None
+
+    def __init__(self, invitation):
+        self.invitation = invitation
+        self.object = invitation.object
+
+    def get_origin_url(self):
+        raise NotImplementedError
+
+    def get_success_url(self):
+        return self.get_object_url()
+
+    def get_object_url(self):
+        raise NotImplementedError
+
+    def get_object_name(self):
+        return self.object.slug
+
+    def owns_object(self, user):
+        return user in AdminPermission.owners(self.object)
+
+    def redeem(self, user):
+        raise NotImplementedError
+
+    def _get_email_object_description(self):
+        # We don't want to use the current language when
+        # sending the email to the target user.
+        with translation.override(settings.LANGUAGE_CODE):
+            target_type = self.object._meta.verbose_name
+            return f"{self.get_object_name()} {target_type}"
+
+    def send_invitation(self):
+        """Send an email with the invitation to join the object."""
+        email = self.invitation.to_email
+        if not email:
+            email = self.invitation.to_user.email
+
+        from_user = self.invitation.from_user
+        from_name = from_user.get_full_name() or from_user.username
+        object_description = self._get_email_object_description()
+        expiration_date = self.invitation.expiration_date
+        log.info(
+            "Emailing invitation",
+            email=email,
+            invitation_pk=self.invitation.pk,
+            object_type=self.invitation.object_type,
+            object_name=self.invitation.object_name,
+            object_pk=self.invitation.object.pk,
+        )
+        send_email(
+            recipient=email,
+            subject=f"{from_name} has invite you to join the {object_description}",
+            template="invitations/email/invitation.txt",
+            template_html="invitations/email/invitation.html",
+            context={
+                "from_name": from_name,
+                "object_description": object_description,
+                "invite_url": self.invitation.get_absolute_url(),
+                "valid_until": timesince.timeuntil(expiration_date),
+                "expiration_date": formats.date_format(expiration_date),
+            },
+        )
+
+
+class ProjectBackend(Backend):
+
+    klass = Project
+
+    def get_origin_url(self):
+        return reverse("projects_users", args=[self.object.slug])
+
+    def get_object_url(self):
+        return reverse("projects_detail", args=[self.object.slug])
+
+    def redeem(self, user):
+        self.object.users.add(user)
+        return True
+
+
+class OrganizationBackend(Backend):
+
+    klass = Organization
+
+    def get_origin_url(self):
+        return reverse("organization_owners", args=[self.object.slug])
+
+    def get_object_url(self):
+        return reverse("organization_detail", args=[self.object.slug])
+
+    def redeem(self, user):
+        self.object.owners.add(user)
+        return True
+
+
+class TeamBackend(Backend):
+
+    """Team backend."""
+
+    klass = Team
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.organization = self.object.organization
+
+    def get_origin_url(self):
+        return reverse(
+            "organization_team_detail", args=[self.organization.slug, self.object.slug]
+        )
+
+    def get_object_url(self):
+        return reverse(
+            "organization_team_detail", args=[self.organization.slug, self.object.slug]
+        )
+
+    def get_success_url(self):
+        return reverse("organization_detail", args=[self.organization.slug])
+
+    def redeem(self, user):
+        self.organization.add_member(user, self.object)
+        return True
+
+    def owns_object(self, user):
+        return user in AdminPermission.owners(self.organization)
+
+    def get_object_name(self):
+        return f"{self.organization.slug} {self.object.slug}"
+
+
+def get_backend(invitation):
+    """Get the proper backend for the invitation."""
+    backends = [
+        OrganizationBackend,
+        ProjectBackend,
+        TeamBackend,
+    ]
+    for backend in backends:
+        if isinstance(invitation.object, backend.klass):
+            return backend(invitation)
+    raise ValueError(f"Backend not found for object of class {object.__class__}")

readthedocs/core/adapters.py
@@ -1,15 +1,14 @@
 """Allauth overrides."""
 
 import json
-import structlog
 
+import structlog
 from allauth.account.adapter import DefaultAccountAdapter
-from django.conf import settings
 from django.template.loader import render_to_string
 from django.utils.encoding import force_str
 
 from readthedocs.core.utils import send_email
-from readthedocs.organizations.models import TeamMember
+from readthedocs.invitations.models import Invitation
 
 log = structlog.get_logger(__name__)
 
@@ -53,19 +52,15 @@ class AccountAdapter(DefaultAccountAdapter):
         )
 
     def save_user(self, request, user, form, commit=True):
-        """Override default account signup to link user to correct team."""
+        """Override default account signup to redeem invitations at sign-up."""
         user = super().save_user(request, user, form)
-        if not settings.RTD_ALLOW_ORGANIZATIONS:
-            return
 
-        invite_id = request.session.get('invite')
-        if invite_id:
-            try:
-                teammember = TeamMember.objects.get(invite__pk=invite_id)
-                teammember.member = user
-                teammember.save()
-                teammember.invite.delete()
-            except TeamMember.DoesNotExist:
-                log.error(
-                    "Didn't find member related to invite, not adding to team",
-                )
+        invitation_pk = request.session.get("invitation:pk")
+        if invitation_pk:
+            invitation = Invitation.objects.pending().filter(pk=invitation_pk).first()
+            if invitation:
+                log.info("Redeeming invitation at sign-up", invitation_pk=invitation_pk)
+                invitation.redeem(user)
+                invitation.delete()
+            else:
+                log.info("Invitation not found", invitation_pk=invitation_pk)

readthedocs/core/tests/test_signals.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django_dynamic_fixture import get
 
 from readthedocs.builds.models import Version
-from readthedocs.organizations.models import Organization, Team, TeamInvite, TeamMember
+from readthedocs.organizations.models import Organization, Team, TeamMember
 from readthedocs.projects.models import Project
 
 
@@ -60,14 +60,10 @@ class TestProjectOrganizationSignal:
             projects=[project_three],
         )
 
-        get(TeamInvite, team=team_one, organization=org_one)
-
-
         assert Organization.objects.all().count() == 3
         assert Project.objects.all().count() == 3
         assert Version.objects.all().count() == 3
         assert Team.objects.all().count() == 2
-        assert TeamInvite.objects.all().count() == 1
         assert TeamMember.objects.all().count() == 3
         assert User.objects.all().count() == 3
 
@@ -77,6 +73,5 @@ class TestProjectOrganizationSignal:
         assert {project_two, project_three} == set(Project.objects.all())
         assert Version.objects.all().count() == 2
         assert {team_two} == set(Team.objects.all())
-        assert TeamInvite.objects.all().count() == 0
         assert TeamMember.objects.all().count() == 1
         assert User.objects.all().count() == 2

readthedocs/invitations/admin.py
@@ -1,0 +1,28 @@
+"""Invitations admin."""
+
+from django.contrib import admin
+
+from readthedocs.invitations.models import Invitation
+
+
+@admin.register(Invitation)
+class InvitationAdmin(admin.ModelAdmin):
+
+    search_fields = (
+        "from_user__username",
+        "to_user__username",
+        "to_user__email",
+        "to_email",
+    )
+    list_display = (
+        "pk",
+        "from_user",
+        "to_user",
+        "to_email",
+        "object_type",
+        "object",
+    )
+    raw_id_fields = (
+        "from_user",
+        "to_user",
+    )

readthedocs/invitations/apps.py
@@ -1,0 +1,12 @@
+"""
+Invitations app.
+
+This manages user invitations to join a project, organization, team, etc.
+"""
+
+from django.apps import AppConfig
+
+
+class InvitationsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "readthedocs.invitations"

readthedocs/invitations/migrations/0001_initial.py
@@ -1,0 +1,98 @@
+# Generated by Django 3.2.13 on 2022-07-19 00:43
+
+import django.db.models.deletion
+import django_extensions.db.fields
+from django.conf import settings
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+        ("contenttypes", "0002_remove_content_type_name"),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name="Invitation",
+            fields=[
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                (
+                    "created",
+                    django_extensions.db.fields.CreationDateTimeField(
+                        auto_now_add=True, verbose_name="created"
+                    ),
+                ),
+                (
+                    "modified",
+                    django_extensions.db.fields.ModificationDateTimeField(
+                        auto_now=True, verbose_name="modified"
+                    ),
+                ),
+                ("object_id", models.PositiveIntegerField()),
+                (
+                    "to_email",
+                    models.EmailField(
+                        max_length=254, null=True, blank=True, verbose_name="E-mail"
+                    ),
+                ),
+                ("token", models.CharField(max_length=32, unique=True)),
+                (
+                    "content_type",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to="contenttypes.contenttype",
+                    ),
+                ),
+                (
+                    "from_user",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="invitations_sent",
+                        to=settings.AUTH_USER_MODEL,
+                        verbose_name="From user",
+                    ),
+                ),
+                (
+                    "to_user",
+                    models.ForeignKey(
+                        null=True,
+                        blank=True,
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="invitations_received",
+                        to=settings.AUTH_USER_MODEL,
+                        verbose_name="To user",
+                    ),
+                ),
+                (
+                    "expiration_date",
+                    models.DateTimeField(verbose_name="Expiration date"),
+                ),
+            ],
+        ),
+        migrations.AddIndex(
+            model_name="invitation",
+            index=models.Index(
+                fields=["content_type", "object_id"],
+                name="invitations_content_5a26b9_idx",
+            ),
+        ),
+        migrations.AlterUniqueTogether(
+            name="invitation",
+            unique_together={
+                ("to_email", "content_type", "object_id"),
+                ("to_user", "content_type", "object_id"),
+            },
+        ),
+    ]

readthedocs/invitations/models.py
@@ -1,0 +1,207 @@
+"""Invitation models."""
+
+import structlog
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
+from django.urls import reverse
+from django.utils import timezone
+from django.utils.crypto import get_random_string
+from django.utils.functional import cached_property
+from django.utils.translation import gettext_lazy as _
+from django_extensions.db.models import TimeStampedModel
+
+from readthedocs.invitations.backends import get_backend
+
+log = structlog.get_logger(__name__)
+
+
+class InvitationQueryset(models.QuerySet):
+
+    """Invitation queryset."""
+
+    def expired(self, obj=None):
+        queryset = self.filter(expiration_date__lte=timezone.now())
+        if obj:
+            queryset = self._for_object(obj=obj, queryset=queryset)
+        return queryset
+
+    def pending(self, obj=None):
+        queryset = self.filter(expiration_date__gt=timezone.now())
+        if obj:
+            queryset = self._for_object(obj=obj, queryset=queryset)
+        return queryset
+
+    def invite(self, from_user, obj, to_user=None, to_email=None):
+        """
+        Create and send an invitation for `to_user` or `to_email` to join `object`.
+
+        If the invitation already exists, we don't send the invitation again.
+        """
+        if not to_user and not to_email:
+            raise ValueError("A user or email must be provided")
+        fields = dict(
+            content_type=ContentType.objects.get_for_model(obj),
+            object_id=obj.pk,
+        )
+        if to_user:
+            fields["to_user"] = to_user
+        else:
+            fields["to_email"] = to_email
+
+        invitation, created = self.get_or_create(
+            **fields,
+            defaults={
+                "from_user": from_user,
+            },
+        )
+        if created:
+            invitation.send()
+        return invitation, created
+
+    def for_object(self, obj):
+        return self._for_object(obj=obj, queryset=self.all())
+
+    @staticmethod
+    def _for_object(obj, queryset):
+        return queryset.filter(
+            object_id=obj.pk,
+            content_type=ContentType.objects.get_for_model(obj),
+        )
+
+
+class Invitation(TimeStampedModel):
+
+    """
+    Invitation model.
+
+    An invitation can be attached to an existing user or to an email.
+    """
+
+    # Generic foreign key.
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    object = GenericForeignKey("content_type", "object_id")
+
+    # Normal fields.
+    from_user = models.ForeignKey(
+        User,
+        verbose_name=_("From user"),
+        on_delete=models.CASCADE,
+        related_name="invitations_sent",
+    )
+    to_user = models.ForeignKey(
+        User,
+        verbose_name=_("To user"),
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+        related_name="invitations_received",
+    )
+    to_email = models.EmailField(_("E-mail"), null=True, blank=True)
+    token = models.CharField(
+        unique=True,
+        max_length=32,
+    )
+    expiration_date = models.DateTimeField(_("Expiration date"))
+
+    objects = InvitationQueryset.as_manager()
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["content_type", "object_id"]),
+        ]
+        unique_together = [
+            ("to_user", "content_type", "object_id"),
+            ("to_email", "content_type", "object_id"),
+        ]
+
+    @property
+    def username(self):
+        if self.to_user:
+            return self.to_user.username
+        return self.to_email
+
+    @property
+    def expired(self):
+        return timezone.now() > self.expiration_date
+
+    @cached_property
+    def backend(self):
+        return get_backend(self)
+
+    def save(self, *args, **kwargs):
+        if not self.token:
+            self.token = self.generate_token()
+
+        if not self.expiration_date:
+            self.expiration_date = timezone.now() + timezone.timedelta(
+                days=settings.RTD_INVITATIONS_EXPIRATION_DAYS
+            )
+
+        super().save(*args, **kwargs)
+
+    @staticmethod
+    def generate_token():
+        return get_random_string(32)
+
+    def redeem(self, user=None):
+        """
+        Redeem invitation.
+
+        `user` will be used only if the invitation is attached
+        to an email, otherwise `to_user` user will be used.
+        """
+        if self.expired:
+            return False
+        if self.to_user:
+            user = self.to_user
+        log.info(
+            "Redeeming invitation",
+            invitation_pk=self.pk,
+            for_user=user.username,
+            object_type=self.object_type,
+            object_name=self.object_name,
+            object_pk=self.object.pk,
+        )
+        return self.backend.redeem(user=user)
+
+    def get_success_url(self):
+        """URL to redirect after the invitation has been redeemed."""
+        return self.backend.get_success_url()
+
+    def get_origin_url(self):
+        """URL from where the invitations for the object are created."""
+        return self.backend.get_origin_url()
+
+    def get_absolute_url(self):
+        return reverse("invitations_redeem", args=[self.token])
+
+    def can_revoke_invitation(self, user):
+        """
+        Check whether the user can revoke the invitation.
+
+        A user can revoke an invitation if it's the owner
+        of the object attached to it.
+        """
+        return self.backend.owns_object(user)
+
+    @property
+    def object_type(self):
+        return self.content_type.name
+
+    @property
+    def object_name(self):
+        return self.backend.get_object_name()
+
+    @property
+    def object_url(self):
+        return self.backend.get_object_url()
+
+    def send(self):
+        self.backend.send_invitation()
+
+    def __str__(self):
+        return f"Invitation for {self.username} to join {self.object}"

readthedocs/invitations/templates/invitations/email/invitation.html
@@ -1,0 +1,17 @@
+{% extends 'core/email/common.html' %}
+{% block content %}
+<p>
+  {{ from_name }} has invited you to join the {{ object_description }}.
+  You can accept or decline this invitation using the following link:
+</p>
+
+<p>
+  <a href="{{ uri }}{{ invite_url }}">
+    {{ uri }}{{ invite_url }}
+  </a>
+</p>
+
+<p>
+  This invitation will expire in <span title="{{ expiration_date }}">{{ valid_until }}</span>.
+</p>
+{% endblock %}

commit 23a63dcc1f6c29ebc7776d8adab3a184b5cde5c0

Fix a typo in "invited" in email notification subjects
The mistake has been introduced in #9440.

readthedocs/invitations/backends.py
@@ -67,7 +67,7 @@ class Backend:
         )
         send_email(
             recipient=email,
-            subject=f"{from_name} has invite you to join the {object_description}",
+            subject=f"{from_name} has invited you to join the {object_description}",
             template="invitations/email/invitation.txt",
             template_html="invitations/email/invitation.html",
             context={