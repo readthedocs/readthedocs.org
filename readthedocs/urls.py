0945bc2c3a69cf83941db13ba61cf85d2ac78091
Santos Gallegos <stsewd@proton.me>
2024-10-02 09:56:03 -0500

    Allauth: add 429 handler
    Allauth expects a handler429 view in urls.py,
    or a 429.html template in the root of the templates directory.
    We don't want to create symlinks, so a custom handler429 view is added.
    
    Ref https://read-the-docs.sentry.io/issues/5835964680/?project=161479&query=is%3Aunresolved&referrer=issue-stream&statsPeriod=14d&stream_index=19

--- readthedocs/urls.py
+++ readthedocs/urls.py
@@ -17,6 +17,9 @@ handler400 = ErrorView.as_view(status_code=400)
 handler403 = ErrorView.as_view(status_code=403)
 handler404 = ErrorView.as_view(status_code=404)
 handler500 = ErrorView.as_view(status_code=500)
+# Rate limit handler for allauth views,
+# this isn't a Django built-in handler.
+handler429 = ErrorView.as_view(status_code=429)
 
 basic_urls = [
     path("", HomepageView.as_view(), name="homepage"),
