bed03bd41bfe5180ecc033e72af811e83d398ec5
Manuel Kaufmann <humitos@gmail.com>
2024-10-30 09:45:11 +0100

    Lint

--- readthedocs/projects/migrations/0128_addons_notifications.py
+++ readthedocs/projects/migrations/0128_addons_notifications.py
@@ -8,15 +8,22 @@ def forward_add_fields(apps, schema_editor):
     AddonsConfig = apps.get_model("projects", "AddonsConfig")
     for addons in AddonsConfig.objects.filter(project__isnull=False):
         addons.notifications_show_on_latest = addons.stable_latest_version_warning_enabled
-        addons.notifications_show_on_non_stable = addons.stable_latest_version_warning_enabled
-        addons.notifications_show_on_external = addons.external_version_warning_enabled
+        addons.notifications_show_on_non_stable = (
+            addons.stable_latest_version_warning_enabled
+        )
+        addons.notifications_show_on_external = (
+            addons.external_version_warning_enabled
+        )
         addons.save()
 
 
 def reverse_remove_fields(apps, schema_editor):
     AddonsConfig = apps.get_model("projects", "AddonsConfig")
     for addons in AddonsConfig.objects.filter(project__isnull=False):
-        addons.stable_latest_version_warning_enabled = addons.notifications_show_on_latest or addons.notifications_show_on_non_stable
+        addons.stable_latest_version_warning_enabled = (
+            addons.notifications_show_on_latest or
+            addons.notifications_show_on_non_stable
+        )
         addons.external_version_warning_enabled = addons.notifications_show_on_external
         addons.save()
 
