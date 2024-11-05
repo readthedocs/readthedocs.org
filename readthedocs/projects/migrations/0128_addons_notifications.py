9210e812ce15ec176e2fab042361725e0e9eee70
Manuel Kaufmann <humitos@gmail.com>
2024-11-05 17:42:27 +0100

    Remove data migration from 0128

--- readthedocs/projects/migrations/0128_addons_notifications.py
+++ readthedocs/projects/migrations/0128_addons_notifications.py
@@ -4,30 +4,6 @@ from django.db import migrations, models
 from django_safemigrate import Safe
 
 
-def forward_add_fields(apps, schema_editor):
-    AddonsConfig = apps.get_model("projects", "AddonsConfig")
-    for addons in AddonsConfig.objects.filter(project__isnull=False):
-        addons.notifications_show_on_latest = (
-            addons.stable_latest_version_warning_enabled
-        )
-        addons.notifications_show_on_non_stable = (
-            addons.stable_latest_version_warning_enabled
-        )
-        addons.notifications_show_on_external = addons.external_version_warning_enabled
-        addons.save()
-
-
-def reverse_remove_fields(apps, schema_editor):
-    AddonsConfig = apps.get_model("projects", "AddonsConfig")
-    for addons in AddonsConfig.objects.filter(project__isnull=False):
-        addons.stable_latest_version_warning_enabled = (
-            addons.notifications_show_on_latest
-            or addons.notifications_show_on_non_stable
-        )
-        addons.external_version_warning_enabled = addons.notifications_show_on_external
-        addons.save()
-
-
 class Migration(migrations.Migration):
     safe = Safe.before_deploy
 
@@ -76,5 +52,4 @@ class Migration(migrations.Migration):
             name="notifications_show_on_non_stable",
             field=models.BooleanField(default=True),
         ),
-        migrations.RunPython(forward_add_fields, reverse_remove_fields),
     ]
