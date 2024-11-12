7bceb65d232c20a77338d10e9b27fdfe2a16f7b6
Anthony <aj@ohess.org>
2024-11-12 04:59:22 -0800

    Fix site-wide new dashboard notification (#11543)
    Notification on new dashboard:
    
    
    ![image](https://github.com/user-attachments/assets/1cc4c608-94c3-4a2f-bab1-6e4622fe6c72)
    
    Notification on old dashboard:
    
    
    ![image](https://github.com/user-attachments/assets/3dbe24a1-9597-418c-82b7-c7962f2c969b)
    
    ----
    
    - Fixes #11209
    - Requires https://github.com/readthedocs/ext-theme/pull/524
    - Refs https://github.com/readthedocs/website/pull/335

--- readthedocs/builds/views.py
+++ readthedocs/builds/views.py
@@ -192,27 +192,9 @@ class BuildDetail(BuildBase, ProjectSpamMixin, DetailView):
 
         build = self.get_object()
 
-        # Temporary notification to point to the same page on the new dashboard
-        #
-        # To support readthedocs.com, we have to point to the login view. We
-        # can't point directly to the build view on the new dashboard as this
-        # will give the users a 404 because they aren't logged in.
-        #
-        # On community, we _don't want this_ as this requires the user to have
-        # a login to view the new dashboard.
-        url_domain = settings.PRODUCTION_DOMAIN
-        if url_domain.startswith("app."):
-            url_domain = url_domain[4:]
-        else:
-            url_domain = f"app.{url_domain}"
-        url_build = build.get_absolute_url()
-        # Point to the login view with the build as ?next. We are expecting
-        # users to have accounts to view this.
-        if settings.RTD_ALLOW_ORGANIZATIONS:
-            url_build = reverse("account_login") + f"?next={url_build}"
-        context["url_switch_dashboard"] = f"https://{url_domain}{url_build}"
-
-        context["notifications"] = build.notifications.all()
+        # We consume these notifications through the API in the new dashboard
+        if not settings.RTD_EXT_THEME_ENABLED:
+            context["notifications"] = build.notifications.all()
         if not build.notifications.filter(
             message_id=BuildAppError.GENERIC_WITH_BUILD_ID
         ).exists():
