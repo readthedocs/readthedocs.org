818d239d45178e9bcd36d72ae9880e3561f33860
Santos Gallegos <stsewd@proton.me>
2024-12-09 16:29:13 -0500

    Update clone URL when remote repository is updated

--- readthedocs/oauth/signals.py
+++ readthedocs/oauth/signals.py
@@ -1,8 +1,10 @@
 import structlog
 from allauth.account.signals import user_logged_in
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+from readthedocs.oauth.models import RemoteRepository
 from readthedocs.oauth.tasks import sync_remote_repositories
 
 log = structlog.get_logger(__name__)
@@ -22,3 +24,9 @@ def sync_remote_repositories_on_login(sender, request, user, *args, **kwargs):
         user_username=user.username,
     )
     sync_remote_repositories.delay(user.pk)
+
+
+@receiver(post_save, sender=RemoteRepository)
+def update_project_clone_url(sender, instance, created, *args, **kwargs):
+    """Update the clone URL for all projects linked to this RemoteRepository."""
+    instance.projects.all().update(repo=instance.clone_url)
