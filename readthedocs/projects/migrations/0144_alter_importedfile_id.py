c141e62b99f773552f58782ef28b715fb734999e
Santos Gallegos <stsewd@proton.me>
2025-01-29 10:36:51 -0500

    Mark as safe

--- readthedocs/projects/migrations/0144_alter_importedfile_id.py
+++ readthedocs/projects/migrations/0144_alter_importedfile_id.py
@@ -1,7 +1,10 @@
 from django.db import migrations, models
+from django_safemigrate import Safe
 
 
 class Migration(migrations.Migration):
+
+    safe = Safe.after_deploy
 
     dependencies = [
         ("projects", "0143_addons_flyout_position"),
