5438a645d09391a75af696f3c055920646056e26
Santos Gallegos <stsewd@proton.me>
2025-06-10 14:25:24 -0500

    Mark as safe

--- readthedocs/builds/migrations/0063_alter_buildcommandresult.py
+++ readthedocs/builds/migrations/0063_alter_buildcommandresult.py
@@ -2,9 +2,12 @@
 
 from django.db import migrations
 from django.db import models
+from django_safemigrate import Safe
 
 
 class Migration(migrations.Migration):
+    safe = Safe.before_deploy()
+
     dependencies = [
         ("builds", "0062_rename_indexes"),
     ]
