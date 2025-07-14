b1f66486676d2de6e0f3c874827e86a8c5be94a2
Santos Gallegos <stsewd@proton.me>
2025-07-14 16:22:09 -0500

    Don't link, that would be confusing

--- readthedocs/integrations/models.py
+++ readthedocs/integrations/models.py
@@ -4,7 +4,6 @@ import json
 import re
 import uuid
 
-from django.conf import settings
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.fields import GenericRelation
 from django.contrib.contenttypes.models import ContentType
@@ -383,15 +382,15 @@ class GitHubAppIntegration(Integration):
         Instead of showing a link to the integration details page, for GHA
         projects we show a link in the UI to the GHA installation page for the
         installation used by the project.
-
-        If the project was disconnected from the remote repository,
-        we can't link to the installation page, but we can still link
-        to the GitHub App page, from where the user can install the app
-        into the correct account.
         """
+        # If the GHA is disconnected we'll disonnect the remote repository and
+        # so we won't have a URL to the installation page the project should be
+        # using. We might want to store this on the model later so a repository
+        # that is removed from the installation can still link to the
+        # installation the project was _previously_ using.
         if self.project.is_github_app_project:
             return self.project.remote_repository.github_app_installation.html_url
-        return f"https://github.com/apps/{settings.GITHUB_APP_NAME}/installations/new/"
+        return None
 
     @property
     def is_active(self) -> bool:
