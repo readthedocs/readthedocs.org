fb2579869c91cca3fa498d90c6fe5e6616c542e0
Manuel Kaufmann <humitos@gmail.com>
2025-07-29 11:24:41 +0200

    Analytics: add `status` and regiter analytics from client only (#12346)
    We are moving the pageview for 404 pages to the client side, similar to
    what we
    are doing for 200 pages.
    
    This should reduce the load that bots produce when scanning our docs and
    hitting
    a bunch of 404 pages.
    
    The new approach allows us to enable analytics 200/404 for all the
    projects
    without a feature flag.
    
    Closes #12307

--- readthedocs/projects/models.py
+++ readthedocs/projects/models.py
@@ -1950,7 +1950,6 @@ class Feature(models.Model):
 
     # Feature constants - this is not a exhaustive list of features, features
     # may be added by other packages
-    RECORD_404_PAGE_VIEWS = "record_404_page_views"
     DISABLE_PAGEVIEWS = "disable_pageviews"
     RESOLVE_PROJECT_FROM_HEADER = "resolve_project_from_header"
     USE_PROXIED_APIS_WITH_PREFIX = "use_proxied_apis_with_prefix"
@@ -1975,10 +1974,6 @@ class Feature(models.Model):
     BUILD_NO_ACKS_LATE = "build_no_acks_late"
 
     FEATURES = (
-        (
-            RECORD_404_PAGE_VIEWS,
-            _("Proxito: Record 404s page views."),
-        ),
         (
             DISABLE_PAGEVIEWS,
             _("Proxito: Disable all page views"),
