851b07702f49485e9a1aa2ebfef5ce7524e654a9
Santos Gallegos <stsewd@proton.me>
2025-08-06 13:24:11 -0500

    Dashboard: optimize projects queryset (constant time)

--- readthedocs/rtd_tests/tests/test_project_views.py
+++ readthedocs/rtd_tests/tests/test_project_views.py
@@ -398,7 +398,7 @@ class TestPrivateViews(TestCase):
                     state=BUILD_STATE_FINISHED,
                 )
 
-        with self.assertNumQueries(24):
+        with self.assertNumQueries(14):
             r = self.client.get(reverse(("projects_dashboard")))
         assert r.status_code == 200
 
