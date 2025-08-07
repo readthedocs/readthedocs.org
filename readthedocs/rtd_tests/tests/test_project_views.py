68e516d2ba7ed424d5513cb0b14b30d30ffefed1
Santos Gallegos <stsewd@proton.me>
2025-08-07 14:24:39 -0500

    Fix test

--- readthedocs/rtd_tests/tests/test_project_views.py
+++ readthedocs/rtd_tests/tests/test_project_views.py
@@ -398,7 +398,7 @@ class TestPrivateViews(TestCase):
                     state=BUILD_STATE_FINISHED,
                 )
 
-        with self.assertNumQueries(24):
+        with self.assertNumQueries(23):
             r = self.client.get(reverse(("projects_dashboard")))
         assert r.status_code == 200
 
