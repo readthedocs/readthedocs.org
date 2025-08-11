17003ebd9c6fbfed245847277460dc3abaa2d1a1
Manuel Kaufmann <humitos@gmail.com>
2025-08-11 20:15:05 +0200

    Match icon

--- readthedocs/filetreediff/dataclasses.py
+++ readthedocs/filetreediff/dataclasses.py
@@ -70,7 +70,7 @@ class FileTreeDiffFileStatus(StrEnum):
         if self == FileTreeDiffFileStatus.added:
             return "➕"
         elif self == FileTreeDiffFileStatus.deleted:
-            return "❌"
+            return "➖"
         elif self == FileTreeDiffFileStatus.modified:
             return "📝"
         return ""
@@ -155,7 +155,7 @@ class FileTreeDiff:
         However, it doesn't group the results by directory.
 
         Ideally, this should sort file names by hierarchy (less deep directory
-        first), groupping them by directory and alphabetically. We should update
+        first), grouping them by directory and alphabetically. We should update
         this function to achieve that goal if we find a simple way to do it.
         """
         parts = file.path.split("/")
