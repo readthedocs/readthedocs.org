669727523b75505f615f1850ba7625f006f41794
Santos Gallegos <stsewd@proton.me>
2025-08-12 09:19:06 -0500

    Build: Denormalize latest build for projects (#12392)
    Sorting by the last build project is expensive, as the query is done for
    each project (even if paginated, since the order applies to all
    projects). For comparison, here are some numbers with data from
    production
    
    ```
    # Ordering by name
    145 ms ± 446 μs per loop (mean ± std. dev. of 5 runs, 5 loops each)
    
    # Ordering by max(builds_date)
    4.37 s ± 254 ms per loop (mean ± std. dev. of 5 runs, 5 loops each)
    ```
    
    The migration is going to take some time to run, I imagine... but should
    be worth it for a snappier dashboard.

--- readthedocs/projects/models.py
+++ readthedocs/projects/models.py
@@ -634,6 +634,17 @@ class Project(models.Model):
         null=True,
     )
 
+    # Denormalized fields
+    latest_build = models.OneToOneField(
+        "builds.Build",
+        verbose_name=_("Latest build"),
+        # No reverse relation needed.
+        related_name="+",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+
     # Property used for storing the latest build for a project when prefetching
     LATEST_BUILD_CACHE = "_latest_build"
 
