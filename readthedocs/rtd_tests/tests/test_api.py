47828fbf1f3b0a5137eb65f465364ea0f9270ab5
Manuel Kaufmann <humitos@gmail.com>
2025-09-11 13:49:24 +0200

    Update test again

--- readthedocs/rtd_tests/tests/test_api.py
+++ readthedocs/rtd_tests/tests/test_api.py
@@ -821,7 +821,7 @@ class APITests(TestCase):
         project.save()
         build_api_key_obj, build_api_key = BuildAPIKey.objects.create_key(project)
         expected = (build_api_key_obj.expiry_date - timezone.now()).seconds
-        self.assertAlmostEqual(expected, 9000, delta=5)
+        self.assertAlmostEqual(expected, 86400, delta=5)
 
     def test_user_doesnt_get_full_api_return(self):
         user_normal = get(User, is_staff=False)
