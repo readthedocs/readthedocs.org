readthedocs/readthedocs.org

commit b204787c53addf7ca120cc7bb46e2697285a110e

Improve SSO querysets (#12447)
We were iterating over several objects like teams and user owners in
order to get projects, and also checking if the organization attached to
that object didn't have SSO with VCS enabled. So instead of doing that
I'm just doing one query, which ignores all objects from organizations
that have SSO with VCS enabled.

Also, instead of calling _get_projects_for_sso_user twice, I adapted
that method so it can fetch projects from where the user is admin or
member in just one call (similar to what the other methods do).

All these changes reduce the number of queries used in the dashboard on
.com

readthedocs/core/permissions.py
@@ -45,44 +45,26 @@ class AdminPermissionBase:
             # when we aren't using organizations.
             return user.projects.all()
 
-        # Internal cache to avoid hitting the database/cache multiple times.
-        _organizations_with_allauth_sso = {}
-
-        def _has_sso_with_allauth_enabled(org):
-            if org.pk not in _organizations_with_allauth_sso:
-                _organizations_with_allauth_sso[org.pk] = cls.has_sso_enabled(
-                    org,
-                    SSOIntegration.PROVIDER_ALLAUTH,
-                )
-            return _organizations_with_allauth_sso[org.pk]
-
-        # Projects from teams
         if admin or member:
+            # Projects from VCS SSO.
+            projects |= cls._get_projects_for_sso_user(user, admin=admin, member=member)
+
+            # Projects from teams that don't have VCS SSO enabled.
             filter = Q()
             if admin:
-                filter |= Q(access=ADMIN_ACCESS)
+                filter |= Q(teams__access=ADMIN_ACCESS)
             if member:
-                filter |= Q(access=READ_ONLY_ACCESS)
-
-            teams = user.teams.filter(filter).select_related(
-                "organization", "organization__ssointegration"
+                filter |= Q(teams__access=READ_ONLY_ACCESS)
+            projects |= Project.objects.filter(filter, teams__members=user).exclude(
+                organizations__ssointegration__provider=SSOIntegration.PROVIDER_ALLAUTH,
             )
-            for team in teams:
-                if not _has_sso_with_allauth_enabled(team.organization):
-                    projects |= team.projects.all()
 
+        # Projects from organizations that don't have VCS SSO enabled,
+        # where the user is an owner.
         if admin:
-            # Org Admin
-            for org in user.owner_organizations.all():
-                if not _has_sso_with_allauth_enabled(org):
-                    # Do not grant admin access on projects for owners if the
-                    # organization has SSO enabled with Authorization on the provider.
-                    projects |= org.projects.all()
-
-            projects |= cls._get_projects_for_sso_user(user, admin=True)
-
-        if member:
-            projects |= cls._get_projects_for_sso_user(user, admin=False)
+            projects |= Project.objects.filter(organizations__owners=user).exclude(
+                organizations__ssointegration__provider=SSOIntegration.PROVIDER_ALLAUTH,
+            )
 
         return projects
 
@@ -107,7 +89,7 @@ class AdminPermissionBase:
         return False
 
     @classmethod
-    def _get_projects_for_sso_user(cls, user, admin=False):
+    def _get_projects_for_sso_user(cls, user, admin=False, member=False):
         from readthedocs.projects.models import Project
 
         return Project.objects.none()

commit 202dfc5e0ea7dbdeb9ce769dd9097ffbc13a5823

Fix SSO queries performance regression (#12509)
This was introduced in
https://github.com/readthedocs/readthedocs.org/pull/12447/. Even locally
the previous query is slow, after trying a couple of things I was able
to fix it. This is the comparison running explain in production:

Current code (rolled back)

```
Out[24]: 'Sort  (cost=7188.29..7209.50 rows=8486 width=715)\n  Sort Key: projects_project.slug\n  ->  Hash Left Join  (cost=3517.65..3936.04 rows=8486 width=715)\n        Hash Cond: (organizations_organization.id = sso_ssointegration.organi
zation_id)\n        Filter: ((organizations_team_projects.team_id = 2457) OR (organizations_team_projects.team_id = 6031) OR (organizations_team_projects.team_id = 2302) OR (organizations_team_projects.team_id = 5976) OR (organizations_team
_projects.team_id = 6032) OR (organizations_organization_projects.organization_id = 5511) OR (organizations_organization_projects.organization_id = 11709) OR (organizations_organization_projects.organization_id = 1236) OR (organizations_org
anization_projects.organization_id = 2991) OR (organizations_organization_projects.organization_id = 8804) OR (organizations_organization_projects.organization_id = 12806) OR (organizations_organization_projects.organization_id = 13165) OR 
((sso_ssointegration.id IS NOT NULL) AND (hashed SubPlan 1)) OR ((sso_ssointegration.id IS NOT NULL) AND (hashed SubPlan 2)))\n        ->  Hash Left Join  (cost=1639.26..2028.00 rows=11278 width=727)\n              Hash Cond: (organizations
_organization_projects.organization_id = organizations_organization.id)\n              ->  Hash Left Join  (cost=1033.89..1393.01 rows=11278 width=723)\n                    Hash Cond: (projects_project.id = organizations_organization_projec
ts.project_id)\n                    ->  Hash Right Join  (cost=790.75..995.15 rows=11278 width=719)\n                          Hash Cond: (organizations_team_projects.project_id = projects_project.id)\n                          ->  Seq Scan

readthedocs/core/permissions.py
@@ -45,9 +45,13 @@ class AdminPermissionBase:
             # when we aren't using organizations.
             return user.projects.all()
 
+        projects_from_sso = Project.objects.none()
+        projects_from_teams = Project.objects.none()
+        projects_from_owners = Project.objects.none()
+
         if admin or member:
             # Projects from VCS SSO.
-            projects |= cls._get_projects_for_sso_user(user, admin=admin, member=member)
+            projects_from_sso = cls._get_projects_for_sso_user(user, admin=admin, member=member)
 
             # Projects from teams that don't have VCS SSO enabled.
             filter = Q()
@@ -55,18 +59,25 @@ class AdminPermissionBase:
                 filter |= Q(teams__access=ADMIN_ACCESS)
             if member:
                 filter |= Q(teams__access=READ_ONLY_ACCESS)
-            projects |= Project.objects.filter(filter, teams__members=user).exclude(
+            projects_from_teams = Project.objects.filter(filter, teams__members=user).exclude(
                 organizations__ssointegration__provider=SSOIntegration.PROVIDER_ALLAUTH,
             )
 
         # Projects from organizations that don't have VCS SSO enabled,
         # where the user is an owner.
         if admin:
-            projects |= Project.objects.filter(organizations__owners=user).exclude(
+            projects_from_owners = Project.objects.filter(organizations__owners=user).exclude(
                 organizations__ssointegration__provider=SSOIntegration.PROVIDER_ALLAUTH,
             )
 
-        return projects
+        # NOTE: We use a filter with Q objects instead of several union operations
+        # (e.g projects_from_sso | projects_from_teams | projects_from_owners),
+        # as the latter can generate a very complex and slow query.
+        return Project.objects.filter(
+            Q(id__in=projects_from_sso)
+            | Q(id__in=projects_from_teams)
+            | Q(id__in=projects_from_owners)
+        )
 
     @classmethod
     def organizations(cls, user, owner=False, member=False):