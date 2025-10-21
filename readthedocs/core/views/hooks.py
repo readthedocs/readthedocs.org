27a8fe80c51b97e35cb4718c15e35ecd63b1886e
Santos Gallegos <stsewd@proton.me>
2025-10-21 16:36:24 -0500

    Repo sync: use project queue for syncing

--- readthedocs/core/views/hooks.py
+++ readthedocs/core/views/hooks.py
@@ -118,9 +118,15 @@ def trigger_sync_versions(project):
             project_slug=version.project.slug,
             version_slug=version.slug,
         )
+
+        options = {}
+        if project.build_queue:
+            options["queue"] = project.build_queue
+
         sync_repository_task.apply_async(
             args=[version.pk],
             kwargs={"build_api_key": build_api_key},
+            **options,
         )
         return version.slug
     except Exception:
