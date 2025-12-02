26a0469110b35edbf32453ec85107d8b05afd6df
Manuel Kaufmann <humitos@gmail.com>
2025-12-02 10:36:20 +0100

    Reduce N+ failed builds to 25 from 50

--- readthedocs/settings/base.py
+++ readthedocs/settings/base.py
@@ -176,7 +176,7 @@ class CommunityBaseSettings(Settings):
     RTD_MAX_CONCURRENT_BUILDS = 4
     RTD_BUILDS_MAX_RETRIES = 25
     RTD_BUILDS_RETRY_DELAY = 5 * 60  # seconds
-    RTD_BUILDS_MAX_CONSECUTIVE_FAILURES = 50  # The project is disabled when hitting this limit on the default version
+    RTD_BUILDS_MAX_CONSECUTIVE_FAILURES = 25  # The project is disabled when hitting this limit on the default version
     RTD_BUILD_STATUS_API_NAME = "docs/readthedocs"
     RTD_ANALYTICS_DEFAULT_RETENTION_DAYS = 30 * 3
     RTD_AUDITLOGS_DEFAULT_RETENTION_DAYS = 30 * 3
