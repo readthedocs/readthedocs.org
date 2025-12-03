bceb15d87f518ce9e7e861bf57e64c1ffb7f9482
Manuel Kaufmann <humitos@gmail.com>
2025-12-03 12:43:31 +0100

    Remove old `get_storage_class`

--- readthedocs/storage/__init__.py
+++ readthedocs/storage/__init__.py
@@ -6,31 +6,8 @@ Some storage backends (notably S3) have a slow instantiation time
 so doing those upfront improves performance.
 """
 
-import warnings
-
 from django.core.files.storage import storages
 from django.utils.functional import LazyObject
-from django.utils.module_loading import import_string
-
-
-def get_storage_class(import_path=None):
-    """
-    Get a storage class from an import path.
-
-    .. deprecated::
-        This function is deprecated. Use Django's ``storages`` API instead:
-        ``from django.core.files.storage import storages``
-        and access storage by alias: ``storages["build-media"]``
-    """
-    warnings.warn(
-        "get_storage_class is deprecated. Use Django's storages API instead: "
-        "from django.core.files.storage import storages; storages['alias']",
-        DeprecationWarning,
-        stacklevel=2,
-    )
-    from django.conf import settings as django_settings
-
-    return import_string(import_path or django_settings.DEFAULT_FILE_STORAGE)
 
 
 class ConfiguredBuildMediaStorage(LazyObject):
