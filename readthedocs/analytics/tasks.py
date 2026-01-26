be689c2586df623e6b3692a029ef8b0da003aa07
Santos Gallegos <stsewd@proton.me>
2026-01-26 12:56:40 -0500

    Tasks: improve deletion of old page views

--- readthedocs/analytics/tasks.py
+++ readthedocs/analytics/tasks.py
@@ -1,8 +1,8 @@
 """Tasks for Read the Docs' analytics."""
 
 from django.conf import settings
-from django.db import connection
 from django.utils import timezone
+from readthedocs.analytics.models import PageView
 
 from readthedocs.worker import app
 
@@ -17,15 +17,9 @@ def delete_old_page_counts():
     retention_days = settings.RTD_ANALYTICS_DEFAULT_RETENTION_DAYS
     days_ago = timezone.now().date() - timezone.timedelta(days=retention_days)
 
-    # NOTE: we are using raw SQL here to avoid Django doing a SELECT first to
-    # send `pre_` and `post_` delete signals
-    # See https://docs.djangoproject.com/en/4.2/ref/models/querysets/#delete
-    with connection.cursor() as cursor:
-        cursor.execute(
-            # "SELECT COUNT(*) FROM analytics_pageview WHERE date BETWEEN %s AND %s",
-            "DELETE FROM analytics_pageview WHERE date BETWEEN %s AND %s",
-            [
-                days_ago - timezone.timedelta(days=90),
-                days_ago,
-            ],
-        )
+    # NOTE: We use _raw_delete to avoid Django fetching all objects
+    # before the deletion. `pre_delete` and `post_delete` signals
+    # won't be sent, this is fine as we don't have any special logic
+    # for the PageView model.
+    qs = PageView.objects.filter(date__lt=days_ago)
+    qs._raw_delete(qs.db)
