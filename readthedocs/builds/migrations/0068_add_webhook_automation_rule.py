f0d2588703ed9768a9e0f65c083177d7a987f36b
Manuel Kaufmann <humitos@gmail.com>
2026-02-10 16:28:50 +0100

    Missing replacement

--- readthedocs/builds/migrations/0068_add_webhook_automation_rule.py
+++ readthedocs/builds/migrations/0068_add_webhook_automation_rule.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name="PushAutomationRule",
+            name="WebhookAutomationRule",
             fields=[],
             options={
                 "proxy": True,
