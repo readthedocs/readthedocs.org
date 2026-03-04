a7e492db979e9426ba1085d4f1c633e775f40f40
Santos Gallegos <stsewd@proton.me>
2026-03-04 18:24:57 -0500

    Mark as safe

--- readthedocs/oauth/migrations/0019_githubapp_add_extra_fields.py
+++ readthedocs/oauth/migrations/0019_githubapp_add_extra_fields.py
@@ -2,9 +2,11 @@
 
 from django.db import migrations
 from django.db import models
+from django_safemigrate import Safe
 
 
 class Migration(migrations.Migration):
+    safe = Safe.before_deploy()
     dependencies = [
         ("oauth", "0018_githubapp"),
     ]
