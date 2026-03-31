8efbd0224d73a7c6b773562da401d67eb9da5e83
Anthony Johnson <aj@ohess.org>
2026-03-31 13:00:25 -0700

    Release 2026.03.31

--- docs/conf.py
+++ docs/conf.py
@@ -81,7 +81,7 @@ html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "/")
 
 master_doc = "index"
 copyright = "Read the Docs, Inc & contributors"
-version = "2026.3.24"
+version = "2026.3.31"
 release = version
 exclude_patterns = ["_build", "shared", "_includes"]
 # Exclude design docs from dev documentation
