b0270f02dea7b10794afad6193d094389aa0d949
Santos Gallegos <stsewd@proton.me>
2026-08-13 19:16:18 -0500

    We need this

--- readthedocs/settings/docker_compose.py
+++ readthedocs/settings/docker_compose.py
@@ -280,6 +280,8 @@ class DockerBaseSettings(CommunityBaseSettings):
                 "BACKEND": "readthedocs.storage.s3_storage.RTDS3Storage",
                 "OPTIONS": {
                     "bucket_name": os.environ.get("RTD_S3_BUILD_UPLOADS_STORAGE_BUCKET", "build-uploads"),
+                    # Explicit default ACL required to generate presigned URLs.
+                    "default_acl": "private",
                 },
             },
         }
