85b5e047b8dd17b9f139c094c73ceaaa6a16045f
Anthony Johnson <aj@ohess.org>
2026-09-11 19:21:58 -0700

    Bring back control tests
    This reverts commit 5e90b4738c1a8348d75f7b2195390eaf9afcb7b0.

--- readthedocs/proxito/tests/test_old_redirects.py
+++ readthedocs/proxito/tests/test_old_redirects.py
@@ -1049,6 +1049,46 @@ class UserRedirectTests(MockStorageMixin, BaseDocServing):
             )
         )
 
+    def test_exact_redirect_with_wildcard_on_superproject(self):
+        # This is just a control, it can be removed with bug resolved
+        fixture.get(
+            Redirect,
+            project=self.project,
+            redirect_type=EXACT_REDIRECT,
+            from_url="/en/2.2/*",
+            to_url="/en/2.0/:splat",
+            force=True,
+        )
+        r = self.client.get(
+            "/en/2.2/", headers={"host": "project.dev.readthedocs.io"}
+        )
+        self.assertEqual(r.status_code, 302)
+        self.assertTrue(
+            r["Location"].startswith("http://project.dev.readthedocs.io/en/2.0")
+        )
+
+    def test_exact_redirect_with_wildcard_on_subproject_with_projects_prefix(self):
+        # This is just a control, it can be removed with bug resolved. Users
+        # shouldn't have to prefix the full path in a subproject.
+        fixture.get(
+            Redirect,
+            project=self.subproject,
+            redirect_type=EXACT_REDIRECT,
+            from_url="/projects/subproject/en/2.2/*",
+            to_url="/projects/subproject/en/2.0/:splat",
+            force=True,
+        )
+        r = self.client.get(
+            "/projects/subproject/en/2.2/",
+            headers={"host": "project.dev.readthedocs.io"},
+        )
+        self.assertEqual(r.status_code, 302)
+        self.assertTrue(
+            r["Location"].startswith(
+                "http://project.dev.readthedocs.io/projects/subproject/en/2.0"
+            )
+        )
+
 
 @override_settings(PUBLIC_DOMAIN="dev.readthedocs.io")
 class UserForcedRedirectTests(BaseDocServing):
