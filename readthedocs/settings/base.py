e0ededc88be95d1c1960420eadbb347ee897c9f1
Manuel Kaufmann <humitos@gmail.com>
2026-09-14 09:55:23 +0200

    Celery: start workers without mingle, gossip and heartbeat (#13296)
    Context:
    https://readthedocs.slack.com/archives/G1RJLT1V3/p1788893733153889

--- readthedocs/settings/base.py
+++ readthedocs/settings/base.py
@@ -677,6 +677,9 @@ class CommunityBaseSettings(Settings):
     # Don't queue a bunch of tasks in the workers
     CELERY_WORKER_PREFETCH_MULTIPLIER = 1
     CELERY_TASK_CREATE_MISSING_QUEUES = True
+    # On broker connection loss, kombu re-queues the unacked task this worker
+    # is running. Cancel it here so it doesn't also keep running (Celery 6 default).
+    CELERY_WORKER_CANCEL_LONG_RUNNING_TASKS_ON_CONNECTION_LOSS = True
 
     # https://github.com/readthedocs/readthedocs.org/issues/12317#issuecomment-3070950434
     # https://docs.celeryq.dev/en/stable/getting-started/backends-and-brokers/redis.html#visibility-timeout
